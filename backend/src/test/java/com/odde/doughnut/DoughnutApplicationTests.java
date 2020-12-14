1eda1e731021ef366fd07378c9064aec19a9dc29
Nikola Vasiljevski <nikola83@gmail.com>
2020-12-14 16:05:18 +0800

    Initial doughnut project setup

---
+++ backend/src/test/java/com/odde/doughnut/DoughnutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odde.doughnut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoughnutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
