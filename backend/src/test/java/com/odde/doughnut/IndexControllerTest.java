b80468f7c59b05b070f0698b5e4c6d088df3cc65
Stanly Lau <stanly@odd-e.com>
2020-12-17 11:37:01 +0800

    Added controller test to test through the stack

---
+++ backend/src/test/java/com/odde/doughnut/IndexControllerTest.java
@@ -0,0 +1,31 @@
+package com.odde.doughnut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+class IndexControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void serving_notes() throws Exception {
+        this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("Serving 0 notes since the beginning of the universe.")));
+    }
+
+}
