1cc570ddab6d10fcf9842535b25fc44dfbf512af
Terry Yin <terry.yinzhe@gmail.com>
2020-12-17 16:32:50 +0800

    parameter

--- backend/src/test/java/com/odde/doughnut/ApplicationContextWithRepositories.java
+++ backend/src/test/java/com/odde/doughnut/ApplicationContextWithRepositories.java
@@ -1,7 +1,6 @@
 package com.odde.doughnut;
 
-import org.junit.jupiter.api.extension.BeforeAllCallback;
-import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.*;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,10 +10,11 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import java.util.function.Consumer;
 
-public class WithRepositories implements BeforeAllCallback {
+public class ApplicationContextWithRepositories implements BeforeAllCallback, ParameterResolver {
+    private ApplicationContext applicationContext;
+
     public void executeDB(Consumer<EntityManager> consumer) {
-        String path = "/src/test/resources/repository.xml";
-        ApplicationContext context = new FileSystemXmlApplicationContext(path);
+        ApplicationContext context = getApplicationContext();
 
         EntityManagerFactory emf = context.getBean("emf", EntityManagerFactory.class);
         EntityManager manager = emf.createEntityManager();
@@ -38,5 +38,24 @@ public class WithRepositories implements BeforeAllCallback {
     @Override
     public void beforeAll(ExtensionContext context) throws Exception {
         clearTable("note");
+    }
+
+    @Override
+    public boolean supportsParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
+        return parameterContext.getParameter().getType().equals(ApplicationContext.class);
+    }
+
+    @Override
+    public Object resolveParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
+        ApplicationContext context = getApplicationContext();
+        return context;
+    }
+
+    private ApplicationContext getApplicationContext() {
+        String path = "/src/test/resources/repository.xml";
+        if (applicationContext == null) {
+            this.applicationContext = new FileSystemXmlApplicationContext(path);
+        }
+        return applicationContext;
     }
 }
--- backend/src/test/java/com/odde/doughnut/DoughnutWithNoSpringTests.java
+++ backend/src/test/java/com/odde/doughnut/DoughnutWithNoSpringTests.java
@@ -5,19 +5,21 @@ import com.odde.doughnut.repositories.NoteRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.FileSystemXmlApplicationContext;
 import org.springframework.ui.Model;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 
-@ExtendWith(WithRepositories.class)
+@ExtendWith(ApplicationContextWithRepositories.class)
 class DoughnutWithNoSpringTests {
+	ApplicationContext context;
+
+    public DoughnutWithNoSpringTests(ApplicationContext context){
+		this.context = context;
+	}
 
 	@Test
 	void contextLoads() {
-		String path = "/src/test/resources/repository.xml";
-		ApplicationContext context = new FileSystemXmlApplicationContext(path);
 		NoteRepository noteRepository = context.getBean("noteRepository", NoteRepository.class);
 
 		IndexController controller = new IndexController(noteRepository);
