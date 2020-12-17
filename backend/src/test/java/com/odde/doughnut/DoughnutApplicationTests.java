f2a665eddcf5e16c7ef417e8af88f186a9cb0ff7
Stanly Lau <stanly@odd-e.com>
2020-12-17 22:04:38 +0800

    Use @ActiveProfiles(profiles = 'test') in spring related tests so that we don't have to set vm options explicity when run

--- backend/src/test/java/com/odde/doughnut/DoughnutApplicationTests.java
+++ backend/src/test/java/com/odde/doughnut/DoughnutApplicationTests.java
@@ -3,11 +3,13 @@ package com.odde.doughnut;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.ui.Model;
 
 import static org.mockito.Mockito.mock;
 
 @SpringBootTest
+@ActiveProfiles(profiles = "test")
 class DoughnutApplicationTests {
 	@Autowired
     IndexController controller;
--- backend/src/test/java/com/odde/doughnut/IndexControllerTest.java
+++ backend/src/test/java/com/odde/doughnut/IndexControllerTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.containsString;
@@ -17,6 +18,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @SpringBootTest
 @ExtendWith(DBCleaner.class)
+@ActiveProfiles(profiles = "test")
 class IndexControllerTest {
 
     @Autowired
