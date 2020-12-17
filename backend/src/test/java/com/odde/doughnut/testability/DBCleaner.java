a5b8187aa53e45e5f35a3cdab55644a7c3fd62f8
Terry Yin <terry.yinzhe@gmail.com>
2020-12-17 18:26:16 +0800

    truncate all tables

--- backend/src/test/java/com/odde/doughnut/testability/DBCleaner.java
+++ backend/src/test/java/com/odde/doughnut/testability/DBCleaner.java
@@ -1,5 +1,6 @@
 package com.odde.doughnut.testability;
 
+import org.hibernate.Metamodel;
 import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.springframework.context.ApplicationContext;
@@ -8,13 +9,31 @@ import org.springframework.transaction.annotation.Transactional;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
+import javax.persistence.metamodel.EntityType;
+import java.util.List;
+import java.util.Set;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 public class DBCleaner implements BeforeEachCallback {
-    public void executeDB(Consumer<EntityManager> consumer) {
+    @Override
+    public void beforeEach(ExtensionContext context) {
+        truncateAllTables();
+    }
+
+    @Transactional
+    private void truncateAllTables() {
+        withEntityManager(entityManager -> getTableNames(entityManager).forEach(t->truncateTable(t, entityManager)));
+    }
+
+    private EntityManager createEntityManager() {
         ApplicationContext context = ApplicationContextForRepositoriesHolder.getApplicationContext();
         EntityManagerFactory emf = context.getBean("emf", EntityManagerFactory.class);
-        EntityManager manager = emf.createEntityManager();
+        return emf.createEntityManager();
+    }
+
+    private void withEntityManager(Consumer<EntityManager> consumer) {
+        EntityManager manager = createEntityManager();
         EntityTransaction transaction = manager.getTransaction();
         transaction.begin();
         consumer.accept(manager);
@@ -22,18 +41,18 @@ public class DBCleaner implements BeforeEachCallback {
         manager.close();
     }
 
-    @Transactional
-    public void clearTable(String tableName) {
-        executeDB(entityManager -> {
-            entityManager.createNativeQuery("SET FOREIGN_KEY_CHECKS = 0").executeUpdate();
-            entityManager.createNativeQuery("TRUNCATE " + tableName).executeUpdate();
-            entityManager.createNativeQuery("ALTER TABLE " + tableName + " AUTO_INCREMENT = 1").executeUpdate();
-            entityManager.createNativeQuery("SET FOREIGN_KEY_CHECKS = 1").executeUpdate();
-        });
+    private void truncateTable(String tableName, EntityManager entityManager) {
+        entityManager.createNativeQuery("SET FOREIGN_KEY_CHECKS = 0").executeUpdate();
+        entityManager.createNativeQuery("TRUNCATE " + tableName).executeUpdate();
+        entityManager.createNativeQuery("ALTER TABLE " + tableName + " AUTO_INCREMENT = 1").executeUpdate();
+        entityManager.createNativeQuery("SET FOREIGN_KEY_CHECKS = 1").executeUpdate();
     }
 
-    @Override
-    public void beforeEach(ExtensionContext context) throws Exception {
-        clearTable("note");
+    private List<String> getTableNames(EntityManager manager) {
+        Metamodel metamodel = (Metamodel) manager.getMetamodel();
+        Set<EntityType<?>> entities = metamodel.getEntities();
+
+        return entities.stream().map(EntityType::getName).collect(Collectors.toList());
     }
+
 }
