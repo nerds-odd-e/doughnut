d6e83cd12bddb08c7d440f16902adac762cbe3f7
Yeong Sheng, Tan <tanyeongsheng@gmail.com>
2020-12-17 17:46:35 +0800

    Pass the test for gradle build for deployment

--- backend/src/test/java/com/odde/doughnut/DoughnutWithNoSpringTests.java
+++ backend/src/test/java/com/odde/doughnut/DoughnutWithNoSpringTests.java
@@ -1,5 +1,8 @@
 package com.odde.doughnut;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+
 import com.odde.doughnut.repositories.NoteRepository;
 import com.odde.doughnut.testability.DBCleaner;
 import org.junit.jupiter.api.Test;
@@ -9,24 +12,19 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.ui.Model;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mock;
-
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(locations = {"classpath:repository.xml"})
 @ExtendWith(DBCleaner.class)
 class DoughnutWithNoSpringTests {
 
-    @Autowired
-    private NoteRepository noteRepository;
+  @Autowired private NoteRepository noteRepository;
 
-    @Test
-    void contextLoads() {
-        IndexController controller = new IndexController(noteRepository);
-        Model model = mock(Model.class);
+  @Test
+  void contextLoads() {
+    IndexController controller = new IndexController(noteRepository);
+    Model model = mock(Model.class);
 
-        String home = controller.home(null, model);
-        assertEquals("login", home);
-    }
-
+    String home = controller.home(null, model);
+    assertEquals("login", home);
+  }
 }
