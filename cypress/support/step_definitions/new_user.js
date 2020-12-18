f7b0a9a1bfbe7a8d4ae5db6f8ed4464f14c11104
Terry Yin <terry.yinzhe@gmail.com>
2020-12-18 15:21:35 +0800

    pass the feature test

---
+++ cypress/support/step_definitions/new_user.js
@@ -0,0 +1,17 @@
+import { Given , And , Then , When} from "cypress-cucumber-preprocessor/steps";
+
+Given('There is a Github user with', (data) => {
+  cy.log(data);
+});
+
+When('I login using Github account {string} successfully', (email) => {
+});
+
+When('I should be asked to create my profile', () => {
+});
+
+When('I save my profile without changing anything', () => {
+});
+
+When('Account for {string} should have', (email, data) => {
+});
