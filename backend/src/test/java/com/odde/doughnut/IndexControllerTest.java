39bf41c3a4500f1c8d37f5f0ed94e92bf5c62f4e
Stanly Lau <stanly@odd-e.com>
2020-12-18 14:48:43 +0800

    Defaulted to run test profile.

--- backend/src/test/java/com/odde/doughnut/IndexControllerTest.java
+++ backend/src/test/java/com/odde/doughnut/IndexControllerTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.containsString;
@@ -20,7 +19,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @SpringBootTest
 @ExtendWith(DBCleaner.class)
-@ActiveProfiles(profiles = "test")
 class IndexControllerTest {
 
     @Autowired
