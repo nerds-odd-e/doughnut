d9483d8ca478924fdf0a82fbc576259da59d0c3a
Yeong Sheng, Tan <tanyeongsheng@gmail.com>
2020-12-23 12:27:57 +0800

    gcp instance setup with mariadb and gnupg

--- backend/instance-startup.sh
+++ backend/instance-startup.sh
@@ -13,7 +13,7 @@ gsutil cp gs://${BUCKET}/${ARTIFACT}-${VERSION}.jar .
 
 # Install dependencies
 apt-get update
-apt-get -y --force-yes install openjdk-11-jdk mariadb
+apt-get -y --force-yes install openjdk-11-jdk mariadb-server mariadb-client mariadb-backup gnupg
 
 # Make Java 8 default
 update-alternatives --set java /usr/lib/jvm/java-11-openjdk-amd64/jre/bin/java
