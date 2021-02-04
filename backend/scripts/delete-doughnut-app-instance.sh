094346deb056ef9f25965e114b7ce8d7795a7eb9
Yeong Sheng, Tan <tanyeongsheng@gmail.com>
2021-02-04 12:48:01 +0800

    gcloud scripts to delete doughnut app-instance & view app-instance logs

---
+++ backend/scripts/delete-doughnut-app-instance.sh
@@ -0,0 +1,1 @@
+gcloud compute instances delete doughnut-app-instance --quiet --zone us-east1-b
---
+++ backend/scripts/view-doughnut-app-instance-logs.sh
@@ -0,0 +1,1 @@
+gcloud compute instances get-serial-port-output doughnut-app-instance --zone us-east1-b
