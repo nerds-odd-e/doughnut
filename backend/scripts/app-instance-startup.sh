4359ccc9356b765e2282ff41af0ce2b29258228e
Yeong Sheng, Tan <tanyeongsheng@gmail.com>
2021-02-04 12:00:31 +0800

    fix build & prod time gradle profiles issues

--- backend/scripts/app-instance-startup.sh
+++ backend/scripts/app-instance-startup.sh
@@ -79,4 +79,4 @@ MYSQL_PASSWORD=$(curl "https://secretmanager.googleapis.com/v1/projects/${PROJEC
 echo "10.142.0.18	db-server" >> /etc/hosts
 
 # Start server
-sh -c "java -jar -Dspring-boot.run.profiles=prod -Dspring.datasource.password=${MYSQL_PASSWORD} /opt/doughnut_app/${ARTIFACT}-${VERSION}.jar" &
+sh -c "java -jar -Dspring-boot.run.profiles=prod -Dspring.datasource.url='jdbc:mysql://db-server:3306/doughnut' -Dspring.datasource.password=${MYSQL_PASSWORD} /opt/doughnut_app/${ARTIFACT}-${VERSION}.jar" &
