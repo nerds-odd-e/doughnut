53f86c30908bd75afe58ff563b3b21ef39302ab6
Yeong Sheng, Tan <tanyeongsheng@gmail.com>
2021-02-05 12:09:31 +0800

    explicit table to model annotation; id column as unsigned int

--- backend/src/main/java/com/odde/doughnut/models/Note.java
+++ backend/src/main/java/com/odde/doughnut/models/Note.java
@@ -1,17 +1,17 @@
 package com.odde.doughnut.models;
 
-import lombok.Setter;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Setter;
 
 @Entity
+@Table(name = "note")
 public class Note {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    @Setter
-    private String title;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  @Setter private String title;
 }
--- backend/src/main/resources/db/migration/V1__create_note_table.sql
+++ backend/src/main/resources/db/migration/V1__create_note_table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE note (
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     title varchar(100) not null,
     PRIMARY KEY (id)
 );
