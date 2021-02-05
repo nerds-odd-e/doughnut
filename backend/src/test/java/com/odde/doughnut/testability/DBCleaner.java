0bf89e91ab9e4e3a03e1aa09061e4cdebbb20091
Yeong Sheng, Tan <tanyeongsheng@gmail.com>
2021-02-05 15:37:30 +0800

    revert DBCleaner

--- backend/src/test/java/com/odde/doughnut/testability/DBCleaner.java
+++ backend/src/test/java/com/odde/doughnut/testability/DBCleaner.java
@@ -45,13 +45,13 @@ public class DBCleaner implements BeforeEachCallback {
   }
 
   private void truncateTable(String tableName, EntityManager entityManager) {
-    // entityManager.createNativeQuery("SET FOREIGN_KEY_CHECKS =
-    // 0").executeUpdate();
+    entityManager.createNativeQuery("SET FOREIGN_KEY_CHECKS =
+    0").executeUpdate();
     entityManager.createNativeQuery("TRUNCATE " + tableName).executeUpdate();
-    // entityManager.createNativeQuery("ALTER TABLE " + tableName + "
-    // AUTO_INCREMENT = 1").executeUpdate();
-    // entityManager.createNativeQuery("SET FOREIGN_KEY_CHECKS =
-    // 1").executeUpdate();
+    entityManager.createNativeQuery("ALTER TABLE " + tableName + "
+    AUTO_INCREMENT = 1").executeUpdate();
+    entityManager.createNativeQuery("SET FOREIGN_KEY_CHECKS =
+    1").executeUpdate();
   }
 
   private List<String> getTableNames(EntityManager manager) {
