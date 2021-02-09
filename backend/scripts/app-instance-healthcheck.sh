c81821a80d4b31d3106a70d0ce834bec8348afb2
Yeong Sheng, Tan <tanyeongsheng@gmail.com>
2021-02-09 16:52:36 +0800

    test HEALTHCHECK_STATUS in while loop

--- backend/scripts/app-instance-healthcheck.sh
+++ backend/scripts/app-instance-healthcheck.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 HEALTHCHECK_STATUS=""
-RETRY=15
+RETRY=30
 while [ ${RETRY} -gt 0 ] && [ "${HEALTHCHECK_STATUS}" != "OK" ]
 do
-  sleep 20
+  sleep 10
+  echo "RETRY (${RETRY})"
   HEALTHCHECK_STATUS=$(curl -s https://dough.odd-e.com/api/healthcheck 2>&1)
-  echo "RETRY (${RETRY}): ${HEALTHCHECK_STATUS}"
+  if [ "${HEALTHCHECK_STATUS}" == "OK" ]; then
+    echo "doughnut-app responded ${HEALTHCHECK_STATUS}!"
+    exit 0
+  fi
   RETRY=$((RETRY-1))
 done
 
 if [ "${HEALTHCHECK_STATUS}" != "OK" ]; then
   echo "doughnut-app NOT RESPONDING!"
   exit 1
-else 
-  echo "doughnut-app responded ${HEALTHCHECK_STATUS}"
-  exit 0
 fi
