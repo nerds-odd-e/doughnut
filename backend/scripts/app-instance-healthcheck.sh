2305ee5a2991c011b50bcf0c234e4e0820dd941d
Yeong Sheng, Tan <tanyeongsheng@gmail.com>
2021-02-09 15:59:28 +0800

    decrement RETRY

--- backend/scripts/app-instance-healthcheck.sh
+++ backend/scripts/app-instance-healthcheck.sh
@@ -3,17 +3,17 @@ set -e
 
 HEALTHCHECK_STATUS=""
 RETRY=5
-while [ "${RETRY}" -gt 0 ] && [ "${HEALTHCHECK_STATUS}" != "OK" ]
+while [ ${RETRY} -gt 0 ] && [ "${HEALTHCHECK_STATUS}" != "OK" ]
 do
   sleep 10
   HEALTHCHECK_STATUS=$(curl -s https://dough.odd-e.com/api/healthcheck 2>&1)
   echo "RETRY (${RETRY}): ${HEALTHCHECK_STATUS}"
-  ((--RETRY))
+  let RETRY--
 done
 
-if [ "${HEALTHCHECK_STATUS}" == "" ]; then
+if [ "${HEALTHCHECK_STATUS}" != "OK" ]; then
   echo "doughnut-app NOT RESPONDING!"
-  exit -1
+  exit 1
 else 
   echo "doughnut-app responded ${HEALTHCHECK_STATUS}"
   exit 0
