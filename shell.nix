6cd0ef226ed631461349bcf7c814a1af7bdc0cf3
Yeong Sheng, Tan <tanyeongsheng@gmail.com>
2021-02-10 18:45:03 +0800

    create initial DB user doughnut if not exists

--- shell.nix
+++ shell.nix
@@ -59,9 +59,9 @@ in mkShell {
     export MYSQL_PID=$!
 
 cat <<EOF > $MYSQL_HOME/init_doughnut_db.sql
+CREATE USER IF NOT EXISTS 'doughnut'@'localhost' IDENTIFIED BY 'doughnut';
 CREATE DATABASE IF NOT EXISTS doughnut_development DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 CREATE DATABASE IF NOT EXISTS doughnut_test        DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-SET PASSWORD FOR 'doughnut'@'localhost' = PASSWORD('doughnut');
 GRANT ALL PRIVILEGES ON doughnut_development.* TO 'doughnut'@'localhost';
 GRANT ALL PRIVILEGES ON doughnut_test.*        TO 'doughnut'@'localhost';
 FLUSH PRIVILEGES;
