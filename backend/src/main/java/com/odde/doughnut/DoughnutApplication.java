d631beaa1776c60d2f1b3a991bdb384cd3e3e54b
Terry Yin <terry.yinzhe@gmail.com>
2021-02-13 11:36:35 +0800

    prod vs dev

--- backend/src/main/java/com/odde/doughnut/DoughnutApplication.java
+++ backend/src/main/java/com/odde/doughnut/DoughnutApplication.java
@@ -11,32 +11,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 @SpringBootApplication
-public class DoughnutApplication extends WebSecurityConfigurerAdapter {
+public class DoughnutApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(DoughnutApplication.class, args);
   }
 
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
-    http.csrf()
-        .disable()
-        .authorizeRequests()
-        .antMatchers("/api/healthcheck")
-        .permitAll();
-
-    http.authorizeRequests(
-            a
-            -> a.antMatchers("/", "/login", "/error", "/webjars/**")
-                   .permitAll()
-                   .anyRequest()
-                   .authenticated())
-        .logout(l -> l.logoutSuccessUrl("/").permitAll())
-//            .httpBasic();
-        .exceptionHandling(
-            e
-            -> e.authenticationEntryPoint(
-                new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
-        .oauth2Login();
-  }
 }
+
--- backend/src/main/java/com/odde/doughnut/ProductionConfiguration.java
+++ backend/src/main/java/com/odde/doughnut/ProductionConfiguration.java
@@ -3,19 +3,22 @@ package com.odde.doughnut;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 @Configuration
-@Profile("prod")
+@Profile({"test", "dev"})
 @Order(200)
-public class NonProductConfiguration extends WebSecurityConfigurerAdapter {
+public class ProductionConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
-    public void configure(HttpSecurity http) throws Exception {
+    protected void configure(HttpSecurity http) throws Exception {
+
         http.csrf()
                 .disable()
                 .authorizeRequests()
@@ -29,22 +32,11 @@ public class NonProductConfiguration extends WebSecurityConfigurerAdapter {
                         .anyRequest()
                         .authenticated())
                 .logout(l -> l.logoutSuccessUrl("/").permitAll())
-                .httpBasic();
+                .exceptionHandling(
+                        e
+                                -> e.authenticationEntryPoint(
+                                new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
+                .oauth2Login();
+        //throw(new RuntimeException("here"));
     }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        PasswordEncoder encoder =
-                PasswordEncoderFactories.createDelegatingPasswordEncoder();
-        auth
-                .inMemoryAuthentication()
-                .withUser("user")
-                .password(encoder.encode("password"))
-                .roles("USER")
-                .and()
-                .withUser("admin")
-                .password(encoder.encode("admin"))
-                .roles("USER", "ADMIN");
-    }
-
 }
