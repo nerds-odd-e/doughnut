385b4f9b25020dd687f05ce7a2b4e5b834af2491
Terry Yin <terry.yinzhe@gmail.com>
2021-02-14 13:53:42 +0800

    show profile

--- backend/src/main/java/com/odde/doughnut/HealthCheckController.java
+++ backend/src/main/java/com/odde/doughnut/HealthCheckController.java
@@ -1,16 +1,14 @@
 
 package com.odde.doughnut;
 
-import java.net.URI;
-import java.net.URISyntaxException;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -19,6 +17,6 @@ class HealthCheckController {
   private Environment environment;
   @GetMapping("/healthcheck")
   public String ping() {
-    return "OK. " + environment.getActiveProfiles();
+    return "OK. Active Profile: " + String.join(", ", environment.getActiveProfiles());
   }
 }
