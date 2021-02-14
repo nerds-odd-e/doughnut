c5a2e083407f0069ad557621ff49039e86725195
Yeong Sheng, Tan <tanyeongsheng@gmail.com>
2021-02-14 22:27:55 +0800

    use cy.request for API invoke

--- cypress/support/step_definitions/user.js
+++ cypress/support/step_definitions/user.js
@@ -1,33 +1,38 @@
-import { Given , And , Then , When, Before} from "cypress-cucumber-preprocessor/steps";
+import {
+  Given,
+  And,
+  Then,
+  When,
+  Before,
+} from "cypress-cucumber-preprocessor/steps";
 
 Before({ tags: "@clean_db" }, () => {
-  cy.visit('/api/testability/clean_db');
-  cy.get("body").should('contain', 'OK');
+  cy.request("/api/testability/clean_db").its("body").should("contain", "OK");
 });
 
-When('I identify myself as a new user', () => {
-  cy.visit('/login');
+When("I identify myself as a new user", () => {
+  cy.visit("/login");
 
   cy.get("#username").type("user");
   cy.get("#password").type("password");
-  cy.get('form.form-signin').submit();
-  cy.location('pathname', { timeout: 10000 }).should('eq', '/');
-  cy.get('input[type="submit"][value="Logout"]').should('be.visible');
+  cy.get("form.form-signin").submit();
+  cy.location("pathname", { timeout: 10000 }).should("eq", "/");
+  cy.get('input[type="submit"][value="Logout"]').should("be.visible");
 });
 
-When('I should be asked to create my profile', () => {
-  cy.get("body").should('contain', 'Please create your profile');
+When("I should be asked to create my profile", () => {
+  cy.get("body").should("contain", "Please create your profile");
 });
 
-When('I save my profile with:', (data) => {
-  data.hashes().forEach(elem =>{
-    for(var propName in elem) {
+When("I save my profile with:", (data) => {
+  data.hashes().forEach((elem) => {
+    for (var propName in elem) {
       cy.get("#" + propName).type(elem[propName]);
     }
   });
   cy.get('input[value="Submit"]').click();
 });
 
-Then('I should see {string} in the page', (content) => {
-  cy.get("body").should('contain', content);
+Then("I should see {string} in the page", (content) => {
+  cy.get("body").should("contain", content);
 });
