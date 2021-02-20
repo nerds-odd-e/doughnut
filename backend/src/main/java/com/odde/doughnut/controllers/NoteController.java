b39927a7e9bb91a9f308abd6569b4f52da99d924
Terry Yin <terry.yinzhe@gmail.com>
2021-02-20 11:31:27 +0800

    move controller action

--- backend/src/main/java/com/odde/doughnut/controllers/NoteController.java
+++ backend/src/main/java/com/odde/doughnut/controllers/NoteController.java
@@ -1,60 +1,17 @@
 package com.odde.doughnut.controllers;
 
 import com.odde.doughnut.models.Note;
-import com.odde.doughnut.models.User;
-import com.odde.doughnut.repositories.NoteRepository;
-import com.odde.doughnut.repositories.UserRepository;
-import com.odde.doughnut.services.LinkService;
-import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.view.RedirectView;
 
 import java.security.Principal;
-import java.util.Date;
-import java.util.List;
 
-@RestController
+@Controller
 public class NoteController {
-    private final NoteRepository noteRepository;
-    private final UserRepository userRepository;
-    private final LinkService linkService;
-
-
-    public NoteController(NoteRepository noteRepository, UserRepository userRepository, LinkService linkService) {
-        this.noteRepository = noteRepository;
-        this.userRepository = userRepository;
-        this.linkService = linkService;
-    }
-
     @GetMapping("/note")
     public String notes(Principal principal, Model model) {
         model.addAttribute("note", new Note());
         return "note";
-    }
-
-    @PostMapping("/note")
-    public RedirectView createNote(Principal principal, Note note, Model model) throws Exception {
-        User currentUser = userRepository.findByExternalIdentifier(principal.getName());
-        if (currentUser == null) throw new Exception("User does not exist");
-        note.setUser(currentUser);
-
-        noteRepository.save(note);
-        return new RedirectView("/review");
-    }
-
-    @GetMapping(value="/getNotes", produces=MediaType.APPLICATION_JSON_VALUE)
-    public List<Note> getNotes(Principal principal) throws Exception {
-        User currentUser = userRepository.findByExternalIdentifier(principal.getName());
-        if (currentUser == null) throw new Exception("User does not exist");
-        return currentUser.getNotesInDescendingOrder();
-    }
-
-    @PostMapping(value = "/linkNote", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-    public RedirectView linkNote(Integer sourceNoteId, Integer targetNoteId, Model model) {
-        linkService.linkNote(sourceNoteId, targetNoteId);
-        return new RedirectView("/review");
     }
 }
---
+++ backend/src/test/java/com/odde/doughnut/controllers/NoteRestControllerTests.java
@@ -0,0 +1,130 @@
+package com.odde.doughnut.controllers;
+
+import com.odde.doughnut.models.Note;
+import com.odde.doughnut.models.User;
+import com.odde.doughnut.repositories.NoteRepository;
+import com.odde.doughnut.repositories.UserRepository;
+import com.odde.doughnut.services.LinkService;
+import com.odde.doughnut.testability.DBCleaner;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.ui.Model;
+import org.springframework.web.servlet.view.RedirectView;
+
+import javax.transaction.Transactional;
+import java.nio.file.attribute.UserPrincipal;
+import java.security.Principal;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations = {"classpath:repository.xml"})
+@ExtendWith(DBCleaner.class)
+@Transactional
+public class NoteRestControllerTests {
+
+    @Autowired
+    private NoteRepository noteRepository;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    private LinkService linkService;
+
+    Session session;
+
+    @Test
+    void shouldBeAbleToSaveNoteWhenThereIsValidUser() throws Exception {
+        Model model = mock(Model.class);
+        NoteRepository noteRepository = mock(NoteRepository.class);
+
+        NoteRestController noteController = new NoteRestController(noteRepository, createMockUserRepository(new User()),linkService );
+
+        RedirectView note = noteController.createNote(createLogin(), new Note(), model);
+        assertEquals(note.getUrl(), "/review");
+    }
+
+    @Test
+    void shouldNotBeAbleToSaveNoteWhenThereIsInvalidUser() {
+        Model model = mock(Model.class);
+        NoteRepository noteRepository = mock(NoteRepository.class);
+        NoteRestController noteController = new NoteRestController(noteRepository, createMockUserRepository(null), linkService);
+
+        Note note = new Note();
+
+        try {
+            noteController.createNote(createLogin(), note, model);
+        } catch (Exception e) {
+            Mockito.verify(noteRepository, times(0)).save(note);
+        }
+    }
+
+    @BeforeEach
+    void setupSession() {
+
+        session = sessionFactory.openSession();
+        linkService = mock (LinkService.class);
+    }
+
+    @Test
+    void shouldGetListOfNotes() throws Exception {
+        User user = createUser();
+        Note note = createNote(user);
+        NoteRestController noteController = new NoteRestController(noteRepository, userRepository, linkService);
+        assertEquals(note.getTitle(), noteController.getNotes(createLogin()).get(0).getTitle());
+    }
+
+    @Test
+    void shouldGetListOfNotesWithMock() throws Exception {
+        UserRepository mockUserRepo = mock(UserRepository.class);
+        User user = mock(User.class);
+        when(mockUserRepo.findByExternalIdentifier(any())).thenReturn(user);
+        when(user.getNotesInDescendingOrder()).thenReturn(Arrays.asList(new Note()));
+
+        NoteRestController noteController = new NoteRestController(noteRepository, mockUserRepo, linkService);
+        List<Note> notes = noteController.getNotes(createLogin());
+        verify(user).getNotesInDescendingOrder();
+        assertEquals(1, notes.size());
+    }
+
+    private Note createNote(User user) {
+        Note note = new Note();
+        note.setUser(user);
+        note.setTitle("Sedition");
+        note.setDescription("Incite violence");
+        note.setCreatedDatetime(new Date());
+
+        session.save(note);
+        return note;
+    }
+
+    private User createUser() {
+        User user = new User();
+        user.setExternalIdentifier("1234567");
+        user.setName("my name");
+        session.save(user);
+        return user;
+    }
+
+    private UserRepository createMockUserRepository(User user) {
+        UserRepository userRepository = mock(UserRepository.class);
+        when(userRepository.findByExternalIdentifier("1234567")).thenReturn(user);
+        return userRepository;
+    }
+
+    private Principal createLogin() {
+        return (UserPrincipal) () -> "1234567";
+    }
+}
