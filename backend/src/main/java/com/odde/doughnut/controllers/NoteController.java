99b905e9501f7e928bf3f2e6a12f01241f200bd9
Terry Yin <terry.yinzhe@gmail.com>
2021-02-22 14:46:56 +0800

    user current user

--- backend/src/main/java/com/odde/doughnut/controllers/NoteController.java
+++ backend/src/main/java/com/odde/doughnut/controllers/NoteController.java
@@ -1,13 +1,11 @@
 package com.odde.doughnut.controllers;
 
 import com.odde.doughnut.models.Note;
-import com.odde.doughnut.models.User;
 import com.odde.doughnut.repositories.NoteRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -29,20 +27,19 @@ public class NoteController {
     }
 
     @GetMapping("/all_my_notes")
-    public String all_my_notes(@RequestAttribute("currentUser") User currentUser, Model model) {
-        model.addAttribute("all_my_notes", currentUser.getNotes());
+    public String all_my_notes(Model model) {
+        model.addAttribute("all_my_notes", currentUser.getUser().getNotes());
         return "all_my_notes";
     }
 
     @GetMapping("/link/{id}")
     public String link(
-            @RequestAttribute("currentUser") User currentUser,
             @PathVariable("id") String id,
             @RequestParam(required = false) String searchTerm,
             Model model
     ) {
         Note sourceNote = noteRepository.findById(Integer.valueOf(id)).get();
-        List<Note> linkableNotes = currentUser.filterLinkableNotes(sourceNote, searchTerm);
+        List<Note> linkableNotes = currentUser.getUser().filterLinkableNotes(sourceNote, searchTerm);
         model.addAttribute("linkableNotes", linkableNotes);
         model.addAttribute("sourceNote", sourceNote);
         return "link";
