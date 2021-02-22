b3847d06a031cf9c104ba710f669a27eebab93c4
Terry Yin <terry.yinzhe@gmail.com>
2021-02-22 14:48:56 +0800

    remove interceptor

--- backend/src/main/java/com/odde/doughnut/controllers/ReviewController.java
+++ backend/src/main/java/com/odde/doughnut/controllers/ReviewController.java
@@ -1,11 +1,9 @@
 package com.odde.doughnut.controllers;
 
 import com.odde.doughnut.models.Note;
-import com.odde.doughnut.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestAttribute;
 
 import java.util.List;
 
@@ -18,8 +16,8 @@ public class ReviewController {
     }
 
     @GetMapping("/review")
-    public String review(@RequestAttribute("currentUser") User currentUser, Model model) {
-        List<Note> notes = currentUser.getNotesInDescendingOrder();
+    public String review(Model model) {
+        List<Note> notes = currentUser.getUser().getNotesInDescendingOrder();
         model.addAttribute("notes", notes);
         return "review";
     }
