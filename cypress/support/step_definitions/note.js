27beb2da52ca45c06069b3347671f44713429365
Terry Yin <terry.yinzhe@gmail.com>
2021-02-26 12:53:06 +0800

    start breadcrumb

--- cypress/support/step_definitions/note.js
+++ cypress/support/step_definitions/note.js
@@ -17,6 +17,14 @@ When("I create top level note with:", (data) => {
   cy.createNotes(data.hashes());
 });
 
+When("I create note belonging to {string}:", (noteTitle, data) => {
+  cy.visit("/notes");
+  cy.findByText(noteTitle).click();
+  cy.findByText("Add Child Note").click();
+  cy.findByText(noteTitle );
+  cy.createNotes(data.hashes());
+});
+
 Then("I should see these notes belonging to the user at the top level of all my notes", (data) => {
     cy.visit("/notes");
     cy.expectNotes(data);
@@ -24,13 +32,6 @@ Then("I should see these notes belonging to the user at the top level of all my 
 
 Then("I should see these notes belonging to the user", (data) => {
     cy.expectNotes(data);
-});
-
-When("I create note belonging to {string}:", (noteTitle, data) => {
-  cy.visit("/notes");
-  cy.findByText(noteTitle).click();
-  cy.findByText("Add Child Note").click();
-  cy.createNotes(data.hashes());
 });
 
 When("I delete top level note {string}", (noteTitle) => {
