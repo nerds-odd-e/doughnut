e3fef1fe0a50d59ce6aafd889c2812e412498102
Terry Yin <terry.yinzhe@gmail.com>
2021-02-27 23:12:10 +0800

    display

--- backend/src/main/java/com/odde/doughnut/modelDecorators/NoteDecorator.java
+++ backend/src/main/java/com/odde/doughnut/modelDecorators/NoteDecorator.java
@@ -2,8 +2,10 @@ package com.odde.doughnut.modelDecorators;
 
 import com.odde.doughnut.models.Note;
 import com.odde.doughnut.repositories.NoteRepository;
+import org.apache.logging.log4j.util.Strings;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class NoteDecorator {
@@ -24,5 +26,12 @@ public class NoteDecorator {
 
     public String getTitle() {
         return note.getTitle();
+    }
+
+    public List<String> getDescriptionLines() {
+        if (Strings.isEmpty(note.getDescription())) {
+            return new ArrayList<>();
+        }
+        return Arrays.asList(note.getDescription().split("\n"));
     }
 }
