e5f8aa32523c5ecffb9235db37694d52ad6e209e
Terry Yin <terry.yinzhe@gmail.com>
2021-03-05 10:24:32 +0800

    extract to command

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -127,3 +127,42 @@ Cypress.Commands.add("timeTravelTo", (day, hour) => {
   }).its("body").should("contain", "OK")
 });
 
+Cypress.Commands.add("initialReviewOneNoteIfThereIs", ({review_type, title, additional_info}) => {
+    if(review_type == "end of review") {
+        cy.findByText("You have done all the reviews for today.").should("be.visible");
+    }
+    else {
+        cy.findByText(title, {selector: '#note-title'})
+        switch(review_type) {
+        case "single note": {
+            if(additional_info) {
+                cy.get('#note-description').should("contain", additional_info);
+            }
+            break;
+        }
+
+        case  "picture note": {
+            if(additional_info) {
+                const [expectedDescription, expectedPicture] = additional_info.split("; ")
+                cy.get('#note-description').should("contain", expectedDescription);
+                cy.get('#note-picture').find('img').should('have.attr', 'src').should('include',expectedPicture);
+            }
+            break;
+        }
+
+        case "related notes": {
+            if(additional_info) {
+                additional_info.split(", ").forEach(expectedLinkTarget =>
+                    cy.findByText(expectedLinkTarget, {selector: '#note-links li'})
+                )
+            }
+            break;
+        }
+
+        default:
+            expect(review_type).equal("a known review page type");
+        }
+
+        cy.findByText("Next").click();
+    }
+});
