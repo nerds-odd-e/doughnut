31ad1da582f092a4e464cc0b1c537d5615c78e82
Terry Yin <terry.yinzhe@gmail.com>
2021-03-05 10:30:16 +0800

    pass the inital side

--- cypress/support/step_definitions/review.js
+++ cypress/support/step_definitions/review.js
@@ -20,7 +20,11 @@ Given("It's day {int}, {int} hour", (day, hour) => {
 Then("I should be able to follow these review actions:", (data) => {
   data.hashes().forEach(({day, old_notes_to_review, initial_review}) => {
     cy.timeTravelTo(day, 8);
-    cy.log(initial_review)
+    cy.visit('/review');
+    initial_review.split(", ").forEach(noteIndex => {
+        const [review_type, title] = ["single note", `Note ${noteIndex}`];
+        cy.initialReviewOneNoteIfThereIs({review_type, title});
+    });
   });
 });
 
