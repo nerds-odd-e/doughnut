42600eb44f65d3fc4973c264bed8b8d7617f16b5
Terry Yin <terry.yinzhe@gmail.com>
2021-03-05 10:20:14 +0800

    step def

--- cypress/support/step_definitions/review.js
+++ cypress/support/step_definitions/review.js
@@ -9,44 +9,43 @@ import {
 Then("I do these initial reviews in sequence:", (data) => {
   cy.visit('/review');
   data.hashes().forEach(({review_type, title, additional_info}) => {
-    switch(review_type) {
-    case "single note": {
-        cy.findByText(title, {selector: '#note-title'})
-        if(additional_info) {
-            cy.get('#note-description').should("contain", additional_info);
-        }
-        break;
-    }
-
-    case  "picture note": {
-        cy.findByText(title, {selector: '#note-title'})
-        if(additional_info) {
-            const [expectedDescription, expectedPicture] = additional_info.split("; ")
-            cy.get('#note-description').should("contain", expectedDescription);
-            cy.get('#note-picture').find('img').should('have.attr', 'src').should('include',expectedPicture);
-        }
-        break;
-    }
-
-    case "related notes": {
-        cy.findByText(title, {selector: '#note-title'})
-        if(additional_info) {
-            additional_info.split(", ").forEach(expectedLinkTarget =>
-                cy.findByText(expectedLinkTarget, {selector: '#note-links li'})
-            )
-        }
-        break;
-    }
-
-    case "end of review": {
+    if(review_type == "end of review") {
         cy.findByText("You have done all the reviews for today.").should("be.visible");
-        return;
     }
-    default:
-        expect(review_type).equal("a known review page type");
-    }
+    else {
+        cy.findByText(title, {selector: '#note-title'})
+        switch(review_type) {
+        case "single note": {
+            if(additional_info) {
+                cy.get('#note-description').should("contain", additional_info);
+            }
+            break;
+        }
 
-    cy.findByText("Next").click();
+        case  "picture note": {
+            if(additional_info) {
+                const [expectedDescription, expectedPicture] = additional_info.split("; ")
+                cy.get('#note-description').should("contain", expectedDescription);
+                cy.get('#note-picture').find('img').should('have.attr', 'src').should('include',expectedPicture);
+            }
+            break;
+        }
+
+        case "related notes": {
+            if(additional_info) {
+                additional_info.split(", ").forEach(expectedLinkTarget =>
+                    cy.findByText(expectedLinkTarget, {selector: '#note-links li'})
+                )
+            }
+            break;
+        }
+
+        default:
+            expect(review_type).equal("a known review page type");
+        }
+
+        cy.findByText("Next").click();
+    }
   });
 })
 
