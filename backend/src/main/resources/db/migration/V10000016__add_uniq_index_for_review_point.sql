3d43292e7e9857742a24639faf379d3aa91a110d
Terry Yin <terry.yinzhe@gmail.com>
2021-03-07 12:36:28 +0800

    new clean produ

--- backend/src/main/resources/db/migration/V10000016__add_uniq_index_for_review_point.sql
+++ backend/src/main/resources/db/migration/V10000016__add_uniq_index_for_review_point.sql
@@ -1,4 +1,2 @@
 ALTER TABLE review_point
-    ADD COLUMN initial_reviewed_at TIMESTAMP NOT NULL;
-
-UPDATE review_point SET initial_reviewed_at = last_reviewed_at;
+    ADD COLUMN initial_reviewed_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP;
--- backend/src/main/resources/db/migration/V10000017__add_next_review_at_for_review_point.sql
+++ backend/src/main/resources/db/migration/V10000017__add_next_review_at_for_review_point.sql
@@ -1,7 +1,2 @@
 ALTER TABLE review_point
-    ADD COLUMN next_review_at TIMESTAMP;
-
-UPDATE review_point SET next_review_at = last_reviewed_at;
-
-ALTER TABLE review_point
-    MODIFY next_review_at TIMESTAMP NOT NULL;
+    ADD COLUMN next_review_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP;
