640e5b83744383fe4c89a1d3560b5548a7be749d
Terry Yin <terry.yinzhe@gmail.com>
2021-03-12 15:04:28 +0800

    trigger

--- backend/src/main/resources/db/migration/V10000022__add_removed_from_review_to_review_point.sql
+++ backend/src/main/resources/db/migration/V10000022__add_removed_from_review_to_review_point.sql
@@ -2,3 +2,6 @@ ALTER TABLE review_point
     ADD COLUMN removed_from_review BOOLEAN;
 
 UPDATE review_point SET removed_from_review = 0;
+
+ALTER TABLE review_point
+    MODIFY removed_from_review BOOLEAN NOT NULL DEFAULT 0;
