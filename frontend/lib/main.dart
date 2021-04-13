df68399a8e904205bca7dc62c657e2069cd2dde7
Yeong Sheng, Tan <tanyeongsheng@gmail.com>
2021-04-13 20:06:07 +0800

    rename main StatelessWidget class as 'DoughnutApp'

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(DoughnutApp());
 }
 
-class MyApp extends StatelessWidget {
+class DoughnutApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Doughnut Flutter Frontend',
+      title: 'Doughnut Frontend',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -22,13 +22,14 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Doughnut Flutter Frontend Home Page'),
+      home: DoughnutHomePage(title: 'Doughnut Frontend'),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, this.title}) : super(key: key);
+class DoughnutHomePage extends StatefulWidget {
+  DoughnutHomePage({Key? key, this.title}) : super(key: key);
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -42,10 +43,10 @@ class MyHomePage extends StatefulWidget {
   final String? title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _DoughnutHomePageState createState() => _DoughnutHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _DoughnutHomePageState extends State<DoughnutHomePage> {
   int _counter = 0;
 
   void _incrementCounter() {
