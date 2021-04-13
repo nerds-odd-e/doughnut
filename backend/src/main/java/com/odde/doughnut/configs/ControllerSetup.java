8d63f6ba51917028c1b3b801811e18f7ecbc11c7
CSDUser <CSDUser@deathstar.local>
2021-04-13 17:57:13 +0900

    add ExceptionHandler

--- backend/src/main/java/com/odde/doughnut/configs/ControllerSetup.java
+++ backend/src/main/java/com/odde/doughnut/configs/ControllerSetup.java
@@ -1,9 +1,12 @@
 package com.odde.doughnut.configs;
 
 import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 public class ControllerSetup
@@ -13,5 +16,13 @@ public class ControllerSetup
     {
         StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(false);
         binder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String handleSystemException(Exception e) {
+        // ExceptionHandling
+        System.out.println(e.getStackTrace());
+        return "/error";
     }
 }
