237b140655667a13d81299d2140a5acf5a43d27c
Terry Yin <terry.yinzhe@gmail.com>
2021-04-18 19:19:14 +0800

    get profile straight

--- backend/src/test/java/com/odde/doughnut/integration/RobotsTests.java
+++ backend/src/test/java/com/odde/doughnut/integration/RobotsTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -14,6 +15,7 @@ import static org.hamcrest.Matchers.containsString;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 class RobotsTests {
 	@Autowired
 	private MockMvc mockMvc;
