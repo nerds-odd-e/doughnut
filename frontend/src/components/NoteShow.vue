b640b05de15ec1a4b137186e905559cf5e4d594c
Terry Yin <terry.yinzhe@gmail.com>
2021-06-14 16:55:35 +0800

    url

--- frontend/src/components/NoteShow.vue
+++ frontend/src/components/NoteShow.vue
@@ -1,13 +1,15 @@
 <template>
   <div v-if="note">
-  <!-- <div th:fragment="noteShow(note, level, forBazaar)"> -->
-      <!-- th:replace=":: noteShowWithTitle(${note}, ${level}, ${forBazaar}, ~{:: .note-title-show}, ~{:: .note-body-show})"> -->
-      <h2 :class="'h' + level"> {{note.noteContent.title}}</h2>
-      <pre class="note-body" style="white-space: pre-wrap;">{{note.noteContent.description}}</pre>
+      <NoteShowWithTitle :note="note" :level="level" :forBazaar="forBazaar">
+        <h2 :class="'h' + level"> {{note.noteContent.title}}</h2>
+        <pre class="note-body" style="white-space: pre-wrap;">{{note.noteContent.description}}</pre>
+      </NoteShowWithTitle>
   </div>
+
 </template>
 
 <script setup>
+import NoteShowWithTitle from "./NoteShowWithTitle.vue"
 import { ref, defineProps } from "vue"
 
 const props = defineProps({noteid: Number, level: Number, forBazaar: Boolean})
---
+++ frontend/src/components/NoteShowWithTitle.vue
@@ -0,0 +1,34 @@
+<template>
+    <div th:object="${note.noteContent}">
+        <slot></slot>
+        <div v-if="!!note.noteContent.url">
+            <label v-if="note.noteContent.urlIsVideo">Video Url:</label>
+            <label v-else>Url:</label>
+            <a :href="note.noteContent.url">{{note.noteContent.url}}</a>
+        </div>
+    </div>
+    <!-- <div th:replace=":: showPicture(${note}, 0.2)"/>
+    <ul>
+        <li th:each="linkType:${note.linkTypes(currentUser?.entity)}">
+            <span th:text="${linkType.label}"/>
+            <span class="badge badge-light mr-1" th:each="link:${note.linksOfTypeThroughDirect(linkType)}">
+                <span th:unless="${forBazaar}" th:remove="tag">
+                    <span th:replace=":: linkWithHtmlLink(${link}, ${link.targetNote})"/>
+                </span>
+                <a th:href="'/bazaar/notes/' + ${link.targetNote.id}" th:text="${link.targetNote.noteContent.title}"
+                   th:if="${forBazaar}"/>
+            </span>
+            <span class="badge badge-warning mr-1"
+                  th:each="link:${note.linksOfTypeThroughReverse(linkType, currentUser?.entity)}">
+                <span th:unless="${forBazaar}" th:remove="tag">
+                    <span th:replace=":: linkWithHtmlLink(${link}, ${link.sourceNote})"/>
+                </span>
+                <a th:href="'/bazaar/notes/' + ${link.sourceNote.id}" th:text="${link.sourceNote.noteContent.title}" th:if="${forBazaar}"/>
+            </span>
+        </li>
+    </ul> -->
+</template>
+
+<script setup>
+  const props = defineProps({note: Object, level: Number, forBazaar: Boolean})
+</script>
