a1d66d4251bcf5e7471d431c5111afbdcf989bb7
Terry Yin <terry.yinzhe@gmail.com>
2021-06-15 13:22:19 +0800

    note show

--- frontend/src/components/notes/NoteButtons.vue
+++ frontend/src/components/notes/NoteButtons.vue
@@ -3,35 +3,39 @@
     <a class="btn btn-sm" :href="`/notes/${note.id}/edit`" role="button" title="edit note">
         <SvgEdit/>
     </a>
-    <!-- <a role="button" class="btn btn-sm" title="Cards View" th:href="@{/notes/articles/{id}(id=${note.id})}">
-        <svg th:replace="_fragments/svgs :: article"/>
+    <a role="button" class="btn btn-sm" title="Cards View" :href="`/notes/articles/${note.id}`">
+        <SvgArticle/>
     </a>
-    <a class="btn btn-sm" th:href="@{/links/{id}/link(id=${note.id})}" role="button" title="link note">
-        <svg th:replace="_fragments/svgs :: linkNote"/>
+    <a class="btn btn-sm" :href="`/links/${note.id}/link`" role="button" title="link note">
+        <SvgLinkNote/>
     </a>
     <a class="btn btn-light dropdown-toggle"
             data-toggle="dropdown" aria-haspopup="true"
             aria-expanded="false" role="button" title="more options">
-        <svg th:replace="_fragments/svgs :: cog"/>
+        <SvgCog/>
     </a>
     <div class="dropdown-menu dropdown-menu-right">
-        <a  class="dropdown-item" th:href="@{/notes/{id}/review_setting(id=${note.id})}"
-           title="edit review settings">
-            <svg th:replace="_fragments/svgs :: reviewSetting"/>
+        <a  class="dropdown-item" :href="`/notes/${note.id}/review_setting`" title="edit review settings">
+            <SvgReviewSetting/>
             Edit review settings
         </a>
-        <form th:action="@{/notes/{id}/delete(id=${note.id})}" th:method="delete"
+        <form :action="`/notes/${note.id}/delete`" method="post"
                       onsubmit="return confirm('Are you sure to delete?')">
             <button class="dropdown-item" title="delete note">
-                <svg th:replace="_fragments/svgs :: remove"/>
+                <SvgRemove/>
                 Delete
             </button>
         </form>
-    </div> -->
+    </div>
 </div>
 </template>
 
 <script setup>
   import SvgEdit from "../svgs/SvgEdit.vue"
+  import SvgArticle from "../svgs/SvgArticle.vue"
+  import SvgLinkNote from "../svgs/SvgLinkNote.vue"
+  import SvgCog from "../svgs/SvgCog.vue"
+  import SvgReviewSetting from "../svgs/SvgReviewSetting.vue"
+  import SvgRemove from "../svgs/SvgRemove.vue"
   const props = defineProps({note: Object})
 </script>
