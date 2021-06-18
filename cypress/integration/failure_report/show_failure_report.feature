3915b040eff5450ad9e3072f48173656022fcfa3
Terry Yin <terry.yinzhe@gmail.com>
2021-06-19 07:25:20 +0800

    reduce exception

--- cypress/integration/failure_report/show_failure_report.feature
+++ cypress/integration/failure_report/show_failure_report.feature
@@ -5,3 +5,8 @@ Feature: failure report
     And I've logged in as "developer"
     Then I should see "RuntimeException" in the failure report
 
+  @slow
+  Scenario: add issue and failure-report
+    Given Use real github sandbox and there are no open issues on github
+    When Someone triggered an exception
+    Then I should see a new open issue on github
--- frontend/src/partials.js
+++ frontend/src/partials.js
@@ -1,17 +1,6 @@
 import { createApp } from 'vue'
-import Review from './pages/Review.vue'
 import NoteApp from './NoteApp.vue'
 import {router} from './routes'
-
-const partialApp = createApp(Review);
-const partialEl = document.getElementById('partials');
-if (partialEl) {
-    partialApp.provide('noteid', partialEl.dataset.noteid)
-    partialApp.provide('linkid', partialEl.dataset.linkid)
-    partialApp.provide('reviewPointId', partialEl.dataset.reviewpointid)
-    partialApp.provide('sadOnly', partialEl.dataset.sadonly === "true")
-    partialApp.mount('#partials')
-}
 
 const app = createApp(NoteApp)
 app.use(router)
