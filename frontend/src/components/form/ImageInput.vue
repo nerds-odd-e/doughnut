6cf2596e6da4fce4cc0a6399c9672aff34604d80
TerryYin <terry@odd-e.com>
2021-06-24 13:13:09 +0800

    all fields

---
+++ frontend/src/components/form/ImageInput.vue
@@ -0,0 +1,32 @@
+<template>
+  <InputWithType v-bind="{scopeName, field, errors}">
+      <input
+              class="text-input-control form-control put_error_class_here"
+              :id="`${scopeName}-${field}`"
+              type="file"
+              :name="field"
+              @input="$emit('update:modelValue', $event.target.value)"
+              :placeholder="placeholder"
+              :autofocus="autofocus"
+              autocomplete="off"
+              autocapitalize="off"
+      />
+  </InputWithType>
+</template>
+
+<script>
+import InputWithType from "./InputWithType.vue"
+export default {
+  props: {
+    modelValue: String,
+     scopeName: String,
+      field: String,
+      placeholder: {type: String, default: null},
+      autofocus: {type: Boolean, default: false},
+      errors: Object
+      },
+  emits: ['update:modelValue'],
+  components: {InputWithType}
+}
+
+</script>
--- frontend/src/components/notes/NoteFormBody.vue
+++ frontend/src/components/notes/NoteFormBody.vue
@@ -1,20 +1,22 @@
 <template>
     <TextInput scopeName='note' field='title' v-model="modelValue.title"/>
     <TextArea scopeName='note' field='description' v-model="modelValue.description"/>
-    <div th:replace="_fragments/forms :: imageInput('note', 'uploadPicture', 'Optional. upload own picture.')"/>
-    <div th:replace="_fragments/forms :: textInput('note', 'pictureUrl', 'Full url of existing picture.', false)"/>
-    <div th:replace="_fragments/forms :: checkInput('note', 'useParentPicture')"/>
-    <div th:replace="_fragments/forms :: textInput('note', 'pictureMask', null, false)"/>
-    <div th:replace="_fragments/forms :: textInput('note', 'url', null, false)"/>
-    <div th:replace="_fragments/forms :: checkInput('note', 'urlIsVideo')"/>
-    <div th:replace="_fragments/forms :: checkInput('note', 'hideTitleInArticle')"/>
-    <div th:replace="_fragments/forms :: checkInput('note', 'showAsBulletInArticle')"/>
-    <div th:replace="_fragments/forms :: checkInput('note', 'skipReview')"/>
+    <ImageInput scopeName='note' field='uploadPictureProxy' placeholder="Optional. upload own picture." v-model="modelValue.uploadPictureProxy"/>
+    <TextInput scopeName='note' field='pictureUrl' placeholder="Full url of existing picture." v-model="modelValue.pictureUrl"/>
+    <CheckInput scopeName='note' field='useParentPicture' v-model="modelValue.useParentPicture"/>
+    <TextInput scopeName='note' field='pictureMask' v-model="modelValue.pictureMask"/>
+    <TextInput scopeName='note' field='url' v-model="modelValue.url"/>
+    <CheckInput scopeName='note' field='urlIsVideo' v-model="modelValue.urlIsVideo"/>
+    <CheckInput scopeName='note' field='hideTitleInArticle' v-model="modelValue.hideTitleInArticle"/>
+    <CheckInput scopeName='note' field='showAsBulletInArticle' v-model="modelValue.showAsBulletInArticle"/>
+    <CheckInput scopeName='note' field='skipReview' v-model="modelValue.skipReview"/>
 </template>
 
 <script setup>
 import TextInput from "../form/TextInput.vue"
+import CheckInput from "../form/CheckInput.vue"
 import TextArea from "../form/TextArea.vue"
+import ImageInput from "../form/ImageInput.vue"
 import { ref, defineProps } from "vue"
 
 const props = defineProps({modelValue: {type: Object, required: true }})
