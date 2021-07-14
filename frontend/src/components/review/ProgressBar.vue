55bc9e031a6379f6b4fefe6a9ff4c66914bd98f0
Terry Yin <terry.yinzhe@gmail.com>
2021-07-14 16:47:15 +0800

    missing file

---
+++ frontend/src/components/review/ProgressBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <StopRepeatButton />
+  <PauseRepeatButton v-bind="{noteId, linkId}"/>
+</template>
+<script>
+import StopRepeatButton from "./StopRepeatButton.vue"
+import PauseRepeatButton from "./PauseRepeatButton.vue"
+
+export default {
+  components: { StopRepeatButton, PauseRepeatButton },
+  props: {noteId: Number, linkId: Number},
+  methods: {
+  },
+}
+</script>
