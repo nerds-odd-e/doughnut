b25226e5573532dc743da15d06e731152879e302
Yeong Sheng, Tan <tanyeongsheng@gmail.com>
2021-08-22 20:29:01 +0800

    cypress env config files for local vs CI env run

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -12,19 +12,25 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
+const cucumber = require('cypress-cucumber-preprocessor').default;
+const { initPlugin } = require('cypress-plugin-snapshots/plugin');
+const fs = require('fs-extra');
+const path = require('path');
 /**
  * @type {Cypress.PluginConfig}
  */
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
-}
-
-const cucumber = require('cypress-cucumber-preprocessor').default
-const { initPlugin } = require('cypress-plugin-snapshots/plugin');
-
-module.exports = (on, config) => {
   initPlugin(on, config);
-  on('file:preprocessor', cucumber())
-  return config;
+  on('file:preprocessor', cucumber());
+
+  const file = config.env.configFile || 'development';
+  return getConfigurationByFile(file);
+};
+
+function getConfigurationByFile(file) {
+  const pathToConfigFile = path.resolve('cypress/configFiles', `${file}.json`);
+
+  return fs.readJson(pathToConfigFile);
 }
