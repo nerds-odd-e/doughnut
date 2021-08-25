6978a384b1dd97ac2b25dfb5738fd604377ca20a
janicetyp <janicetyp@gmail.com>
2021-08-25 12:36:40 +0800

    refactored pink banner unit test

--- frontend/tests/notes/NoteShow.spec.js
+++ frontend/tests/notes/NoteShow.spec.js
@@ -4,14 +4,16 @@ import NoteShow from "@/components/notes/NoteShow.vue";
 import { createMockNote } from "./NoteShow-fixtures";
 
 describe("new/updated pink banner", () => {
+  const getBannerCount = async (wrapper) => {
+    return wrapper.queryAllByText("This note has been changed recently.");
+  };
+
   test("should show pink banner if the note was updated within the last 12 hours", async () => {
     const now = moment().format();
     const note = createMockNote({ note: { noteContent: { updatedAt: now } } });
     const wrapper = render(NoteShow, { props: note });
 
-    expect(
-      await wrapper.findByText("This note has been changed recently.")
-    ).toBeTruthy();
+    expect(await getBannerCount(wrapper)).toHaveLength(1);
   });
 
   test("should show 0 pink banners if the note was updated more than 12 hours ago", async () => {
@@ -21,8 +23,6 @@ describe("new/updated pink banner", () => {
     });
     const wrapper = render(NoteShow, { props: note });
 
-    expect(
-      await wrapper.queryAllByText("This note has been changed recently.")
-    ).toHaveLength(0);
+    expect(await getBannerCount(wrapper)).toHaveLength(0);
   });
 });
