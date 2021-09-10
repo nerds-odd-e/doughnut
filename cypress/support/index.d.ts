ef8852acdd80d676330321912a00ccc09fda80a1
Yeong Sheng, Tan <tanyeongsheng@gmail.com>
2021-09-10 12:26:23 +0800

    more precise cypress custom commands type defs

--- cypress/support/index.d.ts
+++ cypress/support/index.d.ts
@@ -1,24 +1,28 @@
 /// <reference types="cypress" />
 
 declare namespace Cypress {
+  interface userSetting {
+    daily_new_notes_count?: number;
+    space_intervals?: number;
+  }
   interface Chainable {
-    cleanDBAndSeedData(): Chainable<any>;
+    cleanDBAndSeedData(): void;
     failure(): void;
     findNoteCardButton(
       noteTitle: string,
       btnTextOrTitle: string,
-    ): Chainable<any>;
-    findNoteCardEditButton(noteTitle: string): Chainable<any>;
-    getFormControl(label: string): Chainable<any>;
-    loginAs(username: string): Chainable<any>;
-    logout(username?: string): Chainable<any>;
-    pageIsLoaded(): Chainable<any>;
+    ): Chainable<Element>;
+    findNoteCardEditButton(noteTitle: string): Chainable<Element>;
+    getFormControl(label: string): Chainable<Element>;
+    loginAs(username: string): void;
+    logout(username?: string): void;
+    pageIsLoaded(): void;
     subscribeToNote(
       noteTitle: string,
       dailyLearningCount: number,
-    ): Chainable<any>;
-    unsubscribeFromNotebook(noteTitle: string): Chainable<any>;
-    updateCurrentUserSettingsWith(hash: any): Chainable<any>;
-    visitMyNotebooks(noteTitle?: string): Chainable<any>;
+    ): Chainable<Element>;
+    unsubscribeFromNotebook(noteTitle: string): Chainable<Element>;
+    updateCurrentUserSettingsWith(settings: userSetting): Chainable<Element>;
+    visitMyNotebooks(noteTitle?: string): Chainable<Element>;
   }
 }
