5a5b524cf8485e3ff41b190edc4abcc9c3956882
Terry Yin <terry@odd-e.com>
2021-09-13 06:52:29 +0000

    fix warnings

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    jest: true,
   },
   extends: [
     "eslint:recommended",
--- frontend/tests/notes/NoteShow.spec.js
+++ frontend/tests/notes/NoteShow.spec.js
@@ -1,23 +1,21 @@
-import { render } from "@testing-library/vue";
+import { render, screen } from "@testing-library/vue";
 import NoteShow from "@/components/notes/NoteShow.vue";
 import { createMockNote } from "./NoteShow-fixtures";
 
 describe("new/updated pink banner", () => {
-  const getBannerCount = async (wrapper) => {
-    return wrapper.queryAllByText("This note has been changed recently.");
-  };
+  const getBannerCount = async () => screen.queryAllByText("This note has been changed recently.");
 
   test("should show pink banner if the note was updated within the last 12 hours", async () => {
     const note = createMockNote({ recentlyUpdated: true });
-    const wrapper = render(NoteShow, { props: note });
+    const view = render(NoteShow, { props: note });
 
-    expect(await getBannerCount(wrapper)).toHaveLength(1);
+    expect(await getBannerCount()).toHaveLength(1);
   });
 
   test("should show 0 pink banners if the note was updated more than 12 hours ago", async () => {
     const note = createMockNote({ recentlyUpdated: false });
-    const wrapper = render(NoteShow, { props: note });
+    const view = render(NoteShow, { props: note });
 
-    expect(await getBannerCount(wrapper)).toHaveLength(0);
+    expect(await getBannerCount()).toHaveLength(0);
   });
 });
