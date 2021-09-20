2bd2e8a24a510dfaaf9863b4b55e47a02f206edd
Terry Yin <terry.yinzhe@gmail.com>
2021-09-20 11:15:40 +0800

    remove redundant

--- frontend/src/components/notes/Breadcrumb.vue
+++ frontend/src/components/notes/Breadcrumb.vue
@@ -1,23 +1,23 @@
 <template>
   <NoteBreadcrumb :ancestors="ancestors">
-    <template #topLink v-if="owns">
-      <template v-if="!!circle">
-        <li class="breadcrumb-item">
-          <router-link :to="{ name: 'circles' }">Circles</router-link>
+    <template #topLink>
+      <li v-if="!owns" class="breadcrumb-item"><router-link :to="{name: 'bazaar'}">Bazaar</router-link></li>
+      <template v-else>
+        <li class="breadcrumb-item" v-if="!circle">
+          <router-link :to="{ name: 'notebooks' }">Top</router-link>
         </li>
-        <li class="breadcrumb-item">
-          <router-link
-            :to="{ name: 'circleShow', params: { circleId: circle.id } }"
-            >{{ circle.name }}</router-link
-          >
-        </li>
+        <template v-else>
+          <li class="breadcrumb-item">
+            <router-link :to="{ name: 'circles' }">Circles</router-link>
+          </li>
+          <li class="breadcrumb-item">
+            <router-link
+              :to="{ name: 'circleShow', params: { circleId: circle.id } }"
+              >{{ circle.name }}</router-link
+            >
+          </li>
+        </template>
       </template>
-      <li class="breadcrumb-item" v-else>
-        <router-link :to="{ name: 'notebooks' }">Top</router-link>
-      </li>
-    </template>
-    <template v-else #topLink>
-      <li class="breadcrumb-item"><router-link :to="{name: 'bazaar'}">Bazaar</router-link></li>
     </template>
     <template v-slot:additional>
       <slot />
