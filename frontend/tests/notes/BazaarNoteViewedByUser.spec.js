58e10daad84661f5e129c59ed05355dfe67340eb
Terry Yin <terry.yinzhe@gmail.com>
2021-09-24 12:11:21 +0800

    add test back

--- frontend/tests/notes/BazaarNoteViewedByUser.spec.js
+++ frontend/tests/notes/BazaarNoteViewedByUser.spec.js
@@ -2,30 +2,21 @@
  * @jest-environment jsdom
  */
 import NoteViewedByUser from "@/components/notes/NoteViewedByUser.vue";
-import { noteViewedByUser } from "./fixtures";
 import { screen } from "@testing-library/vue";
 import _ from "lodash";
 import { renderWithStoreAndMockRoute } from "../helpers";
 import makeMe from "../fixtures/makeMe";
 
-const noteView = _.merge(noteViewedByUser, {
-  owns: false,
-});
-
 describe("a note not owned by viewer", () => {
   test("", async () => {
-    const noteView = makeMe.aNote.deprecatingInBazaar().please();
-    renderWithStoreAndMockRoute(
-      NoteViewedByUser,
-      {
-        propsData: noteView,
-      },
-      null,
-      (store) => {
-        store.commit("loadNotes", [noteView]);
-      }
-    );
-    screen.findByText("Bazaar");
-    // expect(wrapper.findAll(".nav [title='Add to my learning']")).toHaveLength(
+    const noteView = makeMe.aNote.deprecatingInBazaar().please()
+    renderWithStoreAndMockRoute(NoteViewedByUser, {
+      propsData: noteView,
+    }, null, store=> {
+      store.commit('loadNotes', [noteView])
+
+    });
+    await screen.findByText("Bazaar");
+    await screen.findByRole('button', { name: "Add to my learning" })
   });
 });
