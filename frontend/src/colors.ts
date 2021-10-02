7ad942555e3f70a7e5d00c5826262be3f67fc0ee
Yeong Sheng, Tan <tanyeongsheng@gmail.com>
2021-10-02 13:43:13 +0800

    typed colors

---
+++ frontend/src/colors.ts
@@ -0,0 +1,14 @@
+export interface IColors {
+  [property: string]: string
+}
+
+export const colors: IColors = {
+  sourceBackground: '#ffaaaa',
+  targetBackground: '#d4ffaa',
+  source: '#773333',
+  target: '#447733',
+};
+
+const bgcolors = (role: string): string => colors[`${role}Background`];
+
+export default bgcolors;
