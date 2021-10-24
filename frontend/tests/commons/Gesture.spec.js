fc13a1486fba2971f6259d8f92dba07a2a23a76d
Terry Yin <terry.yinzhe@gmail.com>
2021-10-24 15:45:39 +0800

    virtual fingers

--- frontend/tests/commons/Gesture.spec.js
+++ frontend/tests/commons/Gesture.spec.js
@@ -2,7 +2,6 @@
  * @jest-environment jsdom
  */
 import Gesture from "@/components/commons/Gesture";
-import { fromPairs } from "lodash";
 
 describe("Gesture", () => {
   const top = 10
@@ -18,13 +17,26 @@ describe("Gesture", () => {
     expect(gesture.offset.x).toEqual(1.25)
   });
 
-  describe("rotate", () => {
+  describe("rotate and scale (holding shift)", () => {
+    const holdingShiftMoveFromTo = (from, to) => {
+      const gesture = new Gesture({x: 0, y: 0, scale: 1})
+      gesture.newPointer(1, {x: frame.width/2 + left, y: frame.height/2 + top + 10})
+      gesture.move(frame, 1, {x:  frame.width/2 + left, y: frame.height/2 + top + 20})
+      gesture.shiftDown(true)
+      return gesture
+    }
 
     test("does not change offset", async () => {
       const gesture = new Gesture({x: 0, y: 0, scale: 1})
       gesture.newPointer(1, {x: frame.width/2, y: 0})
       gesture.move(frame, 1, {x:  frame.width/2 + 5, y: 0})
       gesture.shiftDown(true)
+      expect(gesture.offset.x).toEqual(0)
+    });
+
+    test("change scale as comparing to center", async () => {
+      const gesture = holdingShiftMoveFromTo()
+      expect(gesture.offset.scale).toEqual(2)
       expect(gesture.offset.x).toEqual(0)
     });
 
