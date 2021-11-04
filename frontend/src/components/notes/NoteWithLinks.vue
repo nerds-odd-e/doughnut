432944bfd5a4be7ce3a7b9737d09fde91d8f9fd2
Terry Yin <terry.yinzhe@gmail.com>
2021-11-04 10:33:41 +0800

    extract

--- frontend/src/components/notes/NoteWithLinks.vue
+++ frontend/src/components/notes/NoteWithLinks.vue
@@ -2,22 +2,7 @@
     <NoteShell class="note-body" v-bind="{id, updatedAt: noteContent.updatedAt}">
       <NoteFrameOfLinks v-bind="{ links }">
         <h2 role="title" class="note-title">{{ title }}</h2>
-        <div class="row">
-          <ShowDescription
-            :class="`col-12 ${twoColumns ? 'col-md-6' : ''}`"
-            :description="noteContent.description"
-          />
-          <ShowPicture
-            :class="`col-12 ${twoColumns ? 'col-md-6' : ''}`"
-            v-bind="{notePicture, pictureMask: noteContent.pictureMask}"
-            :opacity="0.2"
-          />
-        </div>
-        <div v-if="!!noteContent.url">
-          <label v-if="noteContent.urlIsVideo">Video Url:</label>
-          <label v-else>Url:</label>
-          <a :href="noteContent.url">{{ noteContent.url }}</a>
-        </div>
+        <NoteBody v-bind="{notePicture, noteContent}"/>
       </NoteFrameOfLinks>
     </NoteShell>
 </template>
@@ -25,8 +10,7 @@
 <script>
 import NoteFrameOfLinks from "../links/NoteFrameOfLinks.vue";
 import NoteShell from "./NoteShell.vue";
-import ShowPicture from "./ShowPicture.vue";
-import ShowDescription from "./ShowDescription.vue";
+import NoteBody from "./NoteBody.vue";
 
 export default {
   name: "NoteWithLinks",
@@ -40,24 +24,12 @@ export default {
   components: {
     NoteFrameOfLinks,
     NoteShell,
-    ShowPicture,
-    ShowDescription,
-  },
-  computed: {
-    twoColumns() {
-      return !!this.notePicture && !!this.noteContent.description;
-    },
+    NoteBody,
   },
 };
 </script>
 
 <style scoped>
-
-.link-multi + .link-multi::before {
-  padding-right: 0.5rem;
-  color: #6c757d;
-  content: "|";
-}
 
 .note-body {
   padding-left: 10px;
