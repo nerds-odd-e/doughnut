fea1160061e2c6b037f2b816eee3668a8cfd3a67
Yeong Sheng, Tan <tanyeongsheng@gmail.com>
2021-11-04 22:00:22 +0800

    try to pacify vite build See https://github.com/element-plus/element-plus/issues/3219

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,8 +3,14 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      scss: {
+        charset: false
+      }
+    }
+  },
   plugins: [
     vue({
       template: {
