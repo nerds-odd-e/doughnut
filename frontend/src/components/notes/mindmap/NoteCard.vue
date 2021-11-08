1a5e85fe0d9288935c30e52d71a3b49351b6e472
Yeong Sheng, Tan <tanyeongsheng@gmail.com>
2021-11-08 19:18:31 +0800

    removed frontend UT console.log warning

--- frontend/src/components/notes/mindmap/NoteCard.vue
+++ frontend/src/components/notes/mindmap/NoteCard.vue
@@ -32,8 +32,7 @@ export default {
      NoteTitleWithLink },
   computed: {
     highlightClass() {
-      console.log(this.highlightNoteId, this.note.id)
-       return this.highlightNoteId?.toString() === this.note.id.toString() ? 'highlighted' : '' },
+      return this.highlightNoteId?.toString() === this.note.id.toString() ? 'highlighted' : '' },
     coord() { return this.mindmap.coord(this.mindmapSector) },
     size() { return this.mindmap.size() },
   },
