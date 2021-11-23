04b9131880105a5c9509804561fbd6b5c5bababa
Yeong Sheng, Tan <tanyeongsheng@gmail.com>
2021-11-23 23:52:38 +0800

    cypress@9.1.0; less nix tooling

--- shell.nix
+++ shell.nix
@@ -8,10 +8,7 @@ in mkShell {
   MYSQL_HOME = builtins.getEnv "MYSQL_HOME";
   MYSQL_DATADIR = builtins.getEnv "MYSQL_DATADIR";
   buildInputs = [
-    autoconf
-    automake
     coreutils-full
-    gnumake
     gradle
     nodejs-16_x
     yarn
