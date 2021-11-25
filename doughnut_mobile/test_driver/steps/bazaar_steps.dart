eddc9ee9c4dda56b8647e6880beb66b6d8e04a09
Terry Yin <terry.yinzhe@gmail.com>
2021-11-25 12:04:49 +0800

    extract testability

--- doughnut_mobile/test_driver/steps/bazaar_steps.dart
+++ doughnut_mobile/test_driver/steps/bazaar_steps.dart
@@ -1,31 +1,13 @@
-import 'dart:convert';
-
-import 'package:flutter_driver/flutter_driver.dart';
 import 'package:flutter_gherkin/flutter_gherkin.dart';
 import 'package:gherkin/gherkin.dart';
-import 'package:http/http.dart' as http;
+import './testability.dart';
 
 List<StepDefinitionGeneric> bazaarSteps() {
   return [
     when1<String, FlutterWorld>(
       'there is a notebook {string} in the bazaar',
       (notebookName, context) async {
-        http.Response response = await httpPost(
-            'http://localhost:9081/api/testability/seed_notes?external_identifier=old_learner',
-            [
-              {'title': notebookName}
-            ]);
-
-        if (response.statusCode == 200) {
-          context.expect((jsonDecode(response.body) as List).length, 1);
-        }
-
-        http.Response response1 = await httpPost(
-            'http://localhost:9081/api/testability/share_to_bazaar',
-            {'noteTitle': notebookName}
-            );
-        context.expect(response1.body, 'OK');
-
+        await Testability(context).seedNotebookInBazaar(notebookName);
       },
     ),
     given<FlutterWorld>(
@@ -39,12 +21,3 @@ List<StepDefinitionGeneric> bazaarSteps() {
   ];
 }
 
-Future<http.Response> httpPost(
-    String uri, Object object) async {
-  final response = await http.post(Uri.parse(uri),
-      headers: {
-        'Content-Type': 'application/json; charset=UTF-8',
-      },
-      body: jsonEncode(object));
-  return response;
-}
---
+++ doughnut_mobile/test_driver/steps/testability.dart
@@ -0,0 +1,39 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:flutter_gherkin/flutter_gherkin.dart';
+import 'package:gherkin/gherkin.dart';
+
+
+class Testability {
+  StepContext<FlutterWorld> context;
+  Testability(this.context);
+
+  seedNotebookInBazaar(String notebookName) async {
+    http.Response response = await httpPost(
+        'http://localhost:9081/api/testability/seed_notes?external_identifier=old_learner',
+        [
+          {'title': notebookName}
+        ]);
+
+    context.expect((jsonDecode(response.body) as List).length, 1);
+
+    http.Response response1 = await httpPost(
+        'http://localhost:9081/api/testability/share_to_bazaar',
+        {'noteTitle': notebookName}
+    );
+    context.expect(response1.body, 'OK');
+  }
+
+  Future<http.Response> httpPost(
+      String uri, Object object) async {
+    final response = await http.post(Uri.parse(uri),
+        headers: {
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: jsonEncode(object));
+    context.expect(response.statusCode, 200);
+    return response;
+  }
+}
+
