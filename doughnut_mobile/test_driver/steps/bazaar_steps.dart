975f62876eb882fd344dce19d83c0304d645acb7
Terry Yin <terry.yinzhe@gmail.com>
2021-11-25 11:15:33 +0800

    test ci

--- doughnut_mobile/test_driver/steps/bazaar_steps.dart
+++ doughnut_mobile/test_driver/steps/bazaar_steps.dart
@@ -1,13 +1,28 @@
+import 'dart:convert';
+
 import 'package:flutter_driver/flutter_driver.dart';
 import 'package:flutter_gherkin/flutter_gherkin.dart';
 import 'package:gherkin/gherkin.dart';
+import 'package:http/http.dart' as http;
 
 List<StepDefinitionGeneric> bazaarSteps() {
   return [
 
     when1<String, FlutterWorld>(
       'there is a notebook {string} in the bazaar',
-      (notebookName, context) async {},
+      (notebookName, context) async {
+        final response = await http.post(Uri.parse('http://localhost:9081/api/testability/seed_bazaar_notebook'),
+          body: jsonEncode(<String, String>{
+            'notebookName': notebookName,
+          }),
+            );
+
+        if (response.statusCode == 200) {
+          context.expect(jsonDecode(response.body)['status'], 'OK');
+          return;
+        }
+        context.expect(response.statusCode, 200, reason: 'testability api call failed (${response.statusCode})\n${response.body}');
+      },
     ),
 
     given<FlutterWorld>(
