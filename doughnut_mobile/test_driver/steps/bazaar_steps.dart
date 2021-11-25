f200fe0660e2c778b11f1cd51eba363ab984d599
Terry Yin <terry.yinzhe@gmail.com>
2021-11-25 11:34:55 +0800

    seed note from flutter

--- doughnut_mobile/test_driver/steps/bazaar_steps.dart
+++ doughnut_mobile/test_driver/steps/bazaar_steps.dart
@@ -11,14 +11,15 @@ List<StepDefinitionGeneric> bazaarSteps() {
     when1<String, FlutterWorld>(
       'there is a notebook {string} in the bazaar',
       (notebookName, context) async {
-        final response = await http.post(Uri.parse('http://localhost:9081/api/testability/seed_bazaar_notebook'),
-          body: jsonEncode(<String, String>{
-            'notebookName': notebookName,
-          }),
+        final response = await http.post(Uri.parse('http://localhost:9081/api/testability/seed_notes?external_identifier=old_learner'),
+          headers: {
+            'Content-Type': 'application/json; charset=UTF-8',
+          },
+          body: jsonEncode([{'title': notebookName}]),
             );
 
         if (response.statusCode == 200) {
-          context.expect(jsonDecode(response.body)['status'], 'OK');
+          context.expect((jsonDecode(response.body) as List).length, 1);
           return;
         }
         context.expect(response.statusCode, 200, reason: 'testability api call failed (${response.statusCode})\n${response.body}');
