6911c8ee299bc667ea4a0fe1fbe7ceb4421864b0
Yeong Sheng, Tan <tanyeongsheng@gmail.com>
2021-11-30 18:31:07 +0800

    fix backend UT

--- backend/src/test/java/com/odde/doughnut/testability/DBMigrationTest.java
+++ backend/src/test/java/com/odde/doughnut/testability/DBMigrationTest.java
@@ -1,20 +1,19 @@
 package com.odde.doughnut.testability;
 
+import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.flywaydb.core.Flyway;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DBMigrationTest {
-	@Autowired
-	private Flyway flyway;
+  @Autowired private Flyway flyway;
 
-	@Test
-	@Tag("dbMigrate")
-	void migrate() {
-		flyway.migrate();
-	}
-
+  @Test
+  @Tag("dbMigrate")
+  void migrate() {
+    flyway.repair();
+    flyway.migrate();
+  }
 }
