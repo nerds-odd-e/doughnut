b954c4af388618321a817982caeb045f1806daea
Terry Yin <terry.yinzhe@gmail.com>
2021-12-02 13:36:54 +0800

    add level to ui

--- frontend/src/components/review/ReviewSettingForm.vue
+++ frontend/src/components/review/ReviewSettingForm.vue
@@ -1,6 +1,12 @@
 <template>
   <CheckInput
     scopeName="review_setting"
+    field="level"
+    v-model="modelValue.level"
+    :errors="errors.level"
+  />
+  <TextInput
+    scopeName="review_setting"
     field="rememberSpelling"
     v-model="modelValue.rememberSpelling"
     :errors="errors.rememberSpelling"
@@ -9,8 +15,9 @@
 
 <script>
 import CheckInput from "../form/CheckInput.vue";
+import TextInput from "../form/TextInput.vue";
 export default {
   props: { modelValue: Object, errors: Object },
-  components: { CheckInput },
+  components: { CheckInput, TextInput },
 };
 </script>
