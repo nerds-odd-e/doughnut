d433869dd6e8dc33a59f7e4d952101f10e873b29
Terry Yin <terry.yinzhe@gmail.com>
2022-01-19 14:15:46 +0800

    remove dead code

--- frontend/src/components/commons/Popups.vue
+++ frontend/src/components/commons/Popups.vue
@@ -1,52 +1,55 @@
 <template>
-  <Modal
-    v-if="popupInfo && popupInfo.type === 'alert'"
-    class="popups"
-    @close_request="resolve(true)"
-  >
-    <template v-slot:header>
-      <h2>Information</h2>
-    </template>
-    <template v-slot:body>
-      <div>
-        <span>{{ popupInfo.message }}</span>
-      </div>
-      <button class="btn btn-success" v-on:click="resolve(true)">OK</button>
-    </template>
-  </Modal>
+  <template v-if="popupInfo">
+    <Modal
+      v-if="popupInfo.type === 'alert'"
+      class="popups"
+      @close_request="resolve(true)"
+    >
+      <template v-slot:header>
+        <h2>Information</h2>
+      </template>
+      <template v-slot:body>
+        <div>
+          <span>{{ popupInfo.message }}</span>
+        </div>
+        <button class="btn btn-success" v-on:click="resolve(true)">OK</button>
+      </template>
+    </Modal>
 
-  <Modal
-    v-if="popupInfo && popupInfo.type === 'confirm'"
-    class="popups"
-    @close_request="resolve(false)"
-  >
-    <template v-slot:header>
-      <h2>Please confirm</h2>
-    </template>
-    <template v-slot:body>
-      <div>
-        <span>{{ popupInfo.message }}</span>
-      </div>
-      <button class="btn btn-success" v-on:click="resolve(true)">OK</button>
-      <button class="btn btn-secondary" v-on:click="resolve(false)">
-        Cancel
-      </button>
-    </template>
-  </Modal>
+    <Modal
+      v-if="popupInfo.type === 'confirm'"
+      class="popups"
+      @close_request="resolve(false)"
+    >
+      <template v-slot:header>
+        <h2>Please confirm</h2>
+      </template>
+      <template v-slot:body>
+        <div>
+          <span>{{ popupInfo.message }}</span>
+        </div>
+        <button class="btn btn-success" v-on:click="resolve(true)">OK</button>
+        <button class="btn btn-secondary" v-on:click="resolve(false)">
+          Cancel
+        </button>
+      </template>
+    </Modal>
 
-  <Modal
-    v-if="popupInfo && popupInfo.type === 'dialog'"
-    class="popups"
-    @close_request="resolve(null)"
-  >
-    <template v-slot:body>
-      <component
-        :is="popupInfo.component"
-        v-bind="popupInfo.attrs"
-        @done="resolve($event)"
-      />
-    </template>
-  </Modal>
+    <Modal
+      v-if="popupInfo.type === 'dialog'"
+      class="popups"
+      @close_request="resolve(null)"
+    >
+      <template v-slot:body>
+        <component
+          :is="popupInfo.component"
+          v-bind="popupInfo.attrs"
+          @done="resolve($event)"
+        />
+      </template>
+    </Modal>
+  </template>
+</template>
 </template>
 
 <script>
