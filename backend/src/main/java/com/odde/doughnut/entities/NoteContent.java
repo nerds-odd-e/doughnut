73fb94e22218f733baeb510a458f131c51c86429
Terry Yin <terry.yinzhe@gmail.com>
2022-01-22 18:11:55 +0800

    updatedAt

--- backend/src/main/java/com/odde/doughnut/entities/NoteContent.java
+++ backend/src/main/java/com/odde/doughnut/entities/NoteContent.java
@@ -111,6 +111,10 @@ public class NoteContent {
         this.textContent.setUpdatedAt(value);
     }
 
+    public Timestamp getUpdatedAt() {
+        return this.textContent.getUpdatedAt();
+    }
+
     public String getDescription() {
         return this.textContent.getDescription();
     }
