2e7360b3d02d667e6ce604108417d0c91cebf5bb
Terry Yin <terry.yinzhe@gmail.com>
2022-01-23 13:01:05 +0800

    semicolon

--- backend/src/main/resources/db/migration/V10000071__add_updated_at_back_in_note.sql
+++ backend/src/main/resources/db/migration/V10000071__add_updated_at_back_in_note.sql
@@ -1,2 +1,2 @@
-ALTER TABLE note ADD `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-update note set updated_at = created_at
+ALTER TABLE note ADD `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP;
+update note set updated_at = created_at;
