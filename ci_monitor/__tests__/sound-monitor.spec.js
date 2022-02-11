53c8e83ab0cec83804ce6fbeb6566fdecf594f59
Yeong Sheng, Tan <tanyeongsheng@gmail.com>
2022-02-11 16:23:44 +0800

    prepare to deprecate request for got node_module

--- ci_monitor/__tests__/sound-monitor.spec.js
+++ ci_monitor/__tests__/sound-monitor.spec.js
@@ -8,7 +8,7 @@ const {
 
 jest.mock("request");
 
-const html = ` 
+const html = `
 <div id="check_suite_2467107019">
         <div title="This workflow run completed successfully." class="d-flex flex-items-center flex-justify-center">
         <a class="Link--primary css-truncate css-truncate-target" aria-label="Run 976 of dough CI CD" href="/nerds-odd-e/doughnut/actions/runs/737396864">move code</a>
