c92e3f87692855d5f530cf18ee63ba768232d408
Yeong Sheng, Tan <tanyeongsheng@gmail.com>
2022-02-12 16:12:13 +0800

    doughnut-dev setup script

---
+++ setup-doughnut-dev.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+
+export NIXPKGS_ALLOW_UNFREE=1
+export unameOut="$(uname -s)"
+export os_type=Linux
+
+case "${unameOut}" in
+    Linux*)     os_type=Linux;;
+    Darwin*)    os_type=Mac;;
+    *)          os_type="UNKNOWN:${unameOut}"
+esac
+
+nix_installed_path=$(which nix 2>/dev/null)
+if [[ $nix_installed_path == *"not found"* ]]; then
+	if [ "${os_type}" = "Mac" ]; then
+		curl -k -L https://nixos.org/nix/install --darwin-use-unencrypted-nix-store-volume | sh
+	else
+		curl -k -L https://nixos.org/nix/install --no-daemon | sh
+	fi
+	mkdir -p ~/.config/nix
+	echo 'experimental-features = nix-command flakes' >> ~/.config/nix/nix.conf
+fi
+
+. ~/.nix-profile/etc/profile.d/nix.sh
+nix develop -c $SHELL
