2b4570dd58081cac03aaa858ba5fe6981d5063b9
janicetyp <janicetyp@gmail.com>
2022-02-22 12:20:55 +0800

    [Sharpie] add dependencies for websocket

---
+++ backend/src/main/java/com/odde/doughnut/models/MessageModel.java
@@ -0,0 +1,18 @@
+package com.odde.doughnut.models;
+
+public class MessageModel {
+
+  private String message;
+
+  public MessageModel(String message) {
+    this.message = message;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+
+  public void setMessage(String message) {
+    this.message = message;
+  }
+}
