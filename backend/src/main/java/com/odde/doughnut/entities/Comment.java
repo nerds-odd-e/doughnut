acf5c9dbc85a38ba348b90817c628e83e6e1d76f
menglu <song_menglu@mom.gov.sg>
2022-02-22 12:31:22 +0800

    [mint] add comment entity

---
+++ backend/src/main/java/com/odde/doughnut/entities/Comment.java
@@ -0,0 +1,57 @@
+package com.odde.doughnut.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "comment")
+
+public class Comment {
+
+    @Id
+    @Getter
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    @JsonIgnore
+    @Getter
+    @Setter
+    private User user;
+
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "note_id", referencedColumnName = "id")
+    @JsonIgnore
+    @Getter
+    @Setter
+    private Note note;
+
+    @Getter
+    @Setter
+    private String content;
+
+    @Column(name = "created_at")
+    @Getter
+    @Setter
+    private Timestamp createdAt;
+
+    @Column(name = "updated_at")
+    @Setter
+    @Getter
+    private Timestamp updatedAt;
+
+    @Column(name = "deleted_at")
+    @Setter
+    @Getter
+    private Timestamp deletedAt;
+
+    @Override
+    public String toString() {
+        return "Comment{" + "id=" + id + ", content='" + getContent() + '\'' + '}';
+    }
+}
