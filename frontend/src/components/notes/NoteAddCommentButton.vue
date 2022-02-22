62742a4573f989e3f6e44f6af29c45b0b1129590
janicetyp <janicetyp@gmail.com>
2022-02-22 16:16:25 +0800

    [yanchao/oleksii]Add comment button

---
+++ frontend/src/components/notes/NoteAddCommentButton.vue
@@ -0,0 +1,47 @@
+<script>
+import NoteStatistics from "./NoteCommentForm.vue";
+
+export default {
+  name: "NoteAddCommentButton",
+  props: { noteId: [String, Number], linkid: [String, Number] },
+  components: { NoteCommentForm },
+  data() {
+    return { noteAddCommentComponentName: null };
+  },
+  methods: {
+    toggleCommentForm() {
+      if (!this.noteAddCommentComponentName) {
+        this.noteAddCommentComponentName = "NoteCommentForm";
+      } else {
+        this.noteAddCommentComponentName = null;
+      }
+    },
+  },
+  watch: {
+    noteId() {
+      this.noteAddCommentComponentName = null;
+    },
+    linkid() {
+      this.noteAddCommentComStatisticsponentName = null;
+    },
+  },
+};
+</script>
+
+<template>
+  <a
+    @click="toggleCommentForm()"
+    class="btn btn-sm"
+    role="button"
+    title="statistics"
+  >
+    Comment
+  </a>
+  <div>toggleStatistics
+    <component
+      :is="noteAddCommentComponentName"
+      :noteId="noteId"
+      :linkid="linkid"
+    ></component>
+  </div>
+</template>
---
+++ frontend/src/components/notes/NoteCommentForm.vue
@@ -0,0 +1,32 @@
+<template>
+  <div v-if="noteCommentForm">
+    This is a future comment form
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const props = defineProps({ noteId: [String, Number], linkid: [String, Number] });
+/*const statistics = ref(null);
+const url = () => {
+  if (!!props.noteId) {
+    return `/api/notes/${props.noteId}/statistics`;
+  }
+  return `/api/links/${props.linkid}/statistics`;
+};
+const fetchData = async () => {
+  fetch(url())
+    .then((res) => {
+      return res.json();
+    })
+    .then((articles) => {
+      statistics.value = articles;
+    })
+    .catch((error) => {
+      window.alert(error);
+    });
+};
+
+fetchData(); */S
+</script>
