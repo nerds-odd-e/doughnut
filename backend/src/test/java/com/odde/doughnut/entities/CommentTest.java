e7bcd1640c7b9cf8e0a551831df296d66d50ce1c
menglu <song_menglu@mom.gov.sg>
2022-02-22 16:13:37 +0800

    [mint] update comment entity test

--- backend/src/test/java/com/odde/doughnut/entities/CommentTest.java
+++ backend/src/test/java/com/odde/doughnut/entities/CommentTest.java
@@ -9,8 +9,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.containsInRelativeOrder;
-import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.*;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(locations = {"classpath:repository.xml"})
@@ -19,14 +18,14 @@ public class CommentTest {
     @Autowired
     MakeMe makeMe;
 
-
-
     @Test
     void hasComment() {
         User user = makeMe.aUser().please();
         Note note = makeMe.aNote().please();
         Comment comment = makeMe.aComment(note, user).please();
-        
+
+        assertThat(comment.getId(), is(notNullValue()));
+        assertThat(comment.getUser().getId(), equalTo(user.getId()));
         assertThat(comment.getNote().getId(), equalTo(note.getId()));
     }
 }
