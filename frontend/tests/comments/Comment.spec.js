a92a5735efdb52d040980138358856b38a51d906
eliz <elizabeth_lim@mom.gov.sg>
2022-02-24 14:56:43 +0800

    [Krispy] Updated unit test to Comment Card

--- frontend/tests/comments/Comment.spec.js
+++ frontend/tests/comments/Comment.spec.js
@@ -2,18 +2,18 @@
  * @jest-environment jsdom
  */
  import CommentCard from "@/components/comment/Comment.vue";
- import { mount } from "@vue/test-utils";
+ import { screen, render } from "@testing-library/vue";
  
 describe('Testing Comment Card', () => {
   describe('should render comment card with the correct text in content prop', () =>{
     it('should display string in content prop', () => {
-      const wrapper = mount(CommentCard, { propsData: { content: "this is a comment" } });
-      expect(wrapper.text().includes('this is a comment')).toBe(true);
+      render(CommentCard, { props: { content: "this is a comment" } });
+      expect(screen.getByText('this is a comment'));
     })
 
     it('should not display string not in content prop', () => {
-      const wrapper = mount(CommentCard, { propsData: { content: "this is another comment" } });
-      expect(wrapper.text().includes('this is a comment')).toBe(false);
+      render(CommentCard, { props: { content: "this is another comment" } });
+      expect(screen.queryByText('this is a comment')).toBe(null);
     })
   })   
 })
