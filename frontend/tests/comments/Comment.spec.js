5792be2ebda757b056370b59fe1de685f0ddf9a8
zhang xiang <zhangxiang19940120@gmail.com>
2022-02-25 03:33:08 +0000

    [sharpie] refactor unit test for comment

--- frontend/tests/comments/Comment.spec.js
+++ frontend/tests/comments/Comment.spec.js
@@ -5,21 +5,28 @@
  import { screen } from "@testing-library/vue";
  import store from '../../src/store/index.js'
  import { renderWithStoreAndMockRoute } from "../helpers";
+
+ const _generateComment = (userId, content) => {
+   return {user: {id: userId}, content}
+ }
  
 describe('Testing Comment Card', () => {
   describe('should render comment card with the correct text in content prop', () =>{
+    const userId = 1
+    const content1 = 'this is a comment'
+
     beforeAll(() => {
-      store.state.currentUser = {id: 1}
+      store.state.currentUser = {id: userId}
     })
 
     it('should display string in content prop', () => {
-      renderWithStoreAndMockRoute(store, CommentCard, { props: { comment: {user: {id: 1}, content: "this is a comment"} } });
-      expect(screen.getByText('this is a comment'));
+      renderWithStoreAndMockRoute(store, CommentCard, { props: { comment: _generateComment(userId, content1) } });
+      expect(screen.getByText(content1));
     })
 
     it('should not display string not in content prop', () => {
-      renderWithStoreAndMockRoute(store, CommentCard, { props: { comment: {user: {id: 1}, content: "this is another comment" } } });
-      expect(screen.queryByText('this is a comment')).toBe(null);
+      renderWithStoreAndMockRoute(store, CommentCard, { props: { comment: _generateComment(userId, "this is another comment") } });
+      expect(screen.queryByText(content1)).toBe(null);
     })
   })   
 })
