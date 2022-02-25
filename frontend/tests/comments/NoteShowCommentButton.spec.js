7d3ec6b96184b9110816883cabd298016a2a90a5
zhang xiang <zhangxiang19940120@gmail.com>
2022-02-25 04:17:34 +0000

    [sharpie] refactor unit test for comment

--- frontend/tests/comments/NoteShowCommentButton.spec.js
+++ frontend/tests/comments/NoteShowCommentButton.spec.js
@@ -1,26 +1,32 @@
 /**
  * @jest-environment jsdom
  */
- import { screen } from "@testing-library/vue";
- import { renderWithStoreAndMockRoute } from "../helpers";
- import store from "../../src/store/index.js";
- import NoteShowCommentButton from "@/components/notes/NoteShowCommentButton.vue";
- 
+import { screen } from "@testing-library/vue";
+import { renderWithStoreAndMockRoute } from "../helpers";
+import store from "../../src/store/index.js";
+import NoteShowCommentButton from "@/components/notes/NoteShowCommentButton.vue";
+import makeMe from "../fixtures/makeMe.ts";
+
 describe('List of comment cards', () => {
-  describe('List of comments', () =>{
+  describe('List of comments', () => {
+    const userId = 1
+    const content1 = 'this is a comment'
+    const content2 = 'this is another comment'
+    const comment1 = makeMe.aComment.fromUser(userId).please()
+
     beforeAll(() => {
-      store.state.currentUser = {id: 1}
+      store.state.currentUser = { id: userId }
     })
 
     it('Display 1 comment', () => {
-      renderWithStoreAndMockRoute(store, NoteShowCommentButton, { props: { comments: [{user: {id: 1}, content:"this is a comment"}] } });
-      expect(screen.getByText('this is a comment'));
+      renderWithStoreAndMockRoute(store, NoteShowCommentButton, { props: { comments: [comment1] } });
+      expect(screen.getByText(content1));
     })
 
     it('Display 2 comments', () => {
-      renderWithStoreAndMockRoute(store, NoteShowCommentButton, { props: { comments: [{user: {id: 1}, content:"this is a comment"}, {user: {id: 1}, content:"this is another one"}] } });
-      expect(screen.getByText('this is a comment'));
-      expect(screen.getByText('this is another one'));
+      renderWithStoreAndMockRoute(store, NoteShowCommentButton, { props: { comments: [comment1, makeMe.aComment.fromUser(userId).content(content2).please()] } });
+      expect(screen.getByText(content1));
+      expect(screen.getByText(content2));
     })
   })
 })
