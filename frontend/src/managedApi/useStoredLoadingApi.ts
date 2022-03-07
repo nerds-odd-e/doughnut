ee8095f91a6c2ba7eddbcfd2c3f3525e93a0506d
Terry Yin <terry.yinzhe@gmail.com>
2022-03-07 13:14:48 +0800

    lint

--- frontend/src/managedApi/useStoredLoadingApi.ts
+++ frontend/src/managedApi/useStoredLoadingApi.ts
@@ -2,10 +2,10 @@ import useStore from "../store/pinia_store";
 import createStoredApi from  "./createStoredApi";
 import useLoadingApi from "./useLoadingApi";
 
-export default function(options={initalLoading: false, hasFormError: false}) {
+export default function(loadingOptions={initalLoading: false, hasFormError: false}) {
     const piniaStore = useStore();
     return {
-      ...useLoadingApi(options),
+      ...useLoadingApi(loadingOptions),
       piniaStore,
       storedApi(options={}) { return createStoredApi(this, options) }
     }
