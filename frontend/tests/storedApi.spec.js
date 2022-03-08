cb89f74cfb2004c730ffbd8733ca3ff521413b30
Yeong Sheng, Tan <tanyeongsheng@gmail.com>
2022-03-08 15:21:55 +0800

    standardize to use "it" instead of "test" block

--- frontend/tests/storedApi.spec.js
+++ frontend/tests/storedApi.spec.js
@@ -1,51 +1,53 @@
 /**
  * @jest-environment jsdom
  */
-import store from "./fixtures/testingStore.js";
-import makeMe from "./fixtures/makeMe";
-import useStoredLoadingApi from "../src/managedApi/useStoredLoadingApi";
+import store from './fixtures/testingStore.js';
+import makeMe from './fixtures/makeMe';
+import useStoredLoadingApi from '../src/managedApi/useStoredLoadingApi';
 
 beforeEach(() => {
   fetch.resetMocks();
 });
 
-describe("storedApiCollection", () => {
-  const note = makeMe.aNote.please()
+describe('storedApiCollection', () => {
+  const note = makeMe.aNote.please();
   const sa = useStoredLoadingApi().storedApi;
 
-  describe("delete note", () => {
-
+  describe('delete note', () => {
     beforeEach(() => {
       fetch.mockResponseOnce(JSON.stringify({}));
       store.loadNotes([note]);
     });
 
-    test("should call the api", async () => {
-      await sa.deleteNote(note.id)
+    it('should call the api', async () => {
+      await sa.deleteNote(note.id);
       expect(fetch).toHaveBeenCalledTimes(1);
-      expect(fetch).toHaveBeenCalledWith(`/api/notes/${note.id}/delete`, expect.anything());
+      expect(fetch).toHaveBeenCalledWith(
+        `/api/notes/${note.id}/delete`,
+        expect.anything()
+      );
     });
 
-    test("should change the store", async () => {
-      await sa.deleteNote(note.id)
-      expect(store.getNoteById(note.id)).toBeUndefined()
+    it('should change the store', async () => {
+      await sa.deleteNote(note.id);
+      expect(store.getNoteById(note.id)).toBeUndefined();
     });
 
-    test("should remove children notes", async () => {
-      const child = makeMe.aNote.under(note).please()
+    it('should remove children notes', async () => {
+      const child = makeMe.aNote.under(note).please();
       store.loadNotes([child]);
-      await sa.deleteNote(note.id)
-      expect(store.getNoteById(child.id)).toBeUndefined()
+      await sa.deleteNote(note.id);
+      expect(store.getNoteById(child.id)).toBeUndefined();
     });
 
-    test("should remove child from list", async () => {
-      const child = makeMe.aNote.under(note).please()
+    it('should remove child from list', async () => {
+      const child = makeMe.aNote.under(note).please();
       store.loadNotes([child]);
-      const childrenCount = store.getChildrenIdsByParentId(note.id).length
-      await sa.deleteNote(child.id)
-      expect(store.getChildrenIdsByParentId(note.id)).toHaveLength(childrenCount - 1)
+      const childrenCount = store.getChildrenIdsByParentId(note.id).length;
+      await sa.deleteNote(child.id);
+      expect(store.getChildrenIdsByParentId(note.id)).toHaveLength(
+        childrenCount - 1
+      );
     });
-
   });
 });
-
