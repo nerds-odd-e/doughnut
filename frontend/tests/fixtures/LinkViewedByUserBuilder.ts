07e13867283c3203ac54543d30d9ffe8939b32fc
Yeong Sheng, Tan <tanyeongsheng@gmail.com>
2022-03-09 15:59:29 +0800

    unused constructor

--- frontend/tests/fixtures/LinkViewedByUserBuilder.ts
+++ frontend/tests/fixtures/LinkViewedByUserBuilder.ts
@@ -1,19 +1,15 @@
 import Builder from "./Builder"
-import NoteBuilder from "./NoteBuilder" 
+import NoteBuilder from "./NoteBuilder"
 import NotePositionBuilder from "./NotePositionBuilder"
 
 class LinkViewedByUserBuilder extends Builder {
   data: any;
 
-  constructor(parentBuilder?: Builder) {
-    super(parentBuilder);
-  }
-
   do(): any {
     return {
       id: 8,
       linkTypeLabel: "a link",
-      sourceNoteWithPosition:  {
+      sourceNoteWithPosition: {
         note: new NoteBuilder().do(),
         notePosition: new NotePositionBuilder().do()
       },
