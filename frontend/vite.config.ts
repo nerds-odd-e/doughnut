7802a72ede4692b256d6a2af71006d0348cb7129
Yeong Sheng, Tan <tanyeongsheng@gmail.com>
2022-03-09 13:10:55 +0800

    fix vite build by removing unused config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,8 +3,6 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 export default defineConfig({
   css: {
@@ -32,13 +30,6 @@ export default defineConfig({
       ],
       dts: true, // generate TypeScript declaration
     }),
-    Components({
-      resolvers: [
-        ElementPlusResolver(),
-      ],
-      dts: true,
-      include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
-    })
   ],
   server: {
     proxy: {
