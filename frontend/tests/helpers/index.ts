237019f646b3663e9caa109fd2f9c4d4bce31945
Terry Yin <terry.yinzhe@gmail.com>
2022-03-09 17:43:41 +0800

    all ut warnings are gone

--- frontend/tests/helpers/index.ts
+++ frontend/tests/helpers/index.ts
@@ -1,52 +1,7 @@
-import { merge } from "lodash";
-import { mount } from "@vue/test-utils";
 import { createTestingPinia, TestingPinia } from "@pinia/testing";
 import { DefineComponent } from "vue";
 import createPiniaStore from '../../src/store/createPiniaStore';
 import RenderingHelper  from "./RenderingHelper";
-
-type Options = Record<string, unknown>;
-
-const withMockRoute = <T>(
-  comp: T,
-  options: Options = {},
-  currentRoute: any,
-  func: (comp: any, options: Options) => any
-) => {
-  const mockRouter = {
-    push: jest.fn(),
-  };
-
-  const wrapper = func(
-    comp,
-    merge(options, {
-      global: {
-        directives: {
-          focus() { }
-        },
-        mocks: {
-          $route: currentRoute,
-          $router: mockRouter,
-        },
-        stubs: {
-          "router-view": true,
-          "router-link": {
-            props: ["to"],
-            template: `<a class="router-link" :to='JSON.stringify(to)'><slot/></a>`,
-          },
-        },
-      },
-    })
-  );
-
-  return { wrapper, mockRouter };
-};
-
-const mountWithMockRoute = (
-  comp: any,
-  options: Options = {},
-  currentRoute: any
-) => withMockRoute(comp, options, currentRoute, mount);
 
 type PiniaStore = ReturnType<typeof createPiniaStore>
 
@@ -69,4 +24,4 @@ class StoredComponentTestHelper {
 
 }
 
-export { StoredComponentTestHelper, mountWithMockRoute };
+export { StoredComponentTestHelper };
--- frontend/tests/links/LinkNote.spec.ts
+++ frontend/tests/links/LinkNote.spec.ts
@@ -2,21 +2,22 @@
  * @jest-environment jsdom
  */
 import LinkNoteFinalize from '@/components/links/LinkNoteFinalize.vue';
-import { mount } from '@vue/test-utils';
-import { createTestingPinia } from '@pinia/testing';
 import makeMe from '../fixtures/makeMe';
+import { StoredComponentTestHelper } from '../helpers';
 
 describe('LinkNoteFinalize', () => {
-  createTestingPinia();
+  let helper: StoredComponentTestHelper;
+
+  beforeEach(() => {
+    helper = new StoredComponentTestHelper();
+  });
 
   it('going back', async () => {
     const note = makeMe.aNote.please();
-    const wrapper = mount(LinkNoteFinalize, {
-      propsData: {
+    const { wrapper } = helper.component(LinkNoteFinalize).withProps({
         note,
         targetNote: note,
-      },
-    });
+      }).mount()
     await wrapper.find('.go-back-button').trigger('click');
     expect(wrapper.emitted().goBack).toHaveLength(1);
   });
