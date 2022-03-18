970808f48846d5a8a30692909b91a23259b7f35d
Terry Yin <terry.yinzhe@gmail.com>
2022-03-18 10:42:57 +0800

    all lint warnings removed

--- frontend/src/managedApi/ManagedApi.ts
+++ frontend/src/managedApi/ManagedApi.ts
@@ -1,8 +1,9 @@
 import { Ref } from "vue";
 import Api from "./restful/Api";
+import { JsonData } from "./restful/RestfulFetch";
 
 interface ManagedComponent {
-  formErrors: Ref<any> | undefined;
+  formErrors: Ref<JsonData> | undefined;
   loading: Ref<boolean>;
 }
 
@@ -41,15 +42,15 @@ class ManagedApi {
 
   restGet(url: string) { return this.around(this.api.restGet(url)); }
 
-  restPost(url: string, data: any) { return this.around(this.api.restPost(url, data)); }
+  restPost(url: string, data: JsonData) { return this.around(this.api.restPost(url, data)); }
 
-  restPatch(url: string, data: any) { return this.around(this.api.restPatch(url, data)); }
+  restPatch(url: string, data: JsonData) { return this.around(this.api.restPatch(url, data)); }
 
-  restPostMultiplePartForm(url: string, data: any) { return this.around(this.api.restPostMultiplePartForm(url, data)); }
+  restPostMultiplePartForm(url: string, data: JsonData) { return this.around(this.api.restPostMultiplePartForm(url, data)); }
 
-  restPatchMultiplePartForm(url: string, data: any) { return this.around(this.api.restPatchMultiplePartForm(url, data)); }
+  restPatchMultiplePartForm(url: string, data: JsonData) { return this.around(this.api.restPatchMultiplePartForm(url, data)); }
 
-  restPostWithHtmlResponse(url: string, data: any) { return this.around(this.api.restPostWithHtmlResponse(url, data)); }
+  restPostWithHtmlResponse(url: string, data: JsonData) { return this.around(this.api.restPostWithHtmlResponse(url, data)); }
 }
 
 export default ManagedApi;
