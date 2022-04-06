48ceeb053f6ce32ddb65119bd6e4d1ac08272c16
Yeong Sheng, Tan <tanyeongsheng@gmail.com>
2022-04-06 21:25:04 +0800

    more robust doughnut dev nix tooling setup

--- setup-doughnut-dev.sh
+++ setup-doughnut-dev.sh
@@ -7,54 +7,55 @@ export unameOut="$(uname -s)"
 export os_type=Unsupported
 
 get_os_type() {
-    case "${unameOut}" in
-      Linux*) os_type=Linux ;;
-      Darwin*) os_type=Mac ;;
-      *) os_type="UNKNOWN:${unameOut}" ;;
-    esac
+  case "${unameOut}" in
+    Linux*) os_type=Linux ;;
+    Darwin*) os_type=Mac ;;
+    *) os_type="UNKNOWN:${unameOut}" ;;
+  esac
 }
 
 download_nixpkg_manager_install_script() {
-    rm -f install-nix
-    curl -o install-nix https://releases.nixos.org/nix/nix-2.7.0/install
-    chmod +x ./install-nix
+  rm -f install-nix
+  curl -o install-nix https://releases.nixos.org/nix/nix-2.7.0/install
+  chmod +x ./install-nix
 }
 
 configure_nix_flakes() {
-    if [[ ! -f ${HOME}/.config/nix/nix/nix.conf ]]; then
-      mkdir -p ${HOME}/.config/nix
-      touch ${HOME}/.config/nix/nix.conf
-    fi
+  if [ ! -f ${HOME}/.config/nix/nix/nix.conf ]; then
+    mkdir -p ${HOME}/.config/nix
+    touch ${HOME}/.config/nix/nix.conf
+  fi
 
-    if ! grep -Fxq "experimental-features = nix-command flakes" ${HOME}/.config/nix/nix.conf; then
-      echo 'experimental-features = nix-command flakes' >> ${HOME}/.config/nix/nix.conf
-    fi
-    nixpkg_script_activate
+  if ! grep -Fxq "experimental-features = nix-command flakes" ${HOME}/.config/nix/nix.conf; then
+    echo 'experimental-features = nix-command flakes' >> ${HOME}/.config/nix/nix.conf
+  fi
+  nixpkg_script_activate
 }
 
 nixpkg_script_activate() {
-    if [[ ! -f ${HOME}/.nix-profile/etc/profile.d/nix.sh ]]; then
-      user=$(whoami)
-      ln -sf /nix/var/nix/profiles/per-user/${user}/profile ${HOME}/.nix-profile
-    fi
-    . ${HOME}/nix-profile/etc/profile.d/nix.sh
+  if [ ! -f ${HOME}/.nix-profile/etc/profile.d/nix.sh ]; then
+    user=$(whoami)
+    ln -sf /nix/var/nix/profiles/per-user/${user}/profile ${HOME}/.nix-profile
+  fi
+  
+  [ -f ${HOME}/.nix-profile/etc/profile.d/nix.sh ] &&  . ${HOME}/nix-profile/etc/profile.d/nix.sh
 }
 
 install_nixpkg_manager() {
-    get_os_type
-    if ! command -v nix >/dev/null 2>&1; then
-      download_nixpkg_manager_install_script
-      if [ "${os_type}" = "Mac" ]; then
-        ./install-nix --darwin-use-unencrypted-nix-store-volume
-      elif [ "${os_type}" = "Linux" ]; then
-        ./install-nix --no-daemon
-      else
-        echo "Unsupported OS Platform for Nix development enviroment. Exiting!!!"
-        exit 1
-      fi
-      rm -f ./install-nix
+  get_os_type
+  if ! command -v nix >/dev/null 2>&1; then
+    download_nixpkg_manager_install_script
+    if [ "${os_type}" = "Mac" ]; then
+      ./install-nix --darwin-use-unencrypted-nix-store-volume
+    elif [ "${os_type}" = "Linux" ]; then
+      ./install-nix --no-daemon
+    else
+      echo "Unsupported OS Platform for Nix development enviroment. Exiting!!!"
+      exit 1
     fi
-    configure_nix_flakes
+    rm -f ./install-nix
+  fi
+  configure_nix_flakes
 }
 
 install_nixpkg_manager
