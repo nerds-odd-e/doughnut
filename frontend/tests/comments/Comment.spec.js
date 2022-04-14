05265d3759d2ec2cb51f183d9014fb05b117ee89
kodofish <kodofish@gmail.com>
2022-04-14 16:17:02 +0800

    Test: Implement `delete comment` unit test in frontend

--- frontend/tests/comments/Comment.spec.js
+++ frontend/tests/comments/Comment.spec.js
@@ -2,18 +2,16 @@
  * @jest-environment jsdom
  */
 import helper from '../helpers';
+import NoteCardsView from '@/components/notes/views/NoteCardsView.vue';
+import makeMe from "../fixtures/makeMe";
 
 helper.resetWithApiMock(beforeEach, afterEach)
 
 describe('delete comment', () => {
   it('should be call once', async () => {
-    // const noteRealm = makeMe.aNoteRealm.please();
-    // const stubResponse = {
-    //   notePosition: makeMe.aNotePosition.please(),
-    //   notes: [noteRealm],
-    // };
-    //
-    // helper.apiMock.expecting(`/api/notes/${noteRealm.id}`, stubResponse);
-    // helper.component(NoteCardsView).withProps({noteId: noteRealm.id}).render()
+    const noteRealm = makeMe.aNoteRealm.please();
+    const wrapper = helper.component(NoteCardsView).withProps(
+      {noteId: noteRealm.id}).mount()
+    // await wrapper.find('comment-1-delete').trigger('click');
   });
 });
