d9f8a0e264c88048f764cc821c8fbb0d27c52d10
Terry Yin <terry.yinzhe@gmail.com>
2022-04-20 12:17:14 +0800

    hard code frontend api call

--- frontend/src/components/notes/CommentCreateDialog.vue
+++ frontend/src/components/notes/CommentCreateDialog.vue
@@ -1,16 +1,25 @@
 <template>
   <TextInput field="comment" v-focus/>
-  <button>Submit</button>
+  <button @click="createComment">Submit</button>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import TextInput from "../form/TextInput.vue";
+import useLoadingApi from "../../managedApi/useLoadingApi";
 
 export default defineComponent({
+  setup() {
+    return useLoadingApi();
+  },
   components: {
     TextInput,
   },
   emits: ["done"],
+  methods: {
+    createComment() {
+      this.managedApi.restPost("/api/notes/123/createComment", {});
+    }
+  }
 });
 </script>
--- frontend/tests/notes/CommentCreateDialog.spec.ts
+++ frontend/tests/notes/CommentCreateDialog.spec.ts
@@ -5,9 +5,12 @@ import { screen } from '@testing-library/vue';
 import CommentCreateDialog from '@/components/notes/CommentCreateDialog.vue';
 import helper from '../helpers';
 
+helper.resetWithApiMock(beforeEach, afterEach)
+
 describe('create a comment', () => {
   it('renders a dialog for comment with submit button', async () => {
-    helper.component(CommentCreateDialog).render()
-    await screen.findByText('Submit');
+    helper.component(CommentCreateDialog).render();
+    (await screen.findByText('Submit')).click();
+    helper.apiMock.verifyCall("/api/notes/123/createComment");
   });
 });
