3a68ef4ecf5cffb30b5c32d880808b837983737a
Terry Yin <terry.yinzhe@gmail.com>
2022-04-30 12:52:18 +0800

    and to or

--- backend/src/main/resources/db/migration/V10000083__change_review_point_to_use_things.sql
+++ backend/src/main/resources/db/migration/V10000083__change_review_point_to_use_things.sql
@@ -1,3 +1,3 @@
 UPDATE review_point
-INNER JOIN thing ON thing.note_id = review_point.note_id AND thing.link_id = review_point.link_id
+INNER JOIN thing ON thing.note_id = review_point.note_id OR thing.link_id = review_point.link_id
 SET thing_id = thing.id;
