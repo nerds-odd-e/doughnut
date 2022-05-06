118d236f0430216d99d2485256d266ffd9b7de66
Terry Yin <terry.yinzhe@gmail.com>
2022-05-06 17:08:13 +0800

    extract static html out

--- frontend/src/components/notes/mindmap/NoteMindmap.vue
+++ frontend/src/components/notes/mindmap/NoteMindmap.vue
@@ -1,39 +1,12 @@
 <template>
   <svg class="mindmap-canvas">
-    <defs>
-      <radialGradient id="myGradient">
-        <stop offset="10%" stop-color="white" />
-        <stop offset="30%" stop-color="#87CEEB" />
-        <stop offset="100%" stop-color="azure" />
-      </radialGradient>
-    </defs>
+    <MindmapSvgDefs />
     <circle
       :cx="-offset.x / 8"
       :cy="-offset.y / 8"
       :r="3000 * offset.scale"
       fill="url('#myGradient')"
     />
-    <marker
-      id="arrowhead"
-      markerWidth="8"
-      markerHeight="6"
-      refX="8"
-      refY="3"
-      orient="auto"
-    >
-      <polygon points="0 0, 8 3, 0 6" style="stroke-width: 0" />
-    </marker>
-    <marker
-      id="treearrow"
-      class="parent-child"
-      markerWidth="4"
-      markerHeight="3"
-      refX="3"
-      refY="1.5"
-      orient="auto"
-    >
-      <polygon points="0 0, 4 1.5, 0 3" style="stroke-width: 0" />
-    </marker>
 
     <NoteMindmapScaffold v-bind="{ noteId, mindmapSector }">
       <template #default="{ note, links, mindmapSector }">
@@ -75,6 +48,7 @@ import { defineComponent, PropType } from "vue";
 import NoteMindmapScaffold from "./NoteMindmapScaffold.vue";
 import NoteCard from "./NoteCard.vue";
 import NoteParentChildConnection from "./NoteParentChildConnection.vue";
+import MindmapSvgDefs from "./MindmapSvgDefs.vue";
 import NoteLinks from "./NoteLinks.vue";
 import useStoredLoadingApi from "../../../managedApi/useStoredLoadingApi";
 import MindmapSector from "../../../models/MindmapSector";
@@ -94,12 +68,16 @@ export default defineComponent({
   },
   components: {
     NoteMindmapScaffold,
+    MindmapSvgDefs,
     NoteCard,
     NoteParentChildConnection,
     NoteLinks,
   },
   emits: ["selectNote"],
   computed: {
+    noteRealms() {
+      return this.piniaStore.noteRealms;
+    },
     mindmapSector() {
       return new MindmapSector(
         0,
