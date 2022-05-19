a653036beaaf853335babf553dd64955a7022888
Yeong Sheng <yeongsheng@odd-e.com>
2022-05-19 17:01:04 +0800

    fix cypress lint

--- cypress/step_definitions/mountebank_sample.ts
+++ cypress/step_definitions/mountebank_sample.ts
@@ -4,18 +4,12 @@
 // @ts-check
 
 import { Given, Then, When } from "@badeball/cypress-cucumber-preprocessor"
-import {
-  HttpMethod,
-  Imposter,
-  Mountebank,
-  DefaultStub,
-} from '@anev/ts-mountebank';
+import { HttpMethod, Imposter, Mountebank, DefaultStub } from '@anev/ts-mountebank'
 
 Given("I have a record on {string} on the external service", async (record) => {
-  const mb = new Mountebank();
-  let imposter = new Imposter().withPort(5000).withStub(
-      new DefaultStub(`/external/${record}`, HttpMethod.GET, "foo", 200));
-  await mb.createImposter(imposter);
+  const mb = new Mountebank()
+  const imposter = new Imposter().withPort(5000).withStub(new DefaultStub(`/external/${record}`, HttpMethod.GET, "foo", 200))
+  await mb.createImposter(imposter)
 })
 
 When("I ask for the {string} record", (record) => {
