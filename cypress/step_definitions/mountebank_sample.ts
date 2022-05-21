0d0dce7e1f9665e3d5bd16003c7c481671bb9db9
Yeong Sheng, Tan <tanyeongsheng@gmail.com>
2022-05-21 22:19:49 +0800

    start mb in CI

--- cypress/step_definitions/mountebank_sample.ts
+++ cypress/step_definitions/mountebank_sample.ts
@@ -13,10 +13,7 @@ Given("I have a record on {string} on the external service", async (record) => {
 })
 
 When("I ask for the {string} record", (record) => {
-  const args = { record }
-  cy.origin("localhost:5000", { args }, ({ record }) => {
-    cy.request(`/external/${record}`).as("resp")
-  })
+  cy.request(`http://localhost:5000/external/${record}`).as("resp")
 })
 
 Then("I should get the expected payload", (record) => {
