164c8a6a916d2b64fe73daaf5a91d5bd0e53df29
Terry Yin <terry.yinzhe@gmail.com>
2022-05-22 14:35:50 +0800

    hard code

--- frontend/src/pages/NoteShowPage.vue
+++ frontend/src/pages/NoteShowPage.vue
@@ -1,26 +1,16 @@
 <template>
   <NoteRealmAsync
-    v-bind="{
-      noteId,
-      viewType: sanitizedViewType,
-      expandChildren: true,
-    }"
+    v-bind="{ noteId, viewType: 'cards', expandChildren: true }"
     :key="noteId"
   />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import { sanitizeViewTypeName } from "../models/viewTypes";
 import NoteRealmAsync from "../components/notes/NoteRealmAsync.vue";
 
 export default defineComponent({
-  props: { noteId: { type: Number, required: true }, viewType: String },
+  props: { noteId: { type: Number, required: true } },
   components: { NoteRealmAsync },
-  computed: {
-    sanitizedViewType() {
-      return sanitizeViewTypeName(this.viewType);
-    },
-  },
 });
 </script>
