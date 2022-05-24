b68a7746eb2bb440dc076efc9a8f2e19885544ca
FT <tan_fang_tian@mom.gov.sg>
2022-05-24 17:40:10 +0800

    fix lint

--- cypress/step_definitions/wikidata.ts
+++ cypress/step_definitions/wikidata.ts
@@ -9,23 +9,24 @@ Given("there is a wikidata record {string}", (term) => {
   cy.log(term)
 })
 
-When("I fetch for {string} from Wikidata",(term) => {
-  cy.request(`/api/wikidata/${term}`).then(resp => {
+When("I fetch for {string} from Wikidata", (term) => {
+  cy.request(`/api/wikidata/${term}`).then((resp) => {
     cy.wrap(resp.body).as("wikidata_payload")
   })
 })
 
 Then("I can get a payload {string}", (expectedPayload) => {
-  cy.get("@wikidata_payload").then(payload => {
+  cy.get("@wikidata_payload").then((payload) => {
     expect(payload).to.equal(expectedPayload)
   })
 })
 
 When("I associate {string} with wikidata id {string}", (noteTitle, wikiDataId) => {
   // UI steps
+  cy.log(noteTitle)
+  cy.log(wikiDataId)
 })
 
 Then("I should see association confirmation message {string}", (message) => {
   cy.log(message)
 })
-
