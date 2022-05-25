631a9f61417c03981d99d9431af05c36bd65e609
Yeong Sheng, Tan <tanyeongsheng@gmail.com>
2022-05-25 10:43:13 +0800

    revert backend:lint

--- backend/src/test/java/com/odde/doughnut/controllers/WikiDataControllerTests.java
+++ backend/src/test/java/com/odde/doughnut/controllers/WikiDataControllerTests.java
@@ -4,10 +4,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
 import com.odde.doughnut.models.WikiDataModel;
-
 import java.io.IOException;
-import org.junit.jupiter.api.BeforeEach;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
