a989a3c2eb9657a552e2efa0dfbda3d5570d46ea
kim <kimhung.ho@titansoft.com.sg>
2022-05-25 13:41:58 +0800

    fix lint issues

--- cypress/step_definitions/wikidata.ts
+++ cypress/step_definitions/wikidata.ts
@@ -7,14 +7,16 @@ import { And, Given, Then, When } from "@badeball/cypress-cucumber-preprocessor"
 
 When("I associate {string} with wikidata id {string}", (noteTitle, wikiDataId) => {
   // UI steps
+  return;
 })
 
 Then("I should see association confirmation message {string}", (message) => {
   // TODO
+  return;
 })
 
 When("I confirm the association with different title", () => {
-
+return;
 })
 
 Then("I should see association icon for note title {string}", (noteTitle) => {
