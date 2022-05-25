87fe62c092d643d846b259d339533b5b2a7eb4aa
kim <kimhung.ho@titansoft.com.sg>
2022-05-25 16:36:29 +0800

    apply lint

--- backend/src/test/java/com/odde/doughnut/controllers/WikiDataControllerTests.java
+++ backend/src/test/java/com/odde/doughnut/controllers/WikiDataControllerTests.java
@@ -9,10 +9,8 @@ import com.odde.doughnut.models.LanguageValueModel;
 import com.odde.doughnut.models.WikiDataInfo;
 import com.odde.doughnut.models.WikiDataModel;
 import com.odde.doughnut.services.WikiDataService;
-
 import java.io.IOException;
 import java.util.Map;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.platform.commons.util.StringUtils;
@@ -23,8 +21,7 @@ import org.mockito.MockitoAnnotations;
 class WikiDataControllerTests {
 
   RestWikidataController controller;
-  @Mock
-  WikiDataService wikiDataService;
+  @Mock WikiDataService wikiDataService;
   private final String searchId = "Q1234";
   private final String searchIdWithWikipediaLink = "Q200";
   private final String noteTitle = "TDD";
@@ -42,68 +39,79 @@ class WikiDataControllerTests {
     return new WikiDataModel() {
       {
         entities =
-          Map.ofEntries(
-            Map.entry(searchId,
-              new WikiDataInfo() {
-                {
-                  title = noteTitle;
-                  labels =
-                    Map.ofEntries(
-                      Map.entry(
-                        englishLanguage,
-                        new LanguageValueModel() {
-                          {
-                            language = englishLanguage;
-                            value = noteTitle;
-                          }
-                        }));
-                }
-              }),
-            Map.entry(searchIdWithWikipediaLink,
-              new WikiDataInfo() {{
-                title = "random";
-                labels = Map.ofEntries(
-                  Map.entry(englishLanguage, new LanguageValueModel() {{
-                    language = englishLanguage;
-                    value = "random";
-                  }})
-                );
-                sitelinks = Map.ofEntries(
-                  Map.entry(englishLanguage, new LanguageValueModel() {{
-                    language = englishLanguage;
-                    value = "SomeEnglishLink";
-                  }})
-                );
-              }}));
+            Map.ofEntries(
+                Map.entry(
+                    searchId,
+                    new WikiDataInfo() {
+                      {
+                        title = noteTitle;
+                        labels =
+                            Map.ofEntries(
+                                Map.entry(
+                                    englishLanguage,
+                                    new LanguageValueModel() {
+                                      {
+                                        language = englishLanguage;
+                                        value = noteTitle;
+                                      }
+                                    }));
+                      }
+                    }),
+                Map.entry(
+                    searchIdWithWikipediaLink,
+                    new WikiDataInfo() {
+                      {
+                        title = "random";
+                        labels =
+                            Map.ofEntries(
+                                Map.entry(
+                                    englishLanguage,
+                                    new LanguageValueModel() {
+                                      {
+                                        language = englishLanguage;
+                                        value = "random";
+                                      }
+                                    }));
+                        sitelinks =
+                            Map.ofEntries(
+                                Map.entry(
+                                    englishLanguage,
+                                    new LanguageValueModel() {
+                                      {
+                                        language = englishLanguage;
+                                        value = "SomeEnglishLink";
+                                      }
+                                    }));
+                      }
+                    }));
       }
     };
   }
 
   @Test
   void GivenSearchIdHasValue_ShouldBeAbleToGetWikiDataTitleAndId()
-    throws IOException, InterruptedException {
+      throws IOException, InterruptedException {
     WikiDataDto resultObj = controller.fetchWikiDataDto(searchId);
     assertThat(resultObj.WikiDataId, equalTo(searchId));
     assertThat(resultObj.WikiDataTitleInEnglish, equalTo(noteTitle));
   }
 
   @Test
-  void GivenSearchIdHasNoValue_ShouldReturnEmpty()
-    throws IOException, InterruptedException {
+  void GivenSearchIdHasNoValue_ShouldReturnEmpty() throws IOException, InterruptedException {
     WikiDataDto resultObj = controller.fetchWikiDataDto("Q1");
     assertThat(resultObj, samePropertyValuesAs(new WikiDataDto()));
   }
 
   @Test
   void GivenSearchIdHasWikipediaEnglishLink_ShouldBeAbleToRetrieveLink()
-    throws IOException, InterruptedException {
+      throws IOException, InterruptedException {
     WikiDataDto resultObj = controller.fetchWikiDataDto(searchIdWithWikipediaLink);
     assertThat(resultObj.WikipediaEnglishLink, equalTo("SomeEnglishLink"));
   }
 
   @Test
   void GivenSearchIdHasNoWikipediaEnglishLink_ShouldReturnEmptyLink()
-    throws IOException, InterruptedException {
+      throws IOException, InterruptedException {
     WikiDataDto resultObj = controller.fetchWikiDataDto(searchId);
     assertThat(StringUtils.isBlank(resultObj.WikipediaEnglishLink), is(true));
   }
