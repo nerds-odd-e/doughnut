34eb954a0e52aa98b5b698553fe8007bf26caf5b
kim <kimhung.ho@titansoft.com.sg>
2022-05-25 14:21:21 +0800

    fix lint

--- backend/src/main/java/com/odde/doughnut/services/WikiDataService.java
+++ backend/src/main/java/com/odde/doughnut/services/WikiDataService.java
@@ -4,14 +4,13 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.odde.doughnut.models.WikiDataModel;
-import org.springframework.stereotype.Component;
-
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
+import org.springframework.stereotype.Component;
 
 @Component
 public class WikiDataService {
@@ -19,17 +18,16 @@ public class WikiDataService {
     HttpResponse<String> response = CallWikiDataApi(wikiDataId);
     ObjectMapper mapper = new ObjectMapper();
     mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-    return mapper.readValue(response.body(), new TypeReference<>() {
-    });
+    return mapper.readValue(response.body(), new TypeReference<>() {});
   }
 
   private HttpResponse<String> CallWikiDataApi(String wikiDataId)
-    throws IOException, InterruptedException {
+      throws IOException, InterruptedException {
     String url = ConstructWikiDataUrl(wikiDataId);
     final HttpRequest.Builder builder = HttpRequest.newBuilder(URI.create(url));
     HttpRequest request = builder.build();
     HttpResponse.BodyHandler<String> bodyHandler =
-      HttpResponse.BodyHandlers.ofString(StandardCharsets.UTF_8);
+        HttpResponse.BodyHandlers.ofString(StandardCharsets.UTF_8);
     return HttpClient.newBuilder().build().send(request, bodyHandler);
   }
 
