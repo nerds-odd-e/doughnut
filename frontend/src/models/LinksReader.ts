c530b7e76beae78e2d0e790864b1b2a6154d5f76
Terry Yin <terry.yinzhe@gmail.com>
2022-06-05 16:42:08 +0800

    fix warning

--- frontend/src/models/LinksReader.ts
+++ frontend/src/models/LinksReader.ts
@@ -8,34 +8,32 @@ class LinksReader {
     this.links = links;
   }
 
+  private filterTypes(cond: (t: Generated.LinkType) => boolean) {
+    return Object.fromEntries(
+      Object.entries(this.links).filter((t) => cond(t[0] as Generated.LinkType))
+    );
+  }
+
   get hierachyLinks() {
     const tTypes = taggingTypes;
     const gTypes = groupedTypes;
-    return Object.fromEntries(
-      Object.entries(this.links).filter(
-        (t) => !tTypes.includes(t[0]) && !gTypes.includes(t[0])
-      )
-    );
+    return this.filterTypes((t) => !tTypes.includes(t) && !gTypes.includes(t));
   }
 
   get childrenLinks() {
     const gTypes = groupedTypes;
-    return Object.fromEntries(
-      Object.entries(this.links).filter((t) => !gTypes.includes(t[0]))
-    );
+    return this.filterTypes((t) => !gTypes.includes(t));
   }
 
   get tagLinks() {
     const tTypes = taggingTypes;
-    return Object.fromEntries(
-      Object.entries(this.links).filter((t) => tTypes.includes(t[0]))
-    );
+    return this.filterTypes((t) => tTypes.includes(t));
   }
 
   get groupedLinks() {
     const tTypes = groupedTypes;
     return Object.entries(this.links)
-      .filter((t) => tTypes.includes(t[0]))
+      .filter((t) => tTypes.includes(t[0] as Generated.LinkType))
       .map((t) => t[1]);
   }
 
