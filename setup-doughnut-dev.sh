37874fc7ef652bb74504cdf0cc23f0ad3dd646aa
Yeong Sheng, Tan <tanyeongsheng@gmail.com>
2022-06-10 21:04:35 +0800

    remove irrelevant doughnut nix install message

--- setup-doughnut-dev.sh
+++ setup-doughnut-dev.sh
@@ -36,8 +36,6 @@ ensure_nix_profile() {
     user=$(whoami)
     ln -sf /nix/var/nix/profiles/per-user/${user}/profile ${HOME}/.nix-profile
   fi
-
-  [ -f ${HOME}/.nix-profile/etc/profile.d/nix.sh ] && echo "Please start a new shell & execute '. ~/nix-profile/etc/profile.d/nix.sh'"
 }
 
 install_nixpkg_manager() {
