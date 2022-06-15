0b7fd9b290aff86c3f228f89f7837851c791796d
rangga <dwi.rangga@tech.jago.com>
2022-06-15 16:10:32 +0700

    format lint

--- backend/src/test/java/com/odde/doughnut/controllers/RestWikiDataControllerTests.java
+++ backend/src/test/java/com/odde/doughnut/controllers/RestWikiDataControllerTests.java
@@ -11,7 +11,6 @@ import com.odde.doughnut.services.HttpClientAdapter;
 import com.odde.doughnut.testability.TestabilitySettings;
 import java.io.IOException;
 import java.util.ArrayList;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -24,8 +23,7 @@ import org.springframework.validation.BindException;
 class RestWikiDataControllerTests {
 
   RestWikidataController controller;
-  @Mock
-  HttpClientAdapter httpClientAdapter;
+  @Mock HttpClientAdapter httpClientAdapter;
 
   TestabilitySettings testabilitySettings = new TestabilitySettings();
 
@@ -49,25 +47,32 @@ class RestWikiDataControllerTests {
     }
 
     private String getEntityDataJsonSearch(String search) {
-      return "{\"searchinfo\":{\"search\":\"" + search
+      return "{\"searchinfo\":{\"search\":\""
+          + search
           + "\"},\"search\":[{\"id\":\"Q64\",\"title\":\"Q64\",\"pageid\":190,\"display\""
-          + ":{\"label\":{\"value\":\"" + search
+          + ":{\"label\":{\"value\":\""
+          + search
           + "\",\"language\":\"en\"},\"description\":{\"value\":\"federal state, capital and largest city of Germany\""
           + ",\"language\":\"en\"}},\"repository\":\"wikidata\",\"url\":\"//www.wikidata.org/wiki/Q64\",\"concepturi\":"
-          + "\"http://www.wikidata.org/entity/Q64\",\"label\":\"" + search
+          + "\"http://www.wikidata.org/entity/Q64\",\"label\":\""
+          + search
           + "\",\"description\":\"federal state, capital and largest city of Germany\""
-          + ",\"match\":{\"type\":\"label\",\"language\":\"en\",\"text\":\"" + search
+          + ",\"match\":{\"type\":\"label\",\"language\":\"en\",\"text\":\""
+          + search
           + "\"}}],\"search-continue\":10,\"success\":1}";
     }
 
     private String getEntityDataJsonSearchEmpty(String search) {
-      return "{\"searchinfo\":{\"search\":\"" + search + "\"},\"search\":[],\"search-continue\":10,\"success\":1}";
+      return "{\"searchinfo\":{\"search\":\""
+          + search
+          + "\"},\"search\":[],\"search-continue\":10,\"success\":1}";
     }
 
     @Test
     void serviceNotAvailable() throws IOException, InterruptedException {
       Mockito.when(httpClientAdapter.getResponseString(any())).thenThrow(new IOException());
-      BindException exception = assertThrows(BindException.class, () -> controller.fetchWikiDataByID("Q1"));
+      BindException exception =
+          assertThrows(BindException.class, () -> controller.fetchWikiDataByID("Q1"));
       assertThat(exception.getErrorCount(), equalTo(1));
     }
 
@@ -114,23 +119,22 @@ class RestWikiDataControllerTests {
     @Test
     void serviceNotAvailableAtSearchWikidata() throws IOException, InterruptedException {
       Mockito.when(httpClientAdapter.getResponseString(any())).thenThrow(new IOException());
-      BindException exception = assertThrows(BindException.class, () -> controller.fetchsWikiDataBySearch("berlin"));
+      BindException exception =
+          assertThrows(BindException.class, () -> controller.fetchsWikiDataBySearch("berlin"));
       assertThat(exception.getErrorCount(), equalTo(1));
     }
 
     @Test
-    void shouldFetchDataAtSearchWikidata() throws IOException,
-        InterruptedException, BindException {
+    void shouldFetchDataAtSearchWikidata() throws IOException, InterruptedException, BindException {
       Mockito.when(httpClientAdapter.getResponseString(any()))
           .thenReturn(getEntityDataJsonSearch("berlin"));
       controller.fetchsWikiDataBySearch("berlin");
-      Mockito.verify(httpClientAdapter)
-          .getResponseString(any());
+      Mockito.verify(httpClientAdapter).getResponseString(any());
     }
 
     @Test
-    void shouldParseAndGetAtSearchWikidata() throws IOException, InterruptedException,
-        BindException {
+    void shouldParseAndGetAtSearchWikidata()
+        throws IOException, InterruptedException, BindException {
       Mockito.when(httpClientAdapter.getResponseString(any()))
           .thenReturn(getEntityDataJsonSearch("berlin"));
       ArrayList<WikidataSearchEntity> result = controller.fetchsWikiDataBySearch("berlin");
