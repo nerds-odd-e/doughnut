13a2fc704f97614c757a220522668763bdc70708
Terry Yin <terry.yinzhe@gmail.com>
2022-06-29 12:50:45 +0800

    new ut

--- frontend/tests/pages/InitialReviewPage.spec.ts
+++ frontend/tests/pages/InitialReviewPage.spec.ts
@@ -2,6 +2,7 @@
  * @jest-environment jsdom
  */
 import InitialReviewPage from "@/pages/InitialReviewPage.vue";
+import NoteCardsView from "@/components/notes/views/NoteCardsView.vue";
 import flushPromises from "flush-promises";
 import helper from "../helpers";
 import makeMe from "../fixtures/makeMe";
@@ -27,25 +28,39 @@ describe("repeat page", () => {
     expect(mockRouterPush).toHaveBeenCalledWith({ name: "reviews" });
   });
 
-  it("normal view", async () => {
-    const note = makeMe.aNoteRealm.please();
-    const reviewPoint = makeMe.aReviewPoint.ofNote(note).please();
-    helper.apiMock
-      .expectingGet("/api/reviews/initial")
-      .andReturnOnce([reviewPoint, reviewPoint]);
-    helper.apiMock.expectingGet(`/api/notes/${note.id}`).andReturnOnce({
-      notePosition: makeMe.aNotePosition.please(),
-      notes: [note],
+  describe("normal view", () => {
+    const noteRealm = makeMe.aNoteRealm.please();
+    const reviewPoint = makeMe.aReviewPoint.ofNote(noteRealm).please();
+
+    beforeEach(() => {
+      helper.apiMock
+        .expectingGet("/api/reviews/initial")
+        .andReturnOnce([reviewPoint, reviewPoint]);
+      helper.apiMock.expectingGet(`/api/notes/${noteRealm.id}`).andReturnOnce({
+        notePosition: makeMe.aNotePosition.please(),
+        notes: [noteRealm],
+      });
     });
 
-    const wrapper = renderer.currentRoute({ name: "initial" }).mount();
-    await flushPromises();
-    expect(mockRouterPush).toHaveBeenCalledTimes(0);
-    expect(wrapper.findAll(".initial-review-paused")).toHaveLength(0);
-    expect(wrapper.findAll(".pause-stop")).toHaveLength(1);
-    expect(wrapper.find(".progress-text").text()).toContain(
-      "Initial Review: 0/2"
-    );
+    it("normal view", async () => {
+      const wrapper = renderer.currentRoute({ name: "initial" }).mount();
+      await flushPromises();
+      expect(mockRouterPush).toHaveBeenCalledTimes(0);
+      expect(wrapper.findAll(".initial-review-paused")).toHaveLength(0);
+      expect(wrapper.findAll(".pause-stop")).toHaveLength(1);
+      expect(wrapper.find(".progress-text").text()).toContain(
+        "Initial Review: 0/2"
+      );
+    });
+
+    (["levelChanged"] as "levelChanged"[]).forEach((event) => {
+      it(`reloads when ${event}`, async () => {
+        const wrapper = renderer.currentRoute({ name: "initial" }).mount();
+        await flushPromises();
+        helper.apiMock.expectingGet("/api/reviews/initial").andReturnOnce([]);
+        wrapper.findComponent(NoteCardsView).vm.$emit(event);
+      });
+    });
   });
 
   it("minimized view", async () => {
