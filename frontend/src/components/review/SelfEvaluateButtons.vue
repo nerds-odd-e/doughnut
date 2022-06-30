96ab9fc3a0eb60267838159cde6b0c255df18390
Terry Yin <terry.yinzhe@gmail.com>
2022-06-30 19:04:30 +0800

    reduce buttons

--- frontend/src/components/review/SelfEvaluateButtons.vue
+++ frontend/src/components/review/SelfEvaluateButtons.vue
@@ -2,30 +2,12 @@
   <div class="btn-group" role="group" aria-label="First group">
     <button
       class="btn btn-light"
-      id="repeat-reset"
-      name="reset"
-      @click="processForm('reset')"
-      title="reset the interval to mininum"
-    >
-      <SvgFailed />
-    </button>
-    <button
-      class="btn btn-light"
       id="repeat-sad"
       name="sad"
       @click.once="processForm('sad')"
       title="reduce next repeat interval (days) by half"
     >
       <SvgSad />
-    </button>
-    <button
-      class="btn btn-light"
-      id="repeat-satisfied"
-      name="satisfying"
-      @click="processForm('satisfying')"
-      title="use normal repeat interval (days)"
-    >
-      <SvgSatisfying />
     </button>
     <button
       class="btn btn-light"
@@ -39,19 +21,18 @@
   </div>
 </template>
 
-<script>
+<script lang="ts">
+import { defineComponent } from "vue";
 import SvgSad from "../svgs/SvgSad.vue";
-import SvgSatisfying from "../svgs/SvgSatisfying.vue";
-import SvgFailed from "../svgs/SvgFailed.vue";
 import SvgHappy from "../svgs/SvgHappy.vue";
 
-export default {
+export default defineComponent({
   emits: ["selfEvaluate"],
-  components: { SvgSad, SvgSatisfying, SvgFailed, SvgHappy },
+  components: { SvgSad, SvgHappy },
   methods: {
-    processForm(evaluation) {
+    processForm(evaluation: Generated.SelfEvaluate) {
       this.$emit("selfEvaluate", evaluation);
     },
   },
-};
+});
 </script>
