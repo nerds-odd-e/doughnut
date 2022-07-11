eef474f5a428c25f76be99d92bd27da5dcc40391
Terry Yin <terry.yinzhe@gmail.com>
2022-07-11 09:40:13 +0800

    split breadcrumb

--- frontend/src/components/toolbars/Breadcrumb.vue
+++ frontend/src/components/toolbars/Breadcrumb.vue
@@ -1,35 +1,10 @@
 <template>
-  <BasicBreadcrumb v-bind="{ ancestors }">
-    <template #topLink v-if="notebook">
-      <li v-if="fromBazaar" class="breadcrumb-item">
-        <router-link :to="{ name: 'bazaar' }">Bazaar</router-link>
-      </li>
-      <template v-else>
-        <li class="breadcrumb-item" v-if="!circle">
-          <router-link :to="{ name: 'notebooks' }">Top</router-link>
-        </li>
-        <template v-else>
-          <li class="breadcrumb-item">
-            <router-link :to="{ name: 'circles' }">Circles</router-link>
-          </li>
-          <li class="breadcrumb-item">
-            <router-link
-              :to="{ name: 'circleShow', params: { circleId: circle.id } }"
-              >{{ circle.name }}</router-link
-            >
-          </li>
-        </template>
-      </template>
-    </template>
-    <template #additional>
-      <slot />
-    </template>
-  </BasicBreadcrumb>
+  <BreadcrumbWithCircle v-bind="{ ancestors, circle, fromBazaar }" />
 </template>
 
 <script lang="ts">
 import { defineComponent, PropType } from "vue";
-import BasicBreadcrumb from "../commons/BasicBreadcrumb.vue";
+import BreadcrumbWithCircle from "./BreadcrumbWithCircle.vue";
 
 export default defineComponent({
   name: "Breadcrumb",
@@ -37,15 +12,13 @@ export default defineComponent({
     ancestors: Array,
     notebook: Object as PropType<Generated.NotebookViewedByUser>,
   },
-  components: {
-    BasicBreadcrumb,
-  },
+  components: { BreadcrumbWithCircle },
   computed: {
     fromBazaar() {
       return this.notebook?.fromBazaar;
     },
     circle() {
-      return this.notebook ? this.notebook.ownership.circle : null;
+      return this.notebook ? this.notebook.ownership.circle : undefined;
     },
   },
 });
---
+++ frontend/src/components/toolbars/BreadcrumbWithCircle.vue
@@ -0,0 +1,47 @@
+<template>
+  <BasicBreadcrumb v-bind="{ ancestors }">
+    <template #topLink>
+      <li><SvgForward /></li>
+      <li v-if="fromBazaar" class="breadcrumb-item">
+        <router-link :to="{ name: 'bazaar' }">Bazaar</router-link>
+      </li>
+      <template v-else>
+        <li class="breadcrumb-item" v-if="!circle">
+          <router-link :to="{ name: 'notebooks' }">Top</router-link>
+        </li>
+        <template v-else>
+          <li class="breadcrumb-item">
+            <router-link :to="{ name: 'circles' }">Circles</router-link>
+          </li>
+          <li class="breadcrumb-item">
+            <router-link
+              :to="{ name: 'circleShow', params: { circleId: circle.id } }"
+              >{{ circle.name }}</router-link
+            >
+          </li>
+        </template>
+      </template>
+    </template>
+    <template #additional>
+      <slot />
+    </template>
+  </BasicBreadcrumb>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+import BasicBreadcrumb from "../commons/BasicBreadcrumb.vue";
+import SvgForward from "../svgs/SvgForward.vue";
+
+export default defineComponent({
+  props: {
+    ancestors: Array,
+    circle: Object as PropType<Generated.Circle>,
+    fromBazaar: Boolean,
+  },
+  components: {
+    BasicBreadcrumb,
+    SvgForward,
+  },
+});
+</script>
