a6db3cb4162f66256430f712f64a95ec4e3707a0
Terry Yin <terry.yinzhe@gmail.com>
2022-09-06 12:41:54 +0800

    Revert "@badeball/cypress-cucumber-preprocessor@12.2.0 @swc/core@1.2.247 esbuild@0.15.7"
    This reverts commit 01612ddf287d92af6b4dd5fbdcc3e5114854a7d6.

--- cypress/config/ci.ts
+++ cypress/config/ci.ts
@@ -1,24 +1,4 @@
 import { defineConfig } from "cypress"
-import createBundler from "@bahmutov/cypress-esbuild-preprocessor";
-import { addCucumberPreprocessorPlugin } from "@badeball/cypress-cucumber-preprocessor"
-import createEsbuildPlugin from "@badeball/cypress-cucumber-preprocessor/esbuild";
-
-async function setupNodeEvents(
-  on: Cypress.PluginEvents,
-  config: Cypress.PluginConfigOptions
-): Promise<Cypress.PluginConfigOptions> {
-  await addCucumberPreprocessorPlugin(on, config);
-
-  on(
-    "file:preprocessor",
-    createBundler({
-      plugins: [createEsbuildPlugin(config)],
-    })
-  );
-
-  require("../plugins/index.ts").default(on, config)
-  return config;
-}
 
 export default defineConfig({
   env: {
@@ -33,9 +13,11 @@ export default defineConfig({
   watchForFileChanges: false,
   environment: "ci",
   e2e: {
+    setupNodeEvents(on, config) {
+      return require("../plugins/index.ts").default(on, config)
+    },
     specPattern: "cypress/integration/**/*.feature",
     excludeSpecPattern: ["**/*.{js,ts}", "**/__snapshots__/*", "**/__image_snapshots__/*"],
     baseUrl: "http://localhost:9081",
-    setupNodeEvents,
   },
 })
