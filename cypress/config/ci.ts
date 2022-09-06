552a23428fd2bb99a0c5635d2125478e96948ac3
Yeong Sheng, Tan <tanyeongsheng@gmail.com>
2022-09-06 12:56:56 +0800

    make cy:open work with @badeball/cypress-cucumber-preprocessor@12.2.0

--- cypress/config/ci.ts
+++ cypress/config/ci.ts
@@ -1,4 +1,24 @@
 import { defineConfig } from "cypress"
+import createBundler from "@bahmutov/cypress-esbuild-preprocessor";
+import { addCucumberPreprocessorPlugin } from "@badeball/cypress-cucumber-preprocessor"
+import { createEsbuildPlugin } from "@badeball/cypress-cucumber-preprocessor/esbuild";
+
+async function setupNodeEvents(
+  on: Cypress.PluginEvents,
+  config: Cypress.PluginConfigOptions
+): Promise<Cypress.PluginConfigOptions> {
+  await addCucumberPreprocessorPlugin(on, config)
+
+  on(
+    "file:preprocessor",
+    createBundler({
+      plugins: [createEsbuildPlugin(config)],
+    })
+  );
+
+  require("../plugins/index.ts").default(on, config)
+  return config
+}
 
 export default defineConfig({
   env: {
