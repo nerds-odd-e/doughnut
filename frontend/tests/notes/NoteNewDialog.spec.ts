45c186720636090ed4000a3b29b3d4273a9d7751
Terry Yin <terry.yinzhe@gmail.com>
2022-09-11 17:11:46 +0800

    refactoring frontend ut

--- frontend/tests/notes/NoteNewDialog.spec.ts
+++ frontend/tests/notes/NoteNewDialog.spec.ts
@@ -32,24 +32,48 @@ describe("adding new note", () => {
     expect(wrapper.text()).toContain("mythical");
   });
 
-  it("search wikidata for result suggestions and replace title if title is the same but case is different", async () => {
-    const searchInput = "rock";
-    const searchResult = makeMe.aWikidataSearchEntity.label("Rock").please();
-    helper.apiMock.expectingPost(`/api/notes/search`).andReturnOnce([]);
-    helper.apiMock
-      .expectingGet(`/api/wikidata/search/${searchInput}`)
-      .andReturnOnce([searchResult]);
-    const wrapper = helper
-      .component(NoteNewDialog)
-      .withStorageProps({ parentId: 123 })
-      .mount();
-    await wrapper.find("input#note-title").setValue(searchInput);
-    await wrapper.find("button#search-wikidata").trigger("click");
-    await flushPromises();
-    const result = await wrapper.find('select[name="wikidataSearchResult"]');
-    result.findAll("option").at(1)?.setValue();
-    await flushPromises();
-    const input = <HTMLInputElement>wrapper.find("input#note-title").element;
-    expect(input.value).toBe("Rock");
+  describe("search wikidata entry", () => {
+    let wrapper;
+
+    beforeEach(() => {
+      helper.apiMock.expectingPost(`/api/notes/search`).andReturnOnce([]);
+      wrapper = helper
+        .component(NoteNewDialog)
+        .withStorageProps({ parentId: 123 })
+        .mount();
+    });
+
+    const titleInput = () => {
+      return wrapper.find("input#note-title");
+    };
+
+    const searchAndSelectFirstResult = async (key: string) => {
+      await titleInput().setValue(key);
+      await wrapper.find("button#search-wikidata").trigger("click");
+      await flushPromises();
+      const result = await wrapper.find('select[name="wikidataSearchResult"]');
+      result.findAll("option").at(1)?.setValue();
+      await flushPromises();
+    };
+
+    it("search wikidata for result suggestions and replace title if title is the same but case is different", async () => {
+      const searchResult = makeMe.aWikidataSearchEntity.label("Rock").please();
+      helper.apiMock
+        .expectingGet(`/api/wikidata/search/${"rock"}`)
+        .andReturnOnce([searchResult]);
+      await searchAndSelectFirstResult("rock");
+      expect(<HTMLInputElement>titleInput().element.value).toBe("Rock");
+    });
+
+    it("search wikidata for result suggestions and replace title if title is the same but case is different", async () => {
+      const searchResult = makeMe.aWikidataSearchEntity
+        .label("Canine")
+        .please();
+      helper.apiMock
+        .expectingGet(`/api/wikidata/search/${"dog"}`)
+        .andReturnOnce([searchResult]);
+      await searchAndSelectFirstResult("dog");
+      expect(<HTMLInputElement>titleInput().element.value).toBe("dog");
+    });
   });
 });
