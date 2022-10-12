fafafe22eacbce696d470497a8a1574db2760510
Terry Yin <terry.yinzhe@gmail.com>
2022-10-12 11:31:43 +0800

    type vs hash

--- backend/src/main/java/com/odde/doughnut/services/wikidataApis/WikidataValue.java
+++ backend/src/main/java/com/odde/doughnut/services/wikidataApis/WikidataValue.java
@@ -3,25 +3,24 @@ package com.odde.doughnut.services.wikidataApis;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.odde.doughnut.entities.Coordinate;
-import com.odde.doughnut.services.wikidataApis.thirdPartyEntities.WikidataClaimItem;
 import java.time.Instant;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 import java.util.Map;
 
-public record WikidataValue(WikidataClaimItem wikidataClaimItem) {
+public record WikidataValue(
+    com.odde.doughnut.services.wikidataApis.thirdPartyEntities.WikidataMainsnak mainsnak) {
 
   public WikidataId toWikiClass() {
-    wikidataClaimItem.assertWikibaseType();
-    return new WikidataId(this.wikidataClaimItem.getValue1().get("id").textValue());
+    mainsnak.assertWikibaseType();
+    return new WikidataId(this.mainsnak.getValue().get("id").textValue());
   }
 
   public Coordinate getCoordinate() {
-    if (wikidataClaimItem.isGlobeCoordinate()) {
+    if (mainsnak.isGlobeCoordinate()) {
       Map<String, Object> data =
-          new ObjectMapper()
-              .convertValue(this.wikidataClaimItem.getValue1(), new TypeReference<>() {});
+          new ObjectMapper().convertValue(this.mainsnak.getValue(), new TypeReference<>() {});
 
       var latitude = (Double) data.get("latitude");
       var longitude = (Double) data.get("longitude");
@@ -31,17 +30,17 @@ public record WikidataValue(WikidataClaimItem wikidataClaimItem) {
   }
 
   private String getStringValue() {
-    wikidataClaimItem.assertStringType();
-    return wikidataClaimItem.getValue1().textValue();
+    mainsnak.assertStringType();
+    return mainsnak.getValue().textValue();
   }
 
   public String format() {
-    wikidataClaimItem.assertTimeType();
+    mainsnak.assertTimeType();
     DateTimeFormatter formatter =
         DateTimeFormatter.ofPattern("dd MMMM yyyy")
             .withZone(ZoneId.systemDefault())
             .localizedBy(Locale.ENGLISH);
-    String inputTime = wikidataClaimItem.getValue1().get("time").textValue();
+    String inputTime = mainsnak.getValue().get("time").textValue();
     Instant instant = Instant.parse(inputTime.substring(1));
     return formatter.format(instant) + (inputTime.startsWith("-") ? " B.C." : "");
   }
--- backend/src/main/java/com/odde/doughnut/services/wikidataApis/thirdPartyEntities/WikidataClaimItem.java
+++ backend/src/main/java/com/odde/doughnut/services/wikidataApis/thirdPartyEntities/WikidataClaimItem.java
@@ -1,62 +1,24 @@
 package com.odde.doughnut.services.wikidataApis.thirdPartyEntities;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.odde.doughnut.services.wikidataApis.WikidataValue;
-import java.util.Map;
 import java.util.Optional;
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class WikidataClaimItem {
-  static String DATA_VALUE_KEY = "datavalue";
   static String VALUE_KEY = "value";
   static String VALUE_TYPE_KEY = "type";
 
-  private Map<String, JsonNode> mainsnak;
+  public WikidataMainsnak mainsnak;
 
   @JsonIgnore
   public Optional<WikidataValue> getValue() {
-    if (!mainsnak.containsKey(DATA_VALUE_KEY)) {
+    if (mainsnak == null || mainsnak.getDatavalue() == null) {
       return Optional.empty();
     }
-    return Optional.of(new WikidataValue(this));
-  }
-
-  public String getType() {
-    return getDataValue().get(VALUE_TYPE_KEY).textValue();
-  }
-
-  public JsonNode getValue1() {
-    return getDataValue().get(VALUE_KEY);
-  }
-
-  private JsonNode getDataValue() {
-    return mainsnak.get(DATA_VALUE_KEY);
-  }
-
-  public void assertTimeType() {
-    if (!("time".compareToIgnoreCase(getType()) == 0)) {
-      throw new RuntimeException(
-          "Unsupported wikidata value type: " + getType() + ", expected time");
-    }
-  }
-
-  public void assertWikibaseType() {
-    if (!("wikibase-entityid".compareToIgnoreCase(getType()) == 0)) {
-      throw new RuntimeException(
-          "Unsupported wikidata value type: " + getType() + ", expected wikibase-entityid");
-    }
-  }
-
-  public boolean isGlobeCoordinate() {
-    return "globecoordinate".compareToIgnoreCase(getType()) == 0;
-  }
-
-  public void assertStringType() {
-    if ("string".compareToIgnoreCase(getType()) != 0) {
-      throw new RuntimeException(
-          "Unsupported wikidata value type: " + getType() + ", expected string");
-    }
+    return Optional.of(new WikidataValue(mainsnak));
   }
 }
