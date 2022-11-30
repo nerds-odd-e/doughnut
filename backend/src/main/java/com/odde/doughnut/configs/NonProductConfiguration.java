f71a6a13d12b3209cb275ba64e5eb20b8bb90746
Yeong Sheng, Tan <tanyeongsheng@gmail.com>
2022-11-30 17:11:07 +0800

    Replace deprecated WebSecurityConfigurerAdapter

--- backend/src/main/java/com/odde/doughnut/configs/NonProductConfiguration.java
+++ backend/src/main/java/com/odde/doughnut/configs/NonProductConfiguration.java
@@ -1,51 +1,31 @@
 package com.odde.doughnut.configs;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @Profile({"e2e", "test"})
-public class NonProductConfiguration extends WebSecurityConfigurerAdapter {
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
+public class NonProductConfiguration {
 
   @Autowired private CommonConfiguration commonConfiguration;
 
-  @Override
-  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-    PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-    auth.inMemoryAuthentication()
-        .withUser("user")
-        .password(encoder.encode("password"))
-        .roles("USER")
-        .and()
-        .withUser("old_learner")
-        .password(encoder.encode("password"))
-        .roles("USER")
-        .and()
-        .withUser("another_old_learner")
-        .password(encoder.encode("password"))
-        .roles("USER")
-        .and()
-        .withUser("manual")
-        .password(encoder.encode("password"))
-        .roles("USER")
-        .and()
-        .withUser("developer")
-        .password(encoder.encode("password"))
-        .roles("USER")
-        .and()
-        .withUser("non_developer")
-        .password(encoder.encode("password"))
-        .roles("USER");
-  }
-
-  @Override
-  public void configure(HttpSecurity http) throws Exception {
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http.csrf()
         .disable()
         .authorizeRequests()
@@ -53,5 +33,30 @@ public class NonProductConfiguration extends WebSecurityConfigurerAdapter {
         .permitAll();
 
     commonConfiguration.commonConfig(http, http.httpBasic().and().formLogin());
+    return http.build();
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) throws Exception {
+    InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+    createUser(manager, "user", passwordEncoder.encode("password"), "USER");
+    createUser(manager, "old_learner", passwordEncoder.encode("password"), "USER");
+    createUser(manager, "another_old_learner", passwordEncoder.encode("password"), "USER");
+    createUser(manager, "manual", passwordEncoder.encode("password"), "USER");
+    createUser(manager, "developer", passwordEncoder.encode("password"), "USER");
+    createUser(manager, "non_developer", passwordEncoder.encode("password"), "USER");
+    return manager;
+  }
+
+  private void createUser(InMemoryUserDetailsManager manager, String userName, String password, String role) {
+    manager.createUser(User.withUsername(userName)
+        .password(password)
+        .roles(role)
+        .build());
   }
 }
--- backend/src/main/java/com/odde/doughnut/configs/ProductionConfiguration.java
+++ backend/src/main/java/com/odde/doughnut/configs/ProductionConfiguration.java
@@ -1,22 +1,31 @@
 package com.odde.doughnut.configs;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @Profile({"prod"})
-public class ProductionConfiguration extends WebSecurityConfigurerAdapter {
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
+public class ProductionConfiguration {
 
   @Autowired private CommonConfiguration commonConfiguration;
 
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
-
-    http.csrf().disable().authorizeRequests().antMatchers("/api/healthcheck").permitAll();
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http.csrf()
+        .disable()
+        .authorizeRequests()
+        .antMatchers("/api/healthcheck")
+        .permitAll();
 
     commonConfiguration.commonConfig(http, http.oauth2Login());
+    return http.build();
   }
 }
