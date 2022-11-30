761f29790a7d33e519be47c9f70bf846a7373158
Yeong Sheng, Tan <tanyeongsheng@gmail.com>
2022-11-30 23:50:29 +0800

    formatting

--- backend/src/main/java/com/odde/doughnut/configs/NonProductConfiguration.java
+++ backend/src/main/java/com/odde/doughnut/configs/NonProductConfiguration.java
@@ -4,8 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -53,10 +51,8 @@ public class NonProductConfiguration {
     return manager;
   }
 
-  private void createUser(InMemoryUserDetailsManager manager, String userName, String password, String role) {
-    manager.createUser(User.withUsername(userName)
-        .password(password)
-        .roles(role)
-        .build());
+  private void createUser(
+      InMemoryUserDetailsManager manager, String userName, String password, String role) {
+    manager.createUser(User.withUsername(userName).password(password).roles(role).build());
   }
 }
