7e4b94077d3990f797e9680ba83035ad961b45fa
Yeong Sheng, Tan <tanyeongsheng@gmail.com>
2022-12-14 18:49:38 +0800

    fix more deprecation warnings

--- backend/src/main/java/com/odde/doughnut/configs/NonProductConfiguration.java
+++ backend/src/main/java/com/odde/doughnut/configs/NonProductConfiguration.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -17,7 +17,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @Profile({"e2e", "test"})
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
+@EnableMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 public class NonProductConfiguration {
 
   @Autowired private CommonConfiguration commonConfiguration;
