b9383f2d43f72491062cab72bfd05ade4604bf68
Terry Yin <terry.yinzhe@gmail.com>
2023-01-02 17:21:43 +0800

    format

--- backend/src/main/java/com/odde/doughnut/entities/Thingy.java
+++ backend/src/main/java/com/odde/doughnut/entities/Thingy.java
@@ -1,11 +1,10 @@
 package com.odde.doughnut.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 
 @MappedSuperclass
