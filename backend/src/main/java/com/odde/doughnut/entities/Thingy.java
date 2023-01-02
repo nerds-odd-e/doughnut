44214027fbe72c01fed49728663c2354e0a76731
Terry Yin <terry.yinzhe@gmail.com>
2023-01-02 16:11:09 +0800

    jason ignore

--- backend/src/main/java/com/odde/doughnut/entities/Thingy.java
+++ backend/src/main/java/com/odde/doughnut/entities/Thingy.java
@@ -4,6 +4,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 
 @MappedSuperclass
@@ -16,5 +18,6 @@ public abstract class Thingy {
 
   abstract void setThing(Thing thing);
 
+  @JsonIgnore
   public abstract Thing getThing();
 }
