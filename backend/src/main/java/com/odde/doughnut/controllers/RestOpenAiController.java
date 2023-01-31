30256c4abc4df3d5a0c25b88c75bbc601ba43a99
marvinatorrr <marvinatorrr@gmail.com>
2023-01-31 12:28:28 +0800

    fix formatting

--- backend/src/main/java/com/odde/doughnut/controllers/RestOpenAiController.java
+++ backend/src/main/java/com/odde/doughnut/controllers/RestOpenAiController.java
@@ -1,7 +1,6 @@
 package com.odde.doughnut.controllers;
 
 import com.odde.doughnut.exceptions.UnexpectedNoAccessRightException;
-import com.odde.doughnut.services.MyOpenAiService;
 import com.theokanning.openai.OpenAiService;
 import com.theokanning.openai.completion.CompletionRequest;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,10 +14,11 @@ public class RestOpenAiController {
   @PostMapping(value = "/{title}/openai")
   @Transactional
   public String getOpenAiResponse(@PathVariable(name = "title") String title)
-    throws BindException, UnexpectedNoAccessRightException {
-    OpenAiService service = new OpenAiService("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");
+      throws BindException, UnexpectedNoAccessRightException {
+    OpenAiService service =
+        new OpenAiService("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");
     CompletionRequest completionRequest =
-      CompletionRequest.builder().prompt(title).model("davinci").echo(true).build();
+        CompletionRequest.builder().prompt(title).model("davinci").echo(true).build();
     return service.createCompletion(completionRequest).getChoices().get(0).getText();
   }
 }
