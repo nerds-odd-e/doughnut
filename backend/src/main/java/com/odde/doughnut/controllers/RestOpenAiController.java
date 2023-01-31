52c5e73d10cfd623f7e558edb60e892741d8cc22
marvinatorrr <marvinatorrr@gmail.com>
2023-01-31 12:26:52 +0800

    remove openai service in controller

--- backend/src/main/java/com/odde/doughnut/controllers/RestOpenAiController.java
+++ backend/src/main/java/com/odde/doughnut/controllers/RestOpenAiController.java
@@ -2,6 +2,8 @@ package com.odde.doughnut.controllers;
 
 import com.odde.doughnut.exceptions.UnexpectedNoAccessRightException;
 import com.odde.doughnut.services.MyOpenAiService;
+import com.theokanning.openai.OpenAiService;
+import com.theokanning.openai.completion.CompletionRequest;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.*;
@@ -9,16 +11,14 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/OpenAi")
 public class RestOpenAiController {
-  private final MyOpenAiService openAiService;
-
-  public RestOpenAiController(MyOpenAiService openAiService) {
-    this.openAiService = openAiService;
-  }
 
   @PostMapping(value = "/{title}/openai")
   @Transactional
   public String getOpenAiResponse(@PathVariable(name = "title") String title)
-      throws BindException, UnexpectedNoAccessRightException {
-    return openAiService.getOpenAiResponse(title);
+    throws BindException, UnexpectedNoAccessRightException {
+    OpenAiService service = new OpenAiService("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");
+    CompletionRequest completionRequest =
+      CompletionRequest.builder().prompt(title).model("davinci").echo(true).build();
+    return service.createCompletion(completionRequest).getChoices().get(0).getText();
   }
 }
