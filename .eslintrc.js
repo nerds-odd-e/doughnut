5f9e6087c35086915072745fd54dbe0af1016651
Yeong Sheng, Tan <tanyeongsheng@gmail.com>
2023-01-31 17:34:35 +0800

    remove wireit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,60 +1,65 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'cypress', 'prettier'],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "cypress", "prettier"],
   settings: {
-    'import/resolver': {
+    "import/resolver": {
       node: {
-        extensions: ['.js', '.ts'],
+        extensions: [".js", ".ts"],
       },
     },
   },
   extends: [
-    'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-    'plugin:cypress/recommended',
-    'plugin:prettier/recommended',
-    'prettier',
+    "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    "plugin:cypress/recommended",
+    "plugin:prettier/recommended",
+    "prettier",
   ],
   parserOptions: {
     ecmaVersion: 12, // Allows for the parsing of modern ECMAScript features
-    sourceType: 'module', // Allows for the use of imports
-    tsconfigRootDir: './cypress',
-    project: './tsconfig.json',
+    sourceType: "module", // Allows for the use of imports
+    tsconfigRootDir: "./cypress",
+    project: "./tsconfig.json",
   },
-  ignorePatterns: ['tsconfig.json'],
+  ignorePatterns: ["tsconfig.json"],
   rules: {
-    'prettier/prettier': [
-      'warn',
+    "cypress/no-assigning-return-values": "error",
+    "cypress/no-unnecessary-waiting": "error",
+    "cypress/assertion-before-screenshot": "warn",
+    "cypress/no-async-tests": "error",
+    "cypress/no-pause": "error",
+    "prettier/prettier": [
+      "warn",
       {
         printWidth: 100,
         tabWidth: 2,
         singleQuote: false,
-        trailingComma: 'all',
+        trailingComma: "all",
         bracketSpacing: true,
         semi: false,
         useTabs: false,
       },
     ],
-    '@typescript-eslint/no-var-requires': 'off',
-    '@typescript-eslint/indent': 'off',
-    '@typescript-eslint/no-use-before-define': 'warn',
-    '@typescript-eslint/camelcase': 'off',
-    '@typescript-eslint/member-delimiter-style': [
-      'error',
+    "@typescript-eslint/no-var-requires": "off",
+    "@typescript-eslint/indent": "off",
+    "@typescript-eslint/no-use-before-define": "warn",
+    "@typescript-eslint/camelcase": "off",
+    "@typescript-eslint/member-delimiter-style": [
+      "error",
       {
         multiline: {
-          delimiter: 'none',
+          delimiter: "none",
           requireLast: true,
         },
         singleline: {
-          delimiter: 'semi',
+          delimiter: "semi",
           requireLast: false,
         },
       },
     ],
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/no-explicit-any': 'warn',
-    '@typescript-eslint/no-inferrable-types': [
-      'warn',
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/no-explicit-any": "warn",
+    "@typescript-eslint/no-inferrable-types": [
+      "warn",
       {
         ignoreProperties: true,
         ignoreParameters: true,
