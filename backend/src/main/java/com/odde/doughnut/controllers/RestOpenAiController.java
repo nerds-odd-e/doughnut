240c709f2bd428427f8bd3cf4a8f60071602298c
marvinatorrr <marvinatorrr@gmail.com>
2023-02-01 10:51:11 +0800

    Lint

--- backend/src/main/java/com/odde/doughnut/controllers/RestOpenAiController.java
+++ backend/src/main/java/com/odde/doughnut/controllers/RestOpenAiController.java
@@ -3,14 +3,13 @@ package com.odde.doughnut.controllers;
 import com.theokanning.openai.OpenAiService;
 import com.theokanning.openai.completion.CompletionChoice;
 import com.theokanning.openai.completion.CompletionRequest;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/OpenAi")
@@ -22,10 +21,9 @@ public class RestOpenAiController {
   @GetMapping(value = "/{title}")
   @Transactional
   public String getOpenAiResponse(@PathVariable(name = "title") String title) {
-    OpenAiService service =
-      new OpenAiService(OpenAiToken);
+    OpenAiService service = new OpenAiService(OpenAiToken);
     CompletionRequest completionRequest =
-      CompletionRequest.builder().prompt(title).model("text-davinci-003").echo(true).build();
+        CompletionRequest.builder().prompt(title).model("text-davinci-003").echo(true).build();
 
     var choices = service.createCompletion(completionRequest).getChoices();
     System.out.println(choices.size());
