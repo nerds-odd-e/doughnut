a080fdf1d60b1adb807283cf3750299edfb98e9c
Yeong Sheng, Tan <tanyeongsheng@gmail.com>
2023-02-01 13:34:14 +0800

    deploy concerns for OpenAI

--- infra/scripts/create-mig-app-instance-template.sh
+++ infra/scripts/create-mig-app-instance-template.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 SCRIPTPATH="$(
-	cd "$(dirname "$0")" > /dev/null 2>&1
-	pwd -P
+  cd "$(dirname "$0")" >/dev/null 2>&1
+  pwd -P
 )"
 
-gcloud compute instance-templates create doughnut-app-zulu17-gh-issues-token-mig-template \
-	--image doughnut-debian11-mysql80-base-saltstack \
-	--service-account 220715781008-compute@developer.gserviceaccount.com \
-	--service-account doughnut-gcp-svc-acct@carbon-syntax-298809.iam.gserviceaccount.com \
-	--scopes https://www.googleapis.com/auth/cloud-platform \
-	--scopes "userinfo-email,cloud-platform" \
-	--machine-type e2-medium \
-	--metadata-from-file startup-script=${SCRIPTPATH}/mig-zulu17-gh-issues-token-app-instance-startup.sh \
-	--metadata BUCKET=dough-01 \
-	--tags mig-app-srv
+gcloud compute instance-templates create doughnut-app-zulu17-openai-mig-template \
+  --image doughnut-debian11-mysql80-base-saltstack \
+  --service-account 220715781008-compute@developer.gserviceaccount.com \
+  --service-account doughnut-gcp-svc-acct@carbon-syntax-298809.iam.gserviceaccount.com \
+  --scopes https://www.googleapis.com/auth/cloud-platform \
+  --scopes "userinfo-email,cloud-platform" \
+  --machine-type e2-medium \
+  --metadata-from-file startup-script=${SCRIPTPATH}/mig-zulu17-openai-app-instance-startup.sh \
+  --metadata BUCKET=dough-01 \
+  --tags mig-app-srv
