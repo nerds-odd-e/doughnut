b731be624227155c040a04289e9e1d60058d33e2
Jiang Han <jianghan99@gmail.com>
2023-02-01 16:09:51 +0800

    Use BeanPropertyBindingResult instead of creating an anonymous object

--- backend/src/main/java/com/odde/doughnut/controllers/RestNoteController.java
+++ backend/src/main/java/com/odde/doughnut/controllers/RestNoteController.java
@@ -11,16 +11,13 @@ import com.odde.doughnut.services.HttpClientAdapter;
 import com.odde.doughnut.services.WikidataService;
 import com.odde.doughnut.services.wikidataApis.WikidataIdWithApi;
 import com.odde.doughnut.testability.TestabilitySettings;
-import java.beans.PropertyEditor;
 import java.io.IOException;
 import java.sql.Timestamp;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 import javax.annotation.Resource;
 import javax.validation.Valid;
 import lombok.SneakyThrows;
-import org.springframework.beans.PropertyEditorRegistry;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.*;
 import org.springframework.web.bind.annotation.*;
@@ -87,171 +84,7 @@ class RestNoteController {
         LinkCreation linkCreation = new LinkCreation();
         linkCreation.linkType = Link.LinkType.RELATED_TO;
         BindingResult bindingResult =
-            new BindingResult() {
-              @Override
-              public Object getTarget() {
-                return null;
-              }
-
-              @Override
-              public Map<String, Object> getModel() {
-                return null;
-              }
-
-              @Override
-              public Object getRawFieldValue(String field) {
-                return null;
-              }
-
-              @Override
-              public PropertyEditor findEditor(String field, Class<?> valueType) {
-                return null;
-              }
-
-              @Override
-              public PropertyEditorRegistry getPropertyEditorRegistry() {
-                return null;
-              }
-
-              @Override
-              public String[] resolveMessageCodes(String errorCode) {
-                return new String[0];
-              }
-
-              @Override
-              public String[] resolveMessageCodes(String errorCode, String field) {
-                return new String[0];
-              }
-
-              @Override
-              public void addError(ObjectError error) {}
-
-              @Override
-              public String getObjectName() {
-                return null;
-              }
-
-              @Override
-              public void setNestedPath(String nestedPath) {}
-
-              @Override
-              public String getNestedPath() {
-                return null;
-              }
-
-              @Override
-              public void pushNestedPath(String subPath) {}
-
-              @Override
-              public void popNestedPath() throws IllegalStateException {}
-
-              @Override
-              public void reject(String errorCode) {}
-
-              @Override
-              public void reject(String errorCode, String defaultMessage) {}
-
-              @Override
-              public void reject(String errorCode, Object[] errorArgs, String defaultMessage) {}
-
-              @Override
-              public void rejectValue(String field, String errorCode) {}
-
-              @Override
-              public void rejectValue(String field, String errorCode, String defaultMessage) {}
-
-              @Override
-              public void rejectValue(
-                  String field, String errorCode, Object[] errorArgs, String defaultMessage) {}
-
-              @Override
-              public void addAllErrors(Errors errors) {}
-
-              @Override
-              public boolean hasErrors() {
-                return false;
-              }
-
-              @Override
-              public int getErrorCount() {
-                return 0;
-              }
-
-              @Override
-              public List<ObjectError> getAllErrors() {
-                return null;
-              }
-
-              @Override
-              public boolean hasGlobalErrors() {
-                return false;
-              }
-
-              @Override
-              public int getGlobalErrorCount() {
-                return 0;
-              }
-
-              @Override
-              public List<ObjectError> getGlobalErrors() {
-                return null;
-              }
-
-              @Override
-              public ObjectError getGlobalError() {
-                return null;
-              }
-
-              @Override
-              public boolean hasFieldErrors() {
-                return false;
-              }
-
-              @Override
-              public int getFieldErrorCount() {
-                return 0;
-              }
-
-              @Override
-              public List<FieldError> getFieldErrors() {
-                return null;
-              }
-
-              @Override
-              public FieldError getFieldError() {
-                return null;
-              }
-
-              @Override
-              public boolean hasFieldErrors(String field) {
-                return false;
-              }
-
-              @Override
-              public int getFieldErrorCount(String field) {
-                return 0;
-              }
-
-              @Override
-              public List<FieldError> getFieldErrors(String field) {
-                return null;
-              }
-
-              @Override
-              public FieldError getFieldError(String field) {
-                return null;
-              }
-
-              @Override
-              public Object getFieldValue(String field) {
-                return null;
-              }
-
-              @Override
-              public Class<?> getFieldType(String field) {
-                return null;
-              }
-            };
+            new BeanPropertyBindingResult(existingNote.get(), existingNote.get().getTitle());
         restLinkController.linkNoteFinalize(note, existingNote.get(), linkCreation, bindingResult);
       } else {
         createNote(note, createCountryOfOriginNoteCreation(countryOfOrigin.get()));
