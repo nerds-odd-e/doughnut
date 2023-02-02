8fec0d67cc8c5dd99ce44ab2d0741d3d3b03334b
marvinatorrr <marvinatorrr@gmail.com>
2023-02-02 10:56:10 +0800

    fix formatting

--- backend/src/main/java/com/odde/doughnut/services/RealOpenAiWrapperService.java
+++ backend/src/main/java/com/odde/doughnut/services/RealOpenAiWrapperService.java
@@ -3,10 +3,9 @@ package com.odde.doughnut.services;
 import com.theokanning.openai.OpenAiService;
 import com.theokanning.openai.completion.CompletionChoice;
 import com.theokanning.openai.completion.CompletionRequest;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-
-import java.util.stream.Collectors;
 
 @Service("RealOpenAiService")
 public class RealOpenAiWrapperService implements OpenAiWrapperService {
@@ -16,7 +15,7 @@ public class RealOpenAiWrapperService implements OpenAiWrapperService {
   public String getOpenAiResponse(String prompt) {
     var service = new OpenAiService(OpenAiToken);
     CompletionRequest completionRequest =
-      CompletionRequest.builder().prompt(prompt).model("text-davinci-003").echo(true).build();
+        CompletionRequest.builder().prompt(prompt).model("text-davinci-003").echo(true).build();
 
     var choices = service.createCompletion(completionRequest).getChoices();
     return choices.stream().map(CompletionChoice::getText).collect(Collectors.joining(""));
