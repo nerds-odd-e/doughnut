3be1f5d52b2c7404773979e802e16e833c8e5de2
marvinatorrr <marvinatorrr@gmail.com>
2023-02-03 14:24:10 +0800

    Increase openai max token

--- backend/src/main/java/com/odde/doughnut/services/RealOpenAiWrapperService.java
+++ backend/src/main/java/com/odde/doughnut/services/RealOpenAiWrapperService.java
@@ -18,7 +18,12 @@ public class RealOpenAiWrapperService implements OpenAiWrapperService {
   private String getOpenAiResponse(String prompt) {
     var service = new OpenAiService(OpenAiToken);
     CompletionRequest completionRequest =
-        CompletionRequest.builder().prompt(prompt).model(textModel).echo(true).build();
+        CompletionRequest.builder()
+            .prompt(prompt)
+            .maxTokens(256)
+            .model(textModel)
+            .echo(false)
+            .build();
     var choices = service.createCompletion(completionRequest).getChoices();
     return choices.stream().map(CompletionChoice::getText).collect(Collectors.joining(""));
   }
