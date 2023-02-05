5715e32f6b71035877a717bd6ee8c2d5c89a49ba
Terry Yin <terry.yinzhe@gmail.com>
2023-02-05 06:28:37 +0100

    remove interface

--- backend/src/main/java/com/odde/doughnut/services/OpenAiWrapperService.java
+++ backend/src/main/java/com/odde/doughnut/services/OpenAiWrapperService.java
@@ -1,5 +1,34 @@
 package com.odde.doughnut.services;
 
-public interface OpenAiWrapperService {
-  public String getDescription(String item);
+import com.theokanning.openai.OpenAiService;
+import com.theokanning.openai.completion.CompletionChoice;
+import com.theokanning.openai.completion.CompletionRequest;
+import java.util.stream.Collectors;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import retrofit2.HttpException;
+
+@Service
+public class OpenAiWrapperService {
+  @Value("${spring.openai.token}")
+  private String OpenAiToken;
+
+  private String textModel = "text-davinci-003";
+
+  private String getOpenAiResponse(String prompt) {
+    var service = new OpenAiService(OpenAiToken);
+    CompletionRequest completionRequest =
+        CompletionRequest.builder().prompt(prompt).model(textModel).echo(true).build();
+    var choices = service.createCompletion(completionRequest).getChoices();
+    return choices.stream().map(CompletionChoice::getText).collect(Collectors.joining(""));
+  }
+
+  public String getDescription(String item) {
+    try {
+      String prompt = "Tell me about " + item + ".";
+      return getOpenAiResponse(prompt).replace(prompt, "").trim();
+    } catch (HttpException e) {
+      return "";
+    }
+  }
 }
