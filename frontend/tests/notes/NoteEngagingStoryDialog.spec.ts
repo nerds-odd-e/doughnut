5e3a497e70924a2229c38072cae2f2a45e260e3a
Kees Seebregts <kees.seebregts@wartsila.com>
2023-02-10 10:49:41 +0100

    Refactor UT tests of engaging story

--- frontend/tests/notes/NoteEngagingStoryDialog.spec.ts
+++ frontend/tests/notes/NoteEngagingStoryDialog.spec.ts
@@ -1,39 +1,35 @@
 import { flushPromises } from "@vue/test-utils";
 import NoteEngagingStoryDialog from "@/components/notes/NoteEngagingStoryDialog.vue";
-import { expect } from "vitest";
+import { beforeEach, expect } from "vitest";
 import makeMe from "../fixtures/makeMe";
 import helper from "../helpers";
 
 helper.resetWithApiMock(beforeEach, afterEach);
 
+const createWrapper = async () => {
+  const note = makeMe.aNoteRealm.please();
+  helper.apiMock
+    .expectingGet(`/api/ai/ask-engaging-stories/${note.note.id}`)
+    .andReturnOnce({ engagingStory: "This is an engaging story." });
+  const wrapper = helper
+    .component(NoteEngagingStoryDialog)
+    .withStorageProps({ selectedNote: note })
+    .mount();
+  await flushPromises();
+  return wrapper;
+};
+
 describe("NoteEngagingStoryDialog", () => {
   it("fetches engaging story from api", async () => {
-    const note = makeMe.aNoteRealm.please();
-    helper.apiMock
-      .expectingGet(`/api/ai/ask-engaging-stories/${note.note.id}`)
-      .andReturnOnce({ engagingStory: "This is an engaging story." });
-    const wrapper = helper
-      .component(NoteEngagingStoryDialog)
-      .withStorageProps({ selectedNote: note })
-      .mount();
-    await flushPromises();
+    const wrapper = await createWrapper();
     expect(wrapper.find(".engaging-story").text()).toEqual(
       "This is an engaging story."
     );
   });
 
   it("Engaging story dialog close button exits the dialog", async () => {
-    const note = makeMe.aNoteRealm.please();
-    helper.apiMock
-      .expectingGet(`/api/ai/ask-engaging-stories/${note.note.id}`)
-      .andReturnOnce({ engagingStory: "This is an engaging story." });
-    const wrapper = helper
-      .component(NoteEngagingStoryDialog)
-      .withStorageProps({ selectedNote: note })
-      .mount();
-    await flushPromises();
+    const wrapper = await createWrapper();
     await wrapper.find("input[value='Close']").trigger("click");
-
     expect(wrapper.emitted("done")).toBeTruthy();
   });
 });
