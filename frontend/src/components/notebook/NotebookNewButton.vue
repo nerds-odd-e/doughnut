23f4869ada0109090ea1428413e484f170269695
Terry Yin <terry.yinzhe@gmail.com>
2023-02-13 19:32:09 +0800

    notebook new' '

--- frontend/src/components/notebook/NotebookNewButton.vue
+++ frontend/src/components/notebook/NotebookNewButton.vue
@@ -1,21 +1,19 @@
 <template>
-  <PopupButton class="btn-primary" title="New notebook">
+  <PopButton class="btn-primary" title="New notebook">
     <template #button_face>
       <slot />
     </template>
-    <template #dialog_body="{ doneHandler }">
-      <NotebookNewDialog :circle="circle" @done="doneHandler($event)" />
-    </template>
-  </PopupButton>
+    <NotebookNewDialog :circle="circle" />
+  </PopButton>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import NotebookNewDialog from "./NotebookNewDialog.vue";
-import PopupButton from "../commons/Popups/PopupButton.vue";
+import PopButton from "../commons/Popups/PopButton.vue";
 
 export default defineComponent({
   props: { circle: Object },
-  components: { PopupButton, NotebookNewDialog },
+  components: { PopButton, NotebookNewDialog },
 });
 </script>
