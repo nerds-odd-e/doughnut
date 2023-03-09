163ae495dec31fd778c9e03f9a6e139cb91c98d5
Terry Yin <terry.yinzhe@gmail.com>
2023-03-09 14:02:01 +0800

    some new tests

--- backend/src/test/java/com/odde/doughnut/algorithms/ClozeDescriptionTest.java
+++ backend/src/test/java/com/odde/doughnut/algorithms/ClozeDescriptionTest.java
@@ -3,9 +3,13 @@ package com.odde.doughnut.algorithms;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ClozeDescriptionTest {
   ClozeReplacement clozeReplacement = new ClozeReplacement("[..~]", "[...]", "/.../", "<...>");
@@ -51,6 +55,8 @@ public class ClozeDescriptionTest {
     "cat（animal),      cat is an animal,                  [...] is an <...>",
     "cat(animal) dog,  cat is an animal,                  cat is an animal",
     "「いい和悪い」,      然后,                               然后",
+    "олет,             Это самолет,                        Это самолет",
+    "不客气,            😃不客气,                           😃[...]",
     "ignore (complex (brackets)), ignore complex brackets,  ignore complex brackets",
     "cat/dog(animal/weather), dog day is a hot weather,   [...] day is a hot <...>",
     "6,               6year,                              [...]year",
@@ -62,6 +68,24 @@ public class ClozeDescriptionTest {
         equalTo(expectedClozeDescription));
   }
 
+  @Disabled
+  @ParameterizedTest
+  @CsvSource({
+    "moon,     partner of earth,            partner of earth",
+    "sedition, word sedition means this,    word [...] means this",
+    "sting,    existing,                    existing",
+    "鳴く,     羊はなんて鳴くの？,              羊はなんて[...]の？",
+    "rapport,  你知道rapport是什么吗？,        你知道[…]是什么吗？",
+    "олет,     Это самолет,                 Это самолет",
+    "不客气,    😃不客气,                     😃[...]",
+  })
+  void clozeDescriptionTest(String token, String description, String expectedClozeDescription) {
+    String regex = "(?:(?<=[^\\w-])" + Pattern.quote(token) + "(?=[^\\w-]))";
+    Pattern pattern = Pattern.compile(regex);
+    Matcher matcher = pattern.matcher(description);
+    String result = matcher.replaceAll("[...]");
+    assertThat(result, equalTo(expectedClozeDescription));
+  }
   @Test
   void clozeDescriptionWithMultipleLink() {
     assertThat(
