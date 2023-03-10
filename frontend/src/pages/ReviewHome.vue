d7d46bbad25dc12d585dd7d245f4e7f66595d29d
Terry Yin <terry.yinzhe@gmail.com>
2023-03-10 15:34:13 +0800

    remove dead code

--- frontend/src/pages/ReviewHome.vue
+++ frontend/src/pages/ReviewHome.vue
@@ -1,6 +1,5 @@
 <template>
   <ContainerPage v-bind="{ contentExists: reviewing }">
-    <ReviewDoughnutRing v-if="reviewing" :reviewing="reviewing" />
     <ReviewWelcome v-if="!!reviewing" v-bind="{ reviewing }" />
   </ContainerPage>
 </template>
@@ -9,7 +8,6 @@
 import ReviewWelcome from "../components/review/ReviewWelcome.vue";
 import ContainerPage from "./commons/ContainerPage.vue";
 import useLoadingApi from "../managedApi/useLoadingApi";
-import ReviewDoughnutRing from "../components/review/ReviewDoughnutRing.vue";
 
 export default {
   setup() {
@@ -20,7 +18,7 @@ export default {
       reviewing: null,
     };
   },
-  components: { ReviewWelcome, ContainerPage, ReviewDoughnutRing },
+  components: { ReviewWelcome, ContainerPage },
   methods: {
     fetchData() {
       this.api.reviewMethods.overview().then((res) => (this.reviewing = res));
