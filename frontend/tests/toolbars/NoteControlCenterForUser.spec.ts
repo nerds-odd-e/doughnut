900b51ca58176f937dbe1e9da40e82c5a0611176
Ted Hsia <tedhsia@taxigo.com.tw>
2023-03-29 16:07:36 +0800

    add unit-test case for NoteControlCenterForUser

--- frontend/tests/toolbars/NoteControlCenterForUser.spec.ts
+++ frontend/tests/toolbars/NoteControlCenterForUser.spec.ts
@@ -1,4 +1,5 @@
 import NoteControlCenterForUser from "@/components/toolbars/NoteControlCenterForUser.vue";
+import { StoredApi } from "@/store/StoredApiCollection";
 import { VueWrapper } from "@vue/test-utils";
 import makeMe from "tests/fixtures/makeMe";
 import { beforeEach, describe } from "vitest";
@@ -6,13 +7,29 @@ import helper from "../helpers";
 
 describe("NoteControlCenterForUser", () => {
   helper.resetWithApiMock(beforeEach, afterEach);
+
   let wrapper: VueWrapper;
 
-  const mountComponent = (note?: Generated.Note): VueWrapper => {
+  const mountComponent = (
+    note?: Generated.Note,
+    updateTextContent?: StoredApi["updateTextContent"]
+  ): VueWrapper => {
     return helper
       .component(NoteControlCenterForUser)
-      .withStorageProps({ storageAccessor: { selectedNote: note } })
+      .withStorageProps({
+        storageAccessor: {
+          selectedNote: note,
+          api: () => ({ updateTextContent }),
+        },
+      })
       .mount();
+  };
+
+  const mountComponentWithNote = (
+    updateTextContent?: StoredApi["updateTextContent"]
+  ) => {
+    const note = makeMe.aNote.please();
+    wrapper = mountComponent(note, updateTextContent);
   };
 
   it("has only the link-note button when no exist selected note", () => {
@@ -21,8 +38,24 @@ describe("NoteControlCenterForUser", () => {
   });
 
   it("has the suggest button when having selected note", () => {
-    const note = makeMe.aNote.please();
-    wrapper = mountComponent(note);
+    mountComponentWithNote();
     expect(wrapper.findAll(".btn")[4].attributes("title")).toEqual("Suggest1");
+  });
+
+  it("ask api be called once when clicking the suggest button", () => {
+    const updateTextContent: StoredApi["updateTextContent"] = async (
+      _id,
+      noteContent
+    ) => {
+      expect(noteContent.description).toBe("suggestion");
+      return {} as Generated.NoteRealm;
+    };
+
+    mountComponentWithNote(updateTextContent);
+    helper.apiMock
+      .expectingPost(`/api/ai/ask-suggestions`)
+      .andReturnOnce({ suggestion: "suggestion" });
+
+    wrapper.findAll(".btn")[4].trigger("click");
   });
 });
