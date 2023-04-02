3bb388ae567296e679df5712d34222dc69649b4b
Terry Yin <terry.yinzhe@gmail.com>
2023-04-02 20:36:06 +0800

    dismiss

--- cypress/step_definitions/user.ts
+++ cypress/step_definitions/user.ts
@@ -90,6 +90,7 @@ Then("The {string} page is displayed", (pageName) => {
       break
     case "ErrorPage":
       cy.findAllByText("It seems you cannot access this page.")
+      cy.get(".last-error-message").click()
       break
     default:
       cy.failure()
