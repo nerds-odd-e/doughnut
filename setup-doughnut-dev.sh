268065e9920209fd58d21e361b20cd664b787157
Tan Yeong Sheng <tanyeongsheng@gmail.com>
2023-04-24 22:01:18 +0800

    nix 2.15.0

--- setup-doughnut-dev.sh
+++ setup-doughnut-dev.sh
@@ -16,7 +16,7 @@ get_os_type() {
 
 download_nixpkg_manager_install_script() {
   rm -f ./install-nix
-  curl -o install-nix https://nixos.org/releases/nix/nix-2.14.1/install
+  curl -o install-nix https://nixos.org/releases/nix/nix-2.15.0/install
   chmod +x ./install-nix
 }
 
