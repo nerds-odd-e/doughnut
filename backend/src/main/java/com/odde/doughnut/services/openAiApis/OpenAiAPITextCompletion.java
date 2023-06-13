abb5ad36ccca58c406ac1168825130816afd56c1
Darien <frozenfishdongyu@gmail.com>
2023-06-13 17:59:55 +0800

    Change cypress functions for mocking to correct one

--- backend/src/main/java/com/odde/doughnut/services/openAiApis/OpenAiAPITextCompletion.java
+++ backend/src/main/java/com/odde/doughnut/services/openAiApis/OpenAiAPITextCompletion.java
@@ -2,20 +2,17 @@ package com.odde.doughnut.services.openAiApis;
 
 import com.odde.doughnut.entities.json.AiSuggestion;
 import com.theokanning.openai.OpenAiApi;
-import com.theokanning.openai.service.OpenAiService;
 import com.theokanning.openai.completion.chat.ChatCompletionChoice;
 import com.theokanning.openai.completion.chat.ChatCompletionRequest;
 import com.theokanning.openai.completion.chat.ChatMessage;
 import com.theokanning.openai.completion.chat.ChatMessageRole;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 public class OpenAiAPITextCompletion extends OpenAiApiHandlerBase {
 
-  private OpenAiApi openAiApi;
-  private OpenAiService service = new OpenAiService(System.getenv("OPENAI_API_TOKEN"));
+  private final OpenAiApi openAiApi;
   public static final String OPEN_AI_MODEL = "gpt-3.5-turbo";
 
   public OpenAiAPITextCompletion(OpenAiApi openAiApi) {
@@ -23,39 +20,38 @@ public class OpenAiAPITextCompletion extends OpenAiApiHandlerBase {
   }
 
   private List<ChatCompletionChoice> getChatCompletionChoices(
-    ChatCompletionRequest completionRequest) {
+      ChatCompletionRequest completionRequest) {
     return openAiApi.createChatCompletion(completionRequest).blockingGet().getChoices();
-
   }
 
   public AiSuggestion getOpenAiCompletion(String prompt) {
 
     return withExceptionHandler(
-      () -> {
-        ChatCompletionRequest completionRequest = getChatCompletionRequest(prompt);
-        List<ChatCompletionChoice> choices = getChatCompletionChoices(completionRequest);
-        return choices.stream()
-          .findFirst()
-          .map(
-            chatCompletionChoice -> new AiSuggestion(
-              chatCompletionChoice.getMessage().getContent(),
-              chatCompletionChoice.getFinishReason()))
-          .orElse(null);
-      });
+        () -> {
+          ChatCompletionRequest completionRequest = getChatCompletionRequest(prompt);
+          List<ChatCompletionChoice> choices = getChatCompletionChoices(completionRequest);
+          return choices.stream()
+              .findFirst()
+              .map(
+                  chatCompletionChoice ->
+                      new AiSuggestion(
+                          chatCompletionChoice.getMessage().getContent(),
+                          chatCompletionChoice.getFinishReason()))
+              .orElse(null);
+        });
   }
 
   private static ChatCompletionRequest getChatCompletionRequest(String prompt) {
-    System.out.println("4 prompt:" + prompt);
     List<ChatMessage> messages = new ArrayList<>();
     final ChatMessage systemMessage = new ChatMessage(ChatMessageRole.USER.value(), prompt);
     messages.add(0, systemMessage);
 
     return ChatCompletionRequest.builder()
-      .model(OPEN_AI_MODEL)
-      .messages(messages)
-      .n(1)
-      .maxTokens(50)
-      .logitBias(new HashMap<>())
-      .build();
+        .model(OPEN_AI_MODEL)
+        .messages(messages)
+        .n(1)
+        .maxTokens(50)
+        .logitBias(new HashMap<>())
+        .build();
   }
 }
