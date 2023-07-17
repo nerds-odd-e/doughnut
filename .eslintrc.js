c02ae6bdb11cdf4e73c6c112aabe49ee2952c5e5
Tan Yeong Sheng <tanyeongsheng@gmail.com>
2023-07-17 14:51:36 +0800

    esbuild@0.18.13; eslint-unused-imports

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,8 @@ module.exports = {
     "@typescript-eslint",
     "cypress",
     "chai-friendly",
-    "prettier"
+    "prettier",
+    "unused-imports"
   ],
   settings: {
     "import/resolver": {
@@ -73,5 +74,11 @@ module.exports = {
         ignoreParameters: true,
       },
     ],
+    "@typescript-eslint/no-unused-vars": "off",
+    "unused-imports/no-unused-imports": "error",
+    "unused-imports/no-unused-vars": [
+      "warn",
+      { "vars": "all", "varsIgnorePattern": "^_", "args": "after-used", "argsIgnorePattern": "^_" }
+    ]
   },
 };
--- cypress/support/string.extensions.ts
+++ cypress/support/string.extensions.ts
@@ -1,5 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-interface String {
+interface _String {
   commonSenseSplit(separator: string): string[]
 }
 
