5e828ecdbdcd68479c1141d9a6d415e933ce9ada
Terry Yin <terry.yinzhe@gmail.com>
2023-07-31 13:09:30 +0800

    drop table

--- backend/src/main/java/com/odde/doughnut/entities/TextContent.java
+++ backend/src/main/java/com/odde/doughnut/entities/TextContent.java
@@ -1,28 +1,10 @@
 package com.odde.doughnut.entities;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
-@Entity
-@Table(name = "text_content")
 public class TextContent {
-
-  @Id
-  @JsonIgnore
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Integer id;
-
-  @Size(min = 1, max = 100)
-  @Getter
-  @Setter
-  private String title = "";
+  @Getter @Setter private String title = "";
 
   @Getter @Setter private String description;
 }
