184298bd7339b68bba01b3d2220d65265cc6102f
mtan13700 <mtan13700@gmail.com>
2023-08-22 15:27:21 +0800

    WIP: Add test for AnswerResult

---
+++ frontend/tests/components/AnswerResult.spec.ts
@@ -0,0 +1,22 @@
+// import { AnswerResult } from '@/components/revivew/AnswerResult.vue';
+// import { flushPromises } from "@vue/test-utils";
+// import helper from "../helpers";
+
+// helper.resetWithApiMock(beforeEach, afterEach);
+
+// describe("AnswerResult", () => {
+//   it("xxx", async () => {
+//     const wrapper = helper
+//       .component(AnswerResult)
+//       .withProps({
+//         answeredQuestion: {
+//           type: null,
+//           required: true,
+//         },
+//       })
+//       .mount();
+
+//     await flushPromises();
+//     expect(wrapper.emitted()["update:modelValue"]).toBeUndefined();
+//   });
+// });
