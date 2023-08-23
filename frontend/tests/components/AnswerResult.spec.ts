9fcecf6f629181b1c87209abf1dac7b5433886d2
taikusa <=>
2023-08-23 09:32:10 +0800

    Skip failed test

--- frontend/tests/components/AnswerResult.spec.ts
+++ frontend/tests/components/AnswerResult.spec.ts
@@ -1,22 +1,25 @@
-// import { AnswerResult } from '@/components/revivew/AnswerResult.vue';
-// import { flushPromises } from "@vue/test-utils";
-// import helper from "../helpers";
+import { flushPromises } from "@vue/test-utils";
+import AnswerResult from "@/components/review/AnswerResult.vue";
+import helper from "../helpers";
 
-// helper.resetWithApiMock(beforeEach, afterEach);
+helper.resetWithApiMock(beforeEach, afterEach);
 
-// describe("AnswerResult", () => {
-//   it("xxx", async () => {
-//     const wrapper = helper
-//       .component(AnswerResult)
-//       .withProps({
-//         answeredQuestion: {
-//           type: null,
-//           required: true,
-//         },
-//       })
-//       .mount();
+describe("AnswerResult", () => {
+  it.skip("xxx", async () => {
+    // Doing now...
+    const wrapper = helper
+      .component(AnswerResult)
+      .withProps({
+        answeredQuestion: {
+          answerId: 1,
+          correct: false,
+          answerDisplay: "answerDisplay",
+          quizQuestion: {},
+        },
+      })
+      .mount();
 
-//     await flushPromises();
-//     expect(wrapper.emitted()["update:modelValue"]).toBeUndefined();
-//   });
-// });
+    await flushPromises();
+    expect(wrapper.emitted()["update:modelValue"]).toBeUndefined();
+  });
+});
