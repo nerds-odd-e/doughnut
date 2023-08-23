b4b1b9305e40a59fdc20b85e8db871cecf193ca8
jiadong.chen <coolboy096188@gmail.com>
2023-08-23 12:54:44 +0800

    add then statement

--- e2e_test/step_definitions/chat.ts
+++ e2e_test/step_definitions/chat.ts
@@ -2,9 +2,13 @@
 /// <reference types="../support" />
 // @ts-check
 
-import { When } from "@badeball/cypress-cucumber-preprocessor"
+import { When, Then } from "@badeball/cypress-cucumber-preprocessor"
 
 When("I ask to OpenAI {string}", (askStatement: string) => {
   cy.get("#ask-input").should("be.visible").type(askStatement)
   cy.get("#ask-button").click()
+})
+
+Then("I can confirm the answer {}", (answer: string) => {
+  cy.get("#hoge").should("be.visible").contains(answer)
 })
