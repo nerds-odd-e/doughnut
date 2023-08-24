3ca114b23c04e9eab6093060dc41a88a8b939b2a
jiadong.chen <coolboy096188@gmail.com>
2023-08-24 12:13:49 +0800

    add ChatService @Service

--- backend/src/main/java/com/odde/doughnut/services/ChatService.java
+++ backend/src/main/java/com/odde/doughnut/services/ChatService.java
@@ -9,7 +9,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ChatService {
   @Autowired private OpenAiApi openAiApi;
 
