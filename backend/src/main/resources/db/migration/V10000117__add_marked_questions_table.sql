6d480e1532df79ec196f1423f52342f50a99fa6c
kaye <tankaye@gmail.com>
2023-09-05 12:23:41 +0800

    Fixed Marked Question sql

--- backend/src/main/resources/db/migration/V10000117__add_marked_questions_table.sql
+++ backend/src/main/resources/db/migration/V10000117__add_marked_questions_table.sql
@@ -7,8 +7,6 @@ CREATE TABLE `marked_questions` (
   `is_good` bit,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  KEY `fk_note_id` (`note_id`),
-  KEY `fk_user_id` (`user_id`),
   CONSTRAINT `fk_quiz_answer_id_2` FOREIGN KEY (`quiz_question_id`) REFERENCES `quiz_question` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT,
   CONSTRAINT `fk_note_id` FOREIGN KEY (`note_id`) REFERENCES `note` (`id`),
   CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
