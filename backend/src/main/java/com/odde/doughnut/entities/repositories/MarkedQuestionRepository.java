8b20c9691e18abf6d013131d6838d287770d550c
kaye <tankaye@gmail.com>
2023-09-05 12:04:10 +0800

    Added Marked Question sql,entity,repo

---
+++ backend/src/main/java/com/odde/doughnut/entities/repositories/MarkedQuestionRepository.java
@@ -0,0 +1,6 @@
+package com.odde.doughnut.entities.repositories;
+
+import com.odde.doughnut.entities.MarkedQuestion;
+import org.springframework.data.repository.CrudRepository;
+
+public interface MarkedQuestionRepository extends CrudRepository<MarkedQuestion, Integer> {}
