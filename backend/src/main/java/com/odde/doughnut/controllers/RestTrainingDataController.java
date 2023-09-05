781d19667d5cacf8af068bfaeeb534cb6d0ecc19
dodo ade <yudu11@gmail.com>
2023-09-05 14:49:05 +0800

    fix lint

--- backend/src/main/java/com/odde/doughnut/controllers/RestTrainingDataController.java
+++ backend/src/main/java/com/odde/doughnut/controllers/RestTrainingDataController.java
@@ -4,12 +4,11 @@ import com.odde.doughnut.entities.User;
 import com.odde.doughnut.factoryServices.ModelFactoryService;
 import com.odde.doughnut.models.UserModel;
 import com.odde.doughnut.testability.TestabilitySettings;
+import javax.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.annotation.SessionScope;
-
-import javax.annotation.Resource;
 
 @RestController
 @SessionScope
@@ -36,5 +35,4 @@ class RestTrainingDataController {
     User user = currentUser.getEntity();
     return user.getName();
   }
-
 }
