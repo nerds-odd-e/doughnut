184aaecccef06914580293e7b197bbc465249006
jiadong.chen <coolboy096188@gmail.com>
2023-09-05 12:17:33 +0800

    Develop API to extract good training data.

---
+++ e2e_test/step_definitions/developer.ts
@@ -0,0 +1,16 @@
+/// <reference types="cypress" />
+/// <reference types="../support" />
+// @ts-check
+
+import { Given, Then } from "@badeball/cypress-cucumber-preprocessor"
+
+Given("I attempt to export", () => {
+})
+
+Given("I should return an empty JSONL file", () => {
+})
+Given("a file with training data is produced", () => {
+})
+
+Given("that there are no questions marked good at all", () => {
+})
