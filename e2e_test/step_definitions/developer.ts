abcbed36b28d693585f78751ef9fec65b1054758
Terry Yin <terry.yinzhe@gmail.com>
2023-09-10 10:01:07 +0800

    lint

--- e2e_test/step_definitions/developer.ts
+++ e2e_test/step_definitions/developer.ts
@@ -3,13 +3,15 @@
 // @ts-check
 
 import { Given } from "@badeball/cypress-cucumber-preprocessor"
-import { deleteDownloadsFolder } from './utils'
 
-Given("a developer should be able to download the training data with {int} record", (count: number) => {
-  cy.deleteDownloadsFolder()
-  cy.visit("/dev-training-data")
-  cy.findByRole("button", { name: "Download" }).click()
-  cy.readFile("cypress/downloads/trainingdata.txt")
-  .then((content)=> (content.match(/messages/g) || []).length)
-  .should("eq", count)
-})
+Given(
+  "a developer should be able to download the training data with {int} record",
+  (count: number) => {
+    cy.deleteDownloadsFolder()
+    cy.visit("/dev-training-data")
+    cy.findByRole("button", { name: "Download" }).click()
+    cy.readFile("cypress/downloads/trainingdata.txt")
+      .then((content) => (content.match(/messages/g) || []).length)
+      .should("eq", count)
+  },
+)
