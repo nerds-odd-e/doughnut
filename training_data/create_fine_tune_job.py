6892a049e4fa6cf5583563de57c8c3f9396b1689
Terry Yin <terry.yinzhe@gmail.com>
2023-09-14 15:57:24 +0800

    new model

--- training_data/create_fine_tune_job.py
+++ training_data/create_fine_tune_job.py
@@ -2,4 +2,4 @@ import openai
 import os
 openai.api_key = os.getenv("OPENAI_API_TOKEN")
 
-openai.FineTuningJob.create(training_file="file-Gt2vexszM2Da7701XNTCQz0q", model="gpt-3.5-turbo")
+openai.FineTuningJob.create(training_file="file-4JxZbfXMbx2EwcSdwWhTZ1r7", model="gpt-3.5-turbo")
