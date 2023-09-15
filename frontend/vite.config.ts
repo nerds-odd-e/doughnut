deb3ffda59e4ce39350bc4f2bc8d76b54ddc979f
Terry Yin <terry.yinzhe@gmail.com>
2023-09-15 15:11:49 +0800

    perhaps not needed code

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -62,7 +62,6 @@ export default defineConfig({
     proxy: {
       "/api": "http://localhost:9081",
       "/images": "http://localhost:9081",
-      "/login": "http://localhost:9081",
       "/logout": "http://localhost:9081",
       "/users/identify": "http://localhost:9081",
       "/testability": "http://localhost:9081",
