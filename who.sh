4b57c557e8bc48bba35438f72f5914d44af4e1ea
Terry Yin <terry.yinzhe@gmail.com>
2023-09-18 16:39:48 +0200

    fix path

--- who.sh
+++ who.sh
@@ -1,7 +1,7 @@
 yarn sound https://github.com/nerds-odd-e/doughnut/actions 2>&1 | 
 tee /dev/tty | 
 while IFS= read -r line; do
-  if [[ "$line" == *"failed"* ]]; then
-    afplay ~/Downloads/who.mp3 &
+  if [[ "$line" == *"build"* ]]; then
+    afplay docs/who.mp3 &
   fi
 done
