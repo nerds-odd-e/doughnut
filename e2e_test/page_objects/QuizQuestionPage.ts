d516e77b4f4ffe8a3b837bfb2380d00fb19f948f
Tapas <andre.heijstek@hey.com>
2023-09-20 22:02:49 +0800

    refactor their code

--- e2e_test/page_objects/QuizQuestionPage.ts
+++ e2e_test/page_objects/QuizQuestionPage.ts
@@ -38,7 +38,7 @@ const currentQuestion = (stem?: string) => {
           name: "send this question for fine tuning the question generation model",
         })
         .click()
-      cy.get("textarea[placeholder='Add a comment about the question']").type(suggestedQuestion)
+      cy.findByPlaceholderText('Add a comment about the question').type(suggestedQuestion)
       cy.findByRole("button", { name: "OK" }).click()
       cy.pageIsNotLoading()
     },
