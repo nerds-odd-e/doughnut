eed3fd08cde090444366796f84c7a079b956c892
andreas <songyanghan+git@gmail.com>
2023-10-11 15:11:49 +0800

    re fix formatting issues

--- backend/src/main/java/com/odde/doughnut/controllers/RestFineTuningDataController.java
+++ backend/src/main/java/com/odde/doughnut/controllers/RestFineTuningDataController.java
@@ -6,12 +6,11 @@ import com.odde.doughnut.entities.SuggestedQuestionForFineTuning;
 import com.odde.doughnut.exceptions.UnexpectedNoAccessRightException;
 import com.odde.doughnut.factoryServices.ModelFactoryService;
 import com.odde.doughnut.models.UserModel;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.annotation.SessionScope;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 @SessionScope
