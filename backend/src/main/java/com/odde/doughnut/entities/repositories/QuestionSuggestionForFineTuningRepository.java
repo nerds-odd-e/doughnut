4b10b88a50ebed4e1afeeead6c0f9e7ce8c5fa85
andreas <songyanghan+git@gmail.com>
2023-10-11 10:45:25 +0800

    commented out the query to extract only positive feedback

--- backend/src/main/java/com/odde/doughnut/entities/repositories/QuestionSuggestionForFineTuningRepository.java
+++ backend/src/main/java/com/odde/doughnut/entities/repositories/QuestionSuggestionForFineTuningRepository.java
@@ -1,15 +1,12 @@
 package com.odde.doughnut.entities.repositories;
 
 import com.odde.doughnut.entities.SuggestedQuestionForFineTuning;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-
-import java.util.List;
 
 public interface QuestionSuggestionForFineTuningRepository
     extends CrudRepository<SuggestedQuestionForFineTuning, Integer> {
-  @Query("select * from suggested_question_for_fine_tuning where is_positive_feedback = TRUE")
-  public List<SuggestedQuestionForFineTuning> getAllFineTuneData();
+//  @Query("select * from suggested_question_for_fine_tuning where is_positive_feedback = TRUE")
+//  public List<SuggestedQuestionForFineTuning> getAllFineTuneData();
 
 
 }
