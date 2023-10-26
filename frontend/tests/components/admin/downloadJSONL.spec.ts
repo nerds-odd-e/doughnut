f664feaba2818e12d721a8e4cc35750d98eb9e75
Terry Yin <terry.yinzhe@gmail.com>
2023-10-26 19:47:19 +0800

    no null

---
+++ frontend/tests/components/admin/downloadJSONL.spec.ts
@@ -0,0 +1,41 @@
+import { beforeEach, describe, expect, it, vitest } from "vitest";
+import downloadJSONL, {
+  toJSONL,
+} from "../../../src/components/admin/downloadJSONL";
+
+describe("downloadJSONL", () => {
+  // Mock data to be passed to the function
+  const examples: Generated.OpenAIChatGPTFineTuningExample[] = [
+    { messages: [{ role: "user", content: "cnt", function_call: null }] },
+  ];
+
+  const element = {
+    click: vitest.fn(),
+    tagName: "a",
+    download: "",
+    // add any other necessary properties here
+  } as unknown as HTMLAnchorElement;
+
+  beforeEach(() => {
+    // Mock relevant functions and properties
+    global.URL.createObjectURL = vitest.fn();
+    global.URL.revokeObjectURL = vitest.fn();
+
+    document.createElement = vitest.fn(() => element);
+  });
+
+  it("should create and click an anchor element with correct blob data", () => {
+    downloadJSONL(examples, "test.jsonl");
+
+    expect(global.URL.createObjectURL).toHaveBeenCalled();
+    expect(element.download).toBe("test.jsonl");
+    expect(element.click).toHaveBeenCalled();
+    expect(global.URL.revokeObjectURL).toHaveBeenCalled();
+  });
+
+  it("should return a stringified JSONL", () => {
+    const result = toJSONL(examples);
+
+    expect(result).toBe('{"messages":[{"role":"user","content":"cnt"}]}');
+  });
+});
