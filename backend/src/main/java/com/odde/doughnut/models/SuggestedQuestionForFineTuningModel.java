abca22f9803e65491a6ed20b579e9da1c9e4148d
Terry Yin <terry.yinzhe@gmail.com>
2023-10-26 14:37:26 +0800

    delete

--- backend/src/main/java/com/odde/doughnut/models/SuggestedQuestionForFineTuningModel.java
+++ backend/src/main/java/com/odde/doughnut/models/SuggestedQuestionForFineTuningModel.java
@@ -58,4 +58,9 @@ public class SuggestedQuestionForFineTuningModel {
     modelFactoryService.questionSuggestionForFineTuningRepository.save(newObject);
     return newObject;
   }
+
+  public SuggestedQuestionForFineTuning delete() {
+    modelFactoryService.questionSuggestionForFineTuningRepository.delete(entity);
+    return entity;
+  }
 }
