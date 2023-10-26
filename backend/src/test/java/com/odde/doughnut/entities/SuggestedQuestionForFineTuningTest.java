2c681cb663dac3de495efb67559ba875dfc26c15
Terry Yin <terry.yinzhe@gmail.com>
2023-10-26 18:43:31 +0800

    use original data structure

--- backend/src/test/java/com/odde/doughnut/entities/SuggestedQuestionForFineTuningTest.java
+++ backend/src/test/java/com/odde/doughnut/entities/SuggestedQuestionForFineTuningTest.java
@@ -4,9 +4,9 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
 
 import com.odde.doughnut.controllers.json.OpenAIChatGPTFineTuningExample;
-import com.odde.doughnut.controllers.json.SimplifiedOpenAIChatMessage;
 import com.odde.doughnut.services.ai.MCQWithAnswer;
 import com.odde.doughnut.testability.MakeMeWithoutDB;
+import com.theokanning.openai.completion.chat.ChatMessage;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 
@@ -23,8 +23,7 @@ class SuggestedQuestionForFineTuningTest {
 
     OpenAIChatGPTFineTuningExample questionEvaluationFineTuningData =
         suggestedQuestionForFineTuning.toQuestionEvaluationFineTuningData();
-    List<SimplifiedOpenAIChatMessage> goodTrainingData =
-        questionEvaluationFineTuningData.getMessages();
+    List<ChatMessage> goodTrainingData = questionEvaluationFineTuningData.getMessages();
     assertThat(goodTrainingData.get(0).getContent(), containsString("note content"));
     assertThat(goodTrainingData.get(1).getContent(), containsString(mcqWithAnswer.stem));
   }
