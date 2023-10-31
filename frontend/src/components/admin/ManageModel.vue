a2ddf761f6942581ecb4c254bfdd799cf9d08b30
YuWen Hsieh <twilighthook@gmail.com>
2023-10-31 10:42:31 +0800

    feat(Model Manage): add tab for manage model in admin

---
+++ frontend/src/components/admin/ManageModel.vue
@@ -0,0 +1,14 @@
+<template>
+  <p>Hello I am manage model</p>
+</template>
+<script>
+export default {
+  setup() {},
+  components: {},
+  data() {},
+  methods: {
+    fetchData() {},
+  },
+  mounted() {},
+};
+</script>
