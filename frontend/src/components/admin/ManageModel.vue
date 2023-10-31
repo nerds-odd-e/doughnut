51243b7307ed8e26beaccdfd9e6e3a72d394cb70
YuWen Hsieh <twilighthook@gmail.com>
2023-10-31 16:41:53 +0800

    feat: add 2 dropdownlist for model options

--- frontend/src/components/admin/ManageModel.vue
+++ frontend/src/components/admin/ManageModel.vue
@@ -1,12 +1,37 @@
 <template>
-  <div>
-    <div>Question Generation</div>
-    <div>
-      <DropdownList
-        :options="selectionList"
-        scope-name="modelmgmt"
-        field=""
-      ></DropdownList>
+  <div class="manage-model">
+    <div class="model-section">
+      <div class="model-title">Question Generation</div>
+      <div>
+        <DropdownList
+          :options="selectionList"
+          scope-name="modelmgmt"
+          field=""
+          class="model-option"
+        ></DropdownList>
+      </div>
+    </div>
+    <div class="model-section">
+      <div class="model-title">Evaluation</div>
+      <div>
+        <DropdownList
+          :options="selectionList"
+          scope-name="modelmgmt"
+          field=""
+          class="model-option"
+        ></DropdownList>
+      </div>
+    </div>
+    <div class="model-section">
+      <div class="model-title">Other</div>
+      <div>
+        <DropdownList
+          :options="selectionList"
+          scope-name="modelmgmt"
+          field=""
+          class="model-option"
+        ></DropdownList>
+      </div>
     </div>
   </div>
 </template>
@@ -17,12 +42,33 @@ const selectionList = [
   {
     key: "test_key",
     value: "test_value",
-    label: "model 777",
+    label: "model1",
   },
   {
     key: "test_key2",
     value: "test_value2",
-    label: "model888",
+    label: "model2",
   },
 ];
 </script>
+<style scoped>
+.manage-model {
+  margin-top: 10px;
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+}
+.model-title {
+  width: 200px;
+  padding-left: 18px;
+}
+.model-section {
+  display: flex;
+  align-items: center;
+  gap: 18px;
+}
+
+.model-option {
+  min-width: 300px;
+}
+</style>
