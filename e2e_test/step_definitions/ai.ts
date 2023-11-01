031652922565437f9eb6f526c4721ec7d709a3cf
Isaac Lu <qwe12340812s@gmail.com>
2023-11-01 15:41:37 +0800

    Mock get file list on OpenAI

--- e2e_test/step_definitions/ai.ts
+++ e2e_test/step_definitions/ai.ts
@@ -94,7 +94,9 @@ Then("I contest the question", () => {
   cy.findByRole("button", { name: "Doesn't make sense?" }).click()
 })
 
-Given('there is a fine-tuning file "question_gerenation_examples" on my OpenAI account', () => {})
+Given('there is a fine-tuning file "question_gerenation_examples" on my OpenAI account', () => {
+  mock_services.openAi().stubListFiles()
+})
 
 Given(
   'the finetuning for the file "question_generation_examples" will be successful',
