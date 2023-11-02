8e9a9c4401dcaad71bf3ca95bbcc5f7dbbfb38a7
Kevin Hou <kevin.hou1@hp.com>
2023-11-02 15:47:57 +0800

    formatting

--- e2e_test/step_definitions/admin.ts
+++ e2e_test/step_definitions/admin.ts
@@ -1,23 +1,31 @@
 /// <reference types="cypress" />
 /// <reference types="../support" />
 // @ts-check
-import { DataTable, Given, Then, When } from "@badeball/cypress-cucumber-preprocessor"
-import start, { mock_services } from "start"
+import {
+  DataTable,
+  Given,
+  Then,
+  When,
+} from "@badeball/cypress-cucumber-preprocessor";
+import start, { mock_services } from "start";
 
-Given("my question should not be included in the admin's fine-tuning data", () => {
-  start
-    .loginAsAdminAndGoToAdminDashboard()
-    .suggestedQuestionsForFineTuning()
-    .downloadAIQuestionTrainingData()
-    .expectNumberOfRecords(0)
-})
+Given(
+  "my question should not be included in the admin's fine-tuning data",
+  () => {
+    start
+      .loginAsAdminAndGoToAdminDashboard()
+      .suggestedQuestionsForFineTuning()
+      .downloadAIQuestionTrainingData()
+      .expectNumberOfRecords(0);
+  },
+);
 
 When("I upload the feedbacks", () => {
   start
     .loginAsAdminAndGoToAdminDashboard()
     .suggestedQuestionsForFineTuning()
-    .uploadFineTuningTrainingData()
-})
+    .uploadFineTuningTrainingData();
+});
 
 Given(
   "the admin modifies the question suggested {string} to:",
@@ -25,16 +33,19 @@ Given(
     start
       .loginAsAdminAndGoToAdminDashboard()
       .suggestedQuestionsForFineTuning()
-      .updateQuestionSuggestionAndChoice(originalQuestionStem, newQuestion.hashes()[0])
+      .updateQuestionSuggestionAndChoice(
+        originalQuestionStem,
+        newQuestion.hashes()[0],
+      );
   },
-)
+);
 
 Given("an admin duplicates the question {string}", (questionStem: string) => {
   start
     .loginAsAdminAndGoToAdminDashboard()
     .suggestedQuestionsForFineTuning()
-    .duplicateNegativeQuestion(questionStem)
-})
+    .duplicateNegativeQuestion(questionStem);
+});
 
 Given(
   "an admin can retrieve the training data for question generation containing:",
@@ -43,9 +54,9 @@ Given(
       .loginAsAdminAndGoToAdminDashboard()
       .suggestedQuestionsForFineTuning()
       .downloadAIQuestionTrainingData()
-      .expectExampleQuestions(question.hashes())
+      .expectExampleQuestions(question.hashes());
   },
-)
+);
 
 Given(
   "an admin can retrieve the training data for question generation containing {int} examples",
@@ -54,9 +65,9 @@ Given(
       .loginAsAdminAndGoToAdminDashboard()
       .suggestedQuestionsForFineTuning()
       .downloadAIQuestionTrainingData()
-      .expectNumberOfRecords(numOfDownload)
+      .expectNumberOfRecords(numOfDownload);
   },
-)
+);
 
 Given(
   "an admin should be able to download the training data for evaluation containing:",
@@ -65,9 +76,9 @@ Given(
       .loginAsAdminAndGoToAdminDashboard()
       .suggestedQuestionsForFineTuning()
       .downloadFeedbackForEvaluationModel()
-      .expectExampleQuestions(trainingExamples.hashes())
+      .expectExampleQuestions(trainingExamples.hashes());
   },
-)
+);
 
 Given(
   "there should be {int} examples containing {string}",
@@ -75,37 +86,43 @@ Given(
     start
       .assumeAdminDashboardPage()
       .suggestedQuestionsForFineTuning()
-      .expectString(numOfOccurrence, expectedString)
+      .expectString(numOfOccurrence, expectedString);
   },
-)
+);
 
 Given(
   "I am logged in as an admin and click AdminDashboard and go to tab {string}",
   (tabName: string) => {
-    start.loginAsAdminAndGoToAdminDashboard().goToModelManagementTab(tabName)
+    start.loginAsAdminAndGoToAdminDashboard().goToModelManagementTab(tabName);
   },
-)
+);
 
-When("I choose {string} for {string} use", (modelName: string, trainingEngine: string) => {
-  cy.get("select[name='" + trainingEngine + "']").select(modelName)
-  cy.get(".saveBtn").click()
-})
+When(
+  "I choose {string} for {string} use",
+  (modelName: string, trainingEngine: string) => {
+    cy.get("select[name='" + trainingEngine + "']").select(modelName);
+    cy.get(".saveBtn").click();
+  },
+);
 
 Then(
   "I can choose model {string} from GPT in {string} dropdown list",
   (modelName: string, trainingEngine: string) => {
-    cy.get("select[name='" + trainingEngine + "']").select(modelName)
+    cy.get("select[name='" + trainingEngine + "']").select(modelName);
   },
-)
+);
 
-Then("I should be using {string} for {string}", (modelName: string, trainingEngine: string) => {
-  cy.findByText("Failure Reports").click()
-  cy.findByText("Manage Model").click()
-  cy.get("select[name='" + modelName + "']")
-    .find("option:selected")
-    .should("have.text", trainingEngine)
-})
+Then(
+  "I should be using {string} for {string}",
+  (modelName: string, trainingEngine: string) => {
+    cy.findByText("Failure Reports").click();
+    cy.findByText("Manage Model").click();
+    cy.get("select[name='" + modelName + "']")
+      .find("option:selected")
+      .should("have.text", trainingEngine);
+  },
+);
 
 Then("Open AI service should receive the uploaded file.", () => {
-  mock_services.openAi().stubOpenAiUploadResponse()
-})
+  mock_services.openAi().stubOpenAiUploadResponse();
+});
