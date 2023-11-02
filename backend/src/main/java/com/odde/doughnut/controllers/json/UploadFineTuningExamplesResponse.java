a0b518eab898225a321646a55c7729ea826ec2d9
Kevin Hou <kevin.hou1@hp.com>
2023-11-02 11:00:24 +0800

    [refactor] handle msg generation by class

--- backend/src/main/java/com/odde/doughnut/controllers/json/UploadFineTuningExamplesResponse.java
+++ backend/src/main/java/com/odde/doughnut/controllers/json/UploadFineTuningExamplesResponse.java
@@ -6,4 +6,13 @@ import lombok.Setter;
 public class UploadFineTuningExamplesResponse {
   @Getter @Setter private boolean isSuccess;
   @Getter @Setter private String message;
+
+  public static UploadFineTuningExamplesResponse fail(String msg) {
+    return new UploadFineTuningExamplesResponse() {
+      {
+        setSuccess(false);
+        setMessage(msg);
+      }
+    };
+  }
 }
