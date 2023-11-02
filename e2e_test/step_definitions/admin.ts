57b424a071de9c417d5c744d29b06a33d6dd191c
Kevin Hou <kevin.hou1@hp.com>
2023-11-02 14:30:39 +0800

    formatting

--- e2e_test/step_definitions/admin.ts
+++ e2e_test/step_definitions/admin.ts
@@ -1,25 +1,31 @@
-import { DataTable } from "@badeball/cypress-cucumber-preprocessor"
 /// <reference types="cypress" />
 /// <reference types="../support" />
 // @ts-check
+import {
+  DataTable,
+  Given,
+  Then,
+  When,
+} from "@badeball/cypress-cucumber-preprocessor";
+import start from "start";
 
-import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor"
-import start from "start"
-
-Given("my question should not be included in the admin's fine-tuning data", () => {
-  start
-    .loginAsAdminAndGoToAdminDashboard()
-    .suggestedQuestionsForFineTuning()
-    .downloadAIQuestionTrainingData()
-    .expectNumberOfRecords(0)
-})
+Given(
+  "my question should not be included in the admin's fine-tuning data",
+  () => {
+    start
+      .loginAsAdminAndGoToAdminDashboard()
+      .suggestedQuestionsForFineTuning()
+      .downloadAIQuestionTrainingData()
+      .expectNumberOfRecords(0);
+  },
+);
 
 When("I upload the feedbacks", () => {
   start
     .loginAsAdminAndGoToAdminDashboard()
     .suggestedQuestionsForFineTuning()
-    .uploadFineTuningTrainingData()
-})
+    .uploadFineTuningTrainingData();
+});
 
 Given(
   "the admin modifies the question suggested {string} to:",
@@ -27,16 +33,19 @@ Given(
     start
       .loginAsAdminAndGoToAdminDashboard()
       .suggestedQuestionsForFineTuning()
-      .updateQuestionSuggestionAndChoice(originalQuestionStem, newQuestion.hashes()[0])
+      .updateQuestionSuggestionAndChoice(
+        originalQuestionStem,
+        newQuestion.hashes()[0],
+      );
   },
-)
+);
 
 Given("an admin duplicates the question {string}", (questionStem: string) => {
   start
     .loginAsAdminAndGoToAdminDashboard()
     .suggestedQuestionsForFineTuning()
-    .duplicateNegativeQuestion(questionStem)
-})
+    .duplicateNegativeQuestion(questionStem);
+});
 
 Given(
   "an admin can retrieve the training data for question generation containing:",
@@ -45,9 +54,9 @@ Given(
       .loginAsAdminAndGoToAdminDashboard()
       .suggestedQuestionsForFineTuning()
       .downloadAIQuestionTrainingData()
-      .expectExampleQuestions(question.hashes())
+      .expectExampleQuestions(question.hashes());
   },
-)
+);
 
 Given(
   "an admin can retrieve the training data for question generation containing {int} examples",
@@ -56,9 +65,9 @@ Given(
       .loginAsAdminAndGoToAdminDashboard()
       .suggestedQuestionsForFineTuning()
       .downloadAIQuestionTrainingData()
-      .expectNumberOfRecords(numOfDownload)
+      .expectNumberOfRecords(numOfDownload);
   },
-)
+);
 
 Given(
   "an admin should be able to download the training data for evaluation containing:",
@@ -67,9 +76,9 @@ Given(
       .loginAsAdminAndGoToAdminDashboard()
       .suggestedQuestionsForFineTuning()
       .downloadFeedbackForEvaluationModel()
-      .expectExampleQuestions(trainingExamples.hashes())
+      .expectExampleQuestions(trainingExamples.hashes());
   },
-)
+);
 
 Given(
   "there should be {int} examples containing {string}",
@@ -77,28 +86,33 @@ Given(
     start
       .assumeAdminDashboardPage()
       .suggestedQuestionsForFineTuning()
-      .expectString(numOfOccurrence, expectedString)
+      .expectString(numOfOccurrence, expectedString);
   },
-)
+);
 
 Given(
   "I am logged in as an admin and click AdminDashboard and go to tab {string}",
   (tabName: string) => {
-    start.loginAsAdminAndGoToAdminDashboard().goToModelManagementTab(tabName)
+    start.loginAsAdminAndGoToAdminDashboard().goToModelManagementTab(tabName);
   },
-)
+);
 
-When("I choose {string} for {string} use", (modelName: string, generationCategory: string) => {
-  cy.get("select[name=" + generationCategory + "]").select(modelName)
-  cy.get(".button").click()
-})
+When(
+  "I choose {string} for {string} use",
+  (modelName: string, generationCategory: string) => {
+    cy.get("select[name=" + generationCategory + "]").select(modelName);
+    cy.get(".button").click();
+  },
+);
 
 Then(
   "I should be using for {string} for {string}",
   (modelName: string, generationCategory: string) => {},
-)
+);
 
 Then(
   "I can choose the model from GPT in {string} dropdown list",
   (generationCategory: string) => {},
-)
+);
+
+// Then("Open AI service should receive the uploaded file.", () => {});
