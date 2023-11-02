6b6afa0d59214e7c1fd405becc4a4a0f55e12691
Kevin Hou <kevin.hou1@hp.com>
2023-11-02 10:49:21 +0800

    [fix] file uploading feature behavior to consider negative part

--- e2e_test/step_definitions/upload.ts
+++ e2e_test/step_definitions/upload.ts
@@ -2,18 +2,29 @@
 /// <reference types="../support" />
 // @ts-check
 
-import { Given, Then } from "@badeball/cypress-cucumber-preprocessor"
+import { Given, Then } from "@badeball/cypress-cucumber-preprocessor";
 
 Given(
   "I have {int} positive feedbacks and {int} negative feedbacks",
   (positive: number, negative: number) => {
-    cy.get('a[title="send this question for fine tuning the question generation model"]').click()
     for (let i = 0; i < positive; i++) {
-      cy.findByRole("button", { name: "👍 Good" }).click()
+      cy.get(
+        'a[title="send this question for fine tuning the question generation model"]',
+      ).click();
+      cy.findByRole("button", { name: "👍 Good" }).click();
+      cy.findByRole("button", { name: "OK" }).click();
+    }
+
+    for (let i = 0; i < negative; i++) {
+      cy.get(
+        'a[title="send this question for fine tuning the question generation model"]',
+      ).click();
+      cy.findByRole("button", { name: "👎 Bad" }).click();
+      cy.findByRole("button", { name: "OK" }).click();
     }
   },
-)
+);
 
 Then("I should see the message {string}", (message: string) => {
-  cy.findByText(message).should("exist")
-})
+  cy.findByText(message).should("exist");
+});
