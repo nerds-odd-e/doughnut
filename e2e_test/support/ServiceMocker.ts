b0de371e24f879ce788ce9f37d77191157b73291
Leo <leo5j472421@gmail.com>
2023-11-02 15:05:28 +0800

    Refactor code to improve readability and remove unused imports
    - Removed unused import statements in `openAiService.ts` and `ServiceMocker.ts`
    - Refactored code in `openAiService.ts` to improve readability by removing unnecessary line breaks and adding missing semicolons
    - Updated function names in `openAiService.ts` for consistency
    - Removed unnecessary line breaks in `admin.ts`
    - Added missing semicolon at the end of the file

--- e2e_test/support/ServiceMocker.ts
+++ e2e_test/support/ServiceMocker.ts
@@ -1,81 +1,54 @@
-import {
-  DefaultPredicate,
-  FlexiPredicate,
-  HttpMethod,
-  Predicate,
-} from "@anev/ts-mountebank";
+import { DefaultPredicate, FlexiPredicate, HttpMethod, Predicate } from "@anev/ts-mountebank"
 /// <reference types="cypress" />
-import MountebankWrapper from "./MountebankWrapper";
+import MountebankWrapper from "./MountebankWrapper"
 
 class ServiceMocker {
-  private readonly mountebank: MountebankWrapper;
-  readonly serviceName: string;
+  private readonly mountebank: MountebankWrapper
+  readonly serviceName: string
 
   constructor(serviceName: string, port: number) {
-    this.mountebank = new MountebankWrapper(port);
-    this.serviceName = serviceName;
+    this.mountebank = new MountebankWrapper(port)
+    this.serviceName = serviceName
   }
 
   get savedServiceUrlName() {
-    return `saved${this.serviceName}Url`;
+    return `saved${this.serviceName}Url`
   }
 
   install() {
-    return this.mountebank.createImposter();
+    return this.mountebank.createImposter()
   }
 
   get serviceUrl() {
-    return this.mountebank.serviceUrl;
+    return this.mountebank.serviceUrl
   }
 
   public stubByUrl(url: string, data: unknown) {
-    return this.stubGetter(url, {}, data);
+    return this.stubGetter(url, {}, data)
   }
 
   public stubGetter(path: string, queryData: unknown, response: unknown) {
     return this.mockWithPredicate(
-      new FlexiPredicate()
-        .withPath(path)
-        .withMethod(HttpMethod.GET)
-        .withQuery(queryData),
+      new FlexiPredicate().withPath(path).withMethod(HttpMethod.GET).withQuery(queryData),
       response,
-    );
+    )
   }
 
   public stubPoster(path: string, response: unknown) {
-    return this.mockWithPredicate(
-      new DefaultPredicate(path, HttpMethod.POST),
-      response,
-    );
+    return this.mockWithPredicate(new DefaultPredicate(path, HttpMethod.POST), response)
   }
 
-  public mockWithPredicate(
-    predicate: Predicate,
-    response: unknown,
-  ): Promise<void> {
-    return this.mountebank.stubWithPredicate(predicate, response);
+  public mockWithPredicate(predicate: Predicate, response: unknown): Promise<void> {
+    return this.mountebank.stubWithPredicate(predicate, response)
   }
 
   public stubPosterUnauthorized(pathMatcher: string, response: unknown) {
-    return this.mountebank.stubWithErrorResponse(
-      pathMatcher,
-      HttpMethod.POST,
-      401,
-      response,
-    );
+    return this.mountebank.stubWithErrorResponse(pathMatcher, HttpMethod.POST, 401, response)
   }
 
-  public stubGetterWithError500Response(
-    pathMatcher: string,
-    response: unknown,
-  ) {
-    return this.mountebank.stubWithErrorResponse(
-      pathMatcher,
-      HttpMethod.GET,
-      500,
-      response,
-    );
+  public stubGetterWithError500Response(pathMatcher: string, response: unknown) {
+    return this.mountebank.stubWithErrorResponse(pathMatcher, HttpMethod.GET, 500, response)
   }
 }
 
-export default ServiceMocker;
+export default ServiceMocker
