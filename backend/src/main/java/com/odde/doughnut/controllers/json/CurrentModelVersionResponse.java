6aca43ed1eecc405fad355fe303ad11d6bc127ce
YuWen Hsieh <twilighthook@gmail.com>
2023-11-03 10:59:41 +0800

    add order for the json properties

--- backend/src/main/java/com/odde/doughnut/controllers/json/CurrentModelVersionResponse.java
+++ backend/src/main/java/com/odde/doughnut/controllers/json/CurrentModelVersionResponse.java
@@ -1,10 +1,16 @@
 package com.odde.doughnut.controllers.json;
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
+@JsonPropertyOrder({
+  "currentQuestionGenerationModelVersion",
+  "currentEvaluationModelVersion",
+  "currentOthersModelVersion"
+})
 public class CurrentModelVersionResponse {
   String currentQuestionGenerationModelVersion;
   String currentEvaluationModelVersion;
