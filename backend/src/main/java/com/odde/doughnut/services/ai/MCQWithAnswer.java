4ef466451f95d3b8a2f67fd5b5337b27337ca784
Terry Yin <terry.yinzhe@gmail.com>
2023-11-03 14:04:09 +0800

    fix bug parsing legacy json data

--- backend/src/main/java/com/odde/doughnut/services/ai/MCQWithAnswer.java
+++ backend/src/main/java/com/odde/doughnut/services/ai/MCQWithAnswer.java
@@ -1,5 +1,6 @@
 package com.odde.doughnut.services.ai;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyDescription;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -8,6 +9,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.odde.doughnut.factoryServices.quizFacotries.QuizQuestionNotPossibleException;
 import org.apache.logging.log4j.util.Strings;
 
+@JsonIgnoreProperties({"confidence"})
 public class MCQWithAnswer extends MultipleChoicesQuestion {
 
   @JsonPropertyDescription("Index of the correct choice. 0-based.")
