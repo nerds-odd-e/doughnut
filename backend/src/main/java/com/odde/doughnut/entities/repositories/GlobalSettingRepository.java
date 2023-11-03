677d15e9a63d53ab56c24c758ddfe8ee3c62452c
Tapas <andre.heijstek@hey.com>
2023-11-03 11:51:03 +0800

    feat (model mgmt): add GlobalSettingRepository

---
+++ backend/src/main/java/com/odde/doughnut/entities/repositories/GlobalSettingRepository.java
@@ -0,0 +1,6 @@
+package com.odde.doughnut.entities.repositories;
+
+import com.odde.doughnut.entities.GlobalSettings;
+import org.springframework.data.repository.CrudRepository;
+
+public interface GlobalSettingRepository extends CrudRepository<GlobalSettings, Integer> {}
