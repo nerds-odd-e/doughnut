8aa0ad622cbfbcd67e4bd24dd9d33298198b9ba6
Tapas <andre.heijstek@hey.com>
2023-11-03 14:55:57 +0800

    feat(model mgmt): implement get other model version from db

--- backend/src/main/java/com/odde/doughnut/controllers/RestAiController.java
+++ backend/src/main/java/com/odde/doughnut/controllers/RestAiController.java
@@ -12,13 +12,7 @@ import com.odde.doughnut.services.AiModelService;
 import com.theokanning.openai.OpenAiApi;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.annotation.SessionScope;
 
 @RestController
