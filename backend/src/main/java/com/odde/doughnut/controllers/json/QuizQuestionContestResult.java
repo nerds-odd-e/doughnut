4654ed5900c4666f3b48b91e40d3b3401b5cc9a5
Terry Yin <terry.yinzhe@gmail.com>
2023-11-09 11:23:47 +0800

    add ut

--- backend/src/main/java/com/odde/doughnut/controllers/json/QuizQuestionContestResult.java
+++ backend/src/main/java/com/odde/doughnut/controllers/json/QuizQuestionContestResult.java
@@ -1,7 +1,6 @@
 package com.odde.doughnut.controllers.json;
 
 import lombok.Getter;
-import org.springframework.lang.Nullable;
 
 public class QuizQuestionContestResult {
   @Getter public String reason;
