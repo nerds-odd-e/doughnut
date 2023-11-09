dedfda15343dac6303d3d2c347dfcb52556dca95
Terry Yin <terry.yinzhe@gmail.com>
2023-11-09 11:13:43 +0800

    remove unused field

--- backend/src/main/java/com/odde/doughnut/controllers/json/QuizQuestionContestResult.java
+++ backend/src/main/java/com/odde/doughnut/controllers/json/QuizQuestionContestResult.java
@@ -4,7 +4,6 @@ import lombok.Getter;
 import org.springframework.lang.Nullable;
 
 public class QuizQuestionContestResult {
-  @Getter @Nullable public QuizQuestion newQuizQuestion;
   @Getter public String reason;
   @Getter public Boolean legitimated = false;
 }
