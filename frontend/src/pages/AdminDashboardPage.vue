3737124dcf9ee10ef8687b3f3a9fcd566e9d478a
Terry Yin <terry.yinzhe@gmail.com>
2023-11-11 14:15:07 +0800

    undo delete

--- frontend/src/pages/AdminDashboardPage.vue
+++ frontend/src/pages/AdminDashboardPage.vue
@@ -20,14 +20,26 @@
         Failure Reports</a
       >
     </li>
+    <li class="nav-item">
+      <a
+        :class="`nav-link ${activePage === 'manageModel' ? 'active' : ''}`"
+        role="button"
+        href="#"
+        @click="activePage = 'manageModel'"
+      >
+        Manage Model</a
+      >
+    </li>
   </ul>
   <FineTuningData v-if="activePage === 'fineTuningData'" />
   <FailureReportList v-if="activePage === 'failureReport'" />
+  <ManageModel v-if="activePage === 'manageModel'" />
 </template>
 
 <script lang="ts">
 import FineTuningData from "../components/admin/FineTuningData.vue";
 import FailureReportList from "../components/admin/FailureReportList.vue";
+import ManageModel from "../components/admin/ManageModel.vue";
 
 export default {
   data() {
@@ -35,12 +47,14 @@ export default {
       activePage: "fineTuningData" as
         | "fineTuningData"
         | "failureReport"
+        | "manageModel"
         | undefined,
     };
   },
   components: {
     FineTuningData,
     FailureReportList,
+    ManageModel,
   },
 };
 </script>
