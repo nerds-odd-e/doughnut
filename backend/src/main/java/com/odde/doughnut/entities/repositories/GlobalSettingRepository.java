884e7685183325f26892964e9db0618cc88f09d3
Terry Yin <terry.yinzhe@gmail.com>
2023-11-12 10:51:50 +0800

    current time

--- backend/src/main/java/com/odde/doughnut/entities/repositories/GlobalSettingRepository.java
+++ backend/src/main/java/com/odde/doughnut/entities/repositories/GlobalSettingRepository.java
@@ -3,4 +3,6 @@ package com.odde.doughnut.entities.repositories;
 import com.odde.doughnut.entities.GlobalSettings;
 import org.springframework.data.repository.CrudRepository;
 
-public interface GlobalSettingRepository extends CrudRepository<GlobalSettings, Integer> {}
+public interface GlobalSettingRepository extends CrudRepository<GlobalSettings, Integer> {
+  GlobalSettings findByKeyName(String keyName);
+}
