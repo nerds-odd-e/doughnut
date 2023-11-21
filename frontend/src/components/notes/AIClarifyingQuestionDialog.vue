67d8eb164cb86643df13cabba6ba610625305a6e
Johannes Hartenstein <odde.footrest796@simplelogin.com>
2023-11-22 00:14:52 +0800

    added dialog component for clarifying questions

---
+++ frontend/src/components/notes/AIClarifyingQuestionDialog.vue
@@ -0,0 +1,35 @@
+<template>
+  <h3>
+    Clarification question by the AI: <strong>{{ question }}</strong>
+  </h3>
+  <form>
+    <TextInput
+      scope-name="note"
+      field="answerToAI"
+      v-model="answerToAI"
+      v-focus
+    />
+    <input type="submit" value="Send" class="btn btn-primary" />
+  </form>
+</template>
+
+<script>
+import TextInput from "../form/TextInput.vue";
+
+export default {
+  props: {
+    question: string
+  },
+  components: { TextInput },
+  data() {
+    return {
+      answerToAI: string
+    };
+  },
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
