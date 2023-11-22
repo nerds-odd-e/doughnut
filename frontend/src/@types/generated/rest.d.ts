9b504730e4c3b37de47bbf1cb38c0afc1a6571cf
Goran Krstevski <goran.krstevski@intertec.io>
2023-11-22 12:07:01 +0100

    Generate AICompletion type change

--- frontend/src/@types/generated/rest.d.ts
+++ frontend/src/@types/generated/rest.d.ts
@@ -6,6 +6,7 @@ declare namespace Generated {
     interface AiCompletion {
         moreCompleteContent: string;
         finishReason: string;
+        question: string;
     }
 
     interface AiCompletionParams {
