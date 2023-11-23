6561e46c5f8556fc6d2e7a8598d7b6d4805e19b3
Wolfgang Schäfer <wolfgang.schaefer@emnify.com>
2023-11-23 16:34:05 +0100

    Added ClarifyingQuestion class

---
+++ backend/src/main/java/com/odde/doughnut/services/ai/ClarifyingQuestion.java
@@ -0,0 +1,16 @@
+package com.odde.doughnut.services.ai;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyDescription;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+public class ClarifyingQuestion {
+
+  @JsonPropertyDescription(
+      "content of the question as plain string.")
+  @JsonProperty(required = true)
+  public String question;
+}
