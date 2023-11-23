f124646921acc2f6a9e9568d5e7c30dea9ebc9b7
deko96 <bozhinoski@icloud.com>
2023-11-23 16:02:54 +0100

    Format AIClarifyingQuestionDialog

--- frontend/tests/notes/AIClarifyingQuestionDialog.spec.ts
+++ frontend/tests/notes/AIClarifyingQuestionDialog.spec.ts
@@ -1,17 +1,17 @@
-
 import AIClarifyingQuestionDialog from "@/components/notes/AIClarifyingQuestionDialog.vue";
-import helper from "../helpers";
 import { VueWrapper } from "@vue/test-utils";
+import helper from "../helpers";
 
 describe("answering a clarifying question for note details geeration", () => {
-
   let wrapper: VueWrapper;
 
   beforeEach(() => {
     vi.useFakeTimers();
     wrapper = helper
       .component(AIClarifyingQuestionDialog)
-      .withStorageProps({ question: "Do you mean American Football or European Football?" })
+      .withStorageProps({
+        question: "Do you mean American Football or European Football?",
+      })
       .mount();
   });
 
@@ -20,17 +20,17 @@ describe("answering a clarifying question for note details geeration", () => {
     vi.useRealTimers();
   });
 
-
   it("renders the questions correctly", () => {
-    expect(wrapper.text()).toContain("Do you mean American Football or European Football?")
+    expect(wrapper.text()).toContain(
+      "Do you mean American Football or European Football?",
+    );
   });
 
   it("submitting the form propagates the submit event to the parent component", () => {
-    wrapper.find('input[name="answerToAI"]').setValue('Europe Football');
-    wrapper.find('form').trigger('submit');
+    wrapper.find('input[name="answerToAI"]').setValue("Europe Football");
+    wrapper.find("form").trigger("submit");
 
-    const [submitEvent] = wrapper.emitted().submit as string[] || [];
-    expect(submitEvent?.at(0)).toEqual("Europe Football")
+    const [submitEvent] = (wrapper.emitted().submit as string[]) || [];
+    expect(submitEvent?.at(0)).toEqual("Europe Football");
   });
-
-})
+});
