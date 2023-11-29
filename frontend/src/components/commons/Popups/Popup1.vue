42d6bbf9d0f93ca4186c294fff528f31410611f4
Terry Yin <terry.yinzhe@gmail.com>
2023-11-29 16:43:19 +0800

    new bridge built

--- frontend/src/components/commons/Popups/Popup1.vue
+++ frontend/src/components/commons/Popups/Popup1.vue
@@ -1,5 +1,5 @@
 <!-- eslint-disable vue/valid-template-root -->
-<template><slot /></template>
+<template></template>
 
 <script lang="ts">
 //
@@ -16,15 +16,14 @@ export default defineComponent({
   setup() {
     return usePopups();
   },
-  props: { show: Boolean, sidebar: String as PropType<"left" | "right"> },
+  props: {
+    sidebar: String as PropType<"left" | "right">,
+  },
   emits: ["popupDone"],
-  watch: {
-    show() {
-      if (!this.show) return;
-      this.popups
-        .dialog(this.$slots.default, this.sidebar)
-        .then((result) => this.$emit("popupDone", result));
-    },
+  mounted() {
+    this.popups
+      .dialog(this.$slots.default, this.sidebar)
+      .then((result) => this.$emit("popupDone", result));
   },
 });
 </script>
