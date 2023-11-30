c53401fc7c5acac5113332bb9e76e1c24ffc0ac1
Terry Yin <terry.yinzhe@gmail.com>
2023-11-30 11:37:13 +0800

    new bridge'

---
+++ frontend/src/components/commons/Popups/Popup1.vue
@@ -0,0 +1,36 @@
+<!-- eslint-disable vue/valid-template-root -->
+<template>
+  <Modal
+    class="popups"
+    :sidebar="sidebar"
+    @close_request="$emit('popupDone', $event)"
+  >
+    <template #body>
+      <slot />
+    </template>
+  </Modal>
+</template>
+
+<script lang="ts">
+//
+// Usage:
+//  <Popup v-if="showThisDialog">
+//    <h2>Dialog Title</h2>
+//     <button @click="popup.done()">OK</button>
+//  </Popup>
+//
+import { PropType, defineComponent } from "vue";
+
+export default defineComponent({
+  props: {
+    sidebar: String as PropType<"left" | "right">,
+  },
+  emits: ["popupDone"],
+});
+</script>
+
+<style lang="scss" scoped>
+.popups {
+  z-index: 9998;
+}
+</style>
