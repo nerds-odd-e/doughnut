271a7c9768480f63bd8dfaa94d6a0dd6b8020e06
Terry Yin <terry.yinzhe@gmail.com>
2023-12-09 13:20:04 +0800

    one pile

--- backend/src/main/java/com/odde/doughnut/controllers/json/AiCompletionParams.java
+++ backend/src/main/java/com/odde/doughnut/controllers/json/AiCompletionParams.java
@@ -1,11 +1,5 @@
 package com.odde.doughnut.controllers.json;
 
-import static com.theokanning.openai.service.OpenAiService.defaultObjectMapper;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.odde.doughnut.services.ai.ClarifyingQuestion;
-import com.odde.doughnut.services.ai.NoteDetailsCompletion;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.Getter;
@@ -24,23 +18,5 @@ public class AiCompletionParams {
       return "";
     }
     return detailsToComplete;
-  }
-
-  public String complete(JsonNode jsonNode) {
-    try {
-      NoteDetailsCompletion noteDetailsCompletion =
-          defaultObjectMapper().treeToValue(jsonNode, NoteDetailsCompletion.class);
-      return detailsToComplete = getDetailsToComplete() + noteDetailsCompletion.completion;
-    } catch (JsonProcessingException e) {
-      throw new RuntimeException(e);
-    }
-  }
-
-  public static String clarifyingQuestion(JsonNode jsonNode) {
-    try {
-      return defaultObjectMapper().treeToValue(jsonNode, ClarifyingQuestion.class).question;
-    } catch (JsonProcessingException e) {
-      throw new RuntimeException(e);
-    }
   }
 }
