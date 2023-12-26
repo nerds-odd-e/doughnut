599b8538762c6b1a38e1b3b3757b13c6800e05e0
Terry Yin <terry.yinzhe@gmail.com>
2023-12-26 16:23:33 +0800

    get rid of model api

--- backend/src/main/java/com/odde/doughnut/services/ai/OpenAIChatAboutNoteRequestBuilder1.java
+++ backend/src/main/java/com/odde/doughnut/services/ai/OpenAIChatAboutNoteRequestBuilder1.java
@@ -1,22 +1,10 @@
 package com.odde.doughnut.services.ai;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.module.jsonSchema.JsonSchema;
-import com.fasterxml.jackson.module.jsonSchema.JsonSchemaGenerator;
-import com.odde.doughnut.controllers.json.AiCompletionParams;
-import com.odde.doughnut.controllers.json.ClarifyingQuestionAndAnswer;
-import com.odde.doughnut.entities.Note;
-import com.odde.doughnut.services.ai.builder.OpenAIChatRequestBuilder;
 import com.theokanning.openai.completion.chat.*;
 
-import java.util.HashMap;
-import java.util.List;
-
-import static com.theokanning.openai.service.OpenAiService.defaultObjectMapper;
-
 public class OpenAIChatAboutNoteRequestBuilder1 extends OpenAIChatAboutNoteRequestBuilder {
-  public OpenAIChatAboutNoteRequestBuilder1() {
+  public OpenAIChatAboutNoteRequestBuilder1(String modelName) {
+    openAIChatRequestBuilder.model(modelName);
     this.openAIChatRequestBuilder.addTextMessage(
       ChatMessageRole.SYSTEM,
       "This is a PKM system using hierarchical notes, each with a topic and details, to capture atomic concepts.");
