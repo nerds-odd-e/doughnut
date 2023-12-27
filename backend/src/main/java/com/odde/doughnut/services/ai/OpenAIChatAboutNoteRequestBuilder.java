0bb7e0413f441d45ea691ff82a0f3484d419f8d5
Terry Yin <terry.yinzhe@gmail.com>
2023-12-27 17:13:00 +0800

    regrouping

--- backend/src/main/java/com/odde/doughnut/services/ai/OpenAIChatAboutNoteRequestBuilder.java
+++ backend/src/main/java/com/odde/doughnut/services/ai/OpenAIChatAboutNoteRequestBuilder.java
@@ -6,10 +6,8 @@ import com.theokanning.openai.completion.chat.*;
 public class OpenAIChatAboutNoteRequestBuilder extends OpenAIChatAboutNoteRequestBuilderBase {
   public OpenAIChatAboutNoteRequestBuilder(String modelName, Note note) {
     openAIChatRequestBuilder.model(modelName);
-    this.openAIChatRequestBuilder.addTextMessage(
-        ChatMessageRole.SYSTEM,
+    this.openAIChatRequestBuilder.addSystemMessage(
         "This is a PKM system using hierarchical notes, each with a topic and details, to capture atomic concepts.");
-    String noteOfCurrentFocus = note.getNoteDescription();
-    rawNoteContent(noteOfCurrentFocus);
+    openAIChatRequestBuilder.addSystemMessage(note.getNoteDescription());
   }
 }
