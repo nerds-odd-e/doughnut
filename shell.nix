4fefc3066c6150d62872421235c0dd5a0275d43a
Tan Yeong Sheng <tanyeongsheng@gmail.com>
2023-12-27 19:23:37 +0800

    pnpm@8.13.1

--- shell.nix
+++ shell.nix
@@ -162,7 +162,7 @@ in mkShell {
       rm -rf "$PWD/frontend/node_modules"
     fi
 
-    corepack prepare pnpm@8.13.0 --activate
+    corepack prepare pnpm@8.13.1 --activate
     pnpm --frozen-lockfile recursive install
 
     cleanup()
