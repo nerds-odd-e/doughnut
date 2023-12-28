325ce6cced79845129efaac64614aac9b4d58b86
Terry Yin <terry.yinzhe@gmail.com>
2023-12-28 16:32:13 +0800

    fix deployment

--- infra/scripts/perform-rolling-replace-app-mig.sh
+++ infra/scripts/perform-rolling-replace-app-mig.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gcloud compute instance-groups managed rolling-action replace doughnut-app-group --max-surge 1 --max-unavailable 1 --zone us-east1-b
+gcloud compute instance-groups managed rolling-action replace doughnut-app-group --max-surge 0 --max-unavailable 1 --zone us-east1-b
