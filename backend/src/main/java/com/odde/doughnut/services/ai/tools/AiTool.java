c96ecf74015132144c3f4176b2f1a9b6ad2db9f6
Terry Yin <terry.yinzhe@gmail.com>
2024-01-16 18:49:46 +0800

    move resp

--- backend/src/main/java/com/odde/doughnut/services/ai/tools/AiTool.java
+++ backend/src/main/java/com/odde/doughnut/services/ai/tools/AiTool.java
@@ -1,12 +1,24 @@
 package com.odde.doughnut.services.ai.tools;
 
+import static com.odde.doughnut.services.ai.builder.OpenAIChatRequestBuilder.askClarificationQuestion;
+import static com.odde.doughnut.services.ai.tools.AiToolFactory.COMPLETE_NOTE_DETAILS;
+import static com.theokanning.openai.service.OpenAiService.defaultObjectMapper;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kjetland.jackson.jsonSchema.JsonSchemaGenerator;
+import com.odde.doughnut.controllers.json.AiCompletionResponse;
+import com.odde.doughnut.controllers.json.ClarifyingQuestionRequiredAction;
+import com.odde.doughnut.services.ai.ClarifyingQuestion;
+import com.odde.doughnut.services.ai.NoteDetailsCompletion;
 import com.theokanning.openai.assistants.AssistantFunction;
 import com.theokanning.openai.assistants.AssistantToolsEnum;
 import com.theokanning.openai.assistants.Tool;
+import com.theokanning.openai.runs.ToolCall;
+import com.theokanning.openai.runs.ToolCallFunction;
 import java.util.Map;
+import java.util.stream.Stream;
 
 public record AiTool(String name, String description, Class<?> parameters) {
   public Tool getTool() {
@@ -25,5 +37,50 @@ public record AiTool(String name, String description, Class<?> parameters) {
     JsonNode jsonSchema = jsonSchemaGenerator.generateJsonSchema(value);
     JsonNode jsonNode = objectMapper.valueToTree(jsonSchema);
     return objectMapper.convertValue(jsonNode, Map.class);
+  }
+
+  public Stream<AiCompletionResponse> tryConsume(String detailsToComplete, ToolCall toolCall) {
+    ToolCallFunction function = toolCall.getFunction();
+    String arguments = function.getArguments();
+    if (name().equals(function.getName())) {
+      return Stream.empty();
+    }
+    JsonNode jsonNode = null;
+    try {
+      jsonNode = defaultObjectMapper().readTree(arguments);
+    } catch (JsonProcessingException e) {
+      throw new RuntimeException(e);
+    }
+
+    if (function.getName().equals(askClarificationQuestion)) {
+      ClarifyingQuestion result1;
+      try {
+        result1 = defaultObjectMapper().treeToValue(jsonNode, ClarifyingQuestion.class);
+      } catch (JsonProcessingException e) {
+        throw new RuntimeException(e);
+      }
+      AiCompletionResponse result = new AiCompletionResponse();
+      ClarifyingQuestionRequiredAction cqra = new ClarifyingQuestionRequiredAction();
+      cqra.clarifyingQuestion = result1;
+      cqra.toolCallId = toolCall.getId();
+
+      result.setClarifyingQuestionRequiredAction(cqra);
+      return Stream.of(result);
+    } else if (function.getName().equals(COMPLETE_NOTE_DETAILS)) {
+      String result1;
+      try {
+        NoteDetailsCompletion noteDetailsCompletion =
+            defaultObjectMapper().treeToValue(jsonNode, NoteDetailsCompletion.class);
+        result1 = detailsToComplete + noteDetailsCompletion.completion;
+      } catch (JsonProcessingException e) {
+        throw new RuntimeException(e);
+      }
+      AiCompletionResponse result = new AiCompletionResponse();
+      String content = result1;
+      result.setMoreCompleteContent(content);
+      return Stream.of(result);
+    }
+
+    return Stream.empty();
   }
 }
