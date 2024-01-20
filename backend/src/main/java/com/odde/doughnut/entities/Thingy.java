943bf4283cd01248845d2591c166133cf5867eaf
Terry Yin <terry.yinzhe@gmail.com>
2024-01-20 19:08:31 +0800

    more ids

--- backend/src/main/java/com/odde/doughnut/entities/Thingy.java
+++ backend/src/main/java/com/odde/doughnut/entities/Thingy.java
@@ -1,19 +1,10 @@
 package com.odde.doughnut.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
-import lombok.Getter;
 
 @MappedSuperclass
-public abstract class Thingy {
-
-  @Id
-  @Getter
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  protected Integer id;
+public abstract class Thingy extends EntityIdentifiedByIdOnly {
 
   abstract void setThing(Thing thing);
 
