82b7d63d034e5ad677738d9b1c6fb47e04cf9c93
Terry Yin <terry.yinzhe@gmail.com>
2024-01-21 17:59:44 +0800

    use save

--- backend/src/main/java/com/odde/doughnut/entities/BazaarNotebook.java
+++ backend/src/main/java/com/odde/doughnut/entities/BazaarNotebook.java
@@ -2,9 +2,6 @@ package com.odde.doughnut.entities;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
