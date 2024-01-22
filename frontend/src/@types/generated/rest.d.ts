a71340a3d5b8483e67018b93df5d01752ea33a63
Terry Yin <terry.yinzhe@gmail.com>
2024-01-22 18:35:55 +0800

    generate typescript

--- frontend/src/@types/generated/rest.d.ts
+++ frontend/src/@types/generated/rest.d.ts
@@ -367,7 +367,7 @@ declare namespace Generated {
         choices: string[];
     }
 
-    type ErrorType = "OPENAI_UNAUTHORIZED" | "BINDING_ERROR" | "OPENAI_TIMEOUT" | "OPENAI_SERVICE_ERROR";
+    type ErrorType = "OPENAI_UNAUTHORIZED" | "BINDING_ERROR" | "OPENAI_TIMEOUT" | "OPENAI_SERVICE_ERROR" | "WIKIDATA_SERVICE_ERROR";
 
     type LinkType = "no link" | "related to" | "a specialization of" | "an application of" | "an instance of" | "a part of" | "tagged by" | "an attribute of" | "the opposite of" | "author of" | "using" | "an example of" | "before" | "similar to" | "confused with";
 
