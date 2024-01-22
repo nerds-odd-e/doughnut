2b8067e4378d7127a485bb0679439b258c149134
Terry Yin <terry.yinzhe@gmail.com>
2024-01-22 12:29:47 +0800

    stop abuse binding

--- backend/src/main/java/com/odde/doughnut/controllers/RestWikidataController.java
+++ backend/src/main/java/com/odde/doughnut/controllers/RestWikidataController.java
@@ -2,15 +2,14 @@ package com.odde.doughnut.controllers;
 
 import com.odde.doughnut.controllers.json.WikidataEntityData;
 import com.odde.doughnut.controllers.json.WikidataSearchEntity;
+import com.odde.doughnut.exceptions.WikidataServiceErrorException;
 import com.odde.doughnut.services.WikidataService;
 import com.odde.doughnut.services.httpQuery.HttpClientAdapter;
 import com.odde.doughnut.testability.TestabilitySettings;
 import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
-import org.springframework.validation.BeanPropertyBindingResult;
-import org.springframework.validation.BindException;
-import org.springframework.validation.BindingResult;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,27 +31,23 @@ public class RestWikidataController {
 
   @GetMapping("/entity-data/{wikidataId}")
   public Optional<WikidataEntityData> fetchWikidataEntityDataByID(
-      @PathVariable("wikidataId") String wikidataId) throws BindException {
+      @PathVariable("wikidataId") String wikidataId) {
     try {
       return wikidataService.wrapWikidataIdWithApi(wikidataId).fetchWikidataEntityData();
     } catch (IOException e) {
-      throw buildWikidataServiceNotAvailableException("wikidataId");
+      throw new WikidataServiceErrorException(
+          "The wikidata service is not available", HttpStatus.NOT_FOUND);
     }
   }
 
   @GetMapping("/search/{search}")
   public List<WikidataSearchEntity> searchWikidata(@PathVariable("search") String search)
-      throws InterruptedException, BindException {
+      throws InterruptedException {
     try {
       return wikidataService.searchWikidata(search);
     } catch (IOException e) {
-      throw buildWikidataServiceNotAvailableException("search");
+      throw new WikidataServiceErrorException(
+          "The wikidata service is not available", HttpStatus.NOT_FOUND);
     }
-  }
-
-  private BindException buildWikidataServiceNotAvailableException(String fieldName) {
-    BindingResult bindingResult = new BeanPropertyBindingResult(null, fieldName);
-    bindingResult.rejectValue(null, "error.error", "The wikidata service is not available");
-    return new BindException(bindingResult);
   }
 }
