dbcc3167bc3712237dc2670d531f08ad56b55de1
Terry Yin <terry.yinzhe@gmail.com>
2024-01-31 09:33:33 +0800

    removing table

--- backend/src/main/java/com/odde/doughnut/entities/ReviewSetting.java
+++ backend/src/main/java/com/odde/doughnut/entities/ReviewSetting.java
@@ -1,34 +1,18 @@
 package com.odde.doughnut.entities;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
-import lombok.Setter;
 
 @Entity
 @Table(name = "review_setting")
-public class ReviewSetting {
-
-  public static final Integer defaultLevel = 0;
+public class ReviewSetting extends ReviewSettingBase {
 
   @Id
   @Getter
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Integer id;
-
-  @Column(name = "remember_spelling")
-  @Getter
-  @Setter
-  private Boolean rememberSpelling = false;
-
-  @Column(name = "skip_review")
-  @Getter
-  @Setter
-  private Boolean skipReview = false;
-
-  @Getter @Setter private Integer level = defaultLevel;
 }
