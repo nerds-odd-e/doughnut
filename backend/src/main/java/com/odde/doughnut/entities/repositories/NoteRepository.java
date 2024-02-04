75f09f9d428b1442cd2c51f3bd961055effc9230
Terry Yin <terry.yinzhe@gmail.com>
2024-02-04 20:24:17 +0800

    Simplify sql

--- backend/src/main/java/com/odde/doughnut/entities/repositories/NoteRepository.java
+++ backend/src/main/java/com/odde/doughnut/entities/repositories/NoteRepository.java
@@ -27,7 +27,7 @@ public interface NoteRepository extends CrudRepository<Note, Integer> {
   Stream<Note> searchForUserInAllMyCircle(Integer userId, @Param("pattern") String pattern);
 
   @Query(
-      value = selectFromNote + " WHERE note.notebook_id = :notebookId " + searchForTopicLike,
+      value = selectFromNote + searchForTopicLike + " AND note.notebook_id = :notebookId ",
       nativeQuery = true)
   Stream<Note> searchInNotebook(Integer notebookId, @Param("pattern") String pattern);
 
@@ -40,31 +40,24 @@ public interface NoteRepository extends CrudRepository<Note, Integer> {
   List<Note> noteWithWikidataIdWithinNotebook(
       @Param("notebookId") Integer notebookId, @Param("wikidataId") String wikidataId);
 
-  String unionNotebooksFromSubscription =
-      "          SELECT notebook_id as id FROM subscription "
-          + "             WHERE subscription.user_id = :userId ";
-
-  String joinNotebooksBegin =
-      selectFromNote + "  JOIN (" + "          SELECT notebook.id FROM notebook ";
-
-  String joinNotebooksEnd1 = "       ) nb ON nb.id = note.notebook_id " + "  WHERE 1=1 ";
+  String joinNotebooksBegin = selectFromNote + "  JOIN notebook ON notebook.id = note.notebook_id ";
 
   String inAllMySubscriptions =
-      selectFromNote + "  JOIN (" + unionNotebooksFromSubscription + joinNotebooksEnd1;
+      selectFromNote
+          + "  JOIN subscription "
+          + "             ON subscription.user_id = :userId AND subscription.notebook_id = note.notebook_id ";
 
   String inAllMyNotebooks =
       joinNotebooksBegin
           + "             JOIN ownership ON ownership.user_id = :userId "
-          + "             WHERE notebook.ownership_id = ownership.id "
-          + joinNotebooksEnd1;
+          + "             AND notebook.ownership_id = ownership.id ";
 
   String inAllMyCircles =
       joinNotebooksBegin
           + "             JOIN circle_user ON circle_user.user_id = :userId "
           + "             JOIN circle ON circle.id = circle_user.circle_id "
           + "             JOIN ownership ON circle.id = ownership.circle_id "
-          + "             WHERE notebook.ownership_id = ownership.id "
-          + joinNotebooksEnd1;
+          + "             AND notebook.ownership_id = ownership.id ";
 
-  String searchForTopicLike = " AND topic_constructor LIKE :pattern AND note.deleted_at IS NULL ";
+  String searchForTopicLike = " WHERE topic_constructor LIKE :pattern AND note.deleted_at IS NULL ";
 }
