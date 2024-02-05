354d84a0fc0724653d24339b09fe3348198b2379
Terry Yin <terry.yinzhe@gmail.com>
2024-02-05 09:14:03 +0800

    remove all native queries

--- backend/src/main/java/com/odde/doughnut/entities/repositories/NoteRepository.java
+++ backend/src/main/java/com/odde/doughnut/entities/repositories/NoteRepository.java
@@ -9,48 +9,42 @@ import org.springframework.data.repository.query.Param;
 
 public interface NoteRepository extends CrudRepository<Note, Integer> {
 
-  String selectFromNote1 = "SELECT n FROM Note n";
+  String selectFromNote = "SELECT n FROM Note n";
+  String searchForTopicLike = " WHERE n.topicConstructor LIKE :pattern AND n.deletedAt IS NULL ";
 
-  @Query(value = selectFromNote1 + " WHERE n.id IN (:ids)")
+  @Query(value = selectFromNote + " WHERE n.id IN (:ids)")
   Stream<Note> findAllByIds(List<Integer> ids);
 
-  String selectFromNote = "SELECT note.*  from note";
-
-  @Query(value = selectFromNote1 + " where n.topicConstructor = :key")
+  @Query(value = selectFromNote + " where n.topicConstructor = :key")
   Note findFirstByTopicConstructor(@Param("key") String key);
 
-  @Query(value = inAllMyNotebooks + searchForTopicLike)
+  @Query(
+      value = selectFromNote + searchForTopicLike + "  AND n.notebook.ownership.user.id = :userId ")
   Stream<Note> searchForUserInAllMyNotebooks(Integer userId, String pattern);
 
-  @Query(value = inAllMySubscriptions + searchForTopicLike)
+  @Query(
+      value =
+          selectFromNote
+              + " JOIN n.notebook.subscriptions s ON s.user.id = :userId "
+              + searchForTopicLike)
   Stream<Note> searchForUserInAllMySubscriptions(Integer userId, @Param("pattern") String pattern);
 
-  @Query(value = inAllMyCircles + searchForTopicLike)
+  @Query(
+      value =
+          selectFromNote
+              + "              JOIN n.notebook.ownership.circle.members m"
+              + "                ON m.id = :userId "
+              + searchForTopicLike)
   Stream<Note> searchForUserInAllMyCircle(Integer userId, @Param("pattern") String pattern);
 
-  @Query(value = selectFromNote1 + searchForTopicLike + " AND n.notebook.id = :notebookId ")
+  @Query(value = selectFromNote + searchForTopicLike + " AND n.notebook.id = :notebookId ")
   Stream<Note> searchInNotebook(Integer notebookId, @Param("pattern") String pattern);
 
   @Query(
       value =
           selectFromNote
-              + " WHERE note.notebook_id = :notebookId "
-              + " AND note.wikidata_id = :wikidataId AND note.wikidata_id IS NOT NULL AND note.deleted_at IS NULL ",
-      nativeQuery = true)
+              + " WHERE n.notebook.id = :notebookId "
+              + " AND n.wikidataId = :wikidataId AND n.wikidataId IS NOT NULL AND n.deletedAt IS NULL ")
   List<Note> noteWithWikidataIdWithinNotebook(
       @Param("notebookId") Integer notebookId, @Param("wikidataId") String wikidataId);
-
-  String inAllMySubscriptions =
-      selectFromNote1 + " JOIN n.notebook.subscriptions s ON s.user.id = :userId ";
-
-  String inAllMyNotebooks =
-      selectFromNote1 + "  JOIN n.notebook nb ON nb.ownership.user.id = :userId ";
-
-  String inAllMyCircles =
-      selectFromNote1
-          + "  JOIN n.notebook nb "
-          + "              JOIN nb.ownership.circle.members m"
-          + "                ON m.id = :userId ";
-
-  String searchForTopicLike = " WHERE n.topicConstructor LIKE :pattern AND n.deletedAt IS NULL ";
 }
