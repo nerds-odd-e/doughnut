fecf46f22804b6337d2edef2be4aa1c02757673c
Tan Yeong Sheng <tanyeongsheng@gmail.com>
2024-02-13 17:05:17 +0800

    fix warning on "Generating equals/hashCode implementation but without a call to superclass, even though this class does not extend java.lang.Object."

--- backend/src/main/java/com/odde/doughnut/controllers/json/AiCompletionAnswerClarifyingQuestionParams.java
+++ backend/src/main/java/com/odde/doughnut/controllers/json/AiCompletionAnswerClarifyingQuestionParams.java
@@ -1,9 +1,11 @@
 package com.odde.doughnut.controllers.json;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
+@EqualsAndHashCode(callSuper=false)
 @Data
 public class AiCompletionAnswerClarifyingQuestionParams extends AiCompletionParams {
   private String threadId;
