e2b7b60b813bd8d970abb1f6c9c30af0b302ae1f
Terry Yin <terry.yinzhe@gmail.com>
2024-02-17 18:22:54 +0800

    wrong parent

--- backend/src/main/java/com/odde/doughnut/entities/quizQuestions/QuizQuestionSpelling.java
+++ backend/src/main/java/com/odde/doughnut/entities/quizQuestions/QuizQuestionSpelling.java
@@ -2,7 +2,7 @@ package com.odde.doughnut.entities.quizQuestions;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.odde.doughnut.entities.Answer;
-import com.odde.doughnut.entities.QuizQuestionWithNoteChoices;
+import com.odde.doughnut.entities.QuizQuestionEntity;
 import com.odde.doughnut.factoryServices.quizFacotries.QuizQuestionPresenter;
 import com.odde.doughnut.factoryServices.quizFacotries.presenters.SpellingQuizPresenter;
 import jakarta.persistence.DiscriminatorValue;
@@ -10,11 +10,16 @@ import jakarta.persistence.Entity;
 
 @Entity
 @DiscriminatorValue("2")
-public class QuizQuestionSpelling extends QuizQuestionWithNoteChoices {
+public class QuizQuestionSpelling extends QuizQuestionEntity {
 
   @JsonIgnore
   public QuizQuestionPresenter buildPresenter() {
     return new SpellingQuizPresenter(this);
+  }
+
+  @Override
+  public Integer getCorrectAnswerIndex() {
+    return null;
   }
 
   @Override
--- backend/src/main/java/com/odde/doughnut/factoryServices/quizFacotries/presenters/SpellingQuizPresenter.java
+++ backend/src/main/java/com/odde/doughnut/factoryServices/quizFacotries/presenters/SpellingQuizPresenter.java
@@ -1,9 +1,23 @@
 package com.odde.doughnut.factoryServices.quizFacotries.presenters;
 
-import com.odde.doughnut.entities.QuizQuestionWithNoteChoices;
+import com.odde.doughnut.entities.Note;
+import com.odde.doughnut.entities.quizQuestions.QuizQuestionSpelling;
+import com.odde.doughnut.factoryServices.quizFacotries.QuizQuestionPresenter;
 
-public class SpellingQuizPresenter extends ClozeDescriptonQuizPresenter {
-  public SpellingQuizPresenter(QuizQuestionWithNoteChoices quizQuestion) {
-    super(quizQuestion);
+public class SpellingQuizPresenter implements QuizQuestionPresenter {
+  Note note;
+
+  public SpellingQuizPresenter(QuizQuestionSpelling quizQuestion) {
+    note = quizQuestion.getNote();
+  }
+
+  @Override
+  public String mainTopic() {
+    return "";
+  }
+
+  @Override
+  public String stem() {
+    return note.getClozeDescription().clozeDetails();
   }
 }
