8a04182342eb14288c8d216c18c90d04110775e4
Shika <luanmarucntt@gmail.com>
2024-04-03 08:45:33 +0000

    add attach audio unit test

---
+++ backend/src/test/java/com/odde/doughnut/controllers/dto/NoteAccessoriesDTOTest.java
@@ -0,0 +1,16 @@
+package com.odde.doughnut.controllers.dto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+@Transactional
+public class NoteAccessoriesDTOTest {
+
+  @Test
+  void validate() {
+  }
+}
