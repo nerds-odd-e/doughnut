00ce827c18c51f69608017c6a83fc4a36129c77f
Cert-BE <buiphuonglan1107@gmail.com>
2024-04-04 07:53:38 +0000

    [CERT] add identify paragraph

--- backend/src/main/java/com/odde/doughnut/models/Srt.java
+++ backend/src/main/java/com/odde/doughnut/models/Srt.java
@@ -1,12 +1,15 @@
 package com.odde.doughnut.models;
 
+import java.time.Duration;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import lombok.Getter;
 
 public class Srt {
+  private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("HH:mm:ss,SSS");
 
-  @Getter private String srtText;
+  private String srtText;
 
   public Srt(String srtText) {
     this.srtText = srtText;
@@ -14,16 +17,31 @@ public class Srt {
 
   public String convertSrtToText() {
     StringBuilder text = new StringBuilder();
-    String[] lines = srtText.split("\n+");
-    for (int i = 0; i < lines.length; i++) {
-      if (lines[i].substring(0, 1).matches("[a-zA-Z]")) {
-        text.append(lines[i]);
-        if (i < lines.length - 1) {
+    String[] paragraphs = srtText.split("\n\n");
+    for (int i = 0; i < paragraphs.length; i++) {
+      String[] lines = paragraphs[i].split("\n");
+      LocalTime endTime = parseTimestamp(lines[1].split(" --> ")[1]);
+      text.append(lines[2]);
+      for (int j = 3; j < lines.length; j++) {
+        text.append(" ").append(lines[j]);
+      }
+
+      if (i < paragraphs.length - 1) {
+        String nextStartTimeStr = paragraphs[i + 1].split("\n")[1].split(" --> ")[0];
+        LocalTime nextStartTime = parseTimestamp(nextStartTimeStr);
+        Duration gap = Duration.between(endTime, nextStartTime);
+        if (gap.getSeconds() >= (2 * 60)) {
+          text.append("\n");
+        } else {
           text.append("\s");
         }
       }
     }
     return text.toString();
+  }
+
+  private LocalTime parseTimestamp(String timestamp) {
+    return LocalTime.parse(timestamp, FORMATTER);
   }
 
   public boolean isSRTFormat() {
--- backend/src/test/java/com/odde/doughnut/models/SrtTest.java
+++ backend/src/test/java/com/odde/doughnut/models/SrtTest.java
@@ -43,7 +43,17 @@ class SrtTest {
   public void shouldReturnOneParagraph() {
     Srt srt =
         new Srt(
-            "1\n00:00:00,000 --> 00:02:00,000\nThis is an example of a subtitle.\n\n2\n00:02:00,000 --> 00:05:00,000\nThis is Second");
+            "1\n00:00:00,000 --> 00:02:15,000\nThis is an example of a subtitle.\n\n2\n00:02:40,000 --> 00:05:50,000\nThis is Second");
     assertEquals("This is an example of a subtitle. This is Second", srt.convertSrtToText());
+  }
+
+  @Test
+  public void shouldReturnTwoParagraph() {
+    Srt srt =
+        new Srt(
+            "1\n00:00:00,000 --> 00:02:00,000\nThis is an example of a subtitle.\n\n2\n00:02:00,000 --> 00:05:00,000\nThis is Second\n\n3\n00:07:00,000 --> 00:10:00,000\nThis is second paragraph.\n\n4\n00:10:00,000 --> 00:12:00,000\nThis is Second paragraph 2");
+    assertEquals(
+        "This is an example of a subtitle. This is Second\nThis is second paragraph. This is Second paragraph 2",
+        srt.convertSrtToText());
   }
 }
