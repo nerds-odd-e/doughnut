3882fc13c709627f3f0ef38a9e0ee773db0f141a
Tan Yeong Sheng <tanyeongsheng@gmail.com>
2024-05-06 18:40:01 +0800

    at least NodeJS@22 pnpm@9.0.6

--- shell.nix
+++ shell.nix
@@ -147,7 +147,7 @@ in mkShell {
       rm -rf "$PWD/frontend/node_modules"
     fi
 
-    corepack prepare pnpm@9.0.6 --activate
+    corepack prepare pnpm@9.1.0 --activate
     pnpm --frozen-lockfile recursive install
 
     cleanup()
