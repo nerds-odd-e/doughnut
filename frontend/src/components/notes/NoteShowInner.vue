1b3499c60f62bbb2909c514f573a4b866f040ca5
Terry Yin <terry.yinzhe@gmail.com>
2024-05-15 17:42:28 +0800

    use async

---
+++ frontend/src/components/notes/NoteShowInner.vue
@@ -0,0 +1,76 @@
+<template>
+  <NoteRealmLoader v-bind="{ noteId, storageAccessor }">
+    <template #default="{ noteRealm }">
+      <div class="row">
+        <div class="col-md-8 d-flex flex-column p-0">
+          <NoteWithLinks
+            v-bind="{
+              note: noteRealm.note,
+              links: noteRealm.links,
+              readonly,
+              storageAccessor,
+            }"
+          />
+        </div>
+        <div class="col-md-4 d-flex flex-column p-0">
+          <NoteRecentUpdateIndicator
+            v-bind="{
+              id: noteRealm.id,
+              updatedAt: noteRealm.note.updatedAt,
+            }"
+          >
+            <NoteAccessoryAsync v-bind="{ noteId: noteRealm.id, readonly }" />
+            <NoteInfoBar
+              :note-id="noteId"
+              :expanded="expandInfo"
+              :key="noteId"
+              @level-changed="$emit('levelChanged', $event)"
+              @self-evaluated="$emit('selfEvaluated', $event)"
+            />
+          </NoteRecentUpdateIndicator>
+        </div>
+      </div>
+      <ChildrenNotes
+        v-bind="{ expandChildren, storageAccessor }"
+        :notes="noteRealm.children"
+      />
+      <slot />
+      <NoteChatDialog
+        v-bind="{ selectedNote: noteRealm.note, storageAccessor }"
+      />
+    </template>
+  </NoteRealmLoader>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+import NoteWithLinks from "./core/NoteWithLinks.vue";
+import ChildrenNotes from "./ChildrenNotes.vue";
+import NoteInfoBar from "./NoteInfoBar.vue";
+import { StorageAccessor } from "../../store/createNoteStorage";
+import NoteChatDialog from "./NoteChatDialog.vue";
+import NoteAccessoryAsync from "./accessory/NoteAccessoryAsync.vue";
+import NoteRecentUpdateIndicator from "./NoteRecentUpdateIndicator.vue";
+
+export default defineComponent({
+  props: {
+    noteId: { type: Number, required: true },
+    expandChildren: { type: Boolean, required: true },
+    expandInfo: { type: Boolean, default: false },
+    readonly: { type: Boolean, default: true },
+    storageAccessor: {
+      type: Object as PropType<StorageAccessor>,
+      required: true,
+    },
+  },
+  emits: ["levelChanged", "selfEvaluated"],
+  components: {
+    NoteWithLinks,
+    ChildrenNotes,
+    NoteInfoBar,
+    NoteAccessoryAsync,
+    NoteChatDialog,
+    NoteRecentUpdateIndicator,
+  },
+});
+</script>
