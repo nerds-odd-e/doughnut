c49dea75bd063f4d7dca77f76c65e21db1268d2a
Terry Yin <terry.yinzhe@gmail.com>
2024-05-19 11:00:38 +0800

    side bar

--- frontend/src/components/notes/NoteShow.vue
+++ frontend/src/components/notes/NoteShow.vue
@@ -1,15 +1,44 @@
 <template>
   <NoteRealmLoader v-bind="{ noteId, storageAccessor }">
     <template #default="{ noteRealm }">
-      <Breadcrumb v-bind="{ notePosition: noteRealm.notePosition }" />
-      <NoteShowInner
-        v-bind="{
-          noteRealm,
-          expandChildren,
-          readonly,
-          storageAccessor,
-        }"
-      />
+      <div>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#sidebar"
+            aria-controls="sidebar"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <a class="navbar-brand" href="#">Brand</a>
+        </nav>
+        <div class="d-flex">
+          <div
+            class="d-lg-block collapse flex-column flex-shrink-0"
+            id="sidebar"
+          >
+            xxxx
+          </div>
+          <main class="flex-grow-1">
+            <div class="container-fluid">
+              <!-- Your main content goes here -->
+              <Breadcrumb v-bind="{ notePosition: noteRealm.notePosition }" />
+              <NoteShowInner
+                v-bind="{
+                  noteRealm,
+                  expandChildren,
+                  readonly,
+                  storageAccessor,
+                }"
+              />
+            </div>
+          </main>
+        </div>
+      </div>
     </template>
   </NoteRealmLoader>
 </template>
@@ -29,3 +58,14 @@ defineProps({
   },
 });
 </script>
+
+<style lang="scss" scoped>
+@import "bootstrap/scss/bootstrap";
+
+#sidebar {
+  width: 100%;
+  @include media-breakpoint-up(lg) {
+    width: 18rem;
+  }
+}
+</style>
