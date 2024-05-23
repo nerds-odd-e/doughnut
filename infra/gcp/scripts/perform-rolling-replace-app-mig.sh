f1a4dc13d3c6595297d2d7cba90f51a71f2df763
Tan Yeong Sheng <tanyeongsheng@gmail.com>
2024-05-23 16:48:36 +0800

    most-disruptive-allowed-action refresh

--- infra/gcp/scripts/perform-rolling-replace-app-mig.sh
+++ infra/gcp/scripts/perform-rolling-replace-app-mig.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gcloud compute instance-groups managed rolling-action refresh doughnut-app-group --max-surge 1 --max-unavailable 0 --zone us-east1-b
+gcloud compute instance-groups managed rolling-action start-update doughnut-app-group --minimal-action refresh --most-disruptive-allowed-action refresh --max-surge 1 --max-unavailable 0 --zone us-east1-b
