ae3a5bec31a899f77351473455ce84463e3b7f55
Tan Yeong Sheng <tanyeongsheng@gmail.com>
2024-05-23 17:04:50 +0800

    without project & region reference for version option

--- infra/gcp/scripts/perform-rolling-replace-app-mig.sh
+++ infra/gcp/scripts/perform-rolling-replace-app-mig.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gcloud compute instance-groups managed rolling-action start-update doughnut-app-group --minimal-action refresh --most-disruptive-allowed-action refresh --max-surge 1 --max-unavailable 0 --zone us-east1-b --version='template=projects/carbon-syntax-298809/regions/us-east1-b/instanceTemplates/doughnut-app-debian12-zulu21-openai-mig-template'
+gcloud compute instance-groups managed rolling-action start-update doughnut-app-group --minimal-action refresh --most-disruptive-allowed-action refresh --max-surge 1 --max-unavailable 0 --zone us-east1-b --version='template=doughnut-app-debian12-zulu21-openai-mig-template'
