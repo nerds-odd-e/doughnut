f24f504e255e3d74a5079b8c89833de04b5f02ec
Tan Yeong Sheng <tanyeongsheng@gmail.com>
2024-05-24 10:46:41 +0800

    minReadySec is only avail in beta gcloud CLI

--- infra/gcp/scripts/perform-rolling-replace-app-mig.sh
+++ infra/gcp/scripts/perform-rolling-replace-app-mig.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gcloud compute instance-groups managed rolling-action start-update doughnut-app-group --minimal-action replace --most-disruptive-allowed-action replace --max-surge 1 --max-unavailable 0 --minReadySec 50 --zone us-east1-b --version='template=doughnut-app-debian12-zulu21-openai-mig-template'
+gcloud compute instance-groups managed rolling-action start-update doughnut-app-group --minimal-action replace --most-disruptive-allowed-action replace --max-surge 1 --max-unavailable 0 --zone us-east1-b --version='template=doughnut-app-debian12-zulu21-openai-mig-template'
