60d93d460dffdea354c55384ad22b3ea7716c2bb
Tan Yeong Sheng <tanyeongsheng@gmail.com>
2024-05-24 10:39:25 +0800

    retry using maxSurge 1 maxUnavailable 0 with replace rolling-action

--- infra/gcp/scripts/perform-rolling-replace-app-mig.sh
+++ infra/gcp/scripts/perform-rolling-replace-app-mig.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gcloud compute instance-groups managed rolling-action replace doughnut-app-group --max-surge 0 --max-unavailable 1 --zone us-east1-b
+gcloud compute instance-groups managed rolling-action start-update doughnut-app-group --minimal-action replace --most-disruptive-allowed-action replace --max-surge 1 --max-unavailable 0 --minReadySec 50 --zone us-east1-b --version='template=doughnut-app-debian12-zulu21-openai-mig-template'
