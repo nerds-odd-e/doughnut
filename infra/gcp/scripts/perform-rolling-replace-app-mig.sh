3bb57ac44a3c6362452bd39026a362cdceeff01b
Tan Yeong Sheng <tanyeongsheng@gmail.com>
2024-05-25 20:52:23 +0800

    correct options for rolling-action replace

--- infra/gcp/scripts/perform-rolling-replace-app-mig.sh
+++ infra/gcp/scripts/perform-rolling-replace-app-mig.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gcloud compute instance-groups managed rolling-action replace doughnut-app-group --minimal-action restart --most-disruptive-allowed-action replace --max-surge 1 --max-unavailable 0 --zone us-east1-b
+gcloud compute instance-groups managed rolling-action replace doughnut-app-group --max-surge 1 --max-unavailable 0 --zone us-east1-b
