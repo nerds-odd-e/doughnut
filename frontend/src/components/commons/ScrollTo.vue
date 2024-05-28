d632137ec94ca00aa2501c111dae16a633787731
Terry Yin <terry.yinzhe@gmail.com>
2024-05-28 08:25:39 +0800

    remove test warning

--- frontend/src/components/commons/ScrollTo.vue
+++ frontend/src/components/commons/ScrollTo.vue
@@ -8,6 +8,8 @@ import { Ref, nextTick, onMounted, ref, onBeforeUnmount } from "vue";
 
 const scrollRef: Ref<HTMLElement | null> = ref(null);
 
+let observer: IntersectionObserver | null = null;
+
 onMounted(async () => {
   await nextTick();
 
@@ -17,7 +19,7 @@ onMounted(async () => {
     }
   };
 
-  const observer = new IntersectionObserver(handleIntersection, {
+  observer = new IntersectionObserver(handleIntersection, {
     root: null, // Use the viewport as the root
     threshold: 0, // Trigger when the element is not visible at all
   });
@@ -25,12 +27,12 @@ onMounted(async () => {
   if (scrollRef.value) {
     observer.observe(scrollRef.value);
   }
+});
 
-  onBeforeUnmount(() => {
-    if (scrollRef.value) {
-      observer.unobserve(scrollRef.value);
-    }
-  });
+onBeforeUnmount(() => {
+  if (scrollRef.value) {
+    observer.unobserve(scrollRef.value);
+  }
 });
 </script>
 
