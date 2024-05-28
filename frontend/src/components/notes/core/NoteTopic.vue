c4cc3aef8eb571f21bff54a9b79ad1b75be024f4
Terry Yin <terry.yinzhe@gmail.com>
2024-05-28 18:20:42 +0800

    to setup

--- frontend/src/components/notes/core/NoteTopic.vue
+++ frontend/src/components/notes/core/NoteTopic.vue
@@ -2,12 +2,8 @@
   {{ topic }}
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
-
-export default defineComponent({
-  props: {
-    topic: { type: String, required: true },
-  },
+<script setup lang="ts">
+defineProps({
+  topic: { type: String, required: true },
 });
 </script>
