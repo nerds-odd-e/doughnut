030dead2e97bf3a59183fb522cd6612d06c47f2c
Terry Yin <terry.yinzhe@gmail.com>
2024-05-29 11:07:42 +0800

    add icon

--- frontend/src/components/notes/core/NoteTopic.vue
+++ frontend/src/components/notes/core/NoteTopic.vue
@@ -1,8 +1,9 @@
 <template>
   <template v-if="note.targetForTopic">
-    <span>
-      {{ note.topicConstructor.substring(1) }}
+    <span style="font-size: 50%">
+      {{ linkType }}
     </span>
+    <SvgLinkTypeIcon :link-type="linkType" :inverse-icon="true" />
     &nbsp;
     <span>
       {{ note.targetForTopic.topicConstructor }}
@@ -15,9 +16,16 @@
 
 <script setup lang="ts">
 import { Note } from "@/generated/backend";
-import { PropType } from "vue";
+import { PropType, computed, ref } from "vue";
+import SvgLinkTypeIcon from "@/components/svgs/SvgLinkTypeIcon.vue";
 
-defineProps({
+const props = defineProps({
   note: { type: Object as PropType<Note>, required: true },
 });
+
+const reactiveProps = ref(props);
+
+const linkType = computed(() =>
+  reactiveProps.value.note.topicConstructor.substring(1),
+);
 </script>
