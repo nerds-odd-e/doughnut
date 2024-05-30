813a74b8299c7ef98c0b449eb116da6879d558a9
Terry Yin <terry.yinzhe@gmail.com>
2024-05-30 13:48:25 +0800

    setup

--- frontend/src/components/links/LinkNoteFinalize.vue
+++ frontend/src/components/links/LinkNoteFinalize.vue
@@ -37,8 +37,8 @@
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent, PropType } from "vue";
+<script setup lang="ts">
+import { PropType, ref } from "vue";
 import { LinkCreation, Note } from "@/generated/backend";
 import LinkTypeSelect from "./LinkTypeSelect.vue";
 import CheckInput from "../form/CheckInput.vue";
@@ -47,56 +47,45 @@ import SvgGoBack from "../svgs/SvgGoBack.vue";
 import usePopups from "../commons/Popups/usePopups";
 import { StorageAccessor } from "../../store/createNoteStorage";
 
-export default defineComponent({
-  setup() {
-    return {
-      ...usePopups(),
-    };
-  },
-  name: "LinkNoteFinalize",
-  props: {
-    note: { type: Object as PropType<Note>, required: true },
-    targetNote: { type: Object as PropType<Note>, required: true },
-    storageAccessor: {
-      type: Object as PropType<StorageAccessor>,
-      required: true,
-    },
-  },
-  components: { LinkTypeSelect, SvgGoBack, CheckInput, RadioButtons },
-  emits: ["success", "goBack"],
-  data() {
-    return {
-      formData: {
-        asFirstChild: false,
-        linkType: "no link",
-        moveUnder: false,
-      } as LinkCreation,
-      linkFormErrors: {
-        asFirstChild: undefined as string | undefined,
-        linkType: undefined as string | undefined,
-        moveUnder: undefined as string | undefined,
-      },
-    };
-  },
-  methods: {
-    async createLink() {
-      if (this.formData.moveUnder && this.note.parentId === null) {
-        if (
-          !(await this.popups.confirm(
-            `"${this.note.topic}" is a top level notebook. Do you want to move it under other notebook?`,
-          ))
-        ) {
-          return;
-        }
-      }
-      this.storageAccessor
-        .storedApi()
-        .createLink(this.note.id, this.targetNote.id, this.formData)
-        .then((r) => this.$emit("success", r))
-        .catch((res) => {
-          this.linkFormErrors = res;
-        });
-    },
+const { popups } = usePopups();
+const props = defineProps({
+  note: { type: Object as PropType<Note>, required: true },
+  targetNote: { type: Object as PropType<Note>, required: true },
+  storageAccessor: {
+    type: Object as PropType<StorageAccessor>,
+    required: true,
   },
 });
+const emit = defineEmits(["success", "goBack"]);
+
+const formData = ref<LinkCreation>({
+  asFirstChild: false,
+  moveUnder: false,
+  linkType: Note.linkType.NO_LINK,
+});
+
+const linkFormErrors = ref({
+  asFirstChild: undefined as string | undefined,
+  linkType: undefined as string | undefined,
+  moveUnder: undefined as string | undefined,
+});
+
+const createLink = async () => {
+  if (formData.value.moveUnder && props.note.parentId === null) {
+    if (
+      !(await popups.confirm(
+        `"${props.note.topic}" is a top level notebook. Do you want to move it under other notebook?`,
+      ))
+    ) {
+      return;
+    }
+  }
+  props.storageAccessor
+    .storedApi()
+    .createLink(props.note.id, props.targetNote.id, formData.value)
+    .then((r) => emit("success", r))
+    .catch((res) => {
+      linkFormErrors.value = res;
+    });
+};
 </script>
