98d3fefb1d913b9c67188c395e14338505247f2a
KJ <angkj.coding@gmail.com>
2024-06-04 12:58:22 +0800

    Initial Assessment Controller with test

---
+++ backend/src/main/java/com/odde/doughnut/controllers/RestAssessmentController.java
@@ -0,0 +1,33 @@
+package com.odde.doughnut.controllers;
+
+import com.odde.doughnut.entities.Notebook;
+import com.odde.doughnut.entities.QuizQuestion;
+import com.odde.doughnut.exceptions.UnexpectedNoAccessRightException;
+import com.odde.doughnut.models.UserModel;
+import io.swagger.v3.oas.annotations.media.Schema;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/assessment")
+class RestAssessmentController {
+  private final UserModel currentUser;
+
+  public RestAssessmentController(
+      UserModel currentUser) {
+    this.currentUser = currentUser;
+  }
+
+  @PostMapping("/generate-assessment/{notebook}")
+  @Transactional
+  public QuizQuestion generateAssessment(
+    @PathVariable @Schema(type = "integer") Notebook notebook
+  ) throws UnexpectedNoAccessRightException {
+    currentUser.assertLoggedIn();
+    currentUser.assertAuthorization(notebook);
+    return null;
+  }
+}
---
+++ backend/src/test/java/com/odde/doughnut/controllers/RestAssessmentControllerTests.java
@@ -0,0 +1,61 @@
+package com.odde.doughnut.controllers;
+
+import com.odde.doughnut.entities.Note;
+import com.odde.doughnut.entities.Notebook;
+import com.odde.doughnut.factoryServices.ModelFactoryService;
+import com.odde.doughnut.models.UserModel;
+import com.odde.doughnut.testability.MakeMe;
+import com.odde.doughnut.testability.OpenAIChatCompletionMock;
+import com.theokanning.openai.client.OpenAiApi;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@Transactional
+public class RestAssessmentControllerTests {
+  @Mock
+  OpenAiApi openAiApi;
+
+  @Autowired
+  ModelFactoryService modelFactoryService;
+
+  @Autowired
+  MakeMe makeMe;
+  private UserModel userModel;
+  private Notebook notebook;
+  private Note topNote;
+  private RestAssessmentController controller;
+  OpenAIChatCompletionMock openAIChatCompletionMock;
+
+  @BeforeEach
+  void setup() {
+    openAIChatCompletionMock = new OpenAIChatCompletionMock(openAiApi);
+    userModel = makeMe.aUser().toModelPlease();
+    controller = new RestAssessmentController(userModel);
+    topNote = makeMe.aNote().creatorAndOwner(userModel).please();
+  }
+
+  @Nested
+  class generateAssessmentTest {
+    @BeforeEach
+    void setup() {
+      notebook = topNote.getNotebook();
+    }
+    @Test
+    void whenNotLogin() {
+      userModel = modelFactoryService.toUserModel(null);
+      controller = new RestAssessmentController(userModel);
+      assertThrows(ResponseStatusException.class, () -> controller.generateAssessment(notebook));
+    }
+  }
+}
