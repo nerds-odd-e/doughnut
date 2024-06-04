63423d06a7270fdc058a5ff2b7c087c0965fc791
Kewen, Tan <kewen.tan@live.com>
2024-06-04 15:53:30 +0800

    add seed quiz question domain model

---
+++ frontend/src/generated/backend/models/SeedQuizQuestion.ts
@@ -0,0 +1,10 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type SeedQuizQuestion = {
+    seedQuizQuestions?: Array<Record<string, string>>;
+    externalIdentifier?: string;
+    circleName?: string;
+};
+
