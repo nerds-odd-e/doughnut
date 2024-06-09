b4a7ed776619979f742d8857684c4c74582c674d
Terry Yin <terry.yinzhe@gmail.com>
2024-06-09 19:10:35 +0800

    more ut

--- frontend/tests/pages/AssessmentPage.spec.ts
+++ frontend/tests/pages/AssessmentPage.spec.ts
@@ -1,3 +1,4 @@
+import { screen } from "@testing-library/vue";
 import { describe, it } from "vitest";
 import AssessmentPage from "@/pages/AssessmentPage.vue";
 import helper from "../helpers";
@@ -10,18 +11,30 @@ describe("assessment page", () => {
     document.body.appendChild(teleportTarget);
   });
 
-  it("calls API ONCE on mount", async () => {
+  describe("assessment with one quesiton", () => {
     const notebook = makeMe.aNotebook.please();
     const quizQuestion = makeMe.aQuizQuestion.please();
-    helper.managedApi.restAssessmentController.generateAssessmentQuestions = vi
-      .fn()
-      .mockResolvedValue([quizQuestion]);
-    helper
-      .component(AssessmentPage)
-      .withProps({ notebookId: notebook.id })
-      .render();
-    expect(
-      helper.managedApi.restAssessmentController.generateAssessmentQuestions,
-    ).toBeCalledTimes(1);
+    beforeEach(() => {
+      helper.managedApi.restAssessmentController.generateAssessmentQuestions =
+        vi.fn().mockResolvedValue([quizQuestion]);
+    });
+
+    it("calls API ONCE on mount", async () => {
+      helper
+        .component(AssessmentPage)
+        .withProps({ notebookId: notebook.id })
+        .render();
+      expect(
+        helper.managedApi.restAssessmentController.generateAssessmentQuestions,
+      ).toBeCalledTimes(1);
+    });
+
+    it("renders the questions", async () => {
+      helper
+        .component(AssessmentPage)
+        .withProps({ notebookId: notebook.id })
+        .render();
+      await screen.findByText(quizQuestion.multipleChoicesQuestion.stem!);
+    });
   });
 });
