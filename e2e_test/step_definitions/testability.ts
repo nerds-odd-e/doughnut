c497d7d2a67b8156fd3994c127854130864c8904
Tan Yeong Sheng <tanyeongsheng@gmail.com>
2024-06-09 13:31:10 +0800

    remove dead/invalid import from ignored/dead test code

--- e2e_test/step_definitions/testability.ts
+++ e2e_test/step_definitions/testability.ts
@@ -3,8 +3,7 @@
 /// <reference types="../support" />
 // @ts-check
 
-import { Given, Then, When } from "@badeball/cypress-cucumber-preprocessor"
-import TestabilityHelper from "../support/TestabilityHelper"
+import { Then, When } from "@badeball/cypress-cucumber-preprocessor"
 import start from "../start"
 
 When("Someone triggered an exception", () => {
@@ -13,26 +12,6 @@ When("Someone triggered an exception", () => {
 
 Then("an admin should see {string} in the failure report", (content: string) => {
   start.loginAsAdminAndGoToAdminDashboard().goToFailureReportList().shouldContain(content)
-})
-
-When("I should see a new open issue on github", () => {
-  start.testability().then((testability: TestabilityHelper) => {
-    testability.getTestabilityApiSuccessfully(cy, "github_issues").then((response) => {
-      expect(response.body.length).to.equal(1)
-    })
-  })
-})
-
-Given("Use real github sandbox and there are no open issues on github", () => {
-  start
-    .testability()
-    .then((testability: TestabilityHelper) =>
-      testability.postToTestabilityApiSuccessfully(
-        cy,
-        "use_real_sandbox_github_and_close_all_github_issues",
-        {},
-      ),
-    )
 })
 
 Then("The {string} alert {string}", (expectedContent: string, shouldExistOrNot: string) => {
