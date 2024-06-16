5aedf19461dba43e61d07a985b55108f28363991
Terry Yin <terry.yinzhe@gmail.com>
2024-06-16 15:08:06 +0800

    revert change

--- e2e_test/support/commands.ts
+++ e2e_test/support/commands.ts
@@ -82,7 +82,6 @@ Cypress.Commands.add("clearFocusedText", () => {
 
 Cypress.Commands.add("replaceFocusedTextAndEnter", (text) => {
   cy.clearFocusedText().type(text).type("{shift}{enter}")
-  cy.pageIsNotLoading()
 })
 
 Cypress.Commands.add("inPlaceEdit", (noteAttributes) => {
