a9ce5a39c61551d16612fbd4df6c2aedd0bfaaa3
Terry Yin <terry.yinzhe@gmail.com>
2024-06-16 18:06:25 +0800

    use setup syntax

--- frontend/src/components/notes/NoteAddQuestion.vue
+++ frontend/src/components/notes/NoteAddQuestion.vue
@@ -44,102 +44,92 @@
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent, PropType } from "vue";
+<script setup lang="ts">
+import { PropType, computed, ref } from "vue";
 import useLoadingApi from "@/managedApi/useLoadingApi";
 import { Note, QuizQuestionAndAnswer } from "@/generated/backend";
 import isMCQWithAnswerValid from "@/models/isMCQWithAnswerValid";
 import isRefineMCQWithAnswerValid from "@/models/isRefineMCQWithAnswerValid";
 import TextArea from "../form/TextArea.vue";
 
-export default defineComponent({
-  setup() {
-    return useLoadingApi();
-  },
-  props: {
-    note: {
-      type: Object as PropType<Note>,
-      required: true,
-    },
-  },
-  data() {
-    return {
-      quizQuestionAndAnswer: <QuizQuestionAndAnswer>{
-        correctAnswerIndex: 0,
-        quizQuestion: {
-          multipleChoicesQuestion: {
-            stem: "",
-            choices: ["", ""],
-          },
-        },
-      },
-      minOptions: 2, // Minimum number of options
-      maxOptions: 10, // Maximum number of options
-    };
-  },
-  emits: ["close-dialog"],
-  computed: {
-    isValidQuestion() {
-      return isMCQWithAnswerValid(this.quizQuestionAndAnswer);
-    },
-    isValidRefine() {
-      return isRefineMCQWithAnswerValid(this.quizQuestionAndAnswer);
-    },
-    multipleChoicesQuestion() {
-      return this.quizQuestionAndAnswer.quizQuestion.multipleChoicesQuestion;
-    },
-    isUserDidInputQuestion() {
-      return (
-        this.multipleChoicesQuestion.stem &&
-        this.multipleChoicesQuestion.stem.trim().length > 0
-      );
-    },
-
-    isUserDidInputChoices() {
-      for (let i = 0; i < this.multipleChoicesQuestion.choices.length; i += 1) {
-        if (this.multipleChoicesQuestion.choices[i]) {
-          return true;
-        }
-      }
-      return false;
-    },
-  },
-  methods: {
-    addOption() {
-      if (this.multipleChoicesQuestion.choices.length < this.maxOptions) {
-        this.multipleChoicesQuestion.choices.push("");
-      }
-    },
-
-    removeOption() {
-      if (this.multipleChoicesQuestion.choices.length > this.minOptions) {
-        this.multipleChoicesQuestion.choices.pop();
-      }
-    },
-    async submitQuestion() {
-      const quizQuestion = this.quizQuestionAndAnswer;
-      const response =
-        await this.managedApi.restQuizQuestionController.addQuestionManually(
-          this.note.id,
-          quizQuestion,
-        );
-      this.$emit("close-dialog", response);
-    },
-    async refineQuestion() {
-      const quizQuestion = this.quizQuestionAndAnswer;
-      const response =
-        await this.managedApi.restQuizQuestionController.refineQuestion(
-          this.note.id,
-          quizQuestion,
-        );
-      this.quizQuestionAndAnswer = response;
-    },
-    async generateQuestionByAI() {
-      this.quizQuestionAndAnswer =
-        await this.managedApi.restQuizQuestionController.generateAiQuestionWithoutSave(
-          this.note.id,
-        );
-    },
+const { managedApi } = useLoadingApi();
+const props = defineProps({
+  note: {
+    type: Object as PropType<Note>,
+    required: true,
   },
 });
+
+const quizQuestionAndAnswer = ref<QuizQuestionAndAnswer>({
+  correctAnswerIndex: 0,
+  quizQuestion: {
+    multipleChoicesQuestion: {
+      stem: "",
+      choices: ["", ""],
+    },
+  },
+} as QuizQuestionAndAnswer);
+const minOptions = 2; // Minimum number of options
+const maxOptions = 10; // Maximum number of options
+
+const emit = defineEmits(["close-dialog"]);
+
+const isValidQuestion = computed(() =>
+  isMCQWithAnswerValid(quizQuestionAndAnswer.value),
+);
+const isValidRefine = computed(() =>
+  isRefineMCQWithAnswerValid(quizQuestionAndAnswer.value),
+);
+const multipleChoicesQuestion = computed(
+  () => quizQuestionAndAnswer.value.quizQuestion.multipleChoicesQuestion,
+);
+const isUserDidInputChoices = computed(() => {
+  for (let i = 0; i < multipleChoicesQuestion.value.choices.length; i += 1) {
+    if (multipleChoicesQuestion.value.choices[i]) {
+      return true;
+    }
+  }
+  return false;
+});
+const isUserDidInputQuestion = computed(() => {
+  return (
+    multipleChoicesQuestion.value.stem &&
+    multipleChoicesQuestion.value.stem.trim().length > 0
+  );
+});
+
+const addOption = () => {
+  if (multipleChoicesQuestion.value.choices.length < maxOptions) {
+    multipleChoicesQuestion.value.choices.push("");
+  }
+};
+
+const removeOption = () => {
+  if (multipleChoicesQuestion.value.choices.length > minOptions) {
+    multipleChoicesQuestion.value.choices.pop();
+  }
+};
+const submitQuestion = async () => {
+  const quizQuestion = quizQuestionAndAnswer.value;
+  const response =
+    await managedApi.restQuizQuestionController.addQuestionManually(
+      props.note.id,
+      quizQuestion,
+    );
+  emit("close-dialog", response);
+};
+const refineQuestion = async () => {
+  const quizQuestion = quizQuestionAndAnswer.value;
+  const response = await managedApi.restQuizQuestionController.refineQuestion(
+    props.note.id,
+    quizQuestion,
+  );
+  quizQuestionAndAnswer.value = response;
+};
+const generateQuestionByAI = async () => {
+  quizQuestionAndAnswer.value =
+    await managedApi.restQuizQuestionController.generateAiQuestionWithoutSave(
+      props.note.id,
+    );
+};
 </script>
