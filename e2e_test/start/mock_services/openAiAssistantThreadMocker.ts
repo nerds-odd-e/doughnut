8496cde5c14adf76e6ef8a9a6d426254384771e8
Terry Yin <terry.yinzhe@gmail.com>
2024-06-20 13:19:27 +0800

    duplicate the mock in e2e

--- e2e_test/start/mock_services/openAiAssistantThreadMocker.ts
+++ e2e_test/start/mock_services/openAiAssistantThreadMocker.ts
@@ -1,104 +1,30 @@
 import ServiceMocker from "../../support/ServiceMocker"
-import testability from "../testability"
-import createOpenAiChatCompletionMock from "./createOpenAiChatCompletionMock"
 
-const openAiService = () => {
-  const serviceMocker = new ServiceMocker("openAi", 5001)
+const openAiAssistantThreadMocker = (serviceMocker: ServiceMocker) => {
   return {
-    mock() {
-      testability().mockService(serviceMocker)
-    },
-    restore() {
-      testability().restoreMockedService(serviceMocker)
-    },
-
-    restartImposter() {
-      return serviceMocker.install()
-    },
-
-    chatCompletion() {
-      return createOpenAiChatCompletionMock(serviceMocker)
-    },
-
-    stubCreateImage() {
-      return serviceMocker.stubPoster(`/images/generations`, {
-        created: 1589478378,
-        data: [
-          {
-            url: "https://moon",
-            b64_json:
-              "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNk+A8AAQUBAScY42YAAAAASUVORK5CYII=",
-          },
-        ],
-      })
-    },
-
-    stubOpenAiCompletionWithErrorResponse() {
-      return serviceMocker.stubGetterWithError500Response(`/*`, {})
-    },
-
-    async alwaysResponseAsUnauthorized() {
-      await serviceMocker.install()
-      await serviceMocker.stubPosterUnauthorized(`/*`, {
-        status: "BAD_REQUEST",
-        message: "nah nah nah, you need a valid token",
-        error: {
-          "OpenAi Error": "BAD_REQUEST",
-        },
-      })
-    },
-
-    stubOpenAiUploadResponse(shouldSuccess: boolean) {
-      if (shouldSuccess) {
-        return serviceMocker.stubPoster(`/files`, {
-          id: "file-abc123",
-          object: "file",
-          bytes: 175,
-          created_at: 1613677385,
-          filename: "Question-%s.jsonl",
-          purpose: "fine-tune",
-        })
-      } else {
-        return serviceMocker.stubPosterWithError500Response("/v1/files", {})
-      }
-    },
-
-    async stubCreateAssistant(newId: string, _nameOfAssistant: string, modelName: string) {
-      return await serviceMocker.mockMatchsAndNotMatches(
-        `/assistants`,
-        {
-          name: _nameOfAssistant,
-          model: modelName,
-        },
-        undefined,
-        {
-          id: newId,
-        },
-      )
-    },
-
-    async stubCreateThreadRunAndSubmitOutput(threadId: string) {
-      await serviceMocker.stubPoster(`/threads`, {
-        id: threadId,
-      })
-      // for creating a message
-      await serviceMocker.stubPoster(`/threads/${threadId}/messages`, {
-        id: "msg-abc123",
-      })
-      await serviceMocker.stubPoster(`/threads/${threadId}/runs`, {
-        id: "run-abc123",
-        status: "queued",
-      })
-      return await serviceMocker.stubPoster(
-        `/threads/${threadId}/runs/run-abc123/submit_tool_outputs`,
-        {
-          id: "run-abc123",
-          status: "queued",
-        },
-      )
-    },
     thread(threadId: string) {
       return {
+        async stubCreateThreadRunAndSubmitOutput() {
+          await serviceMocker.stubPoster(`/threads`, {
+            id: threadId,
+          })
+          // for creating a message
+          await serviceMocker.stubPoster(`/threads/${threadId}/messages`, {
+            id: "msg-abc123",
+          })
+          await serviceMocker.stubPoster(`/threads/${threadId}/runs`, {
+            id: "run-abc123",
+            status: "queued",
+          })
+          return await serviceMocker.stubPoster(
+            `/threads/${threadId}/runs/run-abc123/submit_tool_outputs`,
+            {
+              id: "run-abc123",
+              status: "queued",
+            },
+          )
+        },
+
         async stubRetrieveRunsThatReplyWithMessage(msg: string) {
           const responses = [
             {
@@ -209,4 +135,4 @@ const openAiService = () => {
   }
 }
 
-export default openAiService
+export default openAiAssistantThreadMocker
