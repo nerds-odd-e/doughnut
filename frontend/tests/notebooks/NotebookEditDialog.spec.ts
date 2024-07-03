72c6d286c2cfc43106d19f5f34d7d8e4111789bb
Jacek Bochenek <jacek@odd-e.com>
2024-07-03 08:58:15 +0200

    Properly failing frontend unit test for the number of questions in assessment bug - now with the testfile actually added

---
+++ frontend/tests/notebooks/NotebookEditDialog.spec.ts
@@ -0,0 +1,13 @@
+import makeMe from 'tests/fixtures/makeMe'
+import helper from "../helpers"
+import NotebookEditDialog from "../../src/components/notebook/NotebookEditDialog.vue"
+
+describe('NoteBookEditDialog.vue', () => {
+
+      it.skip('shows the current number of questions in assessment if set', () => {
+        const notebook = makeMe.aNotebook.numberOfQuestionsInAssessment(4).please()
+        const wrapper = helper.component(NotebookEditDialog).withProps({notebook:notebook}).mount()
+        const input = wrapper.find("input[id='notebook-numberOfQuestionsInAssessment']")
+        expect((input.element as HTMLInputElement).value).toBe('4')
+      })
+})
