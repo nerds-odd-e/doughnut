ea6f7312e204fdea85eb0f3644de0d9c80c3038b
Jacek Bochenek <jacek@odd-e.com>
2024-07-03 10:19:18 +0200

    Linter fix

--- frontend/tests/notebooks/NotebookEditDialog.spec.ts
+++ frontend/tests/notebooks/NotebookEditDialog.spec.ts
@@ -6,7 +6,7 @@ describe('NoteBookEditDialog.vue', () => {
 
       it.skip('shows the current number of questions in assessment if set', () => {
         const notebook = makeMe.aNotebook.numberOfQuestionsInAssessment(4).please()
-        const wrapper = helper.component(NotebookEditDialog).withProps({notebook:notebook}).mount()
+        const wrapper = helper.component(NotebookEditDialog).withProps({notebook}).mount()
         const input = wrapper.find("input[id='notebook-numberOfQuestionsInAssessment']")
         expect((input.element as HTMLInputElement).value).toBe('4')
       })
