147542de523517cd7c2a422f61195a1cda5dd82f
Arne Henrik Storm <arnehenrik.storm@gmail.com>
2024-07-03 11:49:01 +0200

    fixed lint error

--- e2e_test/step_definitions/assessment.ts
+++ e2e_test/step_definitions/assessment.ts
@@ -24,7 +24,7 @@ Then(
 When('I answer with the following answers:',
   function (table: DataTable) {
     start.assumeAssessmentPage().expectQuestion(table.hashes()[0]!.question).answer(table.hashes()[0]!.answer)
-});
+})
 
 When(
   "{int} subsequent attempts of assessment on the {string} notebook should not have the same questions each time",
