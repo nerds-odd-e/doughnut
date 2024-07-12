4bc87609b17d4bdf75bcd3b01bafb0b6e26fcc99
Terry Yin <terry.yinzhe@gmail.com>
2024-07-12 09:25:21 +0800

    page obj

--- e2e_test/start/pageObjects/AssessmentHistoryPage.ts
+++ e2e_test/start/pageObjects/AssessmentHistoryPage.ts
@@ -1,9 +1,18 @@
-export const expectAssessmentHistory = () => {
-  cy.findByText('Assessment History')
-  return {}
-}
-
 export const navigateToAssessmentHistory = () => {
   cy.visit('/assessment-history')
-  return expectAssessmentHistory()
+  cy.findByText('Assessment History')
+  return {
+    expectAssessmentHistory: (rows: Record<string, string>[]) => {
+      cy.get('table tbody tr').should('have.length', rows.length)
+      rows.forEach((row, index) => {
+        cy.get('table tbody tr')
+          .eq(index)
+          .within(() => {
+            cy.findByText(row['notebook topic']!).should('exist')
+            cy.findByText(row.score!).should('exist')
+            cy.findByText(row['total questions']!).should('exist')
+          })
+      })
+    },
+  }
 }
--- e2e_test/step_definitions/assessment_history.ts
+++ e2e_test/step_definitions/assessment_history.ts
@@ -4,18 +4,7 @@ import start from '../start'
 Then(
   'I must see the following assessments in my assessment history:',
   (dataTable: DataTable) => {
-    start.navigateToAssessmentHistory()
-    // look for exactly one row in a table and assert the content of each cell: assessment topic, correct answers, total questions
     const rows = dataTable.hashes()
-    cy.get('table tbody tr').should('have.length', rows.length)
-    rows.forEach((row, index) => {
-      cy.get('table tbody tr')
-        .eq(index)
-        .within(() => {
-          cy.findByText(row['notebook topic'] ?? '').should('exist')
-          cy.findByText(row.score ?? '').should('exist')
-          cy.findByText(row['total questions'] ?? '').should('exist')
-        })
-    })
+    start.navigateToAssessmentHistory().expectAssessmentHistory(rows)
   }
 )
