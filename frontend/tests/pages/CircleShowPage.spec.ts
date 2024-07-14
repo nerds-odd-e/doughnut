4859a78bb840f76ecaff41aadef8d795d9887f3a
Terry Yin <terry.yinzhe@gmail.com>
2024-07-14 16:41:48 +0700

    new ut

--- frontend/tests/pages/CircleShowPage.spec.ts
+++ frontend/tests/pages/CircleShowPage.spec.ts
@@ -2,20 +2,22 @@ import CircleShowPage from "@/pages/CircleShowPage.vue"
 import { describe, it } from "vitest"
 import makeMe from "../fixtures/makeMe"
 import helper from "../helpers"
+import { screen } from "@testing-library/vue"
 
 describe("circle show page", () => {
+  const notebook = makeMe.aNotebook.please()
+  const circleNote = makeMe.aCircleNote.notebooks(notebook).please()
+
   beforeEach(() => {
     const teleportTarget = document.createElement("div")
     teleportTarget.id = "head-status"
     document.body.appendChild(teleportTarget)
-  })
-
-  it("fetch API to be called ONCE on mount", async () => {
-    const notebook = makeMe.aNotebook.please()
-    const circleNote = makeMe.aCircleNote.notebooks(notebook).please()
     helper.managedApi.restCircleController.showCircle = vi
       .fn()
       .mockResolvedValue(circleNote)
+  })
+
+  it("fetch API to be called ONCE on mount", async () => {
     helper
       .component(CircleShowPage)
       .withStorageProps({ circleId: circleNote.id })
@@ -24,4 +26,14 @@ describe("circle show page", () => {
       circleNote.id
     )
   })
+
+  // eslint-disable-next-line vitest/no-disabled-tests
+  it.skip("shows the move notebook button", async () => {
+    helper
+      .component(CircleShowPage)
+      .withStorageProps({ circleId: circleNote.id })
+      .render()
+    screen.getByRole("button", { name: "Move to ..." })
+  })
+
 })
