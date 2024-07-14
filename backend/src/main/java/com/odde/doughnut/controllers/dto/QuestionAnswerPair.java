71dcf2a499c983225776da77fc64a309af4b75f4
Terry Yin <terry.yinzhe@gmail.com>
2024-07-14 14:33:45 +0700

    remove dead code

--- backend/src/main/java/com/odde/doughnut/controllers/dto/QuestionAnswerPair.java
+++
@@ -1,9 +0,0 @@
-package com.odde.doughnut.controllers.dto;
-
-import lombok.Getter;
-import lombok.Setter;
-
-public class QuestionAnswerPair {
-  @Getter @Setter private Integer questionId;
-  @Getter @Setter private Integer answerId;
-}
