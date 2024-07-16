4df2b7de3334216fdd0c53554754188c781fa8d1
Thanakorn Roopsom <tanakon.rs@gmail.com>
2024-07-16 15:30:28 +0700

    Add design certificate layout

--- frontend/src/pages/CertificatePage.vue
+++ frontend/src/pages/CertificatePage.vue
@@ -1,6 +1,16 @@
 <template>
-  <div>
-    <div>Certificate Page {{ props.notebookId }}</div>
+  <div class="certificate-frame">
+    <div class="certificate-container">
+      <span>This to certificate that</span>
+      <span class="reciever-name">Korn</span>
+      <p class="certificate-detail"><span>by completing the qualifications, </span><span>is granted the Certified Vue Expert</span></p>
+      <div class="date-container">
+        <span>on</span>
+        <span class="date">2015-07-31</span>
+        <span>, and expiring on</span>
+        <span class="date">2015-07-31</span>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,3 +19,38 @@ const props = defineProps({
   notebookId: { type: Number, required: true },
 })
 </script>
+
+<style lang="scss" scoped>
+.certificate-frame {
+  border: solid 16px lightskyblue;
+  margin: 32px 64px;
+}
+.certificate-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 80vh;
+  row-gap: 32px;
+  border: solid 2px lightskyblue;
+  margin: 8px;
+}
+.certificate-detail {
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+}
+.reciever-name{
+  font-size: 32px;
+  font-weight: 700;
+  color: lightskyblue;
+}
+.date-container {
+  display: flex;
+  column-gap: 4px;
+}
+.date {
+  color: lightskyblue;
+  text-decoration: underline;
+}
+</style>
