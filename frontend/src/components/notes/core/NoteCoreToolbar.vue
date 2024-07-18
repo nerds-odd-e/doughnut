1048dcc2b7fd415e4b9e2b66fbe1bb4fd87a36ae
zkan <zkan.cs@gmail.com>
2024-07-18 11:31:44 +0700

    Remove unused menu setting

--- frontend/src/components/notes/core/NoteCoreToolbar.vue
+++ frontend/src/components/notes/core/NoteCoreToolbar.vue
@@ -74,12 +74,6 @@
           >
             <Questions v-bind="{ note }" />
           </PopButton>
-          <PopButton
-            btn-class="dropdown-item btn-primary"
-            title="Setting Certified By"
-          >
-            <SettingCertifiedBy />
-          </PopButton>
           <NoteDeleteButton
             class="dropdown-item"
             v-bind="{ noteId: note.id, storageAccessor }"
@@ -114,7 +108,6 @@ import NoteDetailsAutoCompletionButton from "./NoteDetailsAutoCompletionButton.v
 import NoteChatDialog from "../NoteChatDialog.vue"
 import Questions from "../Questions.vue"
 import NoteInfoBar from "../NoteInfoBar.vue"
-import SettingCertifiedBy from "../SettingCertifiedByDialog.vue"
 
 const props = defineProps({
   storageAccessor: {
