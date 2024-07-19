2546be5409be58714f0e1b7f76f70df088607278
Kookkai <tchaitep.chaitep@odds.team>
2024-07-19 11:18:01 +0700

    chore: Add unit test to check if the assessment result is submitted when the last answer is taken

--- frontend/tests/pages/AssessmentPage.spec.ts
+++ frontend/tests/pages/AssessmentPage.spec.ts
@@ -3,6 +3,7 @@ import { describe, it } from "vitest"
 import AssessmentPage from "@/pages/AssessmentPage.vue"
 import helper from "../helpers"
 import makeMe from "../fixtures/makeMe"
+import { flushPromises } from "@vue/test-utils"
 
 vitest.mock("vue-router", () => ({
   useRouter: () => ({
@@ -52,6 +53,39 @@ describe("assessment page", () => {
         .render()
 
       expect(html()).not.toContain("score:")
+    })
+  })
+
+  describe("answering the assessment with two questions", () => {
+    const notebook = makeMe.aNotebook.please()
+    const quizQuestion1 = makeMe.aQuizQuestion
+      .withChoices(["answer1", "answer2"])
+      .please()
+    const quizQuestion2 = makeMe.aQuizQuestion
+      .withChoices(["answer3", "answer4"])
+      .please()
+    beforeEach(() => {
+      helper.managedApi.restAssessmentController.generateAssessmentQuestions =
+        vi.fn().mockResolvedValue([quizQuestion1, quizQuestion2])
+      helper.managedApi.restAssessmentController.submitAssessmentResult = vi
+        .fn()
+        .mockResolvedValue({})
+    })
+
+    it("should submit assessment result when answer all questions", async () => {
+      const wrapper = helper
+        .component(AssessmentPage)
+        .withProps({ notebookId: notebook.id })
+        .render()
+      await flushPromises()
+      ;(await wrapper.findByRole("button", { name: "answer1" })).click()
+      await flushPromises()
+      ;(await wrapper.findByRole("button", { name: "answer3" })).click()
+      await flushPromises()
+
+      expect(
+        helper.managedApi.restAssessmentController.submitAssessmentResult
+      ).toBeCalledTimes(1)
     })
   })
 })
