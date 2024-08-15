cb6d08c156370547b829e700f744d18071cedbb8
tanyeongsheng <tanyeongsheng@gmail.com>
2024-08-15 05:42:10 +0000

    begin to support Google IDX

---
+++ .idx/dev.nix
@@ -0,0 +1,56 @@
+# To learn more about how to use Nix to configure your environment
+# see: https://developers.google.com/idx/guides/customize-idx-env
+{ pkgs, ... }: {
+  # Which nixpkgs channel to use.
+  channel = "unstable"; # or "unstable"
+
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    pkgs.direnv
+    #pkgs.cypress
+    pkgs.corepack_22
+    pkgs.nodejs_22
+    pkgs.jdk22
+  ];
+
+  # Sets environment variables in the workspace
+  env = {};
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [
+      "vscodevim.vim"
+    ];
+
+    # Enable previews
+    previews = {
+      enable = true;
+      previews = {
+        # web = {
+        #   # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
+        #   # and show it in IDX's web preview panel
+        #   command = ["pnpm" "frontend:sut"];
+        #   manager = "web";
+        #   env = {
+        #     # Environment variables to set for your server
+        #     PORT = "$PORT";
+        #   };
+        # };
+      };
+    };
+
+    # Workspace lifecycle hooks
+    workspace = {
+      # Runs when a workspace is first created
+      onCreate = {
+        # Example: install JS dependencies from NPM
+        direnv-allow = "direnv allow";
+        direnv-shell-hook = "sh -c 'echo \"eval \\\"\\$(direnv hook bash)\\\"\" >> ~/.bashrc && echo \"eval \\\"\\$(direnv hook zsh)\\\"\" >> ~/.zshrc'";
+        pnpm-install = "pnpm recursive install";
+      };
+      # Runs when the workspace is (re)started
+      onStart = {
+        nix-develop = "nix develop";
+      };
+    };
+  };
+}
