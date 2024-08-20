9c395ed4e89a2d7d05e1cbeeb4abd928c54745d1
Ståhlström, Jon (External) <jon.stahlstrom_external@wartsila.com>
2024-08-20 11:42:58 +0300

    feat(certificate-expiry): add input field hint for certificate expiry

--- frontend/src/components/notebook/NotebookEditDialog.vue
+++ frontend/src/components/notebook/NotebookEditDialog.vue
@@ -16,6 +16,7 @@
     <TextInput
       scope-name="notebook"
       field="certificateExpiry"
+      hint="Format (1 y 1 m 1 d)"
       v-model="formData.certificateExpiry"
       :errors="errors.certificateExpiry"
       disabled
