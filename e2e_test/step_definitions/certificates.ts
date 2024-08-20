6d9df7d705a5ad89a8520aa3b854ff89ed517bbe
Pasi Honkanen <pasi.honkanen_external@wartsila.com>
2024-08-20 10:46:36 +0300

    Adding contents as linter does not allow empty blocks

--- e2e_test/step_definitions/certificates.ts
+++ e2e_test/step_definitions/certificates.ts
@@ -1,9 +1,12 @@
-import {
-    When,
-    Then,
-  } from '@badeball/cypress-cucumber-preprocessor'
-  import '../support/string_util'
+import { When, Then } from '@badeball/cypress-cucumber-preprocessor'
+import '../support/string_util'
 
-  When('I have a notebook with the name "Certified thing"', () => {})
-  When('I open the notebooks settings', () => {})
-  Then('I should see the default expiration timespan which is 1 year', () => {})
+When('I have a notebook with the name "Certified thing"', () => {
+  return true
+})
+When('I open the notebooks settings', () => {
+  return true
+})
+Then('I should see the default expiration timespan which is 1 year', () => {
+  return true
+})
