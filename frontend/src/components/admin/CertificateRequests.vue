f58d020c12da23d0eea39fd87f6cf7d81848246c
Ville Häsä <ville.hasa_external@wartsila.com>
2024-08-21 14:54:33 +0300

    add optional render for pending certificate approvals

--- frontend/src/components/admin/CertificateRequests.vue
+++ frontend/src/components/admin/CertificateRequests.vue
@@ -1,3 +1,41 @@
 <template>
-  <h2>No certification request found.</h2>
+  <table class="table">
+    <tbody v-if="notebooks?.length">
+      <tr v-for="bazaarNotebook in notebooks" :key="bazaarNotebook.id">
+        <td>
+          <NoteTopicWithLink
+            v-bind="{ noteTopic: bazaarNotebook.notebook.headNote.noteTopic }"
+          />
+        </td>
+        <td>
+          <button
+            class="btn btn-dange"
+          >
+            Approve
+          </button>
+        </td>
+      </tr>
+    </tbody>
+    <h2 v-else>No pending approvals</h2>
+  </table>
 </template>
+
+<script lang="ts" setup>
+import { onMounted, ref } from "vue"
+import useLoadingApi from "@/managedApi/useLoadingApi"
+import { BazaarNotebook } from "@/generated/backend"
+import NoteTopicWithLink from "../notes/NoteTopicWithLink.vue"
+
+const { managedApi } = useLoadingApi()
+
+const notebooks = ref<BazaarNotebook[] | undefined>(undefined)
+
+const fetchData = async () => {
+  notebooks.value = await managedApi.restBazaarController.bazaar()
+  notebooks.value = undefined
+}
+
+onMounted(() => {
+  fetchData()
+})
+</script>
