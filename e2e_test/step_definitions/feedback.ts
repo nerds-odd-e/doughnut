9f2cd4593dd0431321cbace302d68164615aece0
Joshua Blokland <shua.blokland@gmail.com>
2024-08-27 14:37:07 +0200

    fix linting issues frontend

--- e2e_test/step_definitions/feedback.ts
+++ e2e_test/step_definitions/feedback.ts
@@ -1,17 +1,9 @@
-import { Given, Then, When } from '@badeball/cypress-cucumber-preprocessor'
+// import { Given, Then, When } from '@badeball/cypress-cucumber-preprocessor'
 
-Given('I visit the feedback page as a {string}', (userType: string) => {
+// Given('I visit the feedback page as a', () => {})
 
-});
+// When('I have received feedback on a question', () => {})
 
-When('I have received feedback on a question', () => {
+// When('I open that conversation', () => {})
 
-});
-
-When('I open that conversation', () => {
-
-})
-
-Then('I should be able to respond', () => {
-    
-});
+// Then('I should be able to respond', () => {})
