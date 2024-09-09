dd71dfd0f424663a7a99f720b09c2d58741aa81a
Terry Yin <terry.yinzhe@gmail.com>
2024-09-09 15:50:47 +0800

    remove undone feature

--- e2e_test/step_definitions/feedback.ts
+++ e2e_test/step_definitions/feedback.ts
@@ -1,32 +1,5 @@
-import { Given, When, Then } from '@badeball/cypress-cucumber-preprocessor'
+import { Then } from '@badeball/cypress-cucumber-preprocessor'
 import start from '../start'
-
-Given('I visit the feedback page', (userType: string) => {
-  console.log(userType)
-  start.systemSidebar().userOptions().myMessageCenter()
-})
-
-Given(
-  'Pete has given the feedback {string} on a question on notebook {string}',
-  (feedback: string, notebook: string) => {
-    //Start assessment
-    start.navigateToBazaar().selfAssessmentOnNotebook(notebook)
-    //Select wrong answer
-    cy.findByRole('button', { name: 'No' }).click()
-    //Submit feedback
-    cy.findByText('Send feedback').click()
-    cy.findByPlaceholderText('Give feedback about the question').type(feedback)
-    cy.findByRole('button', { name: 'Submit' }).click()
-  }
-)
-
-When('I open that conversation', () => {
-  cy.findByText('There is no feedback currently.').should('not.exist')
-  cy.findByRole('link', {
-    name: 'View chat',
-  }).click()
-  cy.findByText("I don't understand this question").should('be.visible')
-})
 
 Then(
   '{string} can see the feedback {string}',
@@ -34,18 +7,6 @@ Then(
     start.reloginAndEnsureHomePage(user).navigateToMessageCenter()
     cy.reload()
     cy.findByText(feedback).should('be.visible')
-  }
-)
-
-Then(
-  '{string} {string} see the conversation about question {string}',
-  (user: string, canOrCannotSee: string, feedback: string) => {
-    start.reloginAndEnsureHomePage(user).navigateToMessageCenter()
-    if (canOrCannotSee === 'can') {
-      cy.findByText(feedback).should('be.visible')
-    } else {
-      cy.findByText(feedback).should('not.exist')
-    }
   }
 )
 
