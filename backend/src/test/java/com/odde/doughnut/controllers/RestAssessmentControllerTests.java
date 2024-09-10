8571d5b37c7f7cbb22d168fb4872e3c79337da98
Terry Yin <terry.yinzhe@gmail.com>
2024-09-10 14:41:45 +0800

    test randomness

--- backend/src/test/java/com/odde/doughnut/controllers/RestAssessmentControllerTests.java
+++ backend/src/test/java/com/odde/doughnut/controllers/RestAssessmentControllerTests.java
@@ -4,24 +4,18 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import com.odde.doughnut.controllers.dto.AnswerSubmission;
 import com.odde.doughnut.controllers.dto.AssessmentResult;
-import com.odde.doughnut.controllers.dto.Randomization;
 import com.odde.doughnut.entities.*;
-import com.odde.doughnut.exceptions.ApiException;
 import com.odde.doughnut.exceptions.UnexpectedNoAccessRightException;
 import com.odde.doughnut.models.UserModel;
 import com.odde.doughnut.testability.MakeMe;
 import com.odde.doughnut.testability.TestabilitySettings;
 import com.odde.doughnut.testability.builders.NoteBuilder;
-import com.theokanning.openai.client.OpenAiApi;
 import java.sql.Timestamp;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -32,7 +26,6 @@ import org.springframework.web.server.ResponseStatusException;
 @ActiveProfiles("test")
 @Transactional
 public class RestAssessmentControllerTests {
-  @Mock OpenAiApi openAiApi;
   @Autowired MakeMe makeMe;
   private UserModel currentUser;
   private RestAssessmentController controller;
@@ -44,50 +37,6 @@ public class RestAssessmentControllerTests {
     currentUser = makeMe.aUser().toModelPlease();
     controller =
         new RestAssessmentController(makeMe.modelFactoryService, testabilitySettings, currentUser);
-  }
-
-  @Nested
-  class assessmentQuestionOrderTest {
-    private Notebook notebook;
-    private Note topNote;
-    private int representativeNumberOfAttempts = 10;
-
-    Set<Integer> performAssessments(int numberOfAttempts) throws UnexpectedNoAccessRightException {
-      Set<Integer> questionIds = new HashSet<>();
-      for (int i = 0; i < numberOfAttempts; i++) {
-        List<QuizQuestion> assessment = controller.generateAssessmentQuestions(notebook);
-        Integer questionId = assessment.get(0).getId();
-        questionIds.add(questionId);
-      }
-      return questionIds;
-    }
-
-    @BeforeEach
-    void setup() {
-      testabilitySettings.setRandomization(new Randomization(Randomization.RandomStrategy.seed, 1));
-      topNote = makeMe.aHeadNote("OnlineAssessment").creatorAndOwner(currentUser).please();
-      notebook = topNote.getNotebook();
-      notebook.getNotebookSettings().setNumberOfQuestionsInAssessment(1);
-    }
-
-    @Test
-    void shouldPickRandomNotesForAssessment() throws UnexpectedNoAccessRightException {
-      makeMe.theNote(topNote).withNChildrenThat(10, NoteBuilder::hasAnApprovedQuestion).please();
-
-      Set<Integer> questionIds = performAssessments(representativeNumberOfAttempts);
-
-      assertTrue(questionIds.size() > 1, "Expected questions from different notes.");
-    }
-
-    @Test
-    void shouldPickRandomQuestionsFromTheSameNote() throws UnexpectedNoAccessRightException {
-      makeMe
-          .theNote(topNote)
-          .withNChildrenThat(1, noteBuilder -> noteBuilder.hasApprovedQuestions(10))
-          .please();
-      Set<Integer> questionIds = performAssessments(representativeNumberOfAttempts);
-      assertTrue(questionIds.size() > 1, "Expected questions from the same note.");
-    }
   }
 
   @Nested
@@ -132,41 +81,6 @@ public class RestAssessmentControllerTests {
       List<QuizQuestion> assessment =
           controller.generateAssessmentQuestions(bazaarNotebook.getNotebook());
       assertEquals(5, assessment.size());
-    }
-
-    @Test
-    void shouldReturn5QuestionsWhenThereAreMoreThan5NotesWithQuestions()
-        throws UnexpectedNoAccessRightException {
-      makeMe.theNote(topNote).withNChildrenThat(5, NoteBuilder::hasAnApprovedQuestion).please();
-      notebook.getNotebookSettings().setNumberOfQuestionsInAssessment(5);
-      List<QuizQuestion> assessment = controller.generateAssessmentQuestions(notebook);
-      assertEquals(5, assessment.size());
-    }
-
-    @Test
-    void shouldThrowExceptionWhenThereAreNotEnoughQuestions() {
-      makeMe.theNote(topNote).withNChildrenThat(4, NoteBuilder::hasAnApprovedQuestion).please();
-      assertThrows(ApiException.class, () -> controller.generateAssessmentQuestions(notebook));
-    }
-
-    @Test
-    void shouldGetOneQuestionFromEachNoteOnly() {
-      makeMe.theNote(topNote).withNChildrenThat(3, NoteBuilder::hasAnApprovedQuestion).please();
-      assertThrows(ApiException.class, () -> controller.generateAssessmentQuestions(notebook));
-    }
-
-    @Test
-    void shouldGetOnlyOldApprovedQuestionsBeforeNotebookApproval() {
-      makeMe.theNote(topNote).withNChildrenThat(1, NoteBuilder::hasAnApprovedQuestion).please();
-      notebook.setApprovalStatus(ApprovalStatus.APPROVED);
-      notebook.setLast_approval_time(new Timestamp(System.currentTimeMillis()));
-      makeMe.theNote(topNote).withNChildrenThat(1, NoteBuilder::hasAnApprovedQuestion).please();
-      notebook.getNotebookSettings().setNumberOfQuestionsInAssessment(1);
-      try {
-        controller.generateAssessmentQuestions(notebook);
-      } catch (UnexpectedNoAccessRightException e) {
-        throw new RuntimeException(e);
-      }
     }
   }
 
---
+++ backend/src/test/java/com/odde/doughnut/services/AssessmentServiceTests.java
@@ -0,0 +1,125 @@
+package com.odde.doughnut.services;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.odde.doughnut.controllers.dto.Randomization;
+import com.odde.doughnut.entities.*;
+import com.odde.doughnut.exceptions.ApiException;
+import com.odde.doughnut.models.UserModel;
+import com.odde.doughnut.testability.MakeMe;
+import com.odde.doughnut.testability.TestabilitySettings;
+import com.odde.doughnut.testability.builders.NoteBuilder;
+import java.sql.Timestamp;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@Transactional
+public class AssessmentServiceTests {
+  @Autowired MakeMe makeMe;
+  private UserModel currentUser;
+  private AssessmentService service;
+  private final TestabilitySettings testabilitySettings = new TestabilitySettings();
+
+  @BeforeEach
+  void setup() {
+    testabilitySettings.timeTravelTo(makeMe.aTimestamp().please());
+    currentUser = makeMe.aUser().toModelPlease();
+    service = new AssessmentService(makeMe.modelFactoryService, testabilitySettings);
+  }
+
+  @Nested
+  class assessmentQuestionOrderTest {
+    private Notebook notebook;
+    private Note topNote;
+    private int representativeNumberOfAttempts = 10;
+
+    Set<Integer> performAssessments(int numberOfAttempts) {
+      Set<Integer> questionIds = new HashSet<>();
+      for (int i = 0; i < numberOfAttempts; i++) {
+        List<QuizQuestion> assessment = service.generateAssessment(notebook);
+        Integer questionId = assessment.get(0).getId();
+        questionIds.add(questionId);
+      }
+      return questionIds;
+    }
+
+    @BeforeEach
+    void setup() {
+      testabilitySettings.setRandomization(new Randomization(Randomization.RandomStrategy.seed, 1));
+      topNote = makeMe.aHeadNote("OnlineAssessment").creatorAndOwner(currentUser).please();
+      notebook = topNote.getNotebook();
+      notebook.getNotebookSettings().setNumberOfQuestionsInAssessment(1);
+    }
+
+    @Test
+    void shouldPickRandomNotesForAssessment() {
+      makeMe.theNote(topNote).withNChildrenThat(10, NoteBuilder::hasAnApprovedQuestion).please();
+
+      Set<Integer> questionIds = performAssessments(representativeNumberOfAttempts);
+
+      assertTrue(questionIds.size() > 1, "Expected questions from different notes.");
+    }
+
+    @Test
+    void shouldPickRandomQuestionsFromTheSameNote() {
+      makeMe
+          .theNote(topNote)
+          .withNChildrenThat(1, noteBuilder -> noteBuilder.hasApprovedQuestions(10))
+          .please();
+      Set<Integer> questionIds = performAssessments(representativeNumberOfAttempts);
+      assertTrue(questionIds.size() > 1, "Expected questions from the same note.");
+    }
+  }
+
+  @Nested
+  class generateOnlineAssessmentTest {
+    private Notebook notebook;
+    private Note topNote;
+
+    @BeforeEach
+    void setup() {
+      topNote = makeMe.aHeadNote("OnlineAssessment").creatorAndOwner(currentUser).please();
+      notebook = topNote.getNotebook();
+    }
+
+    @Test
+    void shouldReturn5QuestionsWhenThereAreMoreThan5NotesWithQuestions() {
+      makeMe.theNote(topNote).withNChildrenThat(5, NoteBuilder::hasAnApprovedQuestion).please();
+      notebook.getNotebookSettings().setNumberOfQuestionsInAssessment(5);
+      List<QuizQuestion> assessment = service.generateAssessment(notebook);
+      assertEquals(5, assessment.size());
+    }
+
+    @Test
+    void shouldThrowExceptionWhenThereAreNotEnoughQuestions() {
+      makeMe.theNote(topNote).withNChildrenThat(4, NoteBuilder::hasAnApprovedQuestion).please();
+      assertThrows(ApiException.class, () -> service.generateAssessment(notebook));
+    }
+
+    @Test
+    void shouldGetOneQuestionFromEachNoteOnly() {
+      makeMe.theNote(topNote).withNChildrenThat(3, NoteBuilder::hasAnApprovedQuestion).please();
+      assertThrows(ApiException.class, () -> service.generateAssessment(notebook));
+    }
+
+    @Test
+    void shouldGetOnlyOldApprovedQuestionsBeforeNotebookApproval() {
+      makeMe.theNote(topNote).withNChildrenThat(1, NoteBuilder::hasAnApprovedQuestion).please();
+      notebook.setApprovalStatus(ApprovalStatus.APPROVED);
+      notebook.setLast_approval_time(new Timestamp(System.currentTimeMillis()));
+      makeMe.theNote(topNote).withNChildrenThat(1, NoteBuilder::hasAnApprovedQuestion).please();
+      notebook.getNotebookSettings().setNumberOfQuestionsInAssessment(1);
+      service.generateAssessment(notebook);
+    }
+  }
+}
