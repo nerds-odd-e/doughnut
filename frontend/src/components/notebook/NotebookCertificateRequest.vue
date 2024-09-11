85bc0fdba832216a195a4fd3d54c06b7bb75e8f3
Terry Yin <terry.yinzhe@gmail.com>
2024-09-11 11:35:56 +0800

    using the new endpoint

--- frontend/src/components/notebook/NotebookCertificateRequest.vue
+++ frontend/src/components/notebook/NotebookCertificateRequest.vue
@@ -1,44 +1,50 @@
 <template>
   <h4>Request to obtain certificate from assessment</h4>
-  <button id="request-approval-btn" :class="approvalButtonClasses" :disabled="isApprovalButtonDisabled" @click="requestNotebookApproval">
-    {{ approvalButtonText }}
-  </button>
+  <ContentLoader v-if="loaded === false" />
+  <div v-else>
+    <button :class="approvalButtonClasses" :disabled="isApprovalButtonDisabled" @click="requestNotebookApproval">
+      {{ approvalButtonText }}
+    </button>
+  </div>
 </template>
 
 <script setup lang="ts">
-import { PropType, computed } from "vue"
-import { useRouter } from "vue-router"
-import { Notebook } from "@/generated/backend"
+import { PropType, computed, onMounted, ref } from "vue"
+import { Notebook, NotebookCertificateApproval } from "@/generated/backend"
 import useLoadingApi from "@/managedApi/useLoadingApi"
 
 const { managedApi } = useLoadingApi()
-
-const router = useRouter()
 
 const props = defineProps({
   notebook: { type: Object as PropType<Notebook>, required: true },
 })
 
+const loaded = ref(false)
+const approval = ref<NotebookCertificateApproval | undefined>()
+
+onMounted(async () => {
+  approval.value =
+    await managedApi.restNotebookCertificateApprovalController.getApprovalForNotebook(
+      props.notebook.id
+    )
+  loaded.value = true
+})
+
 const approvalButtonText = computed(() => {
-  switch (props.notebook.approvalStatus) {
-    case "NOT_APPROVED":
-      return "Send Request"
-    case "APPROVED":
-      return "Certificate Request Approved"
-    case "PENDING":
-      return "Approval Pending"
-    default:
-      return "Send Request"
+  if (approval.value === undefined) {
+    return "Send Request"
   }
+  if (approval.value.lastApprovalTime) {
+    return "Certificate Request Approved"
+  }
+  return "Approval Pending"
 })
 
 const approvalButtonClasses = computed(() => {
   return {
     btn: true,
-    "btn-primary": props.notebook.approvalStatus === "NOT_APPROVED",
-    "btn-disabled":
-      props.notebook.approvalStatus === "APPROVED" ||
-      props.notebook.approvalStatus === "PENDING",
+    "btn-primary": approval.value === undefined,
+    "btn-disabled": approval.value !== undefined,
     "btn-layout": true,
     "mt-2": true,
     display: "block",
@@ -46,17 +52,13 @@ const approvalButtonClasses = computed(() => {
 })
 
 const isApprovalButtonDisabled = computed(() => {
-  return (
-    props.notebook.approvalStatus === "APPROVED" ||
-    props.notebook.approvalStatus === "PENDING"
-  )
+  return approval.value !== undefined
 })
-const requestNotebookApproval = () => {
-  managedApi.restNotebookCertificateApprovalController
-    .requestApprovalForNotebook(props.notebook.id)
-    .then(() => {
-      router.go(0)
-    })
+const requestNotebookApproval = async () => {
+  approval.value =
+    await managedApi.restNotebookCertificateApprovalController.requestApprovalForNotebook(
+      props.notebook.id
+    )
 }
 </script>
 
