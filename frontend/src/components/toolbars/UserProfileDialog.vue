b44f1455d6a5b1155422a2beb37c0de44eb3457e
Tan Yeong Sheng <tanyeongsheng@gmail.com>
2024-09-13 17:44:18 +0800

    take from main

--- frontend/src/components/toolbars/UserProfileDialog.vue
+++ frontend/src/components/toolbars/UserProfileDialog.vue
@@ -26,68 +26,38 @@
           v-model="formData.spaceIntervals"
           :errors="errors.spaceIntervals"
         />
-        <CheckInput
-          scope-name="user"
-          field="aiQuestionTypeOnlyForReview"
-          v-model="formData.aiQuestionTypeOnlyForReview"
-          :errors="getErrorObject('aiQuestionTypeOnlyForReview')"
-        />
         <input type="submit" value="Submit" class="btn btn-primary" />
       </form>
     </div>
   </ContainerPage>
 </template>
 
-<script lang="ts">
-import { defineComponent, ref, computed } from "vue"
-import CheckInput from "@/components/form/CheckInput.vue"
+<script setup lang="ts">
 import TextInput from "@/components/form/TextInput.vue"
 import { User } from "@/generated/backend"
 import useLoadingApi from "@/managedApi/useLoadingApi"
 import ContainerPage from "@/pages/commons/ContainerPage.vue"
+import { defineEmits, onMounted, ref } from "vue"
 
-export default defineComponent({
-  components: { ContainerPage, TextInput, CheckInput },
-  emits: ["user-updated"],
-  setup(_, { emit }) {
-    const { managedApi } = useLoadingApi()
-    const formData = ref<User | undefined>(undefined)
-    const errors = ref<Record<string, string>>({})
+const { managedApi } = useLoadingApi()
+const emits = defineEmits(["user-updated"])
 
-    const getErrorObject = computed(() => (field: string) => {
-      const error = errors.value[field]
-      return error ? { [field]: error } : undefined
+const formData = ref<User | undefined>()
+const errors = ref<Record<string, string>>({})
+
+const fetchData = async () => {
+  formData.value = await managedApi.restUserController.getUserProfile()
+}
+
+const processForm = async () => {
+  if (!formData.value) return
+  const updated = await managedApi.restUserController
+    .updateUser(formData.value.id, formData.value)
+    .catch((err) => {
+      errors.value = err
     })
+  emits["user-updated"](updated)
+}
 
-    const fetchData = async () => {
-      formData.value = await managedApi.restUserController.getUserProfile()
-    }
-
-    const processForm = async () => {
-      if (!formData.value) return
-      try {
-        const updated = await managedApi.restUserController.updateUser(
-          formData.value.id,
-          formData.value
-        )
-        emit("user-updated", updated)
-      } catch (err) {
-        if (typeof err === "object" && err !== null) {
-          errors.value = err as Record<string, string>
-        } else {
-          console.error("Unexpected error format:", err)
-        }
-      }
-    }
-
-    fetchData()
-
-    return {
-      formData,
-      errors,
-      getErrorObject,
-      processForm,
-    }
-  },
-})
+onMounted(() => fetchData())
 </script>
