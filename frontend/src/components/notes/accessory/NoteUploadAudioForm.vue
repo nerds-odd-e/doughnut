05c3a9c07ec1337c823f82202167ea98bf32b469
Tan Yeong Sheng <tanyeongsheng@gmail.com>
2024-09-13 17:15:07 +0800

    fix NoteUploadAudioForm.vue for vue3.5

--- frontend/src/components/notes/accessory/NoteUploadAudioForm.vue
+++ frontend/src/components/notes/accessory/NoteUploadAudioForm.vue
@@ -3,17 +3,15 @@
     scope-name="note"
     field="uploadAudioFile"
     accept=".mp3, .m4a, .wav"
-    :errors="errors.uploadAudioFile"
-    :model-value="modelValue.uploadAudioFile"
-    @update:model-value="
-      $emit('update:modelValue', { ...modelValue, uploadAudioFile: $event })
-    "
+    :errors="getErrorObject('uploadAudioFile')"
+    :model-value="uploadAudioFileName"
+    @update:model-value="handleFileUpload"
   />
 </template>
 
 <script lang="ts">
 import { AudioUploadDTO } from "@/generated/backend"
-import { PropType, defineComponent } from "vue"
+import { PropType, defineComponent, computed } from "vue"
 import FileInput from "../../form/FileInput.vue"
 
 export default defineComponent({
@@ -23,18 +21,40 @@ export default defineComponent({
       required: true,
     },
     errors: {
-      type: Object,
-      default() {
-        return {}
-      },
+      type: Object as PropType<Record<string, string | undefined>>,
+      default: () => ({}),
     },
-  },
-  data() {
-    return {
-      formData: { ...this.modelValue },
-    }
   },
   emits: ["update:modelValue"],
   components: { FileInput },
+  setup(props, { emit }) {
+    const uploadAudioFileName = computed(() => {
+      const uploadAudioFile = props.modelValue.uploadAudioFile
+      if (uploadAudioFile instanceof File) {
+        return uploadAudioFile.name
+      } else if (uploadAudioFile instanceof Blob) {
+        return "Unnamed audio file"
+      }
+      return undefined
+    })
+
+    const handleFileUpload = (file: File | null) => {
+      emit("update:modelValue", {
+        ...props.modelValue,
+        uploadAudioFile: file || undefined,
+      })
+    }
+
+    const getErrorObject = (field: string) => {
+      const error = props.errors[field]
+      return error ? { [field]: error } : undefined
+    }
+
+    return {
+      uploadAudioFileName,
+      handleFileUpload,
+      getErrorObject,
+    }
+  },
 })
 </script>
