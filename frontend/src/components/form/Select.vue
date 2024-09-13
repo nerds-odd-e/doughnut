862d42a8c5bd351d462112f807ba30bb7e3fb562
Tan Yeong Sheng <tanyeongsheng@gmail.com>
2024-09-13 16:46:51 +0800

    fix type error for errors flagged by vue3.5

--- frontend/src/components/form/Select.vue
+++ frontend/src/components/form/Select.vue
@@ -1,5 +1,5 @@
 <template>
-  <InputWithType v-bind="{ scopeName, field, errors }">
+  <InputWithType v-bind="{ scopeName, field, errors: errorMessage }">
     <select
       :class="`select-control form-control ${!!errors ? 'is-invalid' : ''}`"
       :id="`${scopeName}-${field}`"
@@ -19,22 +19,34 @@
 </template>
 
 <script lang="ts" setup>
-import { PropType, ref, watch } from "vue"
+import { PropType, ref, watch, computed } from "vue"
 import InputWithType from "./InputWithType.vue"
+
+type ErrorRecord = Record<string, string | string[]>
 
 const props = defineProps({
   modelValue: String,
   scopeName: String,
   field: String,
   options: Array as PropType<string[]>,
-  errors: Object,
+  errors: {
+    type: Object as PropType<ErrorRecord | undefined>,
+    default: () => ({}),
+  },
 })
 
 const emit = defineEmits(["update:modelValue"])
-
 const localValue = ref(props.modelValue)
 
 watch(localValue, (newValue) => {
   emit("update:modelValue", newValue)
+})
+
+// Convert errors object to a string message
+const errorMessage = computed(() => {
+  if (props.errors) {
+    return Object.values(props.errors).flat().join(", ")
+  }
+  return undefined
 })
 </script>
