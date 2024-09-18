58417986d5fcfafb425883ebb6cc195ff47894e5
Terry Yin <terry.yinzhe@gmail.com>
2024-09-18 15:29:49 +0800

    to setup

--- frontend/src/components/notes/core/TextContentWrapper.vue
+++ frontend/src/components/notes/core/TextContentWrapper.vue
@@ -9,103 +9,96 @@
   </div>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 import { debounce } from "lodash"
 import type { PropType } from "vue"
-import { defineComponent, ref } from "vue"
+import { onUnmounted, ref, watch } from "vue"
 import { type StorageAccessor } from "../../../store/createNoteStorage"
 
-export default defineComponent({
-  setup(props) {
-    const savedVersion = ref(0)
-    const changerInner = async (
-      noteId: number,
-      newValue: string,
-      version: number,
-      errorHander: (errs: unknown) => void
-    ) => {
-      await props.storageAccessor
-        .storedApi()
-        .updateTextField(noteId, props.field, newValue)
-        .catch(errorHander)
-      savedVersion.value = version
+const { storageAccessor, field, value } = defineProps({
+  field: {
+    type: String as PropType<"edit topic" | "edit details">,
+    required: true,
+  },
+  value: {
+    type: String,
+    required: false,
+  },
+  storageAccessor: {
+    type: Object as PropType<StorageAccessor>,
+    required: true,
+  },
+})
+
+const savedVersion = ref(0)
+const changerInner = async (
+  noteId: number,
+  newValue: string,
+  version: number,
+  errorHander: (errs: unknown) => void
+) => {
+  await storageAccessor
+    .storedApi()
+    .updateTextField(noteId, field, newValue)
+    .catch(errorHander)
+  savedVersion.value = version
+}
+const changer = debounce(changerInner, 1000)
+
+const localValue = ref(value)
+const version = ref(0)
+const errors = ref({} as Record<string, string>)
+
+const wrapperClass = computed(() => {
+  if (version.value !== savedVersion.value) {
+    return "dirty"
+  }
+  return ""
+})
+
+const onUpdate = (noteId: number, newValue: string) => {
+  version.value += 1
+  errors.value = {}
+  localValue.value = newValue
+  changer(noteId, newValue, version.value, setError)
+}
+
+const onBlur = () => {
+  changer.flush()
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const setError = (errs: unknown) => {
+  if (typeof errs === "object" && errs !== null && "status" in errs) {
+    if (errs.status === 401) {
+      errors.value = {
+        topic:
+          "You are not authorized to edit this note. Perhaps you are not logged in?",
+      }
+      return
     }
-    const changer = debounce(changerInner, 1000)
+  }
 
-    return { changer, savedVersion }
-  },
-  props: {
-    field: {
-      type: String as PropType<"edit topic" | "edit details">,
-      required: true,
-    },
-    value: {
-      type: String,
-      required: false,
-    },
-    storageAccessor: {
-      type: Object as PropType<StorageAccessor>,
-      required: true,
-    },
-  },
-  data() {
-    return {
-      localValue: this.value,
-      version: 0,
-      errors: {} as Record<string, string>,
+  if (typeof errs === "object" && errs !== null) {
+    errors.value = errs as Record<string, string>
+  } else {
+    errors.value = { general: String(errs) }
+  }
+}
+
+watch(
+  () => value,
+  () => {
+    if (version.value !== savedVersion.value) {
+      return
     }
-  },
+    localValue.value = value
+  }
+)
 
-  computed: {
-    wrapperClass() {
-      if (this.version !== this.savedVersion) {
-        return "dirty"
-      }
-      return ""
-    },
-  },
-
-  methods: {
-    onUpdate(noteId: number, newValue: string) {
-      this.version += 1
-      this.errors = {}
-      this.localValue = newValue
-      this.changer(noteId, newValue, this.version, this.setError)
-    },
-    onBlur() {
-      this.changer.flush()
-    },
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    setError(errs: unknown) {
-      if (typeof errs === "object" && errs !== null && "status" in errs) {
-        if (errs.status === 401) {
-          this.errors = {
-            topic:
-              "You are not authorized to edit this note. Perhaps you are not logged in?",
-          }
-          return
-        }
-      }
-
-      if (typeof errs === "object" && errs !== null) {
-        this.errors = errs as Record<string, string>
-      } else {
-        this.errors = { general: String(errs) }
-      }
-    },
-  },
-  watch: {
-    value() {
-      if (this.version !== this.savedVersion) {
-        return
-      }
-      this.localValue = this.value
-    },
-  },
-  unmounted() {
-    this.changer.flush()
-    this.changer.cancel()
-  },
+onUnmounted(() => {
+  changer.flush()
+  changer.cancel()
 })
 </script>
 
