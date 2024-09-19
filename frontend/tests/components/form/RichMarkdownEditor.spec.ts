e05f834eaeb65d72e56856c5f25ec34b2ba14e1c
Terry Yin <terry.yinzhe@gmail.com>
2024-09-19 16:55:37 +0800

    unit test

--- frontend/tests/components/form/RichMarkdownEditor.spec.ts
+++ frontend/tests/components/form/RichMarkdownEditor.spec.ts
@@ -3,15 +3,32 @@ import { flushPromises } from "@vue/test-utils"
 import helper from "../../helpers"
 
 describe("RichMarkdownEditor", () => {
-  it("not emit update when the change is from initial value", async () => {
+  const mountEditor = async (initialValue: string, options = {}) => {
     const wrapper = helper
       .component(RichMarkdownEditor)
       .withProps({
-        modelValue: "initial value",
+        modelValue: initialValue,
+        ...options,
       })
       .mount()
-
     await flushPromises()
+    return wrapper
+  }
+
+  it("not emit update when the change is from initial value", async () => {
+    const wrapper = await mountEditor("initial value")
     expect(wrapper.emitted()["update:modelValue"]).toBeUndefined()
+  })
+
+  it("not emit update if readonly", async () => {
+    const wrapper = await mountEditor("# Title", { readonly: true })
+    expect(wrapper.emitted()["update:modelValue"]).toBeUndefined()
+  })
+
+  it("will try to unify the markdown", async () => {
+    const wrapper = await mountEditor("# Title")
+    await flushPromises()
+    const emitted = wrapper.emitted()["update:modelValue"]
+    expect(emitted![0]![0]).toEqual("Title\n=====")
   })
 })
