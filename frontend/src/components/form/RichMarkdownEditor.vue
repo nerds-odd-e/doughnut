204acf6c437f85e940ce8423aa22c656c23561bd
Terry Yin <terry.yinzhe@gmail.com>
2024-09-19 16:44:41 +0800

    Simplify

--- frontend/src/components/form/RichMarkdownEditor.vue
+++ frontend/src/components/form/RichMarkdownEditor.vue
@@ -9,7 +9,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref, watch } from "vue"
+import { computed } from "vue"
 import QuillEditor from "./QuillEditor.vue"
 import markdownizer from "./markdownizer"
 
@@ -25,21 +25,15 @@ const { modelValue } = defineProps({
 
 const emits = defineEmits(["update:modelValue", "blur"])
 
-const htmlValue = ref(markdownizer.markdownToHtml(modelValue))
-
 let currentIntervalMarkdown: string | undefined = undefined
 let currentIntervalHtml: string | undefined = undefined
 
-watch(
-  () => modelValue,
-  (newValue) => {
-    if (newValue === currentIntervalMarkdown) {
-      htmlValue.value = currentIntervalHtml!
-      return
-    }
-    htmlValue.value = markdownizer.markdownToHtml(newValue)
+const htmlValue = computed(() => {
+  if (modelValue === currentIntervalMarkdown) {
+    return currentIntervalHtml!
   }
-)
+  return markdownizer.markdownToHtml(modelValue)
+})
 
 const htmlValueUpdated = (newHtmlValue: string) => {
   const markdownValue = markdownizer.htmlToMarkdown(newHtmlValue)
