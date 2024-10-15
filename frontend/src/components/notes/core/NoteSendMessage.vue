4a08623a56bb2f95b5a6cb5ace11dc2188b8e823
celine <celine110303@gmail.com>
2024-10-15 14:22:43 +0800

    Add new placeholder dialogue for send message to bazaar

---
+++ frontend/src/components/notes/core/NoteSendMessage.vue
@@ -0,0 +1,33 @@
+<template>
+  <PopButton :title="buttonTitle">
+    <template #button_face>
+      <slot />
+    </template>
+    <template #default="{ closer }">
+      <NoteSendMessageDialogue
+        v-bind="{ parentId, storageAccessor }"
+        @close-dialog="closer"
+      />
+    </template>
+  </PopButton>
+</template>
+
+<script lang="ts">
+import type { PropType } from "vue"
+import { defineComponent } from "vue"
+import type { StorageAccessor } from "../../../store/createNoteStorage"
+import PopButton from "../../commons/Popups/PopButton.vue"
+import NoteSendMessageDialogue from "../NoteSendMessageDialogue.vue"
+
+export default defineComponent({
+  props: {
+    parentId: { type: Number, required: true },
+    buttonTitle: { type: String, required: true },
+    storageAccessor: {
+      type: Object as PropType<StorageAccessor>,
+      required: true,
+    },
+  },
+  components: { PopButton, NoteSendMessageDialogue },
+})
+</script>
