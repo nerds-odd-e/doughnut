9c0d2a456add33c4e2cef41645a8d870bd33566e
Terry Yin <terry.yinzhe@gmail.com>
2024-10-23 12:47:53 +0800

    to seutp

--- frontend/src/components/notes/accessory/NoteEditUploadAudioDialog.vue
+++ frontend/src/components/notes/accessory/NoteEditUploadAudioDialog.vue
@@ -17,60 +17,43 @@
   <TextArea :field="`convertedSrt`" v-model="convertedSrt" :rows="8" />
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 import type { AudioUploadDTO } from "@/generated/backend"
 import useLoadingApi from "@/managedApi/useLoadingApi"
-import { defineComponent } from "vue"
+import { ref } from "vue"
 import TextArea from "../../form/TextArea.vue"
 import NoteUploadAudioForm from "./NoteUploadAudioForm.vue"
 
-interface ApiError {
-  message: string
-  code?: number
-}
-
-export default defineComponent({
-  setup() {
-    return { ...useLoadingApi() }
-  },
-  components: {
-    NoteUploadAudioForm,
-    TextArea,
-  },
-  props: {
-    noteId: { type: Number, required: true },
-  },
-  emits: ["closeDialog"],
-  data() {
-    return {
-      formData: {} as AudioUploadDTO,
-      noteFormErrors: {},
-      convertedSrt: "",
-    }
-  },
-
-  methods: {
-    async uploadAudio() {
-      try {
-        const na = await this.managedApi.restNoteController.uploadAudio(
-          this.noteId,
-          this.formData
-        )
-        this.$emit("closeDialog", na)
-      } catch (error: unknown) {
-        this.noteFormErrors = error as ApiError
-      }
-    },
-    async convertToSRT() {
-      try {
-        const response = await this.managedApi.restAiAudioController.convertSrt(
-          this.formData
-        )
-        this.convertedSrt = response.srt
-      } catch (error: unknown) {
-        this.noteFormErrors = error as ApiError
-      }
-    },
-  },
+const { managedApi } = useLoadingApi()
+const { noteId } = defineProps({
+  noteId: { type: Number, required: true },
 })
+
+const emit = defineEmits(["closeDialog"])
+
+const formData = ref<AudioUploadDTO>({})
+const noteFormErrors = ref<Record<string, string | undefined>>({})
+const convertedSrt = ref<string>("")
+
+const uploadAudio = async () => {
+  try {
+    const na = await managedApi.restNoteController.uploadAudio(
+      noteId,
+      formData.value
+    )
+    emit("closeDialog", na)
+  } catch (error: unknown) {
+    noteFormErrors.value = error as Record<string, string | undefined>
+  }
+}
+const convertToSRT = async () => {
+  try {
+    const response = await managedApi.restAiAudioController.convertSrt(
+      formData.value
+    )
+    convertedSrt.value = response?.srt
+  } catch (error: unknown) {
+    noteFormErrors.value = error as Record<string, string | undefined>
+  }
+}
 </script>
