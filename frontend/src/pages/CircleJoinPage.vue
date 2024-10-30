b05ca165e21af45536e4b58b0facc00be4e3517e
Terry Yin <terry.yinzhe@gmail.com>
2024-10-30 08:23:33 +0800

    convert

--- frontend/src/pages/CircleJoinPage.vue
+++ frontend/src/pages/CircleJoinPage.vue
@@ -4,34 +4,30 @@
   </ContainerPage>
 </template>
 
-<script lang="js">
+<script setup>
 import CircleJoinForm from "@/components/circles/CircleJoinForm.vue"
 import useLoadingApi from "@/managedApi/useLoadingApi"
 import loginOrRegisterAndHaltThisThread from "@/managedApi/window/loginOrRegisterAndHaltThisThread"
 import ContainerPage from "./commons/ContainerPage.vue"
+import { onBeforeRouteEnter } from "vue-router"
 
-export default {
-  setup() {
-    return useLoadingApi()
+const props = defineProps({
+  invitationCode: Number,
+  user: {
+    type: Object,
+    required: false,
   },
-  components: { CircleJoinForm, ContainerPage },
-  props: {
-    invitationCode: Number,
-    user: {
-      type: Object,
-      required: false,
-    },
-  },
-  beforeRouteEnter(_to, _from, next) {
-    next(async (vm) => {
-      const x =
-        await vm.managedApi.restCurrentUserInfoController.currentUserInfo()
-      if (!x?.user) {
-        loginOrRegisterAndHaltThisThread()
-        next(false)
-      }
-      next()
-    })
-  },
-}
+})
+
+const managedApi = useLoadingApi()
+
+onBeforeRouteEnter(async (to, from, next) => {
+  const x = await managedApi.restCurrentUserInfoController.currentUserInfo()
+  if (!x?.user) {
+    loginOrRegisterAndHaltThisThread()
+    next(false)
+    return
+  }
+  next()
+})
 </script>
