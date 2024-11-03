b0c5c06d72a3a501d03c5b1a9be9094c7b28af68
Terry Yin <terry.yinzhe@gmail.com>
2024-11-04 06:28:22 +0800

    missing type

--- backend/src/main/java/com/odde/doughnut/controllers/dto/DummyForGeneratingTypes.java
+++ backend/src/main/java/com/odde/doughnut/controllers/dto/DummyForGeneratingTypes.java
@@ -1,10 +1,12 @@
 package com.odde.doughnut.controllers.dto;
 
 import com.odde.doughnut.services.ai.NoteDetailsCompletion;
+import com.theokanning.openai.assistants.message.Message;
 import com.theokanning.openai.assistants.message.content.MessageDelta;
 import com.theokanning.openai.assistants.run.*;
 
 public class DummyForGeneratingTypes {
+  public Message message;
   public MessageDelta messageDelta;
   public Run run;
   public NoteDetailsCompletion noteDetailsCompletion;
--- frontend/src/generated/backend/models/DummyForGeneratingTypes.ts
+++ frontend/src/generated/backend/models/DummyForGeneratingTypes.ts
@@ -2,10 +2,12 @@
 /* istanbul ignore file */
 /* tslint:disable */
 /* eslint-disable */
+import type { Message } from './Message';
 import type { MessageDelta } from './MessageDelta';
 import type { NoteDetailsCompletion } from './NoteDetailsCompletion';
 import type { Run } from './Run';
 export type DummyForGeneratingTypes = {
+    message?: Message;
     messageDelta?: MessageDelta;
     run?: Run;
     noteDetailsCompletion?: NoteDetailsCompletion;
