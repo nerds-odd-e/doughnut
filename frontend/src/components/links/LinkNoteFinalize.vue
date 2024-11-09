6272327c647a04140a5709cba0f06a59fe184165
Terry Yin <terry.yinzhe@gmail.com>
2024-11-09 14:46:22 +0800

    remove dead ui

--- frontend/src/components/links/LinkNoteFinalize.vue
+++ frontend/src/components/links/LinkNoteFinalize.vue
@@ -16,22 +16,6 @@
           v-bind="{ noteTopic: targetNoteTopic }"
       /></strong>
     </div>
-    <CheckInput
-      scope-name="link"
-      v-model="formData.moveUnder"
-      :error-message="linkFormErrors.moveUnder"
-      field="alsoMoveToUnderTargetNote"
-    />
-    <RadioButtons
-      v-if="formData.moveUnder"
-      scope-name="link"
-      v-model="asFirstChildModel"
-      :error-message="linkFormErrors.asFirstChild"
-      :options="[
-        { value: 'true', label: 'as its first child' },
-        { value: 'false', label: 'as its last child' },
-      ]"
-    />
     <button class="btn btn-secondary go-back-button" @click="$emit('goBack')">
       <SvgGoBack />
     </button>
@@ -40,13 +24,11 @@
 
 <script setup lang="ts">
 import type { PropType } from "vue"
-import { ref, computed } from "vue"
+import { ref } from "vue"
 import type { Note } from "@/generated/backend"
 import { LinkCreation, NoteTopic } from "@/generated/backend"
 import LinkTypeSelect from "./LinkTypeSelect.vue"
 import NoteTopicComponent from "../notes/core/NoteTopicComponent.vue"
-import CheckInput from "../form/CheckInput.vue"
-import RadioButtons from "../form/RadioButtons.vue"
 import SvgGoBack from "../svgs/SvgGoBack.vue"
 import usePopups from "../commons/Popups/usePopups"
 import type { StorageAccessor } from "../../store/createNoteStorage"
@@ -65,23 +47,11 @@ const props = defineProps({
 const emit = defineEmits(["success", "goBack"])
 
 const formData = ref<LinkCreation>({
-  asFirstChild: false,
-  moveUnder: false,
   linkType: NoteTopic.linkType.NO_LINK,
 })
 
 const linkFormErrors = ref({
-  asFirstChild: undefined as string | undefined,
   linkType: undefined as string | undefined,
-  moveUnder: undefined as string | undefined,
-})
-
-// Computed property to handle the string <-> boolean conversion for asFirstChild
-const asFirstChildModel = computed({
-  get: () => (formData.value.asFirstChild ?? false).toString(),
-  set: (value: string) => {
-    formData.value.asFirstChild = value === "true"
-  },
 })
 
 const linkTypeSelected = async (linkType: LinkCreation["linkType"]) => {
@@ -96,14 +66,11 @@ const linkTypeSelected = async (linkType: LinkCreation["linkType"]) => {
   }
 
   try {
-    // First create the link
     if (linkType !== NoteTopic.linkType.NO_LINK) {
       await props.storageAccessor
         .storedApi()
         .createLink(props.note.id, props.targetNoteTopic.id, {
           linkType: formData.value.linkType,
-          moveUnder: false,
-          asFirstChild: false,
         })
     }
 
