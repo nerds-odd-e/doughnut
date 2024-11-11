ac2cda26aae0579e71d2b9f59dd63b2f23eeb816
Terry Yin <terry.yinzhe@gmail.com>
2024-11-12 07:19:47 +0800

    lint

--- backend/src/main/java/com/odde/doughnut/services/ai/TopicTitleReplacement.java
+++ backend/src/main/java/com/odde/doughnut/services/ai/TopicTitleReplacement.java
@@ -1,17 +1,21 @@
 package com.odde.doughnut.services.ai;
 
+import static com.odde.doughnut.entities.Note.NOTE_OF_CURRENT_FOCUS;
+
 import com.fasterxml.jackson.annotation.JsonPropertyDescription;
 import com.odde.doughnut.entities.Note;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
-import static com.odde.doughnut.entities.Note.NOTE_OF_CURRENT_FOCUS;
-
 @Data
 public class TopicTitleReplacement {
   @NotNull
   @Size(min = 1, max = Note.MAX_TITLE_LENGTH)
-  @JsonPropertyDescription("The topic to be replaced for the " + NOTE_OF_CURRENT_FOCUS + ". Max size is " + Note.MAX_TITLE_LENGTH)
+  @JsonPropertyDescription(
+      "The topic to be replaced for the "
+          + NOTE_OF_CURRENT_FOCUS
+          + ". Max size is "
+          + Note.MAX_TITLE_LENGTH)
   public String newTopic;
 }
--- backend/src/main/java/com/odde/doughnut/services/ai/tools/AiToolFactory.java
+++ backend/src/main/java/com/odde/doughnut/services/ai/tools/AiToolFactory.java
@@ -112,7 +112,7 @@ Please assume the role of a Memory Assistant, which involves helping me review, 
             "Text completion for the details of the note of focus",
             NoteDetailsCompletion.class),
         new AiTool(
-          SUGGEST_TOPIC_TITLE,
+            SUGGEST_TOPIC_TITLE,
             "Generate a concise and accurate note topic (a title) based on the note content and pass it to the function for the use to update their note. The topic should be a single word, a phrase or at most a single sentence that captures the atomic concept of the note. It should be specific within the note's context path and do not need to include general information that's already in the context path. Keep the existing topic if it's already correct and concise.",
             TopicTitleReplacement.class));
   }
