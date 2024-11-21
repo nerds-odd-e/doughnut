2ffa0ed8270b07b0f4caf9e8f2394090963ec63e
Tan Yeong Sheng <tanyeongsheng@gmail.com>
2024-11-21 10:01:09 +0800

    pnpm@9.14.2

--- shell.nix
+++ shell.nix
@@ -118,8 +118,8 @@ in mkShell {
       [[ -d $HOME/.cache/Cypress ]] || pnpx cypress install --force
     fi
 
-    corepack prepare pnpm@9.14.1 --activate
-    corepack use pnpm@9.14.1
+    corepack prepare pnpm@9.14.2 --activate
+    corepack use pnpm@9.14.2
     pnpm --frozen-lockfile recursive install
     # start biome daemon-server
     pnpm biome stop && pnpm biome start
