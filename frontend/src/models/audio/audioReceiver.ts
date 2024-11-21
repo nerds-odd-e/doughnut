92681f893dd457c3ba2c881d4d011b84533e998c
Terry Yin <terry.yinzhe@gmail.com>
2024-11-21 13:40:45 +0800

    to class

--- frontend/src/models/audio/audioReceiver.ts
+++ frontend/src/models/audio/audioReceiver.ts
@@ -11,61 +11,73 @@ export interface AudioReceiver {
   getBuffer: () => AudioBuffer
 }
 
-export const createAudioReceiver = (): AudioReceiver => {
-  let audioContext: AudioContext | null = null
-  let mediaStream: MediaStream | null = null
-  let audioInput: MediaStreamAudioSourceNode | null = null
-  let workletNode: AudioWorkletNode | null = null
-  const audioBuffer = createAudioBuffer(SAMPLE_RATE)
+export class RawAudioReceiver implements AudioReceiver {
+  private audioContext: AudioContext | null = null
+  private mediaStream: MediaStream | null = null
+  private audioInput: MediaStreamAudioSourceNode | null = null
+  private workletNode: AudioWorkletNode | null = null
+  private audioBuffer: AudioBuffer
 
-  return {
-    initialize: async (deviceId?: string): Promise<void> => {
-      try {
-        mediaStream = await navigator.mediaDevices.getUserMedia({
-          audio: deviceId ? { deviceId: { exact: deviceId } } : true,
-        })
-
-        const audioWorkletUrl = getAudioRecordingWorkerURL()
-        audioContext = new AudioContext({ sampleRate: SAMPLE_RATE })
-        await audioContext.audioWorklet.addModule(audioWorkletUrl)
-        audioInput = audioContext.createMediaStreamSource(mediaStream)
-        workletNode = new AudioWorkletNode(
-          audioContext,
-          "recorder-worklet-processor"
-        )
-
-        workletNode.port.onmessage = (event) => {
-          if (event.data.audioBuffer) {
-            audioBuffer.receiveAudioData(event.data.audioBuffer)
-          }
-        }
-        audioInput.connect(workletNode)
-        workletNode.connect(audioContext.destination)
-      } catch (error) {
-        console.error("Error initializing audio receiver:", error)
-        throw new Error("Failed to initialize audio receiver")
-      }
-    },
-
-    disconnect: () => {
-      if (workletNode) {
-        workletNode.disconnect()
-      }
-      if (audioInput) {
-        audioInput.disconnect()
-      }
-      if (mediaStream) {
-        mediaStream.getTracks().forEach((track) => track.stop())
-      }
-    },
-
-    isInitialized: () => !!audioContext,
-
-    async reconnect(deviceId: string): Promise<void> {
-      this.disconnect()
-      await this.initialize(deviceId)
-    },
-
-    getBuffer: () => audioBuffer,
+  constructor() {
+    this.audioBuffer = createAudioBuffer(SAMPLE_RATE)
   }
+
+  async initialize(deviceId?: string): Promise<void> {
+    try {
+      this.mediaStream = await navigator.mediaDevices.getUserMedia({
+        audio: deviceId ? { deviceId: { exact: deviceId } } : true,
+      })
+
+      const audioWorkletUrl = getAudioRecordingWorkerURL()
+      this.audioContext = new AudioContext({ sampleRate: SAMPLE_RATE })
+      await this.audioContext.audioWorklet.addModule(audioWorkletUrl)
+      this.audioInput = this.audioContext.createMediaStreamSource(
+        this.mediaStream
+      )
+      this.workletNode = new AudioWorkletNode(
+        this.audioContext,
+        "recorder-worklet-processor"
+      )
+
+      this.workletNode.port.onmessage = (event) => {
+        if (event.data.audioBuffer) {
+          this.audioBuffer.receiveAudioData(event.data.audioBuffer)
+        }
+      }
+      this.audioInput.connect(this.workletNode)
+      this.workletNode.connect(this.audioContext.destination)
+    } catch (error) {
+      console.error("Error initializing audio receiver:", error)
+      throw new Error("Failed to initialize audio receiver")
+    }
+  }
+
+  disconnect(): void {
+    if (this.workletNode) {
+      this.workletNode.disconnect()
+    }
+    if (this.audioInput) {
+      this.audioInput.disconnect()
+    }
+    if (this.mediaStream) {
+      this.mediaStream.getTracks().forEach((track) => track.stop())
+    }
+  }
+
+  isInitialized(): boolean {
+    return !!this.audioContext
+  }
+
+  async reconnect(deviceId: string): Promise<void> {
+    this.disconnect()
+    await this.initialize(deviceId)
+  }
+
+  getBuffer(): AudioBuffer {
+    return this.audioBuffer
+  }
+}
+
+export function createAudioReceiver(): AudioReceiver {
+  return new RawAudioReceiver()
 }
