e1d6001a02598dde7cd36598634495c2d7d07a35
Terry Yin <terry.yinzhe@gmail.com>
2024-11-24 21:31:10 +0800

    highlight

--- frontend/tests/toolbars/SidebarControl.spec.ts
+++ frontend/tests/toolbars/SidebarControl.spec.ts
@@ -3,14 +3,10 @@ import type { User } from "@/generated/backend/models/User"
 import { screen } from "@testing-library/vue"
 import makeMe from "@tests/fixtures/makeMe"
 import helper from "@tests/helpers"
-import { ref } from "vue"
 
-const mockedPush = vi.fn()
+const useRouteValue = { name: "" }
 vitest.mock("vue-router", () => ({
-  useRoute: () => vi.fn().mockReturnValue(ref(null)),
-  useRouter: () => ({
-    push: mockedPush,
-  }),
+  useRoute: () => useRouteValue,
 }))
 
 describe("sidebar control", () => {
@@ -32,5 +28,23 @@ describe("sidebar control", () => {
 
     const circlesLink = screen.getByRole("button", { name: "Circles" })
     expect(circlesLink).toBeInTheDocument()
+  })
+
+  it("highlights the notebooks link when on notebooks page", () => {
+    useRouteValue.name = "notebooks"
+
+    helper.component(SidebarControl).withProps({ user }).render()
+
+    const notebooksLink = screen.getByRole("button", { name: "Notebooks" })
+    expect(notebooksLink).toHaveClass("active")
+  })
+
+  it("highlights the circles link when on circle show page", () => {
+    useRouteValue.name = "circleShow"
+
+    helper.component(SidebarControl).withProps({ user }).render()
+
+    const circlesLink = screen.getByRole("button", { name: "Circles" })
+    expect(circlesLink).toHaveClass("active")
   })
 })
