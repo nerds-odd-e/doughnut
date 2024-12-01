93f09384aaf997a8b905a758d8917aa0c51a6a00
Terry Yin <terry.yinzhe@gmail.com>
2024-12-01 20:28:20 +0800

    inline progress bar

--- frontend/src/pages/AssimilationPage.vue
+++ frontend/src/pages/AssimilationPage.vue
@@ -1,20 +1,27 @@
 <template>
-  <ProgressBar
-    v-bind="{
-      paused: minimized,
-      title: `Initial Review: `,
-      finished,
-      toRepeatCount: remainingInitialReviewCountForToday,
-    }"
-    @resume="resume"
-  >
-  </ProgressBar>
+  <TeleportToHeadStatus>
+    <div class="flex-grow-1">
+      <span
+        :class="`progress-bar ${false ? 'thin' : ''}`"
+        v-if="remainingInitialReviewCountForToday !== null"
+      >
+        <span
+          class="progress"
+          :style="`width: ${(finished * 100) / (finished + remainingInitialReviewCountForToday)}%`"
+        >
+        </span>
+        <span class="progress-text">
+          Initial Review: {{ finished }}/{{ finished + remainingInitialReviewCountForToday }}
+        </span>
+      </span>
+    </div>
+  </TeleportToHeadStatus>
   <ContainerPage v-bind="{ contentLoaded: notes !== undefined }">
     <div v-if="notes?.length === 0" class="text-center py-8">
       You have achieved your daily new notes goal.
     </div>
     <Assimilation
-      v-if="!minimized && note"
+      v-if="note"
       v-bind="{ note, storageAccessor }"
       @initial-review-done="initialReviewDone"
       @reload-needed="onReloadNeeded"
@@ -26,20 +33,17 @@
 <script setup lang="ts">
 import type { PropType } from "vue"
 import { computed, onMounted, ref } from "vue"
-import { useRouter } from "vue-router"
 import type { Note } from "@/generated/backend"
 import useLoadingApi from "@/managedApi/useLoadingApi"
 import timezoneParam from "@/managedApi/window/timezoneParam"
-import ProgressBar from "@/components/commons/ProgressBar.vue"
 import Assimilation from "@/components/review/Assimilation.vue"
 import type { StorageAccessor } from "@/store/createNoteStorage"
 import ContainerPage from "./commons/ContainerPage.vue"
+import TeleportToHeadStatus from "@/pages/commons/TeleportToHeadStatus.vue"
 
-const router = useRouter()
 const { managedApi } = useLoadingApi()
 
 defineProps({
-  minimized: Boolean,
   storageAccessor: {
     type: Object as PropType<StorageAccessor>,
     required: true,
@@ -54,9 +58,7 @@ const note = computed(() => notes.value?.[0])
 const remainingInitialReviewCountForToday = computed(
   () => notes.value?.length || 0
 )
-const resume = () => {
-  router.push({ name: "assimilate" })
-}
+
 const initialReviewDone = () => {
   finished.value += 1
   notes.value?.shift()
@@ -78,3 +80,34 @@ const onReloadNeeded = () => {
   loadInitialReview()
 }
 </script>
+
+<style lang="scss" scoped>
+.progress-bar {
+  width: 100%;
+  background-color: gray;
+  height: 25px;
+  border-radius: 10px;
+  position: relative;
+
+  &.thin {
+    height: 5px;
+
+    .progress-text {
+      display: none;
+    }
+  }
+}
+
+.progress {
+  background-color: blue;
+  height: 100%;
+}
+
+.progress-text {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: white;
+}
+</style>
