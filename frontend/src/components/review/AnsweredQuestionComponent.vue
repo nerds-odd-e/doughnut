b4b3adb7f69e2e9433588d9f465300b4fbe88e1f
Terry Yin <terry.yinzhe@gmail.com>
2024-12-03 12:06:51 +0800

    style

--- frontend/src/components/review/AnsweredQuestionComponent.vue
+++ frontend/src/components/review/AnsweredQuestionComponent.vue
@@ -76,8 +76,22 @@ const startConversation = async () => {
   width: 100%
   border-radius: 5px
   padding: 8px 12px
-  margin-top: 10px
+  margin-top: 15px
   background-color: #f9f9f9
+  border: 2px solid #e0e0e0
+  position: relative
+
+  legend
+    padding: 0 10px
+    font-weight: 500
+    color: #666
+    font-size: 0.9rem
+    background-color: #f9f9f9
+    border-radius: 3px
+    position: absolute
+    top: -12px
+    left: 10px
+    width: auto
 
 .conversation-button
   position: fixed
