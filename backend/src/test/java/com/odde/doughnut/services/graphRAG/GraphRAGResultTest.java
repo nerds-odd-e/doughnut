8bd87859b9ad9a1ddd869fe4d77d9b9080e17e03
Terry Yin <terry.yinzhe@gmail.com>
2024-12-07 10:09:29 +0800

    rename test file

--- backend/src/test/java/com/odde/doughnut/services/graphRAG/GraphRAGResultTest.java
+++ backend/src/test/java/com/odde/doughnut/services/graphRAG/GraphRAGResultTest.java
@@ -17,7 +17,7 @@ import org.springframework.transaction.annotation.Transactional;
 @SpringBootTest
 @ActiveProfiles("test")
 @Transactional
-class BareNoteTest {
+class GraphRAGResultTest {
   @Autowired private MakeMe makeMe;
   private final ObjectMapper objectMapper = new ObjectMapper();
   private static final String PARENT_URI_AND_TITLE = "parentUriAndTitle";
