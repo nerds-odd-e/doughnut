34baa3f34fe496f30917e70db51c4b71d700b230
Terry Yin <terry.yinzhe@gmail.com>
2024-12-08 11:26:11 +0800

    constraint

--- backend/src/test/java/com/odde/doughnut/testability/builders/ReificationBuilder.java
+++ backend/src/test/java/com/odde/doughnut/testability/builders/ReificationBuilder.java
@@ -7,34 +7,13 @@ import com.odde.doughnut.testability.MakeMe;
 import java.sql.Timestamp;
 
 public class ReificationBuilder extends EntityBuilder<Note> {
-  User creator = null;
-
   public ReificationBuilder(MakeMe makeMe) {
     super(makeMe, null);
   }
 
   @Override
   protected void beforeCreate(boolean needPersist) {
-    if (entity == null) return;
-    if (creator == null) {
-      if (entity.getParent() != null) {
-        this.creator = entity.getParent().getCreator();
-      }
-    }
-    entity.setCreator(this.creator);
-  }
-
-  @Override
-  protected void afterCreate(boolean needPersist) {
-    if (entity == null) return;
-    if (needPersist) {
-      if (entity.getParent() != null) {
-        makeMe.refresh(entity.getParent());
-      }
-      if (entity.getTargetNote() != null) {
-        makeMe.refresh(entity.getTargetNote());
-      }
-    }
+    throw new UnsupportedOperationException("Please use between method");
   }
 
   public NoteBuilder between(Note from, Note to, LinkType linkType) {
