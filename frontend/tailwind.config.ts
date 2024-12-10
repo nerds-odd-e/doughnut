da755a0ce2d7d4164f49a45288beabd2f85f0c8a
Terry Yin <terry.yinzhe@gmail.com>
2024-12-10 14:14:31 +0800

    get prefix back

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -3,12 +3,12 @@ import daisyui from "daisyui"
 
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  prefix: "daisy-",
   theme: {
     extend: {},
   },
   plugins: [daisyui],
   daisyui: {
-    prefix: "daisy-",
     themes: false,
   },
 } satisfies Config
