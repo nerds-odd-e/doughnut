18d3fd1f59beed8f02acad4f3d7912b6491e773b
choucw045 <a932l41l4@gmail.com>
2025-01-08 13:05:56 +0800

    format

--- backend/src/main/java/com/odde/doughnut/entities/Notebook.java
+++ backend/src/main/java/com/odde/doughnut/entities/Notebook.java
@@ -9,17 +9,14 @@ import com.odde.doughnut.exceptions.ApiException;
 import com.odde.doughnut.models.Randomizer;
 import com.odde.doughnut.services.graphRAG.BareNote;
 import jakarta.persistence.*;
-
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
-
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.lang.NonNull;
@@ -27,212 +24,211 @@ import org.springframework.lang.NonNull;
 @Entity
 @Table(name = "notebook")
 @JsonPropertyOrder({
-    "id",
-    "certifiable",
-    "notebookSettings",
-    "creatorId",
-    "title",
-    "circle",
-    "headNoteId",
-    "shortDetails"
+  "id",
+  "certifiable",
+  "notebookSettings",
+  "creatorId",
+  "title",
+  "circle",
+  "headNoteId",
+  "shortDetails"
 })
 public class Notebook extends EntityIdentifiedByIdOnly {
-    @OneToOne
-    @JoinColumn(name = "creator_id")
-    @JsonIgnore
-    @Getter
-    @Setter
-    private User creatorEntity;
+  @OneToOne
+  @JoinColumn(name = "creator_id")
+  @JsonIgnore
+  @Getter
+  @Setter
+  private User creatorEntity;
 
-    @OneToOne
-    @JoinColumn(name = "ownership_id")
-    @Getter
-    @Setter
-    @JsonIgnore
-    private Ownership ownership;
+  @OneToOne
+  @JoinColumn(name = "ownership_id")
+  @Getter
+  @Setter
+  @JsonIgnore
+  private Ownership ownership;
 
-    @JoinTable(
-        name = "notebook_head_note",
-        joinColumns = {@JoinColumn(name = "notebook_id", referencedColumnName = "id")},
-        inverseJoinColumns = {@JoinColumn(name = "head_note_id", referencedColumnName = "id")})
-    @OneToOne
-    @Getter
-    @Setter
-    @NonNull
-    @JsonIgnore
-    private Note headNote;
+  @JoinTable(
+      name = "notebook_head_note",
+      joinColumns = {@JoinColumn(name = "notebook_id", referencedColumnName = "id")},
+      inverseJoinColumns = {@JoinColumn(name = "head_note_id", referencedColumnName = "id")})
+  @OneToOne
+  @Getter
+  @Setter
+  @NonNull
+  @JsonIgnore
+  private Note headNote;
 
-    @OneToMany(mappedBy = "notebook", cascade = CascadeType.DETACH)
-    @JsonIgnore
-    private final List<Note> notes = new ArrayList<>();
+  @OneToMany(mappedBy = "notebook", cascade = CascadeType.DETACH)
+  @JsonIgnore
+  private final List<Note> notes = new ArrayList<>();
 
-    @Embedded
-    @Getter
-    @NonNull
-    private NotebookSettings notebookSettings = new NotebookSettings();
+  @Embedded @Getter @NonNull private NotebookSettings notebookSettings = new NotebookSettings();
 
-    @Column(name = "deleted_at")
-    @Setter
-    @JsonIgnore
-    private Timestamp deletedAt;
+  @Column(name = "deleted_at")
+  @Setter
+  @JsonIgnore
+  private Timestamp deletedAt;
 
-    @OneToMany(mappedBy = "notebook", cascade = CascadeType.DETACH)
-    @JsonIgnore
-    private List<Subscription> subscriptions;
+  @OneToMany(mappedBy = "notebook", cascade = CascadeType.DETACH)
+  @JsonIgnore
+  private List<Subscription> subscriptions;
 
-    @OneToOne(mappedBy = "notebook")
-    @Getter
-    @JsonIgnore
-    private NotebookCertificateApproval notebookCertificateApproval;
+  @OneToOne(mappedBy = "notebook")
+  @Getter
+  @JsonIgnore
+  private NotebookCertificateApproval notebookCertificateApproval;
 
-    @Column(name = "updated_at")
-    @Getter
-    @Setter
-    @NonNull
-    private Timestamp updated_at;
+  @Column(name = "updated_at")
+  @Getter
+  @Setter
+  @NonNull
+  private Timestamp updated_at;
 
-    @OneToOne(mappedBy = "notebook", fetch = FetchType.LAZY)
-    @JsonIgnore
-    @Getter
-    private NotebookAssistant notebookAssistant;
+  @OneToOne(mappedBy = "notebook", fetch = FetchType.LAZY)
+  @JsonIgnore
+  @Getter
+  private NotebookAssistant notebookAssistant;
 
-    @OneToOne(mappedBy = "notebook", fetch = FetchType.LAZY)
-    @JsonIgnore
-    @Getter
-    private NotebookAiAssistant notebookAiAssistant;
+  @OneToOne(mappedBy = "notebook", fetch = FetchType.LAZY)
+  @JsonIgnore
+  @Getter
+  private NotebookAiAssistant notebookAiAssistant;
 
-    public boolean isCertifiable() {
-        return notebookCertificateApproval != null
-            && notebookCertificateApproval.getLastApprovalTime() != null;
+  public boolean isCertifiable() {
+    return notebookCertificateApproval != null
+        && notebookCertificateApproval.getLastApprovalTime() != null;
+  }
+
+  @JsonIgnore
+  public List<Note> getNotes() {
+    return Note.filterDeletedUnmodifiableNoteList(notes);
+  }
+
+  // Hibernate and JPA does not maintain the consistency of the bidirectional relationships
+  // Here we add the note to the notes of notebook in memory to avoid reload the notebook from
+  // database
+  public void addNoteInMemoryToSupportUnitTestOnly(Note note) {
+    this.notes.add(note);
+  }
+
+  @JsonIgnore
+  public String getNotebookDump() {
+    List<BareNote> noteBriefs = getNoteBriefs();
+    return defaultObjectMapper().valueToTree(noteBriefs).toPrettyString();
+  }
+
+  @JsonIgnore
+  public List<BareNote> getNoteBriefs() {
+    List<BareNote> noteBriefs =
+        notes.stream()
+            .sorted(
+                Comparator.comparing(Note::getParentId, Comparator.nullsFirst(Integer::compare))
+                    .thenComparing(Note::getSiblingOrder, Comparator.nullsFirst(Long::compare)))
+            .map(n -> BareNote.fromNoteWithoutTruncate(n))
+            .toList();
+    ;
+    return noteBriefs;
+  }
+
+  public String getCreatorId() {
+    return creatorEntity.getExternalIdentifier();
+  }
+
+  @JsonIgnore
+  public List<PredefinedQuestion> getApprovedPredefinedQuestionsForAssessment(
+      Randomizer randomizer) {
+    Integer numberOfQuestion = getNotebookSettings().getNumberOfQuestionsInAssessment();
+    if (numberOfQuestion == null || numberOfQuestion == 0) {
+      throw new ApiException(
+          "The assessment is not available",
+          ASSESSMENT_SERVICE_ERROR,
+          "The assessment is not available");
     }
 
-    @JsonIgnore
-    public List<Note> getNotes() {
-        return Note.filterDeletedUnmodifiableNoteList(notes);
+    List<PredefinedQuestion> questions =
+        randomizer.shuffle(getNotes()).stream()
+            .flatMap(
+                note ->
+                    randomizer
+                        .chooseOneRandomly(
+                            note.getPredefinedQuestions().stream()
+                                .filter(PredefinedQuestion::isApproved)
+                                .toList())
+                        .stream())
+            .limit(numberOfQuestion)
+            .toList();
+
+    if (questions.size() < numberOfQuestion) {
+      throw new ApiException(
+          "Not enough questions", ASSESSMENT_SERVICE_ERROR, "Not enough questions");
     }
+    return questions;
+  }
 
-    // Hibernate and JPA does not maintain the consistency of the bidirectional relationships
-    // Here we add the note to the notes of notebook in memory to avoid reload the notebook from
-    // database
-    public void addNoteInMemoryToSupportUnitTestOnly(Note note) {
-        this.notes.add(note);
+  @NonNull
+  public String getTitle() {
+    return headNote.getTopicConstructor();
+  }
+
+  @NonNull
+  public Integer getHeadNoteId() {
+    return headNote.getId();
+  }
+
+  public String getShortDetails() {
+    return headNote.getShortDetails();
+  }
+
+  public Circle getCircle() {
+    return getOwnership().getCircle();
+  }
+
+  @JsonIgnore
+  public NotebookAssistant buildOrEditNotebookAssistant(
+      Timestamp currentUTCTimestamp, User creator, String id) {
+    NotebookAssistant notebookAssistant = getNotebookAssistant();
+    if (notebookAssistant == null) {
+      notebookAssistant = new NotebookAssistant();
+      notebookAssistant.setNotebook(this);
     }
+    notebookAssistant.setCreator(creator);
+    notebookAssistant.setCreatedAt(currentUTCTimestamp);
+    notebookAssistant.setAssistantId(id);
+    return notebookAssistant;
+  }
 
-    @JsonIgnore
-    public String getNotebookDump() {
-        List<BareNote> noteBriefs = getNoteBriefs();
-        return defaultObjectMapper().valueToTree(noteBriefs).toPrettyString();
+  @JsonIgnore
+  public byte[] generateObsidianExport() throws IOException {
+
+    try (var baos = new ByteArrayOutputStream();
+        var zos = new ZipOutputStream(baos)) {
+
+      writeNoteToZip(this.getHeadNote(), zos, "");
+
+      zos.close();
+      return baos.toByteArray();
     }
+  }
 
-    @JsonIgnore
-    public List<BareNote> getNoteBriefs() {
-        List<BareNote> noteBriefs =
-            notes.stream()
-                .sorted(
-                    Comparator.comparing(Note::getParentId, Comparator.nullsFirst(Integer::compare))
-                        .thenComparing(Note::getSiblingOrder, Comparator.nullsFirst(Long::compare)))
-                .map(n -> BareNote.fromNoteWithoutTruncate(n))
-                .toList();
-        ;
-        return noteBriefs;
-    }
-
-    public String getCreatorId() {
-        return creatorEntity.getExternalIdentifier();
-    }
-
-    @JsonIgnore
-    public List<PredefinedQuestion> getApprovedPredefinedQuestionsForAssessment(
-        Randomizer randomizer) {
-        Integer numberOfQuestion = getNotebookSettings().getNumberOfQuestionsInAssessment();
-        if (numberOfQuestion == null || numberOfQuestion == 0) {
-            throw new ApiException(
-                "The assessment is not available",
-                ASSESSMENT_SERVICE_ERROR,
-                "The assessment is not available");
-        }
-
-        List<PredefinedQuestion> questions =
-            randomizer.shuffle(getNotes()).stream()
-                .flatMap(
-                    note ->
-                        randomizer
-                            .chooseOneRandomly(
-                                note.getPredefinedQuestions().stream()
-                                    .filter(PredefinedQuestion::isApproved)
-                                    .toList())
-                            .stream())
-                .limit(numberOfQuestion)
-                .toList();
-
-        if (questions.size() < numberOfQuestion) {
-            throw new ApiException(
-                "Not enough questions", ASSESSMENT_SERVICE_ERROR, "Not enough questions");
-        }
-        return questions;
-    }
-
-    @NonNull
-    public String getTitle() {
-        return headNote.getTopicConstructor();
-    }
-
-    @NonNull
-    public Integer getHeadNoteId() {
-        return headNote.getId();
-    }
-
-    public String getShortDetails() {
-        return headNote.getShortDetails();
-    }
-
-    public Circle getCircle() {
-        return getOwnership().getCircle();
-    }
-
-    @JsonIgnore
-    public NotebookAssistant buildOrEditNotebookAssistant(
-        Timestamp currentUTCTimestamp, User creator, String id) {
-        NotebookAssistant notebookAssistant = getNotebookAssistant();
-        if (notebookAssistant == null) {
-            notebookAssistant = new NotebookAssistant();
-            notebookAssistant.setNotebook(this);
-        }
-        notebookAssistant.setCreator(creator);
-        notebookAssistant.setCreatedAt(currentUTCTimestamp);
-        notebookAssistant.setAssistantId(id);
-        return notebookAssistant;
-    }
-
-    @JsonIgnore
-    public byte[] generateObsidianExport() throws IOException {
-
-        try (var baos = new ByteArrayOutputStream();
-             var zos = new ZipOutputStream(baos)) {
-
-            writeNoteToZip(this.getHeadNote(), zos, "");
-
-            zos.close();
-            return baos.toByteArray();
-        }
-    }
-
-    private void writeNoteToZip(Note note, ZipOutputStream zos, String path) throws IOException {
-        String filePath = path.isEmpty()
+  private void writeNoteToZip(Note note, ZipOutputStream zos, String path) throws IOException {
+    String filePath =
+        path.isEmpty()
             ? note.getTopicConstructor() + ".md"
             : path + "/" + note.getTopicConstructor() + ".md";
-        String fileContent = "# " + note.getTopicConstructor() + "\n" + note.getDetails();
-        zos.putNextEntry(new ZipEntry(filePath));
-        zos.write(fileContent.getBytes());
+    String fileContent = "# " + note.getTopicConstructor() + "\n" + note.getDetails();
+    zos.putNextEntry(new ZipEntry(filePath));
+    zos.write(fileContent.getBytes());
 
-        for (Note child : note.getChildren()) {
-            writeNoteToZip(child, zos, path.isEmpty()
-                ? note.getTopicConstructor()
-                : path + "/" + note.getTopicConstructor());
-        }
+    for (Note child : note.getChildren()) {
+      writeNoteToZip(
+          child,
+          zos,
+          path.isEmpty() ? note.getTopicConstructor() : path + "/" + note.getTopicConstructor());
     }
+  }
 
-    private String sanitizeFileName(String fileName) {
-        return fileName.replaceAll("[\\\\/:*?\"<>|]", "_");
-    }
+  private String sanitizeFileName(String fileName) {
+    return fileName.replaceAll("[\\\\/:*?\"<>|]", "_");
+  }
 }
