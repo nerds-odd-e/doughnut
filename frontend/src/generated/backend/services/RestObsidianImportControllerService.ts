e8a7b838cd479bbe41da7b5cd52277970d6f7128
fusoon.lee <fusoon.lee@titansoft.com.sg>
2025-01-08 15:27:57 +0800

    update openAPI for return empty during import obsidian

--- frontend/src/generated/backend/services/RestObsidianImportControllerService.ts
+++ frontend/src/generated/backend/services/RestObsidianImportControllerService.ts
@@ -2,32 +2,31 @@
 /* istanbul ignore file */
 /* tslint:disable */
 /* eslint-disable */
-import type { NoteRealm } from '../models/NoteRealm';
 import type { CancelablePromise } from '../core/CancelablePromise';
 import type { BaseHttpRequest } from '../core/BaseHttpRequest';
 export class RestObsidianImportControllerService {
     constructor(public readonly httpRequest: BaseHttpRequest) {}
     /**
      * Import Obsidian file
-     * @param parentNoteId Parent note ID
+     * @param notebookId Notebook ID
      * @param formData
-     * @returns NoteRealm OK
+     * @returns any OK
      * @throws ApiError
      */
     public importObsidian(
-        parentNoteId: number,
+        notebookId: number,
         formData?: {
             /**
              * Obsidian zip file to import
              */
             file: Blob;
         },
-    ): CancelablePromise<NoteRealm> {
+    ): CancelablePromise<any> {
         return this.httpRequest.request({
             method: 'POST',
-            url: '/api/obsidian/{parentNoteId}/import',
+            url: '/api/obsidian/{notebookId}/import',
             path: {
-                'parentNoteId': parentNoteId,
+                'notebookId': notebookId,
             },
             formData: formData,
             mediaType: 'multipart/form-data',
