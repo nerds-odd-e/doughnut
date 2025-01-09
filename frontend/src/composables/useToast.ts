e793d23546bd1c166c90ea485a8b2fd2e53fa02c
norman <cwhsu1984@gmail.com>
2025-01-09 14:33:53 +0800

    fix warning

--- frontend/src/composables/useToast.ts
+++ frontend/src/composables/useToast.ts
@@ -4,14 +4,14 @@ export function useToast() {
   const toast = useVueToast()
 
   const showSuccessToast = (message: string, options = {}) => {
-    const toastElement = toast.success(message, {
+    toast.success(message, {
       ...options,
       container: { "data-testid": "toast-message" },
     })
   }
 
   const showErrorToast = (message: string, options = {}) => {
-    const toastElement = toast.error(message, {
+    toast.error(message, {
       ...options,
       container: { "data-testid": "toast-message" },
     })
