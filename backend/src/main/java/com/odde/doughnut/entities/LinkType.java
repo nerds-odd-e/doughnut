aab3107cb317e7f8140af8a470a5cfb4654313c2
Terry Yin <terry.yinzhe@gmail.com>
2025-02-10 15:51:29 +0800

    remove old link questions

--- backend/src/main/java/com/odde/doughnut/entities/LinkType.java
+++ backend/src/main/java/com/odde/doughnut/entities/LinkType.java
@@ -14,127 +14,77 @@ public enum LinkType {
       "specification",
       "a specialization of",
       "a generalization of",
-      new LinkQuestionType[] {
-        LINK_TARGET,
-        LINK_SOURCE,
-        WHICH_SPEC_HAS_INSTANCE,
-        FROM_SAME_PART_AS,
-        FROM_DIFFERENT_PART_AS,
-        DESCRIPTION_LINK_TARGET
-      }),
+      new LinkQuestionType[] {LINK_SOURCE, DESCRIPTION_LINK_TARGET}),
   APPLICATION(
       3,
       "application",
       "an application of",
       "applied to",
-      new LinkQuestionType[] {
-        LINK_TARGET,
-        LINK_SOURCE_WITHIN_SAME_LINK_TYPE,
-        WHICH_SPEC_HAS_INSTANCE,
-        FROM_SAME_PART_AS,
-        FROM_DIFFERENT_PART_AS,
-        DESCRIPTION_LINK_TARGET
-      }),
+      new LinkQuestionType[] {LINK_SOURCE_WITHIN_SAME_LINK_TYPE, DESCRIPTION_LINK_TARGET}),
 
   INSTANCE(
       4,
       "instance",
       "an instance of",
       "has instances",
-      new LinkQuestionType[] {
-        LINK_TARGET,
-        LINK_SOURCE,
-        WHICH_SPEC_HAS_INSTANCE,
-        FROM_SAME_PART_AS,
-        FROM_DIFFERENT_PART_AS,
-        DESCRIPTION_LINK_TARGET
-      }),
+      new LinkQuestionType[] {LINK_SOURCE, DESCRIPTION_LINK_TARGET}),
   /*INTEGRATED*/ PART(
       6,
       "part",
       "a part of",
       "has parts",
-      new LinkQuestionType[] {
-        LINK_TARGET,
-        LINK_SOURCE,
-        WHICH_SPEC_HAS_INSTANCE,
-        FROM_SAME_PART_AS,
-        FROM_DIFFERENT_PART_AS,
-        DESCRIPTION_LINK_TARGET
-      }),
+      new LinkQuestionType[] {LINK_SOURCE, DESCRIPTION_LINK_TARGET}),
   /*NON INTEGRATED*/ TAGGED_BY(
       8,
       "tag target",
       "tagged by",
       "tagging",
-      new LinkQuestionType[] {
-        LINK_TARGET,
-        LINK_SOURCE,
-        WHICH_SPEC_HAS_INSTANCE,
-        WHICH_SPEC_HAS_INSTANCE,
-        DESCRIPTION_LINK_TARGET
-      }),
+      new LinkQuestionType[] {LINK_SOURCE, WHICH_SPEC_HAS_INSTANCE, DESCRIPTION_LINK_TARGET}),
   ATTRIBUTE(
       10,
       "attribute",
       "an attribute of",
       "has attributes",
-      new LinkQuestionType[] {
-        LINK_TARGET,
-        LINK_SOURCE,
-        WHICH_SPEC_HAS_INSTANCE,
-        WHICH_SPEC_HAS_INSTANCE,
-        DESCRIPTION_LINK_TARGET
-      }),
+      new LinkQuestionType[] {LINK_SOURCE, DESCRIPTION_LINK_TARGET}),
 
   OPPOSITE_OF(
       12,
       "opposition",
       "the opposite of",
       "the opposite of",
-      new LinkQuestionType[] {LINK_TARGET, LINK_SOURCE, DESCRIPTION_LINK_TARGET}),
+      new LinkQuestionType[] {LINK_SOURCE, DESCRIPTION_LINK_TARGET}),
   AUTHOR_OF(
       14,
       "author",
       "author of",
       "brought by",
-      new LinkQuestionType[] {LINK_TARGET, LINK_SOURCE, DESCRIPTION_LINK_TARGET}),
+      new LinkQuestionType[] {LINK_SOURCE, DESCRIPTION_LINK_TARGET}),
   USES(
       15,
       "user",
       "using",
       "used by",
-      new LinkQuestionType[] {
-        LINK_TARGET,
-        LINK_SOURCE,
-        WHICH_SPEC_HAS_INSTANCE,
-        FROM_SAME_PART_AS,
-        FROM_DIFFERENT_PART_AS,
-        DESCRIPTION_LINK_TARGET
-      }),
+      new LinkQuestionType[] {LINK_SOURCE, DESCRIPTION_LINK_TARGET}),
   EXAMPLE_OF(
       17,
       "example",
       "an example of",
       "has examples",
       new LinkQuestionType[] {
-        LINK_SOURCE_WITHIN_SAME_LINK_TYPE,
-        CLOZE_LINK_TARGET,
-        FROM_SAME_PART_AS,
-        FROM_DIFFERENT_PART_AS
+        LINK_SOURCE_WITHIN_SAME_LINK_TYPE, CLOZE_LINK_TARGET,
       }),
   PRECEDES(
       19,
       "precedence",
       "before",
       "after",
-      new LinkQuestionType[] {LINK_TARGET, LINK_SOURCE, DESCRIPTION_LINK_TARGET}),
+      new LinkQuestionType[] {LINK_SOURCE, DESCRIPTION_LINK_TARGET}),
   SIMILAR_TO(
       22,
       "note",
       "similar to",
       "similar to",
-      new LinkQuestionType[] {LINK_TARGET, LINK_SOURCE, DESCRIPTION_LINK_TARGET}),
+      new LinkQuestionType[] {LINK_SOURCE, DESCRIPTION_LINK_TARGET}),
   CONFUSE_WITH(23, "note", "confused with", "confused with", new LinkQuestionType[] {});
 
   @JsonValue public final String label;
--- backend/src/main/java/com/odde/doughnut/services/LinkQuestionType.java
+++ backend/src/main/java/com/odde/doughnut/services/LinkQuestionType.java
@@ -7,13 +7,11 @@ import com.odde.doughnut.factoryServices.quizFacotries.factories.*;
 import java.util.function.BiFunction;
 
 public enum LinkQuestionType {
-  LINK_TARGET(LinkTargetPredefinedFactory::new),
   LINK_SOURCE(LinkSourcePredefinedFactory::new),
   LINK_SOURCE_WITHIN_SAME_LINK_TYPE(LinkSourceWithinSameLinkTypePredefinedFactory::new),
   CLOZE_LINK_TARGET(ClozeLinkTargetPredefinedFactory::new),
   DESCRIPTION_LINK_TARGET(DescriptionLinkTargetPredefinedFactory::new),
   WHICH_SPEC_HAS_INSTANCE(WhichSpecHasInstancePredefinedFactory::new),
-  FROM_SAME_PART_AS(FromSamePartAsPredefinedFactory::new),
   FROM_DIFFERENT_PART_AS(FromDifferentPartAsPredefinedFactory::new);
 
   public final BiFunction<Note, PredefinedQuestionServant, PredefinedQuestionFactory>
