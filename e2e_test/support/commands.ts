0ceab49f332d4cea87f959424684ed8b4c1d510f
Terry Yin <terry.yinzhe@gmail.com>
2025-02-11 12:58:30 +0800

    real fix

--- e2e_test/support/commands.ts
+++ e2e_test/support/commands.ts
@@ -49,7 +49,11 @@ Cypress.Commands.add('clearFocusedText', () => {
   // cy.clear for now is an alias of cy.type('{selectall}{backspace}')
   // it doesn't clear the text sometimes.
   // Invoking it twice seems to solve the problem.
-  cy.focused().clear().clear()
+  cy.focused()
+    .should('be.visible')
+    .should('match', 'input, textarea')
+    .clear()
+    .clear()
 })
 
 Cypress.Commands.add('replaceFocusedTextAndEnter', (text) => {
