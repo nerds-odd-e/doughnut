c75cf0e5e0665f52118d3ede886521f916105f97
Terry Yin <terry.yinzhe@gmail.com>
2025-02-13 10:56:55 +0800

    always show to move to next btn

--- frontend/src/components/review/Quiz.vue
+++ frontend/src/components/review/Quiz.vue
@@ -34,28 +34,28 @@
           />
         </template>
       </template>
-      <button
-        v-if="canMoveToEnd"
-        class="daisy-btn daisy-btn-ghost daisy-btn-circle"
-        title="Move to end of list"
-        @click="moveToEnd"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="16"
-          height="16"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        >
-          <line x1="12" y1="5" x2="12" y2="19"></line>
-          <polyline points="19 12 12 19 5 12"></polyline>
-        </svg>
-      </button>
     </template>
+    <button
+      v-if="canMoveToEnd"
+      class="daisy-btn daisy-btn-ghost daisy-btn-circle"
+      title="Move to end of list"
+      @click="moveToEnd"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="16"
+        height="16"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      >
+        <line x1="12" y1="5" x2="12" y2="19"></line>
+        <polyline points="19 12 12 19 5 12"></polyline>
+      </svg>
+    </button>
   </div>
 </template>
 
