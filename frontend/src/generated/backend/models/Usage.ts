a1018b70cedcb293624605e6bf07dc3fce316be4
Terry Yin <terry.yinzhe@gmail.com>
2025-03-31 16:50:04 +0800

    update api doc

--- frontend/src/generated/backend/models/Usage.ts
+++ frontend/src/generated/backend/models/Usage.ts
@@ -2,9 +2,13 @@
 /* istanbul ignore file */
 /* tslint:disable */
 /* eslint-disable */
+import type { CompletionTokensDetails } from './CompletionTokensDetails';
+import type { PromptTokensDetails } from './PromptTokensDetails';
 export type Usage = {
     prompt_tokens?: number;
     completion_tokens?: number;
     total_tokens?: number;
+    prompt_tokens_details?: PromptTokensDetails;
+    completion_tokens_details?: CompletionTokensDetails;
 };
 
