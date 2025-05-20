44c3686dd8bb52171862eaccad880fd4d59a5256
trongnghia-nguyen <nghiant.nguyen@outlook.com>
2025-05-20 12:21:02 +0800

    Create local mcp server

---
+++ doughnut-mcp-server/README.md
@@ -0,0 +1,22 @@
+# doughnut-mcp-server MCP Server
+
+A Model Context Protocol server for doughnut
+
+This is a TypeScript-based MCP server that implements a simple notes system. It demonstrates core MCP concepts by providing:
+
+- Tools for get instruction,...
+
+## Features
+
+### Tools
+- `get_intruction` - Get intruction of Doughnut
+  - Takes an empty object as parameter
+  - Return instruction for Doughnut
+
+## Development
+
+TBD
+
+## Installation
+
+TBD
---
+++ doughnut-mcp-server/src/index.ts
@@ -0,0 +1,85 @@
+/**
+ * This is a MCP server that implements a connection to Doughnut system.
+ * It demonstrates core MCP concepts like resources and tools by allowing:
+ * - Get instrucion
+ */
+
+import { Server } from "@modelcontextprotocol/sdk/server/index.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import {
+  CallToolRequestSchema,
+  ListToolsRequestSchema,
+} from "@modelcontextprotocol/sdk/types.js";
+
+/**
+ * Create an MCP server to connect to Doughnut server
+ */
+const server = new Server(
+  {
+    name: "doughnut-mcp-server",
+    version: "0.1.0",
+  },
+  {
+    capabilities: {
+      resources: {},
+      tools: {},
+      prompts: {},
+    },
+  }
+);
+
+/**
+ * Handler that lists available tools.
+ * Exposes a single "get_instruction" tool that lets clients get instruction.
+ */
+server.setRequestHandler(ListToolsRequestSchema, async () => {
+  return {
+    tools: [
+      {
+        name: "get_instruction",
+        description: "Get instruction",
+        inputSchema: {
+          type: "object"
+        }
+      }
+    ]
+  };
+});
+
+/**
+ * Handler for the create_note tool.
+ * Creates a new note with the provided title and content, and returns success message.
+ */
+server.setRequestHandler(CallToolRequestSchema, async (request) => {
+  switch (request.params.name) {
+    case "get_instruction": {
+
+      return {
+        content: [
+          {
+            type: "text",
+            text: "Doughnut is a Personal Knowledge Management tool"
+          }
+        ]
+      };
+    }
+
+    default:
+      throw new Error("Unknown tool");
+  }
+});
+
+
+/**
+ * Start the server using stdio transport.
+ * This allows the server to communicate via standard input/output streams.
+ */
+async function main() {
+  const transport = new StdioServerTransport();
+  await server.connect(transport);
+}
+
+main().catch((error) => {
+  console.error("Server error:", error);
+  process.exit(1);
+});
