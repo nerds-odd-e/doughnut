7a689812c28ea02d2618b59ab62ad09c3ee7ce89
Terry Yin <terry.yinzhe@gmail.com>
2025-07-08 16:03:55 +0800

    ignore id

--- backend/src/main/java/com/odde/doughnut/controllers/dto/QuestionSuggestionParams.java
+++ backend/src/main/java/com/odde/doughnut/controllers/dto/QuestionSuggestionParams.java
@@ -1,10 +1,12 @@
 package com.odde.doughnut.controllers.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.odde.doughnut.services.ai.MCQWithAnswer;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 @AllArgsConstructor
 @NoArgsConstructor
 public class QuestionSuggestionParams {
