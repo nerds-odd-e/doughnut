b49d5f29999776cfecc71766c8bd95ad8ce61e7b
Terry Yin <terry.yinzhe@gmail.com>
2025-07-08 15:58:33 +0800

    fix mock

--- backend/src/main/java/com/odde/doughnut/controllers/dto/GlobalAiModelSettings.java
+++ backend/src/main/java/com/odde/doughnut/controllers/dto/GlobalAiModelSettings.java
@@ -3,12 +3,14 @@ package com.odde.doughnut.controllers.dto;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @JsonPropertyOrder({"questionGenerationModel", "evaluationModel", "othersModel"})
 public class GlobalAiModelSettings {
-  String questionGenerationModel;
-  String evaluationModel;
-  String othersModel;
+  public String questionGenerationModel;
+  public String evaluationModel;
+  public String othersModel;
 }
