dd7aae602b4433a44ef83b5f1d5f56ccf2af4f74
Terry Yin <terry.yinzhe@gmail.com>
2025-07-08 16:18:31 +0800

    dto

--- backend/src/main/java/com/odde/doughnut/controllers/dto/UserDTO.java
+++ backend/src/main/java/com/odde/doughnut/controllers/dto/UserDTO.java
@@ -1,11 +1,13 @@
 package com.odde.doughnut.controllers.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class UserDTO {
 
   @NotNull
