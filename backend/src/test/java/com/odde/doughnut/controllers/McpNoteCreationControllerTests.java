b947c2c6c6585ce688e18f6848f9c1db974d5102
one team pair 2 <oneteam2@foobar.com>
2025-08-28 11:43:32 +0800

    add unit test for McpNoteCreationController

---
+++ backend/src/test/java/com/odde/doughnut/controllers/McpNoteCreationControllerTests.java
@@ -0,0 +1,101 @@
+package com.odde.doughnut.controllers;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+import com.odde.doughnut.controllers.dto.McpNoteAddDTO;
+import com.odde.doughnut.controllers.dto.NoteCreationDTO;
+import com.odde.doughnut.controllers.dto.NoteSearchResult;
+import com.odde.doughnut.controllers.dto.NoteTopology;
+import com.odde.doughnut.entities.Note;
+import com.odde.doughnut.entities.repositories.NoteRepository;
+import com.odde.doughnut.exceptions.UnexpectedNoAccessRightException;
+import com.odde.doughnut.factoryServices.ModelFactoryService;
+import com.odde.doughnut.models.UserModel;
+import com.odde.doughnut.services.httpQuery.HttpClientAdapter;
+import com.odde.doughnut.services.search.NoteSearchService;
+import com.odde.doughnut.testability.MakeMe;
+import com.odde.doughnut.testability.TestabilitySettings;
+import java.io.IOException;
+import java.util.Arrays;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.BindException;
+import org.springframework.web.context.WebApplicationContext;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@Transactional
+class McpNoteCreationControllerTests {
+
+  private McpNoteCreationController controller;
+  private NoteCreationDTO noteCreation;
+  @MockitoBean private NoteRepository noteRepository;
+  private final TestabilitySettings testabilitySettings = new TestabilitySettings();
+  @Mock HttpClientAdapter httpClientAdapter;
+  @MockitoBean NoteSearchService noteSearchService;
+  @Autowired MakeMe makeMe;
+  @Autowired WebApplicationContext webApplicationContext;
+
+  @Autowired private ModelFactoryService modelFactoryService;
+
+  @BeforeEach
+  void setup() {
+    MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    UserModel userModel = makeMe.aUser().toModelPlease();
+    noteCreation = new NoteCreationDTO();
+    noteCreation.setNewTitle("new note");
+    controller =
+        new McpNoteCreationController(
+            modelFactoryService,
+            userModel,
+            httpClientAdapter,
+            testabilitySettings,
+            noteSearchService,
+            noteRepository);
+
+    Note lordOfTheRingsNote = makeMe.aNote().creatorAndOwner(userModel).please();
+    lordOfTheRingsNote.setTopicConstructor("Lord of the Rings");
+    when(noteRepository.findById(org.mockito.ArgumentMatchers.any()))
+        .thenReturn(java.util.Optional.of(lordOfTheRingsNote));
+
+    var noteTopology = new NoteTopology();
+    noteTopology.setId(1);
+    var searchResult = new NoteSearchResult();
+    searchResult.setNoteTopology(noteTopology);
+    when(noteSearchService.searchForNotes(
+            org.mockito.ArgumentMatchers.any(), org.mockito.ArgumentMatchers.any()))
+        .thenReturn((Arrays.asList(searchResult)));
+  }
+
+  @Nested
+  class CreateNoteTest {
+
+    @Test
+    void shouldCreateNoteSuccessfully()
+        throws UnexpectedNoAccessRightException, BindException, IOException, InterruptedException {
+      var mcpNoteDTO = new McpNoteAddDTO();
+      mcpNoteDTO.parentNote = "Lord of the Rings";
+      mcpNoteDTO.noteCreationDTO = noteCreation;
+      var response = controller.createNote(mcpNoteDTO);
+      assertEquals("Added new note to parent Notebook Lord of the Rings", response);
+    }
+    /*
+    @Test
+    void shouldThrowWhenCreatingNoteWithoutAccess() {
+      Note otherNote = makeMe.aNote().creatorAndOwner(makeMe.aUser().please()).please();
+      BindException exception =
+          assertThrows(
+              BindException.class, () -> controller.createNote(otherNote, noteCreation));
+      assertThat(exception.getMessage(), containsString("No access rights"));
+    }*/
+  }
+}
