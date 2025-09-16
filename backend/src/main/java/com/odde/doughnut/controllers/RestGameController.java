0fc1a3f8d4d4499676b20c8ec5fc28abd8d6f2b7
friendship2 <friendship2@rfv.com>
2025-09-16 17:27:04 +0800

    lint

--- backend/src/main/java/com/odde/doughnut/controllers/RestGameController.java
+++ backend/src/main/java/com/odde/doughnut/controllers/RestGameController.java
@@ -4,9 +4,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.sql.Timestamp;
-
 @RestController
 @RequestMapping("/api/games")
-public class RestGameController {
-}
+public class RestGameController {}
