99aa822fbb630a985cfd6a53ee8bc9cbec356064
Berk Türetken <berkturetken1997@hotmail.com>
2025-10-02 15:52:46 +0300

    Let's not forget to run formatting before pushing next time 🙃

--- e2e_test/step_definitions/notebook.ts
+++ e2e_test/step_definitions/notebook.ts
@@ -191,5 +191,9 @@ Then('I should see popup {string}', (message: string) => {
 })
 
 When('I select the notebook {string} as MCP_Notebook', (noteTitle: string) => {
-  start.routerToNotebooksPage().notebookCard(noteTitle).editNotebookSettings().selectMCPNotebook()
+  start
+    .routerToNotebooksPage()
+    .notebookCard(noteTitle)
+    .editNotebookSettings()
+    .selectMCPNotebook()
 })
