d5f653f563cdfd315946a0dccfe40eff1c380ed5
Terry Yin <terry@odd-e.com>
2025-11-14 11:42:34 +0800

    Remove NotebookAssistantForNoteService and related factory methods
    - Deleted the NotebookAssistantForNoteService class and its associated methods from NotebookAssistantForNoteServiceFactory to streamline the codebase.
    - This removal eliminates unused dependencies and simplifies the service architecture, enhancing overall clarity and maintainability.

--- backend/src/main/java/com/odde/doughnut/services/NotebookAssistantForNoteServiceFactory.java
+++ backend/src/main/java/com/odde/doughnut/services/NotebookAssistantForNoteServiceFactory.java
@@ -2,10 +2,7 @@ package com.odde.doughnut.services;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.odde.doughnut.entities.Note;
-import com.odde.doughnut.entities.Notebook;
-import com.odde.doughnut.entities.NotebookAssistant;
 import com.odde.doughnut.services.ai.ChatCompletionNoteAutomationService;
-import com.odde.doughnut.services.ai.OpenAiAssistant;
 import com.odde.doughnut.services.openAiApis.OpenAiApiHandler;
 import com.theokanning.openai.client.OpenAiApi;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -24,32 +21,6 @@ public final class NotebookAssistantForNoteServiceFactory {
     this.globalSettingsService = globalSettingsService;
     this.openAiApiHandler = new OpenAiApiHandler(openAiApi);
     this.objectMapper = objectMapper;
-  }
-
-  private String getNotebookAssistantId(Notebook notebook) {
-    NotebookAssistant assistant = notebook.getNotebookAssistant();
-    if (assistant != null) {
-      return assistant.getAssistantId();
-    }
-    return null;
-  }
-
-  private NotebookAssistantForNoteService getNotebookAssistantForNoteService(Note note) {
-    String assistantId = getNotebookAssistantId(note.getNotebook());
-    if (assistantId != null) {
-      return getServiceByAssistantId(assistantId, note);
-    }
-    return getDefaultAssistantForNoteService(note);
-  }
-
-  private NotebookAssistantForNoteService getDefaultAssistantForNoteService(Note note) {
-    return getServiceByAssistantId(globalSettingsService.defaultAssistantId().getValue(), note);
-  }
-
-  private NotebookAssistantForNoteService getServiceByAssistantId(String assistantId, Note note) {
-    OpenAiAssistant assistantServiceForNotebook =
-        new OpenAiAssistant(openAiApiHandler, assistantId);
-    return new NotebookAssistantForNoteService(assistantServiceForNotebook, note, objectMapper);
   }
 
   public NoteAutomationService createNoteAutomationService(Note note) {
