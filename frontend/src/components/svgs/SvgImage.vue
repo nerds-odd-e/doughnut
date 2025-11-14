61164103adf5a2501fad4be9a2c07063ed578f94
Terry Yin <terry@odd-e.com>
2025-11-14 16:21:29 +0800

    Update SVG components to use currentColor for fill property
    - Added fill="currentColor" attribute to SvgImage.vue and SvgUrlIndicator.vue, enhancing flexibility in color styling for SVG icons.

--- frontend/src/components/svgs/SvgImage.vue
+++ frontend/src/components/svgs/SvgImage.vue
@@ -5,7 +5,8 @@
     xmlns="http://www.w3.org/2000/svg"
     xmlns:xlink="http://www.w3.org/1999/xlink"
    x="0px" y="0px"
-	 viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve">
+	 viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve"
+	 fill="currentColor">
 <g>
 	<g>
 		<path
