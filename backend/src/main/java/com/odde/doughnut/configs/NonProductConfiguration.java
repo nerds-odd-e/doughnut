f3fe66ef146b0255ffbd8b69254aa98ad402d138
Tan Yeong Sheng <tanyeongsheng@gmail.com>
2025-11-25 12:29:45 +0800

    fix backend source code lint/formatting

--- backend/src/main/java/com/odde/doughnut/configs/NonProductConfiguration.java
+++ backend/src/main/java/com/odde/doughnut/configs/NonProductConfiguration.java
@@ -1,5 +1,7 @@
 package com.odde.doughnut.configs;
 
+import java.util.Arrays;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,9 +20,6 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
-import java.util.List;
 
 @Configuration
 @Profile({"e2e", "test"})
