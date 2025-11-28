c69654cc95d10f1ba228ec52fe899609e166eb46
Terry Yin <terry@odd-e.com>
2025-11-28 12:50:01 +0800

    chore: Add placeholder migration file for future database changes
    - Created a new SQL migration file as a placeholder for upcoming database migrations.
    - Noted that previous migration files have been consolidated into a single baseline file for simplicity.
    - Provided guidelines for naming future migration files to maintain consistency.

---
+++ backend/src/main/resources/db/migration/V300000101__db_migration_placeholder.sql
@@ -0,0 +1,7 @@
+-- This file is here to serve as a placeholder for future database migrations.
+ -- The previous migration files has been collapsed into a single file `V10000063__baseline.sql` for brevity.
+ --
+ -- Please create a new migration file for any future database changes.
+ -- The file name should be in the format `V{version}__{description}.sql`
+ -- For example: `V30000151__change_a_field_type.sql`
+
