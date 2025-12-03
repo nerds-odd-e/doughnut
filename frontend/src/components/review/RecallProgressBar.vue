f01c3656b0dfb1e4a962f40a4c5f30b4f524b27a
Terry Yin <terry@odd-e.com>
2025-12-03 10:58:44 +0800

    refactor: Simplify ProgressBar and RecallProgressBar components
    - Removed unnecessary GlobalBar wrapper from ProgressBar component, streamlining the template structure.
    - Adjusted RecallProgressBar to maintain functionality while improving readability and organization of button templates.
    - Added a scoped style for the repeat-paused class to enhance visual feedback for paused states.

--- frontend/src/components/review/RecallProgressBar.vue
+++ frontend/src/components/review/RecallProgressBar.vue
@@ -1,59 +1,57 @@
 <template>
-  <div class="header" :class="previousAnsweredQuestionCursor !== undefined ? 'repeat-paused' : ''">
-    <ProgressBar
-      v-bind="{ title: `Recalling: `, finished, toRepeatCount }"
-      @resume="$emit('viewLastAnsweredQuestion', undefined)"
-      @showMore="$emit('showMore')"
-    >
-      <template #buttons>
-        <div class="btn-group">
-          <template v-if="previousAnsweredQuestionCursor !== undefined">
-            <button
-              class="btn large-btn"
-              title="view previous answered question"
-              :disabled="finished === 0 || previousAnsweredQuestionCursor === 0"
-              @click="
-                $emit(
-                  'viewLastAnsweredQuestion',
-                  !previousAnsweredQuestionCursor
-                    ? finished - 1
-                    : previousAnsweredQuestionCursor - 1
-                )
-              "
-            >
-              <SvgBackward />
-            </button>
+  <ProgressBar
+    v-bind="{ title: `Recalling: `, finished, toRepeatCount }"
+    @resume="$emit('viewLastAnsweredQuestion', undefined)"
+    @showMore="$emit('showMore')"
+  >
+    <template #buttons>
+      <div class="btn-group">
+        <template v-if="previousAnsweredQuestionCursor !== undefined">
+          <button
+            class="btn large-btn"
+            title="view previous answered question"
+            :disabled="finished === 0 || previousAnsweredQuestionCursor === 0"
+            @click="
+              $emit(
+                'viewLastAnsweredQuestion',
+                !previousAnsweredQuestionCursor
+                  ? finished - 1
+                  : previousAnsweredQuestionCursor - 1
+              )
+            "
+          >
+            <SvgBackward />
+          </button>
 
-            <button
-              class="btn large-btn"
-              title="view next answered question"
-              @click="$emit('viewLastAnsweredQuestion', undefined)"
-            >
-              <SvgResume />
-            </button>
-          </template>
           <button
-            v-else
             class="btn large-btn"
-            title="view last answered question"
-            :disabled="finished === 0"
-            @click="$emit('viewLastAnsweredQuestion', finished - 1)"
+            title="view next answered question"
+            @click="$emit('viewLastAnsweredQuestion', undefined)"
           >
-            <SvgPause />
+            <SvgResume />
           </button>
-          <button
-            v-if="canMoveToEnd && previousAnsweredQuestionCursor === undefined"
-            class="btn large-btn"
-            title="Move to end of list"
-            aria-label="Move to end of list"
-            @click="$emit('moveToEnd', currentIndex)"
-          >
-            <SvgSkip />
-          </button>
-        </div>
-      </template>
-    </ProgressBar>
-  </div>
+        </template>
+        <button
+          v-else
+          class="btn large-btn"
+          title="view last answered question"
+          :disabled="finished === 0"
+          @click="$emit('viewLastAnsweredQuestion', finished - 1)"
+        >
+          <SvgPause />
+        </button>
+        <button
+          v-if="canMoveToEnd && previousAnsweredQuestionCursor === undefined"
+          class="btn large-btn"
+          title="Move to end of list"
+          aria-label="Move to end of list"
+          @click="$emit('moveToEnd', currentIndex)"
+        >
+          <SvgSkip />
+        </button>
+      </div>
+    </template>
+  </ProgressBar>
 </template>
 
 <script setup lang="ts">
@@ -84,8 +82,5 @@ defineEmits(["viewLastAnsweredQuestion", "showMore", "moveToEnd"])
   &:disabled {
     opacity: 0.5;
   }
-}
-.repeat-paused {
-  background-color: rgba(50, 150, 50, 0.8);
 }
 </style>
