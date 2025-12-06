9b2d9f74b65bc9644dec3ea788fbf250a1287b14
Terry Yin <terry@odd-e.com>
2025-12-06 16:57:44 +0800

    refactor: Simplify LoadingThinBar component by removing ContentLoader and adjusting styles
    This commit refactors the LoadingThinBar component by removing the ContentLoader, simplifying the template structure. The styles are updated to change the height from 2px to 1px and reposition the loading bar to the bottom of the screen, enhancing the visual appearance and functionality of the loading indicator.

--- frontend/src/components/commons/LoadingThinBar.vue
+++ frontend/src/components/commons/LoadingThinBar.vue
@@ -1,27 +1,21 @@
 <template>
-  <div class="toolbar-overlap loading-bar">
-    <ContentLoader :height="2" :speed="2" primary-color="#888"></ContentLoader>
-  </div>
+  <div class="toolbar-overlap loading-bar"></div>
 </template>
 
 <script setup lang="ts">
-import { ContentLoader } from "vue-content-loader"
 </script>
 
 <style lang="scss" scoped>
 .toolbar-overlap {
   position: fixed;
   left: 0;
-  top: 0;
+  bottom: 0;
   right: 0;
   width: 100vw;
-  height: 2px;
+  height: 1px;
   z-index: 2147483647;
-
-  svg {
-    width: 100%;
-    height: 100%;
-    z-index: 2147483647;
-  }
+  margin: 0;
+  padding: 0;
+  background-color: #888;
 }
 </style>
