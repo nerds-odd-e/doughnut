5ecfac0bcee3bc56aba10073b5db3c96d762bdc6
Tan Yeong Sheng <tanyeongsheng@gmail.com>
2026-01-16 13:21:30 +0800

    relocate vitest-jsdom test to same dir level as vitest-browser test mode spec

---
+++ frontend/tests/components/notes/NoteTextContentUndo.spec.ts
@@ -0,0 +1,35 @@
+import NoteTextContent from "@/components/notes/core/NoteTextContent.vue"
+import { flushPromises } from "@vue/test-utils"
+import createNoteStorage from "@/store/createNoteStorage"
+import makeMe from "@tests/fixtures/makeMe"
+import helper from "@tests/helpers"
+import { useStorageAccessor } from "@/composables/useStorageAccessor"
+
+describe("undo editing", () => {
+  it("should call addEditingToUndoHistory on submitChange", async () => {
+    const storageAccessor = useStorageAccessor()
+    storageAccessor.value = createNoteStorage()
+
+    const noteRealm = makeMe.aNoteRealm.title("Dummy Title").please()
+    storageAccessor.value.refreshNoteRealm(noteRealm)
+
+    const updatedTitle = "updated"
+    const wrapper = helper
+      .component(NoteTextContent)
+      .withProps({
+        readonly: false,
+        note: noteRealm.note,
+      })
+      .mount()
+
+    const titleEl = wrapper.find('[role="title"]').element as HTMLElement
+    titleEl.innerText = updatedTitle
+    titleEl.dispatchEvent(new Event("input"))
+    titleEl.dispatchEvent(new Event("blur"))
+    await flushPromises()
+
+    expect(storageAccessor.value.peekUndo()).toMatchObject({
+      type: "edit title",
+    })
+  })
+})
