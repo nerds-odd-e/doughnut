15abc0990bfef5d64cc33dca7bad7c718864e278
Tan Yeong Sheng <tanyeongsheng@gmail.com>
2026-02-25 15:31:07 +0800

    pnpm@10.30.2

--- scripts/dev_setup.sh
+++ scripts/dev_setup.sh
@@ -8,8 +8,8 @@ deactivate_nvm() {
 # Setup PNPM and Biome
 setup_pnpm_and_biome() {
   log "Setting up PNPM..."
-  corepack prepare pnpm@10.30.1 --activate
-  corepack use pnpm@10.30.1
+  corepack prepare pnpm@10.30.2 --activate
+  corepack use pnpm@10.30.2
   pnpm --frozen-lockfile recursive install
 
   if [ -e /etc/NIXOS ] || [ -e /etc/nixos ]; then
