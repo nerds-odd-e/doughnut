c4294db2f6aa383623a151ee9305caf7bb4984b8
Terry Yin <terry@odd-e.com>
2026-04-30 00:53:35 +0800

    Add JavaTimeModule to ObjectMapper configuration for improved date handling

--- backend/src/main/java/com/odde/doughnut/configs/ObjectMapperConfig.java
+++ backend/src/main/java/com/odde/doughnut/configs/ObjectMapperConfig.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,6 +16,7 @@ public class ObjectMapperConfig {
     mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
     mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
     mapper.registerModule(new Jdk8Module());
+    mapper.registerModule(new JavaTimeModule());
     return mapper;
   }
 }
