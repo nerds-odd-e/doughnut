c96a831695654de667531c7ba7b30925e55a531e
Tan Yeong Sheng <tanyeongsheng@gmail.com>
2026-05-05 17:17:12 +0800

    pnpm@10.33.3

--- scripts/dev_setup.sh
+++ scripts/dev_setup.sh
@@ -36,8 +36,8 @@ doughnut_needs_pnpm_install() {
 setup_pnpm_and_biome() {
   log "Setting up PNPM..."
   if doughnut_needs_pnpm_install; then
-    corepack prepare pnpm@10.33.2 --activate
-    corepack use pnpm@10.33.2
+    corepack prepare pnpm@10.33.3 --activate
+    corepack use pnpm@10.33.3
     pnpm --frozen-lockfile recursive install && doughnut_workspace_deps_fingerprint >"${PWD}/${DOUGHNUT_PNPM_FINGERPRINT_FILE}"
   else
     log "Skipping corepack/pnpm (workspace fingerprint unchanged). Set DOUGHNUT_SHELL_HOOK_FORCE_PNPM=1 to force."
