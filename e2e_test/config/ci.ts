9e9921a3ca7a86e1675c3f3e6cd88502eab51a29
Terry Yin <terry@odd-e.com>
2026-07-03 16:36:11 +0900

    fix: update tag filter variable name for consistency in CI configuration
    Changed the environment variable from `TAGS` to `tags` in both the CI configuration and documentation to ensure consistency and clarity in usage. This update helps prevent confusion during test runs and aligns with naming conventions.

--- e2e_test/config/ci.ts
+++ e2e_test/config/ci.ts
@@ -4,7 +4,7 @@ import commonConfig from './common'
 export default defineConfig({
   ...commonConfig,
   env: {
-    TAGS: process.env.CI ? 'not @ignore and not @wip' : 'not @ignore',
+    tags: process.env.CI ? 'not @ignore and not @wip' : 'not @ignore',
   },
   video: false,
   watchForFileChanges: false,
